ce4c622543f6839b12de6e202a749616f58f4c70
Tapio Schneider <tapio@caltech.edu>
2018-12-17 16:46:31 -0800

    Update air_pressure.jl

--- src/Thermodynamics/air_pressure.jl
+++ src/Thermodynamics/air_pressure.jl
@@ -1,4 +1,4 @@
-function air_pressure(T, dens, q_v, q_t)
+function air_pressure(T, dens, q_t, q_v)
 # Computes air pressure from equation of state (ideal gas law) given density,
 # temperature, and specific humidities of water vapor and suspended total water
 
