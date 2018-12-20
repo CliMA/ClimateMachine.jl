27028d8e276d9774db923cc4fc3ad0b7e28709f1
Jeremy E Kozdon <jekozdon@nps.edu>
2018-12-20 09:08:36 -0800

    Remove Unitful from ParametersType and PlanetParameters
    I have also add a macro `@exportparameter` to remove the 4th argument
    from parameter. In this case, the parameter just created will be
    exported; by default `@parameter` does not export the parameter it creates.

--- src/Planet-parameters/src/PlanetParameters.jl
+++ src/Planet-parameters/src/PlanetParameters.jl
@@ -5,50 +5,51 @@ Module containing the planetary constants.
 """
 module PlanetParameters
 using ParametersType
-using Unitful
 
 getval = ParametersType.getval
 
 # Physical constants
-@parameter gas_constant       8.3144598u"J/mol/K"                         "Universal gas constant"            true
-@parameter light_speed        2.99792458e8u"m/s"                          "Speed of light in vacuum"          true
-@parameter h_Planck           6.626e-34u"m^2*kg/s"                        "Planck constant"                   true
-@parameter k_Boltzmann        1.381e-23u"m^2*kg/s^2/K"                    "Boltzmann constant"                true
-@parameter Stefan             5.670e-8u"W/m^2/K^4"                        "Stefan-Boltzmann constant"         true
-@parameter astro_unit         1.4959787e11u"m"                            "Astronomical unit"                 true
+@exportparameter gas_constant      8.3144598     "Universal gas constant (J/mol/K)"
+@exportparameter light_speed       2.99792458e8  "Speed of light in vacuum (m/s)"
+@exportparameter h_Planck          6.626e-34     "Planck constant (m^2 kg/s)"
+@exportparameter k_Boltzmann       1.381e-23     "Boltzmann constant (m^2 kg/s^2/K)"
+@exportparameter Stefan            5.670e-8      "Stefan-Boltzmann constant (W/m^2/K^4)"
+@exportparameter astro_unit        1.4959787e11  "Astronomical unit (m)"
 
 # Properties of dry air
-@parameter molmass_air        28.97e-3u"kg/mol"                           "Molecular weight dry air"          true
-@parameter R_d                getval(gas_constant)/getval(molmass_air)    "Gas constant dry air"              true
-@parameter kappa_d            2//7                                        "Adiabatic exponent dry air"        true
-@parameter cp_d               getval(R_d)/getval(kappa_d)                 "Isobaric specific heat dry air"    true
+@exportparameter molmass_air       28.97e-3      "Molecular weight dry air (kg/mol)"
+@exportparameter R_d               gas_constant/
+                                   molmass_air   "Gas constant dry air (J/kg/K)"
+@exportparameter kappa_d           2//7          "Adiabatic exponent dry air"
+@exportparameter cp_d              R_d/kappa_d   "Isobaric specific heat dry air"
 
 # Properties of water
-@parameter dens_liquid        1e3u"kg/m^3"                                "Density of liquid water"           true
-@parameter molmass_water      18.01528e-3u"kg/mol"                        "Molecular weight"                  true
-@parameter R_v                getval(gas_constant)/getval(molmass_water)  "Gas constant water vapor"          true
-@parameter cp_v               1859u"J/kg/K"                               "Isobaric specific heat vapor"      true
-@parameter cp_l               4181u"J/kg/K"                               "Isobaric specific heat liquid"     true
-@parameter cp_i               2100u"J/kg/K"                               "Isobaric specific heat ice"        true
-@parameter Tfreeze            273.15u"K"                                  "Freezing point temperature"        true
-@parameter T0                 273.16u"K"                                  "Triple point temperature"          true
-@parameter L_v0               2.5008e6u"J/kg"                             "Latent heat vaporization at T0"    true
-@parameter L_s0               2.8341e6u"J/kg"                             "Latent heat sublimation at T0"     true
-@parameter L_f0               getval(L_s0)-getval(L_v0)                   "Latent heat of fusion at T0"       true
-@parameter sat_vapor_press_0  611.657u"Pa"                                "Saturation vapor pressure at T0"   true
+@exportparameter dens_liquid       1e3           "Density of liquid water (kg/m^3)"
+@exportparameter molmass_water     18.01528e-3   "Molecular weight (kg/mol)"
+@exportparameter R_v               gas_constant/
+                                   molmass_water "Gas constant water vapor (J/kg/K)"
+@exportparameter cp_v              1859          "Isobaric specific heat vapor (J/kg/K)"
+@exportparameter cp_l              4181          "Isobaric specific heat liquid (J/kg/K)"
+@exportparameter cp_i              2100          "Isobaric specific heat ice (J/kg/K)"
+@exportparameter Tfreeze           273.15        "Freezing point temperature (K)"
+@exportparameter T0                273.16        "Triple point temperature (K)"
+@exportparameter L_v0              2.5008e6      "Latent heat vaporization at T0 (J/kg)"
+@exportparameter L_s0              2.8341e6      "Latent heat sublimation at T0 (J/kg)"
+@exportparameter L_f0              L_s0 - L_v0   "Latent heat of fusion at T0 (J/kg)"
+@exportparameter sat_vapor_press_0 611.657       "Saturation vapor pressure at T0 (Pa)"
 
 # Properties of sea water
-@parameter dens_ocean         1.035e3u"kg/m^3"                            "Reference density sea water"       true
-@parameter cp_ocean           3989.25u"J/kg/K"                            "Specific heat sea water"           true
+@exportparameter dens_ocean        1.035e3       "Reference density sea water (kg/m^3)"
+@exportparameter cp_ocean          3989.25       "Specific heat sea water (J/kg/K)"
 
-# Planetary parameters
-@parameter planet_radius      6.371e6u"m"                                 "Mean planetary radius"             true
-@parameter day                86400u"s"                                   "Length of day"                     true
-@parameter Omega              7.2921159e-5u"1/s"                          "Ang. velocity planetary rotation"  true
-@parameter grav               9.81u"m/s^2"                                "Gravitational acceleration"        true
-@parameter year_anom          365.26*getval(day)                          "Length of anomalistic year"        true
-@parameter orbit_semimaj      1*getval(astro_unit)                        "Length of semimajor orbital axis"  true
-@parameter TSI                1362u"W/m^2"                                "Total solar irradiance"            true
-@parameter mslp               1.01325e5u"Pa"                              "Mean sea level pressure"           true
+# Planetary exportparameter
+@exportparameter planet_radius     6.371e6       "Mean planetary radius (m)"
+@exportparameter day               86400         "Length of day (s)"
+@exportparameter Omega             7.2921159e-5  "Ang. velocity planetary rotation (1/s)"
+@exportparameter grav              9.81          "Gravitational acceleration (m/s^2)"
+@exportparameter year_anom         365.26*day    "Length of anomalistic year (s)"
+@exportparameter orbit_semimaj     1*astro_unit  "Length of semimajor orbital axis (m)"
+@exportparameter TSI               1362          "Total solar irradiance (W/m^2)"
+@exportparameter mslp              1.01325e5     "Mean sea level pressure (Pa)"
 
 end
