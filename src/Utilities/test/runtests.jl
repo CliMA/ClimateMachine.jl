51a0517897b2b09bf93492e8dbc29c6a643c3af2
Charles_Kawczynski <kawczynski.charles@gmail.com>
2019-02-10 16:11:42 -0800

    Several updates based on discussion
    Added T_min to PlanetParameters for saturation adjustment
    Converted E_int to lowercase
    Updated RootSolvers docs
    Removed import Base from RootSolvers
    Added Regula Falsi method with tests
    Added get_solve_methods() to RootSolvers.jl, which returns an iterator of supported algorithms
    Also added min to returned parameter in sat_shum_from_pressure

--- src/Utilities/test/runtests.jl
+++ src/Utilities/test/runtests.jl
@@ -73,6 +73,7 @@ using LinearAlgebra
   E_int         = internal_energy_sat.(T_true, ρ, q_t);
   T             = saturation_adjustment.(E_int, ρ, q_t, T_trial);
   @test norm(T - T_true)/length(T) < 1e-2
+  # @test all(T .≈ T_true)
 
   # corresponding phase partitioning
   q_l_out = zeros(size(T)); q_i_out = zeros(size(T));
@@ -87,31 +88,43 @@ using LinearAlgebra
     T .* 10 .^[R_d/cp_d, R_v/cp_v]
 end
 
-@testset "RootSolvers" begin
-  T = (Float32, Float64)
-  for t in T
-    x_star2 = t(10000.0)
-    f_close(x) = x^2 - x_star2
-    f_args(x, y) = x^2 - y
+@testset "RootSolvers correctness" begin
+  for m in RootSolvers.get_solve_methods()
+    x_star2 = 10000.0
+    f(x, y) = x^2 - y
     x_star = sqrt(x_star2)
-    x_0 = t(0.0)
-    x_1 = t(1.0)
+    x_0 = 0.0
+    x_1 = 1.0
     args = Tuple(x_star2)
-    tol_abs = t(1.0e-3)
+    tol_abs = 1.0e-3
     iter_max = 100
-    x_root, converged = RootSolvers.find_zero(f_args,
+    x_root, converged = RootSolvers.find_zero(f,
                                               x_0, x_1,
                                               args,
                                               IterParams(tol_abs, iter_max),
                                               SecantMethod()
                                               )
-    @test x_root ≈ x_star
-    x_root, converged = RootSolvers.find_zero(f_close,
-                                              x_0, x_1,
-                                              IterParams(tol_abs, iter_max),
-                                              SecantMethod()
-                                              )
-    @test x_root ≈ x_star
+    @test abs(x_root - x_star) < tol_abs
+  end
+end
+
+@testset "RootSolvers convergence" begin
+  for m in RootSolvers.get_solve_methods()
+    for i in 1:4
+      f(x) = sum([rand(1)[1]-0.5 + rand(1)[1]*x^j for j in 1:i])
+      x_0 = rand(1)[1]+0.0
+      x_1 = rand(1)[1]+1.0
+      args = ()
+      tol_abs = 1.0e-3
+      iter_max = 10000
+      x_root, converged = RootSolvers.find_zero(f,
+                                                x_0, x_1,
+                                                args,
+                                                IterParams(tol_abs, iter_max),
+                                                m
+                                                )
+      @test converged
+    end
   end
 end
 
@@ -125,35 +138,27 @@ catch
 end
 @testset "CUDA RootSolvers" begin
   if HAVE_CUDA
-    x_ca = cu(rand(5, 5))
-    x_ca_0 = x_ca
-    x_ca_1 = x_ca.+2
-    t = typeof(x_ca[1])
-    x_star2 = t(10000.0)
-    # const x_star2 = t(10000.0) # must be defined in global scope
-    f_close(x) = x^2 - x_star2
-    f_args(x, y) = x^2 - y
-    args = Tuple(x_star2)
-    x_star = sqrt(x_star2)
-    x_0 = t(0.0)
-    x_1 = t(1.0)
-    tol_abs = t(1.0e-3)
-    iter_max = 100
+    for m in RootSolvers.get_solve_methods()
+      x_ca = cu(rand(5, 5))
+      x_ca_0 = x_ca
+      x_ca_1 = x_ca.+2
+      t = typeof(x_ca[1])
+      x_star2 = t(10000.0)
+      f(x) = x^2 - x_star2
+      f(x, y) = x^2 - y
+      args = Tuple(x_star2)
+      x_star = sqrt(x_star2)
+      x_0 = t(0.0)
+      x_1 = t(1.0)
+      tol_abs = t(1.0e-3)
+      iter_max = 100
 
-    # Test args method
-    x_root, converged = find_zero(f_args, x_0, x_1, args, IterParams(tol_abs, iter_max), SecantMethod())
-    @test x_root ≈ 100.0
-    R = find_zero.(f_args, x_ca_0, x_ca_1, Ref(args), Ref(IterParams(tol_abs, iter_max)), Ref(SecantMethod()))
-    x_roots = [x for (x, y) in R]
-    @test all(x_roots .≈ 100.0)
-
-    # # Test closure method
-    # # This test must be defined when const x_star2 = t(10000.0) is used.
-    #
-    # x_root, converged = find_zero(f_close, x_0, x_1, IterParams(tol_abs, iter_max), SecantMethod())
-    # @test x_root ≈ 100.0
-    # R = find_zero.(f_close, x_ca_0, x_ca_1, Ref(IterParams(tol_abs, iter_max)), Ref(SecantMethod()))
-    # x_roots = [x for (x, y) in R]
-    # @test all(x_roots .≈ 100.0)
+      # Test args method
+      x_root, converged = find_zero(f, x_0, x_1, args, IterParams(tol_abs, iter_max), m)
+      @test x_root ≈ 100.0
+      R = find_zero.(f, x_ca_0, x_ca_1, Ref(args), Ref(IterParams(tol_abs, iter_max)), Ref(m))
+      x_roots = [x for (x, y) in R]
+      @test all(x_roots .≈ 100.0)
+    end
   end
 end
