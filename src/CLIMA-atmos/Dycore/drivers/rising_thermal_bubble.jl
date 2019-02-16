0fcdedee6dfd0fe16bc635160d0a2c9822984669
Jeremy E Kozdon <jekozdon@nps.edu>
2019-02-16 08:36:47 -0800

    [AtmosDycore] Update rising_thermal_bubble test

--- src/CLIMA-atmos/Dycore/drivers/rising_thermal_bubble.jl
+++ src/CLIMA-atmos/Dycore/drivers/rising_thermal_bubble.jl
@@ -1,3 +1,15 @@
+using MPI
+
+using CLIMAAtmosDycore.Topologies
+using CLIMAAtmosDycore.Grids
+using CLIMAAtmosDycore.VanillaAtmosDiscretizations
+using CLIMAAtmosDycore.AtmosStateArrays
+using CLIMAAtmosDycore.LSRKmethods
+using CLIMAAtmosDycore.GenericCallbacks
+using CLIMAAtmosDycore
+using LinearAlgebra
+using Printf
+
 const HAVE_CUDA = try
   using CUDAdrv
   using CUDAnative
@@ -10,21 +22,13 @@ macro hascuda(ex)
   return HAVE_CUDA ? :($(esc(ex))) : :(nothing)
 end
 
-using MPI
-using CLIMAAtmosDycore.Topologies
-using CLIMAAtmosDycore.Grids
-using CLIMAAtmosDycore.VanillaAtmosDiscretizations
-using CLIMAAtmosDycore.AtmosStateArrays
-using CLIMAAtmosDycore.LSRKmethods
-using CLIMAAtmosDycore
-
 using ParametersType
 using PlanetParameters: R_d, cp_d, grav, cv_d
 @parameter gamma_d cp_d/cv_d "Heat capcity ratio of dry air"
 @parameter gdm1 R_d/cv_d "(equivalent to gamma_d-1)"
 
 # FIXME: Will these keywords args be OK?
-function risingthermalbubble(x...; ntrace=0, nmoist=0, dim=3)
+function risingthermalbubble(x...; dim=3)
   DFloat = eltype(x)
   γ::DFloat       = gamma_d
   p0::DFloat      = 100000
@@ -56,143 +60,121 @@ function risingthermalbubble(x...; ntrace=0, nmoist=0, dim=3)
   P = p0 * (R_gas * Θ / p0)^(c_p / c_v)
   T = P / (ρ * R_gas)
   E = ρ * (c_v * T + (u^2 + v^2 + w^2) / 2 + gravity * x[dim])
-  (ρ=ρ, U=U, V=V, W=W, E=E,
-   Qmoist=ntuple(j->(j*ρ), nmoist),
-   Qtrace=ntuple(j->(-j*ρ), ntrace))
+  (ρ=ρ, U=U, V=V, W=W, E=E)
 end
 
-function main()
+function main(mpicomm, DFloat, ArrayType, brickrange, N, timeend; dt=nothing,
+              exact_timeend=true) dim = length(brickrange)
+  topl = BrickTopology(# MPI communicator to connect elements/partition
+                       mpicomm,
+                       # tuple of point element edges in each dimension
+                       # (dim is inferred from this)
+                       brickrange,
+                       periodicity=(true, ntuple(j->false, dim-1)...))
+
+  grid = DiscontinuousSpectralElementGrid(topl,
+                                          # Compute floating point type
+                                          FloatType = DFloat,
+                                          # This is the array type to store
+                                          # data: CuArray = GPU, Array = CPU
+                                          DeviceArray = ArrayType,
+                                          # polynomial order for LGL grid
+                                          polynomialorder = N,
+                                          # how to skew the mesh degrees of
+                                          # freedom (for instance spherical
+                                          # or topography maps)
+                                          # warp = warpgridfun
+                                         )
+
+  # spacedisc = data needed for evaluating the right-hand side function
+  spacedisc = VanillaAtmosDiscretization(grid)
+
+  # This is a actual state/function that lives on the grid
+  initialcondition(x...) = risingthermalbubble(x...; dim=dim)
+  Q = AtmosStateArray(spacedisc, initialcondition)
+
+  # Determine the time step
+  (dt == nothing) && (dt = VanillaAtmosDiscretizations.estimatedt(spacedisc, Q))
+  if exact_timeend
+    nsteps = ceil(Int64, timeend / dt)
+    dt = timeend / nsteps
+  end
+
+  # Initialize the Method (extra needed buffers created here)
+  # Could also add an init here for instance if the ODE solver has some
+  # state and reading from a restart file
+
+  # TODO: Should we use get property to get the rhs function?
+  lsrk = LSRK(getrhsfunction(spacedisc), Q; dt = dt, t0 = 0)
+
+  # Get the initial energy
+  io = MPI.Comm_rank(mpicomm) == 0 ? stdout : open("/dev/null", "w")
+  eng0 = norm(Q)
+  @printf(io, "||Q||₂ (initial) =  %.16e\n", eng0)
+
+  # Set up the information callback
+  timer = [time_ns()]
+  cbinfo = GenericCallbacks.EveryXWallTimeSeconds(10, mpicomm) do (s=false)
+    if s
+      timer[1] = time_ns()
+    else
+      run_time = (time_ns() - timer[1]) * 1e-9
+      (min, sec) = fldmod(run_time, 60)
+      (hrs, min) = fldmod(min, 60)
+      @printf(io,
+              "-------------------------------------------------------------\n")
+      @printf(io, "simtime =  %.16e\n", CLIMAAtmosDycore.gettime(lsrk))
+      @printf(io, "runtime =  %03d:%02d:%05.2f (hour:min:sec)\n", hrs, min, sec)
+      @printf(io, "||Q||₂  =  %.16e\n", norm(Q))
+    end
+    nothing
+  end
+
+  #= Paraview calculators:
+  P = (0.4) * (E  - (U^2 + V^2 + W^2) / (2*ρ) - 9.81 * ρ * coordsZ)
+  theta = (100000/287.0024093890231) * (P / 100000)^(1/1.4) / ρ
+  =#
+  step = [0]
+  mkpath("vtk")
+  cbvtk = GenericCallbacks.EveryXSimulationSteps(10) do (init=false)
+    outprefix = @sprintf("vtk/RTB_%dD_step%04d", dim, step[1])
+    @printf(io,
+            "-------------------------------------------------------------\n")
+    @printf(io, "doing VTK output =  %s\n", outprefix)
+    VanillaAtmosDiscretizations.writevtk(outprefix, Q, spacedisc)
+    step[1] += 1
+    nothing
+  end
+
+  solve!(Q, lsrk; timeend=timeend, callbacks=(cbinfo, cbvtk))
+
+  # Print some end of the simulation information
+  engf = norm(Q)
+  @printf(io, "-------------------------------------------------------------\n")
+  @printf(io, "||Q||₂ ( final ) =  %.16e\n", engf)
+  @printf(io, "||Q||₂ (initial) / ||Q||₂ ( final ) = %+.16e\n", engf / eng0)
+  @printf(io, "||Q||₂ ( final ) - ||Q||₂ (initial) = %+.16e\n", eng0 - engf)
+end
+
+let
   MPI.Initialized() || MPI.Init()
+
   MPI.finalize_atexit()
-
   mpicomm = MPI.COMM_WORLD
-  mpirank = MPI.Comm_rank(mpicomm)
-  mpisize = MPI.Comm_size(mpicomm)
 
-  # FIXME: query via hostname
-  @hascuda device!(mpirank % length(devices()))
+  @hascuda device!(MPI.Comm_rank(mpicomm) % length(devices()))
 
-  timeinitial = 0.0
+  Ne = (10, 10, 10)
+  N = 4
   timeend = 0.1
-  Ne = 10
-  N  = 4
-
-  nmoist = 0
-  ntrace = 0
-
-  for DFloat in (Float64, Float32)
-    for dim in (2,3)
-      for backend in (HAVE_CUDA ? (CuArray, Array) : (Array,))
-
-        # mesh topology / elements (not actual degrees of freedom)
-        topl = BrickTopology(# MPI communicator to connect elements/partition
-                             mpicomm,
-                             # Elements in the brick toplogy (dimensionality
-                             # inferred)
-                             ntuple(j->range(DFloat(0); length=Ne+1, stop=1000),
-                                    dim))
-
-        # grid = actual grid of degrees of freedom
-        grid = DiscontinuousSpectralElementGrid(topl,
-                                                # Compute floating point type
-                                                FloatType = Float64,
-                                                # This is the array type to store
-                                                # data: CuArray = GPU, Array = CPU
-                                                DeviceArray = ArrayType,
-                                                # polynomial order for LGL grid
-                                                polynomialorder = 4,
-                                                # how to skew the mesh degrees of
-                                                # freedom (for instance spherical
-                                                # or topography maps)
-                                                #warp = warpgridfun
-                                               )
-
-        # Set the initial condition with a function
-        AD.initspacestate!(runner, host=true) do (x...)
-          DFloat = eltype(x)
-          γ::DFloat       = gamma_d
-          p0::DFloat      = 100000
-          R_gas::DFloat   = R_d
-          c_p::DFloat     = cp_d
-          c_v::DFloat     = cv_d
-          gravity::DFloat = grav
-
-          r = sqrt((x[1] - 500)^2 + (x[dim] - 350)^2)
-          rc::DFloat = 250
-          θ_ref::DFloat = 300
-          θ_c::DFloat = 0.5
-          Δθ::DFloat = 0
-          if r <= rc
-            Δθ = θ_c * (1 + cos(π * r / rc)) / 2
-          end
-          θ_k = θ_ref + Δθ
-          π_k = 1 - gravity / (c_p * θ_k) * x[dim]
-          c = c_v / R_gas
-          ρ_k = p0 / (R_gas * θ_k) * (π_k)^c
-          ρ = ρ_k
-          u = zero(DFloat)
-          v = zero(DFloat)
-          w = zero(DFloat)
-          U = ρ * u
-          V = ρ * v
-          W = ρ * w
-          Θ = ρ * θ_k
-          P = p0 * (R_gas * Θ / p0)^(c_p / c_v)
-          T = P / (ρ * R_gas)
-          E = ρ * (c_v * T + (u^2 + v^2 + w^2) / 2 + gravity * x[dim])
-          ρ, U, V, W, E
-        end
-
-        # Compute a (bad guess) for the time step
-        base_dt = AD.estimatedt(runner, host=true)
-        nsteps = ceil(Int64, timeend / base_dt)
-        dt = timeend / nsteps
-
-        # Set the time step
-        AD.inittimestate!(runner, dt)
-
-        eng0 = AD.L2solutionnorm(runner; host=true)
-        # mpirank == 0 && @show eng0
-
-        # Setup the info callback
-        io = mpirank == 0 ? stdout : open("/dev/null", "w")
-        show(io, "text/plain", runner.spacerunner)
-        cbinfo =
-          AD.GenericCallbacks.EveryXWallTimeSeconds(10, mpicomm) do
-            println(io, runner.spacerunner)
-          end
-
-        # Setup the vtk callback
-        mkpath("viz")
-        dump_vtk(step) = AD.writevtk(runner,
-                                     "viz/RTB"*
-                                     "_dim_$(dim)"*
-                                     "_DFloat_$(DFloat)"*
-                                     "_backend_$(backend)"*
-                                     "_mpirank_$(mpirank)"*
-                                     "_step_$(step)")
-        step = 0
-        cbvtk = AD.GenericCallbacks.EveryXSimulationSteps(10) do
-          # TODO: We should add queries back to time stepper for this
-          step += 1
-          dump_vtk(step)
-          nothing
-        end
-
-        dump_vtk(0)
-        AD.run!(runner; numberofsteps=nsteps, callbacks=(cbinfo, cbvtk))
-        dump_vtk(nsteps)
-
-        engf = AD.L2solutionnorm(runner; host=true)
-
-        mpirank == 0 && @show engf
-        mpirank == 0 && @show eng0 - engf
-        mpirank == 0 && @show engf/eng0
-        mpirank == 0 && println()
+  for DFloat in (Float64,Float32)
+    for ArrayType in (HAVE_CUDA ? (CuArray, Array) : (Array,))
+      for dim in 2:3
+        brickrange = ntuple(j->range(DFloat(0); length=Ne[j]+1, stop=1000), dim)
+        main(mpicomm, DFloat, ArrayType, brickrange, N, timeend)
       end
     end
   end
-  nothing
+
 end
 
-main()
