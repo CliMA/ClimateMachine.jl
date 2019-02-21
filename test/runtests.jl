f5f78244bb600e5261a162bac57ca7ed2126cc68
Lucas C Wilcox <lucas@swirlee.com>
2019-02-21 12:14:38 -0800

    Respect code coverage when testing subpackages

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,12 @@ subpackags = [
               "Utilities",
              ]
 
+# Code coverage command line options; must correspond to src/julia.h
+# and src/ui/repl.c
+JL_LOG_NONE = 0
+
+coverage = Base.JLOptions().code_coverage != JL_LOG_NONE
+
 for subpackage in subpackags
-  Pkg.test(subpackage)
+  Pkg.test(subpackage; coverage=coverage)
 end
