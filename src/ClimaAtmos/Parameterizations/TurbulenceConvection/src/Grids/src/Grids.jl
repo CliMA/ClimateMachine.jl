f326bdfb890ed42ee8db101dc0772411150c5a6b
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-03-15 10:44:59 -0700

    Improved Grids property names readability

--- src/ClimaAtmos/Parameterizations/TurbulenceConvection/src/Grids/src/Grids.jl
+++ src/ClimaAtmos/Parameterizations/TurbulenceConvection/src/Grids/src/Grids.jl
@@ -13,8 +13,8 @@ export elems, overelems
 using .FieldTypes: Center, Node, get_type, suffix
 
 struct Grid{T}
-  sn :: Int
-  sc :: Int
+  n_nodes :: Int
+  n_elem :: Int
   dz :: T
   dzi :: T
   dzi2 :: T
@@ -32,13 +32,13 @@ struct Grid{T}
 end
 
 function Grid(a::T, b::T, N::Int, Np = 1) where T
-  sn = N+3
-  sc = N+2
+  n_nodes = N+3
+  n_elem = N+2
   dz = T((b-a)/T(N))
   dzi = T(1)/dz
   dzi2 = dzi*dzi
-  zn = Vector{T}([T(i-2)*dz for i in 1:sn])
-  zc = Vector{T}([T(i-1)*dz-T(0.5)*dz for i in 1:sc])
+  zn = Vector{T}([T(i-2)*dz for i in 1:n_nodes])
+  zc = Vector{T}([T(i-1)*dz-T(0.5)*dz for i in 1:n_elem])
   z = Dict(Node => zn, Center => zc)
   gw = typeof(N)(2)
   zc_top = zc[end-gw]
@@ -46,28 +46,32 @@ function Grid(a::T, b::T, N::Int, Np = 1) where T
   zc_surf = zc[gw]
   zn_surf = zn[gw]
   zn_range = zn_top - zn_surf
-  inds = CartesianIndices((sc,))
-  return Grid(sn, sc, dz, dzi, dzi2, zc, zn, z, gw, zc_top, zn_top, zc_surf, zn_surf, zn_range, inds, Np)
+  inds = CartesianIndices((n_elem,))
+  return Grid(n_nodes, n_elem, dz, dzi, dzi2, zc, zn, z, gw, zc_top, zn_top, zc_surf, zn_surf, zn_range, inds, Np)
 end
 
 @inline function over_nodes(grid::Grid)
-  return 1:grid.sn
+  return 1:grid.n_nodes
 end
 
 @inline function over_nodes_real(grid::Grid)
-  return 2:grid.sn-1
+  return 2:grid.n_nodes-1
 end
 
 @inline function over_centers(grid::Grid)
-  return 1:grid.sc
+  return 1:grid.n_elem
+end
+
+@inline function over_elems(grid::Grid)
+  return 1:grid.n_elem
 end
 
 @inline function over_centers_real(grid::Grid)
-  return 2:grid.sc-1
+  return 2:grid.n_elem-1
 end
 
 @inline function over_centers_real_reversed(grid::Grid)
-  return range(grid.sc-1, step = -1, stop = 1)
+  return range(grid.n_elem-1, step = -1, stop = 1)
 end
 
 elems(grid::Grid) = grid.inds
