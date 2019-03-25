bdf63cdaeab0108e2207bce9b7fbec75b8eb8e07
Jeremy E Kozdon <jekozdon@nps.edu>
2019-03-25 08:03:38 -0700

    [AtmosDycore] Add docs for getrhsfunction

--- src/ClimaAtmos/Dycore/src/CLIMAAtmosDycore.jl
+++ src/ClimaAtmos/Dycore/src/CLIMAAtmosDycore.jl
@@ -5,6 +5,13 @@ export solve!, getrhsfunction
 using Requires
 
 abstract type AbstractAtmosDiscretization end
+
+"""
+    getrhsfunction(disc::AbstractAtmosDiscretization)
+
+The spatial discretizations are of the form ``Q̇ = f(Q)``, and this function
+returns the handle to right-hand side function ``f`` of the `disc`
+"""
 getrhsfunction(disc::AbstractAtmosDiscretization) =
 throw(MethodError(getrhsfunction, typeof(disc)))
 
