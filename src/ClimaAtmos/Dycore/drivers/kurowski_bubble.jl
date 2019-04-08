8f6a0bf68d3f6356d1a64201652af40b79e6a94e
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-04-08 17:25:48 -0400

    1D spline interpolation fix: * now done directly in the calculation of the initial conditions * For now only implemented in kurowski_bubble.js * Push for Akshay: still incomplete --> not ready for pull request.

--- src/ClimaAtmos/Dycore/drivers/kurowski_bubble.jl
+++ src/ClimaAtmos/Dycore/drivers/kurowski_bubble.jl
@@ -63,10 +63,10 @@ function interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
     Ne_v = Ne[dim]
     
     #Get sizes
-    (sound_data, nmax, ncols) = read_sounding()
+    (sound_data, nz_sound, ncols) = read_sounding()
     
     #read in the original squal sounding
-    (nzmax, ncols) = size(sound_data)
+    (nz_sound, ncols) = size(sound_data)
 
     Np = (N+1)^dim
     Nq = N + 1
@@ -92,64 +92,83 @@ function interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
     
     # create vector with all the z-values of the current processor
     # (avoids using column structure for better domain decomposition when no rain is used. AM)
-    nz         = Ne_v*N + 1
+    nz_dg         = Ne_v*N + 1
     
-    dataz      = zeros(Float64, nz)
-    datat      = zeros(Float64, nz)
-    dataq      = zeros(Float64, nz)
-    datau      = zeros(Float64, nz)
-    datav      = zeros(Float64, nz)
-    datap      = zeros(Float64, nz)
-    thetav     = zeros(Float64, nz)
-    datapi     = zeros(Float64, nz)
-    ρ      = zeros(Float64, nz)
-    U      = zeros(Float64, nz)
-    V      = zeros(Float64, nz)
-    P      = zeros(Float64, nz)
-    T      = zeros(Float64, nz)
-    E      = zeros(Float64, nz)
-    datarho    = zeros(Float64, nz)
-    ini_data_interp = zeros(Float64, nz, 10)
+    dataz      = zeros(Float64, nz_dg)
+    datat      = zeros(Float64, nz_dg)
+    dataq      = zeros(Float64, nz_dg)
+    datau      = zeros(Float64, nz_dg)
+    datav      = zeros(Float64, nz_dg)
+    datap      = zeros(Float64, nz_dg)
+    thetav     = zeros(Float64, nz_dg)
+    datapi     = zeros(Float64, nz_dg)
+    ρ          = zeros(Float64, nz_dg)
+    U          = zeros(Float64, nz_dg)
+    V          = zeros(Float64, nz_dg)
+    P          = zeros(Float64, nz_dg)
+    T          = zeros(Float64, nz_dg)
+    E          = zeros(Float64, nz_dg)
+    datarho    = zeros(Float64, nz_dg)
+    ini_data_interp = zeros(Float64, nz_dg, 10)
 
+   
     # WARNING:
     # FIXME
     # 1) REWRITE THIS TO WORK in PARALELL. NOW ONLY WORJKS IN SERIAL
     # 2) REWRITE THIS FOR 3D
-    z          = vgeo[1, 1, _z, 1]
+    z1          = vgeo[1, 1, _z, 1]
     zmax       = maximum(vgeo[:, :, _z, :])
-    dataz[1]   = z
-    zprev      = z
-    xmin       = 1.0e-8; #Take this value from the grid if xmin != 0.0
-    nzmax      = 2
+    xmin       = minimum(vgeo[:, :, _x, :]); #Take this value from the grid if xmin != 0.0
+    dataz[1]   = z1
+    zprev      = z1
+    nz         = 2
     @inbounds for e = 1:nelem
         for j = 1:Nq
             x = vgeo[1, j, _x, e]
             z = vgeo[1, j, _z, e]
-            if abs(x - xmin) <= 1.0e-5
-                if (abs(z - zprev) > 1.0e-5 && z <= zmax+1.0e-5) #take un-repeated values
-                    dataz[nzmax]   = z
-                    zprev          = z                   
-                    nzmax          = nzmax + 1
+            if (abs(x - xmin) <= 1.0e-8)
+                if (abs(z - zprev) > 1.0e-8 && z <= zmax + 1.0e-8) #take un-repeated values
+                    dataz[nz] = z
+                    zprev     = z
+                    @printf( "Z GRID[%d]: %.16e %.16e\n", nz, x, dataz[nz])
+                    nz        = nz + 1
+                    
                 end
             end       
         end
     end
-    nzmax = nzmax - 1
-    if(nzmax != nz)
-        error(" function interpolate_sounding(): 1D INTERPOLATION: ops, something is wrong: nz is wrong!\n")
+    nz = nz - 1
+    if(nz != nz_dg)
+        error(" function interpolate_sounding(): 1D INTERPOLATION: ops, something is wrong: nz_dg is wrong!\n", nz, nz_dg)
     end 
     
     #------------------------------------------------------
     # interpolate to the actual LGL points in vertical
     # dataz is given
-    #------------------------------------------------------
-    spl_tinit = Spline1D(zinit, tinit; k=1)
-    spl_qinit = Spline1D(zinit, qinit; k=1)
-    spl_uinit = Spline1D(zinit, uinit; k=1)
-    spl_vinit = Spline1D(zinit, vinit; k=1)
-    spl_pinit = Spline1D(zinit, pinit; k=1)
+    #------------------------------------------------------    
+    spline_order = 1    
+    spl_tinit    = Spline1D(zinit, tinit; k=spline_order)
+    spl_qinit    = Spline1D(zinit, qinit; k=spline_order)
+    spl_uinit    = Spline1D(zinit, uinit; k=spline_order)
+    spl_vinit    = Spline1D(zinit, vinit; k=spline_order)
+    spl_pinit    = Spline1D(zinit, pinit; k=spline_order)
+    error("LENGTH spline function ", spl_tinit)
+    
+    for iz = 1:nz_dg
+        @printf( "Z GRID[%d]: %.16e\n", iz, dataz[iz])
+        datat[iz] = spl_tinit(dataz[k])
+        dataq[iz] = spl_qinit(dataz[k])
+        datau[iz] = spl_uinit(dataz[k])
+        datav[iz] = spl_vinit(dataz[k])
+        if(dataz[k] > 14000.0)
+            dataq[k] = 0.0
+        end
+        
+    end
+   
+    
     if ncols == 5
-        for k = 1:nz
+        for k = 1:nz_dg
             datat[k] = spl_tinit(dataz[k])
             dataq[k] = spl_qinit(dataz[k])
             datau[k] = spl_uinit(dataz[k])
@@ -159,7 +178,7 @@ function interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
             end
         end
     elseif ncols == 6
-        for k = 1:nz
+        for k = 1:nz_dg
             datat[k] = spl_tinit(dataz[k])
             dataq[k] = spl_qinit(dataz[k])
             datau[k] = spl_uinit(dataz[k])
@@ -169,11 +188,11 @@ function interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
                 dataq[k] = 0.0
             end
         end
-    end
-    
+    end    
     #------------------------------------------------------
     # END interpolate to the actual LGL points in vertical
     #------------------------------------------------------
+    
 
     c = cv_d/R_d
     rvapor = 461.0
@@ -234,16 +253,55 @@ end
 
 
 # FIXME: Will these keywords args be OK?
-function kurowski_bubble(x...; initial_sounding::Array, ntrace=0, nmoist=0, dim=2)
+function kurowski_bubble(x...; ntrace=0, nmoist=0, dim=2, Ne, N)
     DFloat = eltype(x)
+
+    #Get sizes
+    (sound_data, nz_sound, ncols) = read_sounding()
+    if ncols == 6
+        #height  theta  qv     u      v      press
+        zinit,   tinit, qinit, uinit, vinit, pinit = sound_data[:, 1], sound_data[:, 2], sound_data[:, 3], sound_data[:, 4], sound_data[:, 5], sound_data[:, 6]
+    elseif ncols == 5
+        #height  theta  qv     u      v
+        zinit,   tinit, qinit, uinit, vinit = sound_data[:, 1], sound_data[:, 2], sound_data[:, 3], sound_data[:, 4], sound_data[:, 5]
+    end
+
     
-    p0::DFloat      = 85000.0 
-    gravity::DFloat = grav
-    q_tot::DFloat   = 0.0
-
-    (nzmax, ~) = size(initial_sounding)
-
-    q_tot        = 0.014*exp(-x[dim]/500)
+    #------------------------------------------------------
+    # interpolate to the actual LGL points in vertical
+    # dataz is given
+    #------------------------------------------------------
+    spline_order = 1
+    spl_tinit    = Spline1D(zinit, tinit; k=spline_order)
+    spl_qinit    = Spline1D(zinit, qinit; k=spline_order)
+    spl_uinit    = Spline1D(zinit, uinit; k=spline_order)
+    spl_vinit    = Spline1D(zinit, vinit; k=spline_order)
+    if (ncols == 6)
+        spl_pinit    = Spline1D(zinit, pinit; k=spline_order)
+    end
+    
+    #
+    # Initialize 1D data array for 1D column on DG grid
+    #
+    Ne_v       = Ne[dim]
+    nz_dg      = Ne_v*N + 1
+    dataz      = zeros(Float64, nz_dg)
+    datat      = zeros(Float64, nz_dg)
+    dataq      = zeros(Float64, nz_dg)
+    datau      = zeros(Float64, nz_dg)
+    datav      = zeros(Float64, nz_dg)
+    datap      = zeros(Float64, nz_dg)
+    thetav     = zeros(Float64, nz_dg)
+    datapi     = zeros(Float64, nz_dg)
+    ρ          = zeros(Float64, nz_dg)
+    U          = zeros(Float64, nz_dg)
+    V          = zeros(Float64, nz_dg)
+    P          = zeros(Float64, nz_dg)
+    T          = zeros(Float64, nz_dg)
+    E          = zeros(Float64, nz_dg)
+    datarho    = zeros(Float64, nz_dg)
+        
+    #q_tot        = 0.014*exp(-x[dim]/500)
     
     R_gas        = gas_constant_air(q_tot, 0.0, 0.0)
     c_p          = cp_m(q_tot, 0.0, 0.0)
@@ -253,40 +311,39 @@ function kurowski_bubble(x...; initial_sounding::Array, ntrace=0, nmoist=0, dim=
     Rovercp      = R_gas/c_p
     cvovercp     = c_v/c_p
 
-    
-    pi0    =     1.0
+    c      = cv_d/R_d
+    rvapor = 461.0
+    levap  = 2.5e+6
+    es0    = 6.1e+2
+    c2     = R_d/cp_d
+    c1     = 1.0/c2
+    g      = grav
+    pi0    =     1.0    
     theta0 =   283.0
     p0     = 85000.0
     rho0   = p0/(R_gas * theta0) * (pi0)^cvoverR
 
     #20% of relative humidity in the background
-    RH0    = 20.0                                 
-    # find the matching height
-
-    # find the matching height
-    maxt  = 0.0
-    count = 1
-    for k = 1:nzmax
-        dataz = initial_sounding[k, 1]
-        z     = x[dim]
-        z2test = Float64(floor(100.0 * dataz))/100.0
-        z1test = Float64(floor(100.0 * z))/100.0
-        if ( abs(z1test - z2test) <= 0.2)
-            count=k
-            break
-        end
+    RH0    = 20.0
+    
+    thetav = spl_tinit(x[dim])
+    dataq  = spl_qinit(x[dim])
+    uvelo  = spl_uinit(x[dim])
+    vvelo  = spl_vinit(x[dim])
+    
+    # convert qv from g/kg to g/g
+    dataq = dataq.*1.0e-3
+    
+    # calculate the hydrostatically balanced exner potential and pressure
+    if(ncols == 5)
+        datapi[1] = 1.0
+        datap[1]  = p0
     end
-
-    dataz   = initial_sounding[count, 1] #z
-    datat   = initial_sounding[count, 2] #theta
-    dataq   = initial_sounding[count, 3] #qv
-    datau   = initial_sounding[count, 4] #u
-    datav   = initial_sounding[count, 5] #v
-    datap   = initial_sounding[count, 6] #p
-    datarho = initial_sounding[count, 7] #rho
-    datapi  = initial_sounding[count, 8] #exner
-    thetav  = initial_sounding[count, 9] #thetav
-
+    thetav[1] = datat[1]*(1.0 + 0.608*dataq[1])
+    
+    
+    @show(thetav)
+    #=
     θ = thetav
     π = datapi
     ρ = datarho
@@ -333,7 +390,11 @@ function kurowski_bubble(x...; initial_sounding::Array, ntrace=0, nmoist=0, dim=
     
     # Total energy 
     E = ρ * MoistThermodynamics.total_energy(e_kin, e_pot, T, q_tot, 0.0, 0.0)
-
+    =#
+    ρ = x[dim]
+    U = x[dim]
+    V = x[dim]
+    E = x[dim]
     (ρ=ρ, U=U, V=V, W=W, E=E, Qmoist=(ρ * q_tot, 0.0, 0.0)) 
 
 
@@ -372,13 +433,14 @@ function main(mpicomm, DFloat, ArrayType, brickrange, nmoist, ntrace, N, Ne,
                                            nmoist=nmoist)
 
     # This is a actual state/function that lives on the grid    
-    vgeo = grid.vgeo
-    initial_sounding       = interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
+    #vgeo = grid.vgeo
+    #initial_sounding       = interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
     initialcondition(x...) = kurowski_bubble(x...;
-                                             initial_sounding=initial_sounding, 
                                              ntrace=ntrace,
                                              nmoist=nmoist,
-                                             dim=dim)
+                                             dim=dim,
+                                             Ne=Ne,
+                                             N=N)
     Q = MPIStateArray(spacedisc, initialcondition)
 
     # Determine the time step
