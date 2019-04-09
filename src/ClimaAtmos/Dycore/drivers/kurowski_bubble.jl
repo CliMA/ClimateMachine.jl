ac5a9a69de7f25dbf4f88da5aa631aba27f0f817
Akshay Sridhar <asridhar@caltech.edu>
2019-04-09 14:27:46 -0700

    modified:   drivers/kurowski_bubble.jl

--- src/ClimaAtmos/Dycore/drivers/kurowski_bubble.jl
+++ src/ClimaAtmos/Dycore/drivers/kurowski_bubble.jl
@@ -40,117 +40,110 @@ function read_sounding()
     close(fsounding)
     (nzmax, ncols) = size(sounding)
     if nzmax == 0
-       # error("SOUNDING ERROR: The Sounding file is empty!")
+        error("SOUNDING ERROR: The Sounding file is empty!")
     end
     return (sounding, nzmax, ncols)
 end
 
-function kurowski_bubble(x...; ntrace=0, nmoist=0, dim=2, Ne, N)
-
-  DFloat = eltype(x)
-  p0::DFloat = MSLP
-  c_p::DFloat = cp_d 
-  c_v::DFloat = cv_d
+function kurowski_bubble(x...; ntrace=0, nmoist=0, dim=3)
+  dim = 2
+  q_tot 	= 0.014
+  DFloat 	= eltype(x)
+  p0::DFloat 	= 85000.0
+  R_gas::DFloat = gas_constant_air(q_tot,0.0,0.0)
+  c_p::DFloat 	= cp_m(q_tot, 0.0, 0.0) 
+  c_v::DFloat 	= cv_m(q_tot, 0.0, 0.0)
+  cpoverR       = c_p/R_gas
+  cvoverR       = c_v/R_gas
+  Rovercp       = R_gas/c_p
+  cvovercp	= c_v/c_p
   gravity::DFloat = grav
-  q_tot::DFloat = 0
-
   # ----------------------------------------------------
   # GET DATA FROM INTERPOLATED ARRAY ONTO VECTORS
   # DRIVER FOR 6 COLUMN SOUNDING DATA
   # ----------------------------------------------------
-  (sounding, nz_sound, ncols) = read_sounding()
-  #height  theta  qv     u      v      press
-  zinit,   tinit, qvinit, uinit, vinit, pinit = sounding[:, 1],sounding[:, 2],sounding[:, 3],sounding[:, 4],sounding[:, 5],sounding[:, 6]
+  (sounding, _, ncols) = read_sounding()
+
+  #height theta qv    u     v     pressure
+  zinit, tinit, qinit, uinit, vinit, pinit  = sounding[:, 1],
+                                              sounding[:, 2],
+                                              sounding[:, 3],
+                                              sounding[:, 4],
+                                              sounding[:, 5],
+                                              sounding[:, 6]
   #------------------------------------------------------
   # GET DESCRIPTION OF INTERPOLATED SPLINE 
   #------------------------------------------------------
-  spl_tinit    = Spline1D(zinit, tinit)
-  spl_qvinit    = Spline1D(zinit, qvinit)
-  spl_uinit    = Spline1D(zinit, uinit)
-  spl_vinit    = Spline1D(zinit, vinit)
-  if (ncols == 6)
-      spl_pinit    = Spline1D(zinit, pinit)
-  end
-   
+  spl_tinit    = Spline1D(zinit, tinit; k=1)
+  spl_qinit    = Spline1D(zinit, qinit; k=1)
+  spl_uinit    = Spline1D(zinit, uinit; k=1)
+  spl_vinit    = Spline1D(zinit, vinit; k=1)
+  spl_pinit    = Spline1D(zinit, pinit; k=1)
   # --------------------------------------------------
   # INITIALISE ARRAYS FOR INTERPOLATED VALUES
   # --------------------------------------------------
+  datat          = spl_tinit(x[dim])
+  dataq          = spl_qinit(x[dim])
+  datau          = spl_uinit(x[dim])
+  datav          = spl_vinit(x[dim])
+  datap          = spl_pinit(x[dim])
   
-  θ = spl_tinit(x[dim])
-  dataqv = spl_qvinit(x[dim])
-  u = spl_uinit(x[dim])
-  v = spl_vinit(x[dim])
-  P = spl_pinit(x[dim])
-  if(x[dim]> 14000.0)
-      dataqv = 0.0
-  end
+  rvapor        = 461.0
+  levap         = 2.5e6
+  es0           = 6.11e2
+  pi0           = 1.0
+  p0            = 85000.0
+  theta0        = 283.0
+  R_gas         = gas_constant_air(q_tot,0.0,0.0)
+  c2            = R_d / cp_d
+  c1            = 1.0 / c2
   
-  q_tot        = 0.014 #*exp(-x[dim]/500)
-  R_gas        = gas_constant_air(q_tot, 0.0, 0.0)
-  R_d          = gas_constant_air(0.0, 0.0, 0.0)
-  c_p          = cp_m(q_tot, 0.0, 0.0)
-  c_v          = cv_m(q_tot, 0.0, 0.0)
-  cpoverR      = c_p/R_gas
-  cvoverR      = c_v/R_gas
-  Rovercp      = R_gas/c_p
-  cvovercp     = c_v/c_p
+  dataq         = dataq * 1e-3
+  thetav        = datat * (1.0 + 0.608 * dataq)
+  datapi        = (datap ./ MSLP) .^ (c2)
+  datarho 	= datap/(R_gas * datapi * thetav)
+  pi0           = 1.0
+  rho0		= p0/(R_gas * theta0) * (pi0)^cvoverR
+  RH_0          = 20.0
+  rc            = 300.0
+  r             = sqrt((x[1] - 700)^2 + (x[dim] - 500.0)^2)
+  R_gas         = gas_constant_air(0.0,0.0,0.0)
 
-  rvapor = 461.0
-  levap  = 2.5e+6
-  es0    = 6.1e+2
-  pi0    =     1.0    
-  theta0 =   283.0
-  p0     = 85000.0
+  thetac        = 2.0
+  dtheta        = 0.0
+  sigma         = 6.0
   
-  # Virtual potential temperature from \theta sounding data 
-  θ = θ * (1.0 + 0.608*dataqv)
-  rho0   = p0/(R_gas * theta0) * (pi0)^cvoverR
-  ρ = ((p0^Rovercp)*P^cvovercp)/(R_gas * θ)
-  
-  # convert qv from g/kg to g/g
-  dataqv = dataqv.*1.0e-3
-  
-  # calculate the hydrostatically balanced exner potential and pressure
-  P_exner = exner(P, q_tot, 0.0, 0.0)
-  T = P / (ρ * R_gas)
-  # Grabowski
-  es      = 611.0*exp(2.52e6/461.0*(1.0/273.16 - 1.0/T))
-  qvs     = 287.04/461.0 * es/(P - es)   # saturation mixing ratio
-  # Get qv from RH: #20% of relative humidity in the background
-  RH_0    = 20.0
-  qv_k    = qvs * RH_0/100.0
-  # Perturbation    
-  thetac = 2.0
-  dtheta = 0.0
-  dqr    = 0.0
-  dqc    = 0.0
-  dqv    = 0.0
-  dRH    = 0.0
-  sigma  = 6.0
-  rc     = 300.0
-  r      = sqrt( (x[1] - 700)^2 + (x[dim] - 400.0)^2 )
-  dtheta = thetac*exp(-(r/rc)^sigma)    
-  dRH    = 80.0 * exp(-(r/rc)^sigma)
-  dqv    = dRH * qvs/100.0
-  q_tot  = qv_k + dqv
-  u      = u
-  v      = v
-  w      = 0
-  U      = ρ * u
-  V      = ρ * v
-  W      = ρ * w
+  ρ	        = datarho
+  P		= datap
+  T		= P / ρ / R_gas
+
+  es            = es0*exp(levap/rvapor*(1.0/T_0 - 1.0/T))
+  qvs           = 287.04/rvapor * es/(P - es) 
+
+  qv_k          = qvs * RH_0/100.0
+
+  rc		= 300.0
+  r		= sqrt((x[1]-700)^2+(x[dim]-400)^2)
+  dtheta	= thetac * exp(-(r/rc)^sigma)
+  dRH		= 80.0 * exp(-(r/rc)^sigma)
+  dqv		= dRH * qvs / 100.0
+  q_tot         = qv_k + dqv
+  u, v, w       = 0.0, 0.0, 0.0
+  U      	= ρ * u
+  V      	= ρ * v
+  W      	= ρ * w
   # Calculation of energy per unit mass
   e_kin = (u^2 + v^2 + w^2) / 2  
   e_pot = gravity * x[dim]
-  e_int = MoistThermodynamics.internal_energy(T, q_tot, 0.0, 0.0)
+  e_int = internal_energy(T, q_tot, 0.0, 0.0)
   # Total energy 
-  E = ρ * MoistThermodynamics.total_energy(e_kin, e_pot, T, q_tot, 0.0, 0.0)
+  E = ρ * total_energy(e_kin, e_pot, T, q_tot, 0.0, 0.0)
   (ρ=ρ, U=U, V=V, W=V, E=E, Qmoist=(ρ * q_tot, 0.0, 0.0)) 
 
 end
 
 function main(mpicomm, DFloat, ArrayType, brickrange, nmoist, ntrace, N, Ne, 
-              timeend; gravity=true, viscosity=0, dt=nothing,
+              timeend; gravity=true, viscosity=25, dt=nothing,
               exact_timeend=true) 
     dim = length(brickrange)
     topl = BrickTopology(# MPI communicator to connect elements/partition
@@ -188,8 +181,7 @@ function main(mpicomm, DFloat, ArrayType, brickrange, nmoist, ntrace, N, Ne,
                                              ntrace=ntrace,
                                              nmoist=nmoist,
                                              dim=dim,
-                                             Ne=Ne,
-                                             N=N)
+                                             )
     Q = MPIStateArray(spacedisc, initialcondition)
 
     # Determine the time step
@@ -229,10 +221,6 @@ function main(mpicomm, DFloat, ArrayType, brickrange, nmoist, ntrace, N, Ne,
         nothing
     end
 
-    #= Paraview calculators:
-    P = (0.4) * (E  - (U^2 + V^2 + W^2) / (2*ρ) - 9.81 * ρ * coordsZ)
-    theta = (100000/287.0024093890231) * (P / 100000)^(1/1.4) / ρ
-    =#
     step = [0]
     mkpath("vtk")
     cbvtk = GenericCallbacks.EveryXSimulationSteps(1000) do (init=false)
@@ -263,14 +251,13 @@ let
 
     @hascuda device!(MPI.Comm_rank(mpicomm) % length(devices()))
 
-    viscosity = 0.0
+    viscosity = 75.0
     nmoist = 3
     ntrace = 0
-    Ne = (20, 60)
+    Ne = (25, 50)
     N = 4
     dim = 2
-    timeend = 100
-    viscosity = 50.0
+    timeend = 100000
 
     xmin = 0.0
     xmax = 1400.0
@@ -282,7 +269,7 @@ let
         brickrange = (range(DFloat(xmin); length=Ne[1]+1, stop=xmax),
                       range(DFloat(zmin); length=Ne[2]+1, stop=zmax))
 
-        main(mpicomm, DFloat, ArrayType, brickrange, nmoist, ntrace, N, Ne, timeend)
+        main(mpicomm, DFloat, ArrayType, brickrange, nmoist, ntrace, N, Ne, timeend) 
     end
 end
 
