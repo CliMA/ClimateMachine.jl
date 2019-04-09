92b3943a9317f0e1f698e9737cece15da9884b6b
Akshay Sridhar <asridhar@caltech.edu>
2019-04-08 17:02:51 -0700

    modified:   drivers/kurowski_bubble.jl

--- src/ClimaAtmos/Dycore/drivers/kurowski_bubble.jl
+++ src/ClimaAtmos/Dycore/drivers/kurowski_bubble.jl
@@ -32,310 +32,120 @@ using CLIMA.PlanetParameters: R_d, cp_d, grav, cv_d, MSLP, T_0
 
 
 # {{{
-#        SOUNDING operations:
-# read_sound()
-# interpolate_sounding()
-# Interpolate the sounding along the FIRST column of the grid.
 
 function read_sounding()
     #read in the original squal sounding
     fsounding  = open(joinpath(@__DIR__, "./soundings/sounding_GC1991.dat"))
-    sound_data = readdlm(fsounding)
+    sounding = readdlm(fsounding)
     close(fsounding)
-    (nzmax, ncols) = size(sound_data)
+    (nzmax, ncols) = size(sounding)
     if nzmax == 0
-        error(" SOUNDING ERROR: The Sounding file is empty!")
+       # error("SOUNDING ERROR: The Sounding file is empty!")
     end
-    return (sound_data, nzmax, ncols)
+    return (sounding, nzmax, ncols)
 end
 
+function kurowski_bubble(x...; ntrace=0, nmoist=0, dim=2, Ne, N)
 
-function interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
-    # !!!WARNING!!! This function can only work for sturctured grids with vertical boundaries!!!
-    # !!!TO BE REWRITTEN FOR THE GENERAL CODE!!!!
-    # {{{ FIXME: remove this after we've figure out how to pass through to kernel
+  DFloat = eltype(x)
+  p0::DFloat = MSLP
+  c_p::DFloat = cp_d 
+  c_v::DFloat = cv_d
+  gravity::DFloat = grav
+  q_tot::DFloat = 0
 
-    p0::Float64      = MSLP
-    R_gas::Float64   = R_d
-    c_p::Float64     = cp_d
-    c_v::Float64     = cv_d
-    gravity::Float64 = grav
-    Ne_v = Ne[dim]
-    
-    #Get sizes
-    (sound_data, nmax, ncols) = read_sounding()
-    
-    #read in the original squal sounding
-    (nzmax, ncols) = size(sound_data)
+  # ----------------------------------------------------
+  # GET DATA FROM INTERPOLATED ARRAY ONTO VECTORS
+  # DRIVER FOR 6 COLUMN SOUNDING DATA
+  # ----------------------------------------------------
+  (sounding, nz_sound, ncols) = read_sounding()
+  #height  theta  qv     u      v      press
+  zinit,   tinit, qvinit, uinit, vinit, pinit = sounding[:, 1],sounding[:, 2],sounding[:, 3],sounding[:, 4],sounding[:, 5],sounding[:, 6]
+  #------------------------------------------------------
+  # GET DESCRIPTION OF INTERPOLATED SPLINE 
+  #------------------------------------------------------
+  spl_tinit    = Spline1D(zinit, tinit)
+  spl_qvinit    = Spline1D(zinit, qvinit)
+  spl_uinit    = Spline1D(zinit, uinit)
+  spl_vinit    = Spline1D(zinit, vinit)
+  if (ncols == 6)
+      spl_pinit    = Spline1D(zinit, pinit)
+  end
+   
+  # --------------------------------------------------
+  # INITIALISE ARRAYS FOR INTERPOLATED VALUES
+  # --------------------------------------------------
+  
+  θ = spl_tinit(x[dim])
+  dataqv = spl_qvinit(x[dim])
+  u = spl_uinit(x[dim])
+  v = spl_vinit(x[dim])
+  P = spl_pinit(x[dim])
+  if(x[dim]> 14000.0)
+      dataqv = 0.0
+  end
+  
+  q_tot        = 0.014 #*exp(-x[dim]/500)
+  R_gas        = gas_constant_air(q_tot, 0.0, 0.0)
+  R_d          = gas_constant_air(0.0, 0.0, 0.0)
+  c_p          = cp_m(q_tot, 0.0, 0.0)
+  c_v          = cv_m(q_tot, 0.0, 0.0)
+  cpoverR      = c_p/R_gas
+  cvoverR      = c_v/R_gas
+  Rovercp      = R_gas/c_p
+  cvovercp     = c_v/c_p
 
-    Np = (N+1)^dim
-    Nq = N + 1
-    (~, ~, nelem) = size(vgeo)
-    Ne_v = Ne[dim]
-
-    #Reshape vgeo:
-    if(dim == 2)
-        _x, _z = 12, 13
-        vgeo = reshape(vgeo, Nq, Nq, size(vgeo,2), nelem)      
-    elseif(dim == 3)
-        _x, _z = 12, 14
-        vgeo= reshape(vgeo, Nq, Nq, Nq, size(vgeo,2), nelem)
-    end
-    
-    if ncols == 6
-        #height  theta  qv     u      v      press
-        zinit,   tinit, qinit, uinit, vinit, pinit = sound_data[:, 1], sound_data[:, 2], sound_data[:, 3], sound_data[:, 4], sound_data[:, 5], sound_data[:, 6]
-    elseif ncols == 5
-        #height  theta  qv     u      v
-        zinit,   tinit, qinit, uinit, vinit = sound_data[:, 1], sound_data[:, 2], sound_data[:, 3], sound_data[:, 4], sound_data[:, 5]
-    end
-    
-    # create vector with all the z-values of the current processor
-    # (avoids using column structure for better domain decomposition when no rain is used. AM)
-    nz         = Ne_v*N + 1
-    
-    dataz      = zeros(Float64, nz)
-    datat      = zeros(Float64, nz)
-    dataq      = zeros(Float64, nz)
-    datau      = zeros(Float64, nz)
-    datav      = zeros(Float64, nz)
-    datap      = zeros(Float64, nz)
-    thetav     = zeros(Float64, nz)
-    datapi     = zeros(Float64, nz)
-    ρ      = zeros(Float64, nz)
-    U      = zeros(Float64, nz)
-    V      = zeros(Float64, nz)
-    P      = zeros(Float64, nz)
-    T      = zeros(Float64, nz)
-    E      = zeros(Float64, nz)
-    datarho    = zeros(Float64, nz)
-    ini_data_interp = zeros(Float64, nz, 10)
-
-    # WARNING:
-    # FIXME
-    # 1) REWRITE THIS TO WORK in PARALELL. NOW ONLY WORJKS IN SERIAL
-    # 2) REWRITE THIS FOR 3D
-    z          = vgeo[1, 1, _z, 1]
-    zmax       = maximum(vgeo[:, :, _z, :])
-    dataz[1]   = z
-    zprev      = z
-    xmin       = 1.0e-8; #Take this value from the grid if xmin != 0.0
-    nzmax      = 2
-    @inbounds for e = 1:nelem
-        for j = 1:Nq
-            x = vgeo[1, j, _x, e]
-            z = vgeo[1, j, _z, e]
-            if abs(x - xmin) <= 1.0e-5
-                if (abs(z - zprev) > 1.0e-5 && z <= zmax+1.0e-5) #take un-repeated values
-                    dataz[nzmax]   = z
-                    zprev          = z                   
-                    nzmax          = nzmax + 1
-                end
-            end       
-        end
-    end
-    nzmax = nzmax - 1
-    if(nzmax != nz)
-        error(" function interpolate_sounding(): 1D INTERPOLATION: ops, something is wrong: nz is wrong!\n")
-    end 
-    
-    #------------------------------------------------------
-    # interpolate to the actual LGL points in vertical
-    # dataz is given
-    #------------------------------------------------------
-    spl_tinit = Spline1D(zinit, tinit; k=1)
-    spl_qinit = Spline1D(zinit, qinit; k=1)
-    spl_uinit = Spline1D(zinit, uinit; k=1)
-    spl_vinit = Spline1D(zinit, vinit; k=1)
-    spl_pinit = Spline1D(zinit, pinit; k=1)
-    if ncols == 5
-        for k = 1:nz
-            datat[k] = spl_tinit(dataz[k])
-            dataq[k] = spl_qinit(dataz[k])
-            datau[k] = spl_uinit(dataz[k])
-            datav[k] = spl_vinit(dataz[k])
-            if(dataz[k] > 14000.0)
-                dataq[k] = 0.0
-            end
-        end
-    elseif ncols == 6
-        for k = 1:nz
-            datat[k] = spl_tinit(dataz[k])
-            dataq[k] = spl_qinit(dataz[k])
-            datau[k] = spl_uinit(dataz[k])
-            datav[k] = spl_vinit(dataz[k])
-            datap[k] = spl_pinit(dataz[k])
-            if(dataz[k] > 14000.0)
-                dataq[k] = 0.0
-            end
-        end
-    end
-    
-    #------------------------------------------------------
-    # END interpolate to the actual LGL points in vertical
-    #------------------------------------------------------
-
-    c = cv_d/R_d
-    rvapor = 461.0
-    levap  = 2.5e+6
-    es0    = 6.1e+2
-    c2     = R_d/cp_d
-    c1     = 1.0/c2
-    g      = grav
-    pi0    = 1.0
-    theta0 = 300.5
-    theta0 = 283
-    p0     = 85000.0
-    
-    # convert qv from g/kg to g/g
-    dataq = dataq.*1.0e-3
-
-    # calculate the hydrostatically balanced exner potential and pressure
-    if(ncols == 5)
-        datapi[1] = 1.0
-        datap[1]  = p0
-    end
-    thetav[1] = datat[1]*(1.0 + 0.608*dataq[1])
-    
-    for k = 2:nzmax
-        thetav[k] = datat[k]*(1.0 + 0.608*dataq[k])
-        if(ncols == 5)
-            datapi[k] = datapi[k-1] - (gravity/(c_p*0.5*(thetav[k]+thetav[k-1])))*(dataz[k] - dataz[k-1])
-            #Pressure is computed only if it is NOT passed in the sounding file
-            datap[k] = p0*datapi[k]^(c_p/R_gas)
-        end
-    end
-
-if(ncols == 6)
-    for k = 1:nzmax
-        datapi[k] = (datap[k]/MSLP)^c2
-    end
-end
-
-for k = 1:nzmax
-    datarho[k] = datap[k]/(R_gas * datapi[k] * thetav[k])
-    e          = dataq[k] * datap[k] * rvapor/(dataq[k] * rvapor + R_gas)
-end        
-
-for k = 1:nzmax
-    ini_data_interp[k, 1] = dataz[k]   #z
-    ini_data_interp[k, 2] = datat[k]   #theta
-    ini_data_interp[k, 3] = dataq[k]   #qv
-    ini_data_interp[k, 4] = datau[k]   #u
-    ini_data_interp[k, 5] = datav[k]   #v
-    ini_data_interp[k, 6] = datap[k]   #p
-    ini_data_interp[k, 7] = datarho[k] #rho
-    ini_data_interp[k, 8] = datapi[k]  #exner
-    ini_data_interp[k, 9] = thetav[k]  #thetav
-end
-
-return ini_data_interp
-end
-
-
-# FIXME: Will these keywords args be OK?
-function kurowski_bubble(x...; initial_sounding::Array, ntrace=0, nmoist=0, dim=2)
-    DFloat = eltype(x)
-    
-    p0::DFloat      = 85000.0 
-    gravity::DFloat = grav
-    q_tot::DFloat   = 0.0
-
-    (nzmax, ~) = size(initial_sounding)
-
-    q_tot        = 0.014*exp(-x[dim]/500)
-    
-    R_gas        = gas_constant_air(q_tot, 0.0, 0.0)
-    c_p          = cp_m(q_tot, 0.0, 0.0)
-    c_v          = cv_m(q_tot, 0.0, 0.0)
-    cpoverR      = c_p/R_gas
-    cvoverR      = c_v/R_gas
-    Rovercp      = R_gas/c_p
-    cvovercp     = c_v/c_p
-
-    
-    pi0    =     1.0
-    theta0 =   283.0
-    p0     = 85000.0
-    rho0   = p0/(R_gas * theta0) * (pi0)^cvoverR
-
-    #20% of relative humidity in the background
-    RH0    = 20.0                                 
-    # find the matching height
-
-    # find the matching height
-    maxt  = 0.0
-    count = 1
-    for k = 1:nzmax
-        dataz = initial_sounding[k, 1]
-        z     = x[dim]
-        z2test = Float64(floor(100.0 * dataz))/100.0
-        z1test = Float64(floor(100.0 * z))/100.0
-        if ( abs(z1test - z2test) <= 0.2)
-            count=k
-            break
-        end
-    end
-
-    dataz   = initial_sounding[count, 1] #z
-    datat   = initial_sounding[count, 2] #theta
-    dataq   = initial_sounding[count, 3] #qv
-    datau   = initial_sounding[count, 4] #u
-    datav   = initial_sounding[count, 5] #v
-    datap   = initial_sounding[count, 6] #p
-    datarho = initial_sounding[count, 7] #rho
-    datapi  = initial_sounding[count, 8] #exner
-    thetav  = initial_sounding[count, 9] #thetav
-
-    θ = thetav
-    π = datapi
-    ρ = datarho
-    P = datap
-    #T = π * θ
-    T = P / (ρ * R_gas)
-
-    
-    # Grabowski:
-    es      = 611.0*exp(2.52e6/461.0*(1.0/273.16 - 1.0/T))
-    qvs     = 287.04/461.0 * es/(P - es)   # saturation mixing ratio
-
-    # Get qv from RH:
-    qv_k    = qvs * RH0/100.0
-
-    # Perturbation    
-    thetac = 2.0
-    dtheta = 0.0
-    dqr    = 0.0
-    dqc    = 0.0
-    dqv    = 0.0
-    dRH    = 0.0
-    sigma  = 6.0
-    
-    rc     =  300.0
-    r      = sqrt( (x[1] - 700)^2 + (x[dim] - 400.0)^2 )
-    dtheta = thetac*exp(-(r/rc)^sigma)    
-    dRH    = 80.0 * exp(-(r/rc)^sigma)
-    dqv    = dRH * qvs/100.0
-    
-    q_tot  = qv_k + dqv
-    u      = 0
-    v      = 0
-    w      = 0
-    U      = ρ * u
-    V      = ρ * v
-    W      = ρ * w
-    
-    
-    # Calculation of energy per unit mass
-    e_kin = (u^2 + v^2 + w^2) / 2  
-    e_pot = gravity * x[dim]
-    e_int = MoistThermodynamics.internal_energy(T, q_tot, 0.0, 0.0)
-    
-    # Total energy 
-    E = ρ * MoistThermodynamics.total_energy(e_kin, e_pot, T, q_tot, 0.0, 0.0)
-
-    (ρ=ρ, U=U, V=V, W=W, E=E, Qmoist=(ρ * q_tot, 0.0, 0.0)) 
-
+  rvapor = 461.0
+  levap  = 2.5e+6
+  es0    = 6.1e+2
+  pi0    =     1.0    
+  theta0 =   283.0
+  p0     = 85000.0
+  
+  # Virtual potential temperature from \theta sounding data 
+  θ = θ * (1.0 + 0.608*dataqv)
+  rho0   = p0/(R_gas * theta0) * (pi0)^cvoverR
+  ρ = ((p0^Rovercp)*P^cvovercp)/(R_gas * θ)
+  
+  # convert qv from g/kg to g/g
+  dataqv = dataqv.*1.0e-3
+  
+  # calculate the hydrostatically balanced exner potential and pressure
+  P_exner = exner(P, q_tot, 0.0, 0.0)
+  T = P / (ρ * R_gas)
+  # Grabowski
+  es      = 611.0*exp(2.52e6/461.0*(1.0/273.16 - 1.0/T))
+  qvs     = 287.04/461.0 * es/(P - es)   # saturation mixing ratio
+  # Get qv from RH: #20% of relative humidity in the background
+  RH_0    = 20.0
+  qv_k    = qvs * RH_0/100.0
+  # Perturbation    
+  thetac = 2.0
+  dtheta = 0.0
+  dqr    = 0.0
+  dqc    = 0.0
+  dqv    = 0.0
+  dRH    = 0.0
+  sigma  = 6.0
+  rc     = 300.0
+  r      = sqrt( (x[1] - 700)^2 + (x[dim] - 400.0)^2 )
+  dtheta = thetac*exp(-(r/rc)^sigma)    
+  dRH    = 80.0 * exp(-(r/rc)^sigma)
+  dqv    = dRH * qvs/100.0
+  q_tot  = qv_k + dqv
+  u      = u
+  v      = v
+  w      = 0
+  U      = ρ * u
+  V      = ρ * v
+  W      = ρ * w
+  # Calculation of energy per unit mass
+  e_kin = (u^2 + v^2 + w^2) / 2  
+  e_pot = gravity * x[dim]
+  e_int = MoistThermodynamics.internal_energy(T, q_tot, 0.0, 0.0)
+  # Total energy 
+  E = ρ * MoistThermodynamics.total_energy(e_kin, e_pot, T, q_tot, 0.0, 0.0)
+  (ρ=ρ, U=U, V=V, W=V, E=E, Qmoist=(ρ * q_tot, 0.0, 0.0)) 
 
 end
 
@@ -372,13 +182,14 @@ function main(mpicomm, DFloat, ArrayType, brickrange, nmoist, ntrace, N, Ne,
                                            nmoist=nmoist)
 
     # This is a actual state/function that lives on the grid    
-    vgeo = grid.vgeo
-    initial_sounding       = interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
+    #vgeo = grid.vgeo
+    #initial_sounding       = interpolate_sounding(dim, N, Ne, vgeo, nmoist, ntrace)
     initialcondition(x...) = kurowski_bubble(x...;
-                                             initial_sounding=initial_sounding, 
                                              ntrace=ntrace,
                                              nmoist=nmoist,
-                                             dim=dim)
+                                             dim=dim,
+                                             Ne=Ne,
+                                             N=N)
     Q = MPIStateArray(spacedisc, initialcondition)
 
     # Determine the time step
@@ -452,9 +263,10 @@ let
 
     @hascuda device!(MPI.Comm_rank(mpicomm) % length(devices()))
 
+    viscosity = 0.0
     nmoist = 3
     ntrace = 0
-    Ne = (50, 50)
+    Ne = (20, 60)
     N = 4
     dim = 2
     timeend = 100
@@ -463,7 +275,7 @@ let
     xmin = 0.0
     xmax = 1400.0
     zmin = 0.0
-    zmax = 1200.0
+    zmax = 4000.0
     
     DFloat = Float64
     for ArrayType in (HAVE_CUDA ? (CuArray, Array) : (Array,))
