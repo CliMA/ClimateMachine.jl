2e540e905428a63936b17b431fe0f862e986d361
Jeremy E Kozdon <jekozdon@nps.edu>
2019-04-11 20:07:01 -0700

    split travis tests into two sets
    travis has been timing out on some test, so I thought it might be good
    to split these into two test sets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,31 @@
 using Test, Pkg
 
-for submodule in ["Utilities/ParametersType",
-                  "Utilities/PlanetParameters",
-                  "Utilities/RootSolvers",
-                  "Utilities/MoistThermodynamics",
-                  "Atmos/Parameterizations/SurfaceFluxes",
-                  "Atmos/Parameterizations/TurbulenceConvection",
-                  "Mesh",
-                  "DGmethods",
-                  ]
+test_set = try
+  parse(Int, ENV["TEST_SET"])
+catch
+  0
+end
 
-  println("Testing $submodule")
-  include(joinpath("../src",submodule,"test/runtests.jl"))
+if test_set == 0 || test_set == 1
+  for submodule in ["Utilities/ParametersType",
+                    "Utilities/PlanetParameters",
+                    "Utilities/RootSolvers",
+                    "Utilities/MoistThermodynamics",
+                    "Atmos/Parameterizations/SurfaceFluxes",
+                    "Atmos/Parameterizations/TurbulenceConvection",
+                    "Mesh",
+                   ]
+
+    println("Testing $submodule")
+    include(joinpath("../src",submodule,"test/runtests.jl"))
+  end
+end
+
+if test_set == 0 || test_set == 2
+  for submodule in ["DGmethods",
+                   ]
+
+    println("Testing $submodule")
+    include(joinpath("../src",submodule,"test/runtests.jl"))
+  end
 end
