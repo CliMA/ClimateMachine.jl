5b4722d478ba564d5ab81de5e6d57863d951cc71
Jeremy E Kozdon <jekozdon@nps.edu>
2019-04-13 06:11:29 -0700

    Add test groups for travis CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,29 @@
 using Test, Pkg
 
 ENV["JULIA_LOG_LEVEL"] = "WARN"
+test_set = parse(Int, get(ENV, "JULIA_CLIMA_TEST_SET", "0"))
 
-for submodule in ["Utilities/ParametersType",
-                  "Utilities/PlanetParameters",
-                  "Utilities/RootSolvers",
-                  "Utilities/MoistThermodynamics",
-                  "Atmos/Parameterizations/SurfaceFluxes",
-                  "Atmos/Parameterizations/TurbulenceConvection",
-                  "Mesh",
-                  "DGmethods",
-                  "Atmos/Dycore"
-                  ]
+if test_set == 0 || test_set == 1
+  for submodule in ["Utilities/ParametersType",
+                    "Utilities/PlanetParameters",
+                    "Utilities/RootSolvers",
+                    "Utilities/MoistThermodynamics",
+                    "Atmos/Parameterizations/SurfaceFluxes",
+                    "Atmos/Parameterizations/TurbulenceConvection",
+                    "Mesh"
+                   ]
 
-  println("Testing $submodule")
-  include(joinpath("../src",submodule,"test/runtests.jl"))
+    println("Testing $submodule")
+    include(joinpath("../src",submodule,"test/runtests.jl"))
+  end
+end
+
+if test_set == 0 || test_set == 2
+  for submodule in ["DGmethods",
+                    "Atmos/Dycore"
+                   ]
+
+    println("Testing $submodule")
+    include(joinpath("../src",submodule,"test/runtests.jl"))
+  end
 end
