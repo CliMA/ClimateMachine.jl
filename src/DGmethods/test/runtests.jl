6c834c78e15d40d647a6c3af3656c2194a5ba1c6
Frank Giraldo <fxgirald@nps.edu>
2019-05-07 08:43:49 -0700

    Fixed all suggestions for PR #211 as suggested by jkozdon and simonbyrne

--- src/DGmethods/test/runtests.jl
+++ src/DGmethods/test/runtests.jl
@@ -28,6 +28,8 @@ using MPI, Test
                  (3, "conservation/sphere.jl")
                  (1, "compressible_Navier_Stokes/mms_bc.jl")
                  (3, "compressible_Navier_Stokes/mms_bc.jl")
+                 (1, "Sphere/advection_sphere.jl")
+                 (2, "Sphere/advection_sphere.jl")
                 ]
     cmd =  `mpiexec -n $n $(Base.julia_cmd()) --startup-file=no --project=$(Base.active_project()) --code-coverage=$coverage_opt $(joinpath(testdir, f))`
     @info "Running MPI test..." n f cmd
