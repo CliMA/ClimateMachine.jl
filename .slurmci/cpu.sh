38d5e8854be25f225aa20c3f44d619d69c6b27b8
Simon Byrne <simonbyrne@gmail.com>
2019-05-24 12:53:24 -0700

    add scripts and jobs

---
+++ .slurmci/cpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --time=1:00:00     # walltime
+#SBATCH --nodes=1          # number of nodes
+#SBATCH --mem-per-cpu=4G   # memory per CPU core
+
+set -euo pipefail
+
+export JULIA_DEPOT_PATH="${HOME}/.slurmci/clima_cpu"
+
+module load julia/1.1.0 cmake/3.10.2 openmpi/3.1.2
+
+mpiexec julia --startup-file=no --project $1
---
+++ .slurmci/jobs.jl
@@ -0,0 +1,9 @@
+[SlurmJob(`.slurmci/cpu-init.sh`) =>
+ [SlurmJob(`.slurmci/cpu.sh test/DGmethods/Euler/isentropic_vortex_standalone.jl`, ntasks=3),
+  SlurmJob(`.slurmci/cpu.sh test/DGmethods/Euler/isentropic_vortex_standalone_aux.jl`, ntasks=3),
+  SlurmJob(`.slurmci/cpu.sh test/DGmethods/Euler/isentropic_vortex_standalone_source.jl`, ntasks=3),
+  SlurmJob(`.slurmci/cpu.sh test/DGmethods/Euler/isentropic_vortex_standalone_bc.jl`, ntasks=3),
+  SlurmJob(`.slurmci/cpu.sh test/DGmethods/conservation/sphere.jl`, ntasks=3),
+  SlurmJob(`.slurmci/cpu.sh test/DGmethods/compressible_Navier_Stokes/mms_bc.jll`, ntasks=3),
+  SlurmJob(`.slurmci/cpu.sh test/DGmethods/sphere/advection_sphere.jl`, ntasks=2)]
+ ]
