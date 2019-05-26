2c970a8fe63470405c6aa26bde0791a198250c19
Simon Byrne <simonbyrne@gmail.com>
2019-05-26 09:57:43 -0700

    don't run tests through mpiexec

--- .slurmci/jobs.jl
+++ .slurmci/jobs.jl
@@ -1,5 +1,5 @@
 [SlurmJob(`.slurmci/cpu-init.sh`) =>
- [SlurmJob(`.slurmci/cpu.sh test/runtests.jl`, ntasks=1)
+ [SlurmJob(`.slurmci/cpu-test.sh`)
   SlurmJob(`.slurmci/cpu.sh test/Atmos/Dycore/tracers_test.jl`, ntasks=3)
   SlurmJob(`.slurmci/cpu.sh test/Atmos/Dycore/rising_thermal_bubble.jl`, ntasks=3)
   SlurmJob(`.slurmci/cpu.sh test/DGmethods/Euler/isentropic_vortex_standalone.jl`, ntasks=3)
@@ -13,7 +13,7 @@
   SlurmJob(`.slurmci/cpu.sh examples/DGmethods/ex_002_solid_body_rotation.jl`, ntasks=3)
   ]
  SlurmJob(`.slurmci/gpu-init.sh`) =>
- [SlurmJob(`.slurmci/gpu.sh test/runtests.jl`, ntasks=1)
+ [SlurmJob(`.slurmci/gpu-test.sh`)
   SlurmJob(`.slurmci/gpu.sh test/DGmethods/Euler/isentropic_vortex_standalone.jl`, ntasks=3)
   SlurmJob(`.slurmci/gpu.sh test/DGmethods/Euler/isentropic_vortex_standalone_aux.jl`, ntasks=3)
   SlurmJob(`.slurmci/gpu.sh test/DGmethods/Euler/isentropic_vortex_standalone_source.jl`, ntasks=3)
