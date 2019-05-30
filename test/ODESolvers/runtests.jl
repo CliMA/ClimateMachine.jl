b463f983634877b92f72e0ae641aca6ec46114c3
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-05-30 14:53:36 -0700

    More tests for SSP time integrators

--- test/ODESolvers/runtests.jl
+++ test/ODESolvers/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using CLIMA
 using CLIMA.ODESolvers
 using CLIMA.LowStorageRungeKuttaMethod
+using CLIMA.StrongStabilityPreservingRungeKuttaMethod
 
 let 
   function rhs!(dQ, Q, time)
@@ -9,21 +10,27 @@ let
   end
   exactsolution(q0, time) = q0 * exp(sin(time))
 
+  method_order = [(LowStorageRungeKutta, 4),
+                  (StrongStabilityPreservingRungeKutta33, 3),
+                  (StrongStabilityPreservingRungeKutta34, 3)
+                 ]
+
   @testset "ODE Solvers Convergence" begin
     q0 = 1.0
     finaltime = 20.0
     dts = [2.0 ^ (-k) for k = 0:7]
 
-    errors = similar(dts)
-    for (n, dt) in enumerate(dts)
-      Q = [q0]
-      lsrk = LowStorageRungeKutta(rhs!, Q; dt = dt, t0 = 0.0)
-      solve!(Q, lsrk; timeend = finaltime)
-      errors[n] = abs(Q[1] - exactsolution(q0, finaltime))
+    for (method, expected_order) in method_order
+      errors = similar(dts)
+      for (n, dt) in enumerate(dts)
+        Q = [q0]
+        lsrk = method(rhs!, Q; dt = dt, t0 = 0.0)
+        solve!(Q, lsrk; timeend = finaltime)
+        errors[n] = abs(Q[1] - exactsolution(q0, finaltime))
+      end
+      rates = log2.(errors[1:end-1] ./ errors[2:end])
+      @test isapprox(rates[end], expected_order; atol = 0.1)
     end
-
-    rates = log2.(errors[1:end-1] ./ errors[2:end])
-    @test isapprox(rates[end], 4.0; atol = 0.1)
   end
 
   @static if haspkg("CuArrays")
@@ -36,17 +43,18 @@ let
       finaltime = 20.0
       dts = [2.0 ^ (-k) for k = 0:7]
 
-      errors = similar(dts)
-      for (n, dt) in enumerate(dts)
-        Q = CuArray(q0s)
-        lsrk = LowStorageRungeKutta(rhs!, Q; dt = dt, t0 = 0.0)
-        solve!(Q, lsrk; timeend = finaltime)
-        Q = Array(Q)
-        errors[n] = maximum(abs.(Q .- exactsolution.(q0s, finaltime)))
+      for (method, expected_order) in method_order
+        errors = similar(dts)
+        for (n, dt) in enumerate(dts)
+          Q = CuArray(q0s)
+          lsrk = method(rhs!, Q; dt = dt, t0 = 0.0)
+          solve!(Q, lsrk; timeend = finaltime)
+          Q = Array(Q)
+          errors[n] = maximum(abs.(Q .- exactsolution.(q0s, finaltime)))
+        end
+        rates = log2.(errors[1:end-1] ./ errors[2:end])
+        @test isapprox(rates[end], expected_order; atol = 0.1)
       end
-
-      rates = log2.(errors[1:end-1] ./ errors[2:end])
-      @test isapprox(rates[end], 4.0; atol = 0.1)
     end
   end
 end
