5dd6df221a1c4e4886716e8e0a2ab0eaed7bc566
Simon Byrne <simonbyrne@gmail.com>
2019-06-04 14:55:57 -0700

    rejig Slurm jobs

--- .slurmci/cpu.sh
+++ .slurmci/cpu.sh
@@ -5,12 +5,11 @@
 #SBATCH --mem-per-cpu=4G   # memory per CPU core
 
 set -euo pipefail
-
-# to avoid race conditions
-export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot_cpu"
-
 set -x #echo on
 
-module load julia/1.1.0 cmake/3.10.2 openmpi/3.1.2
+export PATH="${PATH}:${HOME}/julia-1.2/bin"
+export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot/common"
 
-mpiexec julia --startup-file=no --project $1
+module load cmake/3.10.2 openmpi/3.1.2
+
+mpiexec julia --color=no --project $1
