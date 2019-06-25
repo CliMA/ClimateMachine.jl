6c223646c3660a67e9b223ed909dc1ad950f98f6
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-06-25 11:03:22 -0700

    Test MPIStateArrays reductions on GPU

--- test/Arrays/reductions.jl
+++ test/Arrays/reductions.jl
@@ -10,23 +10,52 @@ Sys.iswindows() || (isinteractive() && MPI.finalize_atexit())
 const mpicomm = MPI.COMM_WORLD
 
 mpisize = MPI.Comm_size(mpicomm)
+
 localsize = (4, 6, 8)
 
 A = Array{Float32}(reshape(1:prod(localsize), localsize))
-globalA = vcat([A for _ in 1:mpisize])
+globalA = vcat([A for _ in 1:mpisize]...)
 
 QA = MPIStateArray{Tuple{localsize[1:2]...}, Float32, Array}(mpicomm, localsize[3])
 QA .= A
 
+@test isapprox(norm(QA, p = 1), norm(globalA, 1))
 @test isapprox(norm(QA), norm(globalA))
+@test isapprox(norm(QA, p = Inf), norm(globalA, Inf))
 
 B = Array{Float32}(reshape(reverse(1:prod(localsize)), localsize))
-globalB = vcat([B for _ in 1:mpisize])
+globalB = vcat([B for _ in 1:mpisize]...)
 
 QB = similar(QA)
 QB .= B
 
 @test isapprox(euclidean_distance(QA, QB), norm(globalA .- globalB))
+
+@static if haspkg("CuArrays")
+  using CuArrays
+  CuArrays.allowscalar(false)
+
+  localsize = (4, 6, 8)
+
+  A = Array{Float32}(reshape(1:prod(localsize), localsize))
+  globalA = vcat([A for _ in 1:mpisize]...)
+
+  QA = MPIStateArray{Tuple{localsize[1:2]...}, Float32, CuArray}(mpicomm, localsize[3])
+  QA .= A
+
+  @test isapprox(norm(QA, p = 1), norm(globalA, 1))
+  @test isapprox(norm(QA), norm(globalA))
+  @test isapprox(norm(QA, p = Inf), norm(globalA, Inf))
+
+  B = Array{Float32}(reshape(reverse(1:prod(localsize)), localsize))
+  globalB = vcat([B for _ in 1:mpisize]...)
+
+  QB = similar(QA)
+  QB .= B
+
+  @test isapprox(euclidean_distance(QA, QB), norm(globalA .- globalB))
+end
+
 
 isinteractive() || MPI.Finalize()
 nothing
