68331b1fc23981caae7aed5cf04197ed78ad5025
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-06-26 00:50:51 -0700

    Make tests initialization functions isbitstype

--- test/DGmethods/compressible_Navier_Stokes/dycoms.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms.jl
@@ -526,7 +526,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
                              source! = source!)
 
     # This is a actual state/function that lives on the grid
-    initialcondition(Q, x...) = dycoms!(Val(dim), Q, DFloat(0), x...)
+    initialcondition(Q, x...) = dycoms!(Val(dim), Q, 0, x...)
     Q = MPIStateArray(spacedisc, initialcondition)
 
     lsrk = LSRK54CarpenterKennedy(spacedisc, Q; dt = dt, t0 = 0)
