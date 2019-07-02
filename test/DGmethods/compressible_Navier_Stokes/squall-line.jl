6fa1a55b44beda88a517b06ba72989c029efdc99
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-01 17:07:52 -0700

    minor fix

--- test/DGmethods/compressible_Navier_Stokes/squall-line.jl
+++ test/DGmethods/compressible_Navier_Stokes/squall-line.jl
@@ -100,7 +100,7 @@ const Npoly = 4
 
 # Physical domain extents 
 const (xmin, xmax) = (0,160000)
-const (ymin, ymax) = (0,  1000)
+const (ymin, ymax) = (0,  5000)
 const (zmin, zmax) = (0, 24000)
 
 #Get Nex, Ney from resolution
