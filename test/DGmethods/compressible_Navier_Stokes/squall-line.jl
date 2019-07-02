d428a3b16dc1062d3925e4311726a78a7955b3c7
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-01 18:14:05 -0700

    SQUALL: ready to go for Anna

--- test/DGmethods/compressible_Navier_Stokes/squall-line.jl
+++ test/DGmethods/compressible_Navier_Stokes/squall-line.jl
@@ -597,7 +597,7 @@ function squall_line!(dim, Q, t, spl_tinit, spl_qinit, spl_uinit, spl_vinit,
     datav          = DFloat(spl_vinit(xvert))
     datap          = DFloat(spl_pinit(xvert))
     dataq          = dataq / 1000
-
+    
     θ_c =     5.0
     rx  = 10000.0
     ry  =  1500.0
