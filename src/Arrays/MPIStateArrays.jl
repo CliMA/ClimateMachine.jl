5fed22b59845424d30550914b399488e47ebda37
Maciej Waruszewski <maciej.waruszewski.pl@nps.edu>
2019-07-02 02:42:28 +0000

    Held-Suarez test: make it work on the GPU

--- src/Arrays/MPIStateArrays.jl
+++ src/Arrays/MPIStateArrays.jl
@@ -313,7 +313,7 @@ end
 # }}}
 
 # Integral based metrics
-function LinearAlgebra.norm(Q::MPIStateArray, p::Real=2)
+function LinearAlgebra.norm(Q::MPIStateArray, p::Real=Int32(2))
   T = eltype(Q)
 
   if isfinite(p)
