31f315268f3b3af32e3d9e3d8be774d86bc5d469
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-07-03 13:48:22 -0700

    Got new formulation working

--- src/Atmos/Parameterizations/SurfaceFluxes/SurfaceFluxes.jl
+++ src/Atmos/Parameterizations/SurfaceFluxes/SurfaceFluxes.jl
@@ -43,6 +43,7 @@ using ..RootSolvers
 using ..MoistThermodynamics
 using ..PlanetParameters
 using DocStringExtensions
+using NLsolve
 
 export surface_conditions
 
@@ -61,6 +62,7 @@ struct SurfaceFluxConditions{T}
   Monin_Obukhov_length::T
   friction_velocity::T
   temperature_scale::T
+  pottemp_flux::T
   exchange_coeff_momentum::T
   exchange_coeff_heat::T
 end
@@ -72,21 +74,34 @@ function Base.show(io::IO, sfc::SurfaceFluxConditions)
   println(io, "Monin_Obukhov_length    = ", sfc.Monin_Obukhov_length)
   println(io, "friction_velocity       = ", sfc.friction_velocity)
   println(io, "temperature_scale       = ", sfc.temperature_scale)
+  println(io, "pottemp_flux            = ", sfc.pottemp_flux)
   println(io, "exchange_coeff_momentum = ", sfc.exchange_coeff_momentum)
   println(io, "exchange_coeff_heat     = ", sfc.exchange_coeff_heat)
   println(io, "-----------------------")
 end
 
 """
-    surface_conditions(u_ave::DT, θ_bar::DT, Δz::DT, z_0_m::DT, z_0_h::DT,
-        z::DT, F_m::DT, F_h::DT, a::DT, θ_s::DT, Pr::DT) where DT<:AbstractFloat
+    surface_conditions(u_ave::DT,
+                       θ_bar::DT,
+                       θ_ave::DT,
+                       θ_s::DT,
+                       Δz::DT,
+                       z_0_m::DT,
+                       z_0_h::DT,
+                       z::DT,
+                       F_m::DT,
+                       F_h::DT,
+                       a::DT,
+                       Pr::DT,
+                       pottemp_flux::DT
+                       ) where DT<:AbstractFloat
 
 Surface conditions given
  - `u_ave` volume-averaged horizontal wind speed
  - `θ_ave` volume-averaged potential temperature
  - `θ_bar` basic potential temperature
  - `θ_s` surface potential temperature
- - `Δz` layer thickness
+ - `Δz` layer thickness (not spatial discretization)
  - `z_0_m` roughness length for momentum
  - `z_0_h` roughness length for heat
  - `z` coordinate axis
@@ -94,7 +109,10 @@ Surface conditions given
  - `F_h` heat flux at the top
  - `a` free model parameter with prescribed value of 4.7
  - `Pr` Prantl number at neutral stratification
- - `pottemp_flux` potential temperature flux
+ - [`pottemp_flux`] potential temperature flux (optional)
+
+If `pottemp_flux` is not given, then it is computed by iteration
+of equations 3, 17, and 18 in Nishizawa2018.
 """
 function surface_conditions(u_ave::DT,
                             θ_bar::DT,
@@ -108,41 +126,79 @@ function surface_conditions(u_ave::DT,
                             F_h::DT,
                             a::DT,
                             Pr::DT,
-                            pottemp_flux::DT
+                            pottemp_flux::Union{Nothing, DT}=nothing
                             ) where DT<:AbstractFloat
 
-  @assert 0 <= z <= z_0_m
-  @assert 0 <= z <= z_0_h
+  L_MO = 1.0
+  if pottemp_flux!=nothing
 
-  tol_abs, iter_max = DT(1e-3), 100
-  u_star = compute_physical_scale(Δz, a, tol_abs, iter_max, θ_bar, pottemp_flux, z_0_m, u_ave, 0, 1)
-  θ_star = compute_physical_scale(Δz, a, tol_abs, iter_max, θ_bar, pottemp_flux, z_0_h, θ_ave, θ_s, 1/Pr)
+    @assert 0 <= z <= z_0_m
+    @assert 0 <= z <= z_0_h
+
+    xatol, maxiters = DT(1e-3), 100
+    u_star = compute_friction_velocity(Δz, a, xatol, maxiters, θ_bar, pottemp_flux, z_0_m, u_ave, 1)
+    θ_star = compute_temperature_scale(Δz, a, xatol, maxiters, θ_bar, pottemp_flux, z_0_h, θ_ave, θ_s, 1/Pr, u_star)
+    pottemp_flux_star = pottemp_flux
+
+  else
+
+    initial_x = [100, 15.0, 350.0] # TODO: improve initial guess
+    function f!(F, x)
+        L_MO, u, θ = x[1], x[2], x[3]
+        pottemp_flux = - u * θ
+        F[1] = L_MO - compute_MO_len(u, θ_bar, pottemp_flux)
+        F[2] = u - compute_physical_scale(u, u, Δz, a, θ_bar, pottemp_flux, z_0_m, u_ave, 0, 1)
+        F[3] = θ - compute_physical_scale(θ, u, Δz, a, θ_bar, pottemp_flux, z_0_h, θ_ave, θ_s, 1/Pr)
+    end
+    sol = nlsolve(f!, initial_x, autodiff = :forward)
+    if converged(sol)
+      L_MO, u_star, θ_star = sol.zero
+    else
+      error("Unconverged surface fluxes")
+    end
+    pottemp_flux_star = -u_star^3*θ_bar/(k_Karman*grav*L_MO)
+  end
+
   momentum_flux = -u_star^2
   buoyancy_flux = -u_star*θ_star
-  K_m, K_h, L_MO = compute_exchange_coefficients(z, F_m, F_h, a, u_star, θ_star, θ_bar, Pr, pottemp_flux)
+  K_m, K_h, L_MO = compute_exchange_coefficients(z, F_m, F_h, a, u_star, θ_star, θ_bar, Pr, pottemp_flux_star)
 
   return SurfaceFluxConditions(L_MO,
                                momentum_flux,
                                buoyancy_flux,
                                u_star,
                                θ_star,
+                               pottemp_flux_star,
                                K_m,
                                K_h)
 end
 
-"""
-    compute_buoyancy_flux(shf, lhf, T_b, qt_b, ql_b, qi_b, alpha0_0)
 
-Computes buoyancy flux given sensible heat flux `shf`,
-latent heat flux `lhf`, surface boundary temperature `T_b`,
-total specific humidity `qt_b`, liquid specific humidity `ql_b`,
-ice specific humidity `qi_b` and specific `alpha0_0`.
+function compute_physical_scale(x, u, Δz, a, θ_bar, pottemp_flux, z_0, x_ave, x_s, coeff)
+  L = compute_MO_len(u, θ_bar, pottemp_flux)
+  R_z0 = compute_R_z0(z_0, Δz)
+  temp1 = log(Δz/z_0)
+  temp2 = - compute_Psi_m(Δz/L, L, a)
+  temp3 = z_0/Δz * compute_Psi_m(z_0/L, L, a)
+  temp4 = R_z0 * (compute_psi_m(z_0/L, L, a) - 1)
+  return coeff*k_Karman/(temp1+temp2+temp3+temp4)*(x_ave-x_s)
+end
+
 """
-function compute_buoyancy_flux(shf, lhf, T_b, qt_b, ql_b, qi_b, alpha0_0)
-  cp_ = cp_m(PhasePartition(qt_b, ql_b, qi_b))
-  lv = latent_heat_vapor(T_b)
+    compute_buoyancy_flux(heat_flux_sensible, heat_flux_latent, T_b, q_pt, alpha0_0)
+
+Buoyancy flux given
+ - `heat_flux_sensible` sensible heat flux,
+ - `heat_flux_latent` latent heat flux,
+ - `T_b` surface boundary temperature,
+ - `alpha0_0` specific volume,
+ - `q_pt` `PhasePartition`,
+"""
+function compute_buoyancy_flux(heat_flux_sensible, heat_flux_latent, T_b, q_pt, alpha0_0)
+  cp_ = cp_m(q_pt)
+  l_vap = latent_heat_vapor(T_b)
   temp1 = (molmass_ratio-1)
-  temp2 = (shf + temp1 * cp_ * T_b * lhf /lv)
+  temp2 = (heat_flux_sensible + temp1 * cp_ * T_b * heat_flux_latent /l_vap)
   return (grav * alpha0_0 / cp_ / T_b * temp2)
 end
 
@@ -156,7 +212,7 @@ compute_f_m(ζ) = sqrt(sqrt(1-15*ζ))
 compute_f_h(ζ) = sqrt(1-9*ζ)
 
 """ Computes ψ_m in Eq. A3 Ref. Nishizawa2018 """
-function compute_ψ_m(ζ, L, a)
+function compute_psi_m(ζ, L, a)
   f_m = compute_f_m(ζ)
   return L>=0 ? -a*ζ : log((1+f_m)^2*(1+f_m^2)/8) - 2*atan(f_m)+π/2
 end
@@ -165,7 +221,7 @@ end
 compute_ψ_h(ζ, L, a, Pr) = L>=0 ? -a*ζ/Pr : 2*log((1+compute_f_h(ζ))/2)
 
 """ Computes Ψ_m in Eq. A5 Ref. Nishizawa2018 """
-function compute_Ψ_m(ζ, L, a)
+function compute_Psi_m(ζ, L, a)
   if abs(ζ) < eps(typeof(L))
     return ζ>=0 ? -a*ζ/2 : -15*ζ/8 # Computes Ψ_m in Eq. A13 Ref. Nishizawa2018
   else
@@ -194,71 +250,93 @@ Computes Monin-Obukhov length. Eq. 3 Ref. Nishizawa2018
 compute_MO_len(u, θ_bar, flux) = - u^3* θ_bar / (k_Karman * grav * flux)
 
 
-struct Heat end
-struct Momentum end
-
-
 """
-    transport_flux_m(u)
+    compute_friction_velocity(Δz, a, xatol, maxiters, θ_bar, pottemp_flux, z_0, x_ave, coeff)
 
-Momentum transport flux
+Computes friction velocity `u_star` given
+
+ - `Δz` layer thickness (not spatial discretization)
+ - `a` free model parameter with prescribed value of 4.7
+ - `xatol` absolute error tolerance for iteration
+ - `maxiters` maximum number of iterations
+ - `θ_bar` basic potential temperature
+ - `pottemp_flux` potential temperature flux
+ - `z_0` roughness length (for momentum)
+ - `x_ave` volume-averaged friction velocity
+ - `coeff` equation coefficient (1)
 """
-transport_flux(u, ::Momentum) = -u^2
-
-"""
-    transport_flux_h(u)
-
-Thermal heat transport flux
-"""
-transport_flux_h(u, θ) = -u*θ
-
-"""
-    compute_physical_scale(Δz, a, tol_abs, iter_max, θ_bar, pottemp_flux, z_0, x_ave, x_s, coeff)
-
-`x_star = κ/denom*(x_ave-x_s)`
-
-where
-
-`denom = ln(Δz/z_0) - Ψ_m(Δz/L) + z_0/Δz * Ψ_m(z_0/L) + R_z0 (ψ_m(z_0/L) - 1)`
-"""
-function compute_physical_scale(Δz, a, tol_abs, iter_max, θ_bar, pottemp_flux, z_0, x_ave, x_s, coeff)
+function compute_friction_velocity(Δz, a, xatol, maxiters, θ_bar, pottemp_flux, z_0, x_ave, coeff)
   x_star_0 = x_ave * k_Karman / log(Δz / z_0)
   x_star = x_star_0
-  let x_ave=x_ave, θ_bar=θ_bar, Δz=Δz, z_0=z_0, a=a, tol_abs=tol_abs, iter_max=iter_max
-    # Note the lowercase psi (ψ) and uppercase psi (Ψ):
-    Ψ_m_closure(ζ, L) = compute_Ψ_m(ζ, L, a)
-    ψ_m_closure(ζ, L) = compute_ψ_m(ζ, L, a)
+  let x_ave=x_ave, θ_bar=θ_bar, Δz=Δz, z_0=z_0, a=a, xatol=xatol, maxiters=maxiters
     function compute_x_star(x)
-      L = compute_MO_len(x, θ_bar, pottemp_flux)
+      L = compute_MO_len(x, θ_bar, pottemp_flux) # Note difference with compute_temperature_scale
       R_z0 = compute_R_z0(z_0, Δz)
       temp1 = log(Δz/z_0)
-      temp2 = - Ψ_m_closure(Δz/L, L)
-      temp3 = z_0/Δz * Ψ_m_closure(z_0/L, L)
-      temp4 = R_z0 * (ψ_m_closure(z_0/L, L) - 1)
+      temp2 = - compute_Psi_m(Δz/L, L, a)
+      temp3 = z_0/Δz * compute_Psi_m(z_0/L, L, a)
+      temp4 = R_z0 * (compute_psi_m(z_0/L, L, a) - 1)
+      return coeff*k_Karman/(temp1+temp2+temp3+temp4)*x_ave
+    end
+    compute_roots(x) = x - compute_x_star(x)
+    x_star_1 = compute_x_star(x_star_0)
+    x_star, converged = RootSolvers.find_zero(x -> compute_roots(x), x_star_0, x_star_1, SecantMethod();
+                                              xatol=xatol, maxiters=maxiters)
+  end
+  return x_star
+end
+
+"""
+    compute_temperature_scale(Δz, a, xatol, maxiters, θ_bar, pottemp_flux, z_0, x_ave, x_s, coeff, u_star)
+
+Computes temperature scale `θ_star` given
+
+ - `Δz` layer thickness (not spatial discretization)
+ - `a` free model parameter with prescribed value of 4.7
+ - `xatol` absolute error tolerance for iteration
+ - `maxiters` maximum number of iterations
+ - `θ_bar` basic potential temperature
+ - `pottemp_flux` potential temperature flux
+ - `z_0` roughness length (for heat)
+ - `x_ave` volume-averaged potential temperature
+ - `x_s` surface potential temperature
+ - `coeff` equation coefficient (1/Prantl)
+ - `u_star` friction velocity
+"""
+function compute_temperature_scale(Δz, a, xatol, maxiters, θ_bar, pottemp_flux, z_0, x_ave, x_s, coeff, u_star)
+  x_star_0 = x_ave * k_Karman / log(Δz / z_0)
+  x_star = x_star_0
+  let x_ave=x_ave, θ_bar=θ_bar, Δz=Δz, z_0=z_0, a=a, xatol=xatol, maxiters=maxiters
+    function compute_x_star(x)
+      L = compute_MO_len(u_star, θ_bar, pottemp_flux) # Note difference with compute_friction_velocity
+      R_z0 = compute_R_z0(z_0, Δz)
+      temp1 = log(Δz/z_0)
+      temp2 = - compute_Psi_m(Δz/L, L, a)
+      temp3 = z_0/Δz * compute_Psi_m(z_0/L, L, a)
+      temp4 = R_z0 * (compute_psi_m(z_0/L, L, a) - 1)
       return coeff*k_Karman/(temp1+temp2+temp3+temp4)*(x_ave-x_s)
     end
     compute_roots(x) = x - compute_x_star(x)
     x_star_1 = compute_x_star(x_star_0)
     x_star, converged = RootSolvers.find_zero(x -> compute_roots(x), x_star_0, x_star_1, SecantMethod();
-                                              xatol=tol_abs, maxiters=iter_max)
+                                              xatol=xatol, maxiters=maxiters)
   end
   return x_star
 end
 
-
 """
-    compute_exchange_coefficients(z, F_m, F_h, a, u_star, θ_star, θ_bar, Pr)
+    compute_exchange_coefficients(z, F_m, F_h, a, u_star, θ_star, θ_bar, Pr, pottemp_flux)
 
-Computes exchange transfer coefficients:
+Computes exchange transfer coefficients
 
-  - K_D  momentum exchange coefficient
-  - K_H  thermodynamic exchange coefficient
-  - L_MO Monin-Obukhov length
+  - `K_D`  momentum exchange coefficient
+  - `K_H`  thermodynamic exchange coefficient
+  - `L_MO` Monin-Obukhov length
 """
 function compute_exchange_coefficients(z, F_m, F_h, a, u_star, θ_star, θ_bar, Pr, pottemp_flux)
 
   L_MO = compute_MO_len(u_star, θ_bar, pottemp_flux)
-  ψ_m = compute_ψ_m(z/L_MO, L_MO, a)
+  ψ_m = compute_psi_m(z/L_MO, L_MO, a)
   ψ_h = compute_ψ_h(z/L_MO, L_MO, a, Pr)
 
   K_m = -F_m*k_Karman*z/(u_star * ψ_m) # Eq. 19 in Ref. Nishizawa2018
