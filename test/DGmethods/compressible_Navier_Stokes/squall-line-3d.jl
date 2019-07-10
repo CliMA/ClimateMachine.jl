7a2f7d33ce56c4693d2b0a68e8f33efaf0628b61
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-10 14:17:36 -0700

    name of output directory for 3d squall

--- test/DGmethods/compressible_Navier_Stokes/squall-line-3d.jl
+++ test/DGmethods/compressible_Navier_Stokes/squall-line-3d.jl
@@ -933,7 +933,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
         postprocessarray = MPIStateArray(spacedisc; nstate=npoststates)
 
         step = [0]
-        mkpath("./CLIMA-output-scratch/vtk-squall-line/")
+        mkpath("./CLIMA-output-scratch/vtk-squall-line-3d/")
         cbvtk = GenericCallbacks.EveryXSimulationSteps(3200) do (init=false) #every 1 min = (0.025) * 40 * 60 * 1min
             DGBalanceLawDiscretizations.dof_iteration!(postprocessarray, spacedisc, Q) do R, Q, QV, aux
                 @inbounds let
@@ -965,7 +965,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
                 end
             end
 
-            outprefix = @sprintf("./CLIMA-output-scratch/vtk-squall-line/squall_%dD_mpirank%04d_step%04d", dim,
+            outprefix = @sprintf("./CLIMA-output-scratch/vtk-squall-line-3d/squall_%dD_mpirank%04d_step%04d", dim,
                                  MPI.Comm_rank(mpicomm), step[1])
             @debug "doing VTK output" outprefix
             writevtk(outprefix, Q, spacedisc, statenames,
