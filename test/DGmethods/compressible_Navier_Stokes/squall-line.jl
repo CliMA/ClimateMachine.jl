8b23e70f343af343f5d0664e227610cfc5e03721
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-10 12:03:16 -0700

    sponge changes in x and y

--- test/DGmethods/compressible_Navier_Stokes/squall-line.jl
+++ test/DGmethods/compressible_Navier_Stokes/squall-line.jl
@@ -950,12 +950,13 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
                                        simtime = %.16e
                                        runtime = %s
                                        max(Qtot) = %.16e
-                                       max(Qliq) = %.16e""",
+                                       max(Qliq) = %.16e
+                                       max(Qrai) = %.16e""",
                                ODESolvers.gettime(lsrk),
                                Dates.format(convert(Dates.DateTime,
                                                     Dates.now()-starttime[]),
                                             Dates.dateformat"HH:MM:SS"),
-                               qt_max, ql_max)
+                               qt_max, ql_max, qr_max)
 
                 #@info @sprintf """dt = %25.16e""" dt                
             end
