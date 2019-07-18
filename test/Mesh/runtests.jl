417b5ca0a997073c00e2c22bf696223dc502703d
Jeremy E Kozdon <jekozdon@nps.edu>
2019-07-18 14:08:30 +0000

    Fix using mpiexec within an initialized MPI job

--- test/Mesh/runtests.jl
+++ test/Mesh/runtests.jl
@@ -2,8 +2,6 @@ using MPI, Test
 
 include("../testhelpers.jl")
 
-MPI.Init()
-
 include("BrickMesh.jl")
 include("Elements.jl")
 include("Metrics.jl")
@@ -11,6 +9,12 @@ include("Metrics.jl")
 include("topology.jl")
 include("grid_integral.jl")
 include("filter.jl")
+
+# runmpi won't work if we do not finalize
+# This is not so nice since other tests that are run direction and call MPI.Init
+# will fail if we do finalize here (since runmpi won't work in an initialized
+# state)
+MPI.Initialized() && MPI.Finalize()
 
 @testset "MPI Jobs" begin
 
