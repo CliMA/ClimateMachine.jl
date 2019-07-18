c15256bbb2c74a3e8189c98e9be1d55568a95587
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-07-18 14:21:58 -0700

    Test GMRES linear solver

--- test/LinearSolvers/smallsystem.jl
+++ test/LinearSolvers/smallsystem.jl
@@ -2,18 +2,26 @@ using Test
 using CLIMA
 using CLIMA.LinearSolvers
 using CLIMA.GeneralizedConjugateResidualSolver
+using CLIMA.GeneralizedMinimalResidualSolver
 
 using LinearAlgebra, Random
-
 
 # this test setup is partly based on IterativeSolvers.jl, see e.g
 # https://github.com/JuliaMath/IterativeSolvers.jl/blob/master/test/cg.jl
 @testset "LinearSolvers small full system" begin
   n = 10
 
-  expected_iters = Dict(Float32 => 4, Float64 => 6)
+  methods = ((b, tol) -> GeneralizedConjugateResidual(2, b, tol),
+             (b, tol) -> GeneralizedMinimalResidual(3, b, tol),
+             (b, tol) -> GeneralizedMinimalResidual(n, b, tol)
+            )
 
-  for T in [Float32, Float64]
+  expected_iters = (Dict(Float32 => 4, Float64 => 6),
+                    Dict(Float32 => 4, Float64 => 7),
+                    Dict(Float32 => 1, Float64 => 1)
+                   )
+
+  for (m, method) in enumerate(methods), T in [Float32, Float64]
     Random.seed!(44)
 
     A = rand(T, n, n)
@@ -23,19 +31,19 @@ using LinearAlgebra, Random
     mulbyA!(y, x) = (y .= A * x)
 
     tol = sqrt(eps(T))
-    gcrk = GeneralizedConjugateResidual(2, b, tol)
+    linearsolver = method(b, tol)
     
     x = rand(T, n)
-    iters = linearsolve!(mulbyA!, x, b, gcrk)
+    iters = linearsolve!(mulbyA!, x, b, linearsolver)
 
-    @test iters == expected_iters[T]
+    @test iters == expected_iters[m][T]
     @test norm(A * x - b) / norm(b) <= tol
    
     newtol = 1000tol
-    settolerance!(gcrk, newtol)
+    settolerance!(linearsolver, newtol)
     
     x = rand(T, n)
-    linearsolve!(mulbyA!, x, b, gcrk)
+    linearsolve!(mulbyA!, x, b, linearsolver)
 
     @test norm(A * x - b) / norm(b) <= newtol
     @test norm(A * x - b) / norm(b) >= tol
