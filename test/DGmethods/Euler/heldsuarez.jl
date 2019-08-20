b647322fa241d9bdbd8ed4da406c1ffb7a205712
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-08-20 02:04:19 -0700

    Wip on Held-Suarez in EulerModel

---
+++ test/DGmethods/Euler/heldsuarez.jl
@@ -0,0 +1,255 @@
+using MPI
+using CLIMA
+using CLIMA.Mesh.Topologies
+using CLIMA.Mesh.Grids
+using CLIMA.Mesh.Filters
+using CLIMA.DGmethods
+using CLIMA.DGmethods.NumericalFluxes
+using CLIMA.MPIStateArrays
+using CLIMA.LowStorageRungeKuttaMethod
+using CLIMA.ODESolvers
+using CLIMA.GenericCallbacks
+using LinearAlgebra
+using StaticArrays
+using Logging, Printf, Dates
+using CLIMA.Vtk
+using CLIMA.PlanetParameters: planet_radius, R_d, cv_d, cp_d, MSLP
+using CLIMA.MoistThermodynamics: air_pressure, air_temperature, internal_energy, air_density,
+                                 gas_constant_air, soundspeed_air
+
+@static if haspkg("CuArrays")
+  using CUDAdrv
+  using CUDAnative
+  using CuArrays
+  CuArrays.allowscalar(false)
+end
+
+include("standalone_euler_model.jl")
+
+struct HeldSuarez{DFloat} <: EulerProblem
+  domain_height::DFloat
+end
+
+function parametrized_source!(::ReferenceStateEulerModel, ::HeldSuarez,
+                              source::Vars, state::Vars, aux::Vars)
+
+  δρ, ρu⃗, δρe = state.δρ, state.ρu⃗, state.δρe
+  ρ_ref, ρe_ref = aux.ρ_ref, aux.ρe_ref
+  ρ, ρe = ρ_ref + δρ, ρe_ref + δρe
+  x⃗, ϕ = aux.coord, aux.gravity.ϕ
+
+  e = ρe / ρ
+  u⃗ = ρu⃗ / ρ
+  e_int = e - u⃗' * u⃗ / 2 - ϕ
+  T = air_temperature(e_int)
+  p = air_pressure(T, ρ)
+
+  DFloat = eltype(ρu⃗)
+  # Held-Suarez constants
+  p0 :: DFloat = MSLP
+  ka :: DFloat = 1 / 40 / 86400
+  kf :: DFloat = 1 / 86400
+  ks :: DFloat = 1 / 4 / 86400
+  ΔT_y :: DFloat = 60 
+  ΔT_z :: DFloat = 10   
+  temp0 :: DFloat = 315
+  temp_min :: DFloat = 200
+  hd :: DFloat = 7000 #from Smolarkiewicz JAS 2001 paper    
+  σ_b :: DFloat = 7 / 10
+
+  r = hypot(x⃗...)
+  λ = atan(x⃗[2], x⃗[1])
+  φ = asin(x⃗[3] / r)
+  h = r - DFloat(planet_radius)
+
+  σ = exp(-h/hd)
+#  σ = P/p0
+  Δσ = (σ - σ_b)/(1 - σ_b)
+  π = σ^(R_d/cp_d)
+  c = max(0, Δσ)
+  T_eq = ( temp0 - ΔT_y*sin(φ)^2 - ΔT_z*log(σ)*cos(φ)^2 )*π
+  T_eq = max(temp_min, T_eq)
+  kt = ka + (ks-ka)*c*cos(φ)^4
+  kv = kf*c
+  kt = kt*(1 - T_eq/T)
+
+  source.ρu⃗ += -kv * ρu⃗
+  source.δρe += -(kt * ρ * cv_d * T +  kv * ρu⃗' * ρu⃗ / ρ)
+end
+
+function initial_condition!(::ReferenceStateEulerModel, ::HeldSuarez, state::Vars, _...)
+  state.δρ = 0
+  state.ρu⃗ = SVector(0, 0, 0)
+  state.δρe = 0
+end
+
+function reference_state!(::ReferenceStateEulerModel, ::HeldSuarez, aux::Vars, x⃗)
+  DFloat = eltype(x⃗)
+  
+  r = hypot(x⃗...)
+  h = r - DFloat(planet_radius)
+  ϕ = aux.gravity.ϕ
+
+  T_ref = DFloat(255)
+  scale_height = R_d * T_ref / grav
+
+  P_ref = MSLP * exp(-h / scale_height)
+  ρ_ref = air_density(T_ref, P_ref)
+  ρe_ref = ρ_ref * (internal_energy(T_ref) + ϕ)
+
+  aux.ρ_ref = ρ_ref
+  aux.ρe_ref = ρe_ref
+end
+
+gravitymodel(::HeldSuarez) = SphereGravity(planet_radius)
+
+function run(mpicomm, ArrayType, problem, topl, N, outputtime, timeend, DFloat, dt)
+  grid = DiscontinuousSpectralElementGrid(topl,
+                                          FloatType = DFloat,
+                                          DeviceArray = ArrayType,
+                                          polynomialorder = N,
+                                          meshwarp = Topologies.cubedshellwarp
+                                         )
+
+  dg = DGModel(ReferenceStateEulerModel(true, problem, Coriolis()),
+               grid,
+               Rusanov(),
+               DefaultGradNumericalFlux())
+
+  param = init_ode_param(dg)
+
+  Q = init_ode_state(dg, param, DFloat(0))
+
+  lsrk = LSRK144NiegemannDiehlBusch(dg, Q; dt = dt, t0 = 0)
+
+  filter = ExponentialFilter(grid, 0, 14)
+  cbfilter = GenericCallbacks.EveryXSimulationSteps(1) do
+    Filters.apply!(Q, 1:size(Q, 2), grid, filter)
+    nothing
+  end
+
+  eng0 = norm(Q)
+  @info @sprintf """Starting
+  norm(Q₀) = %.16e""" eng0
+
+  # Set up the information callback
+  starttime = Ref(now())
+  cbinfo = GenericCallbacks.EveryXWallTimeSeconds(60, mpicomm) do (s=false)
+    if s
+      starttime[] = now()
+    else
+      energy = norm(Q)
+      @info @sprintf("""Update
+                     simtime = %.16e
+                     runtime = %s
+                     norm(Q) = %.16e""", ODESolvers.gettime(lsrk),
+                     Dates.format(convert(Dates.DateTime,
+                                          Dates.now()-starttime[]),
+                                  Dates.dateformat"HH:MM:SS"),
+                     energy)
+    end
+  end
+
+  ## Define a convenience function for VTK output
+  VTKDIR = "hs_vtk"
+  mkpath(VTKDIR)
+  function do_output(vtk_step)
+    ## name of the file that this MPI rank will write
+    filename = @sprintf("%s/held_suarez_mpirank%04d_step%04d",
+                        VTKDIR, MPI.Comm_rank(mpicomm), vtk_step)
+
+    ## write the vtk file for this MPI rank
+    statenames = ("δρ", "ρu", "ρv", "ρw", "δρe")
+    auxnames = ("x", "y", "z" ,"ρ_ref", "ρe_ref", "ϕ", "∇ϕ_x", "∇ϕ_y", "∇ϕ_z")
+    writevtk(filename, Q, dg, statenames, param.aux, auxnames)
+
+    ## Generate the pvtu file for these vtk files
+    if MPI.Comm_rank(mpicomm) == 0
+      ## name of the pvtu file
+      pvtuprefix = @sprintf("%s/held_suarez_step%04d", VTKDIR, vtk_step)
+
+      ## name of each of the ranks vtk files
+      prefixes = ntuple(i->
+                        @sprintf("held_suarez_mpirank%04d_step%04d",
+                                 i-1, vtk_step),
+                        MPI.Comm_size(mpicomm))
+
+      ## Write out the pvtu file
+      writepvtu(pvtuprefix, prefixes, (statenames..., auxnames...))
+
+      ## write that we have written the file
+      @info @sprintf("Done writing VTK: %s", pvtuprefix)
+    end
+
+  end
+
+  ## Setup callback for writing VTK every hour of simulation time and dump
+  #initial file
+  vtk_step = 0
+  do_output(vtk_step)
+  cbvtk = GenericCallbacks.EveryXSimulationSteps(floor(outputtime / dt)) do
+    vtk_step += 1
+    do_output(vtk_step)
+    nothing
+  end
+
+  solve!(Q, lsrk, param; timeend=timeend, callbacks=(cbinfo, cbfilter, cbvtk))
+
+  engf = norm(Q)
+
+  @info @sprintf """Finished
+  norm(Q)                 = %.16e
+  norm(Q) - norm(Q₀)      = %.16e
+  """ engf engf-eng0
+  engf
+end
+
+using Test
+let
+  MPI.Initialized() || MPI.Init()
+  mpicomm = MPI.COMM_WORLD
+  ll = uppercase(get(ENV, "JULIA_LOG_LEVEL", "INFO"))
+  loglevel = ll == "DEBUG" ? Logging.Debug :
+  ll == "WARN"  ? Logging.Warn  :
+  ll == "ERROR" ? Logging.Error : Logging.Info
+  logger_stream = MPI.Comm_rank(mpicomm) == 0 ? stderr : devnull
+  global_logger(ConsoleLogger(logger_stream, loglevel))
+  @static if haspkg("CUDAnative")
+    device!(MPI.Comm_rank(mpicomm) % length(devices()))
+  end
+
+  DFloat = Float64
+  polynomialorder = 5
+  num_elem_horz = 6
+  num_elem_vert = 8
+
+  @static if haspkg("CuArrays")
+    ArrayType = CuArray
+  else
+    ArrayType = Array
+  end
+
+  domain_height = DFloat(30e3)
+  hs = HeldSuarez(domain_height)
+
+  Rrange = range(DFloat(planet_radius), length = num_elem_vert + 1,
+                 stop = planet_radius + hs.domain_height)
+
+  topl = StackedCubedSphereTopology(mpicomm, num_elem_horz, Rrange)
+
+  element_size = (hs.domain_height / num_elem_vert)
+  acoustic_speed = soundspeed_air(DFloat(315))
+  lucas_magic_factor = 14
+  dt = lucas_magic_factor * element_size / acoustic_speed / polynomialorder ^ 2
+  
+  seconds = 1
+  minutes = 60
+  hours = 3600
+  days = 86400
+  outputtime = dt
+  timeend = 1minutes
+
+  run(mpicomm, ArrayType, hs, topl, polynomialorder, outputtime, timeend, DFloat, dt)
+
+end
+nothing
--- test/DGmethods/Euler/standalone_euler_model.jl
+++ test/DGmethods/Euler/standalone_euler_model.jl
@@ -1,5 +1,7 @@
 using CLIMA.VariableTemplates
 using StaticArrays
+using CLIMA.PlanetParameters: grav, Omega, cv_d, T_0
+using GPUifyLoops: @unroll
 
 import CLIMA.DGmethods: BalanceLaw, vars_aux, vars_state, vars_gradient,
                         vars_diffusive, flux!, source!, wavespeed,
@@ -8,29 +10,66 @@ import CLIMA.DGmethods: BalanceLaw, vars_aux, vars_state, vars_gradient,
 
 abstract type EulerProblem end
 
-struct EulerModel{P} <: BalanceLaw
+abstract type AbstractEulerModel <: BalanceLaw end
+
+init_state!(m::AbstractEulerModel, x...) = initial_condition!(m, m.problem, x...)
+vars_gradient(::AbstractEulerModel, T) = Tuple{}
+vars_diffusive(::AbstractEulerModel, T) = Tuple{}
+gradvariables!(::AbstractEulerModel, _...) = nothing
+diffusive!(::AbstractEulerModel, _...) = nothing
+
+struct EulerModel{P, G} <: AbstractEulerModel
   problem::P
-  function EulerModel(problem)
-    P = typeof(problem)
-    new{P}(problem)
+  gravity::G
+end
+function EulerModel(problem)
+  gravity = gravitymodel(problem)
+  EulerModel{typeof(problem), typeof(gravity)}(problem, gravity)
+end
+
+function vars_state(::EulerModel, T)
+  @vars begin
+    ρ::T
+    ρu⃗::SVector{3, T}
+    ρe::T
   end
 end
 
-init_state!(m::EulerModel, x...) = initial_condition!(m, m.problem, x...)
-
-function vars_state(::EulerModel, T)
-  NamedTuple{(:ρ, :ρu⃗, :ρe), Tuple{T, SVector{3, T}, T}}
+function vars_aux(m::EulerModel, T)
+  @vars begin
+    gravity::vars_aux(m.gravity, T)
+  end
 end
-
-vars_aux(m::EulerModel, T) = Tuple{}
-vars_gradient(::EulerModel, T) = Tuple{}
-vars_diffusive(::EulerModel, T) = Tuple{}
 
 const γ_exact = 7 // 5
 
 function pressure(ρ::T, ρinv, ρe, ρu⃗, ϕ) where T
   γ::T = γ_exact
-  (γ - 1) * (ρe - ρinv * ρu⃗' * ρu⃗ / 2 - ϕ * ρ)
+  (γ - 1) * (ρe + ρ * cv_d * T_0 - ρinv * ρu⃗' * ρu⃗ / 2 - ϕ * ρ)
+end
+
+function nofluxbc!(scalars, stateP, nM, stateM)
+  @inbounds begin
+    ## scalars are preserved
+    @unroll for s in scalars
+      setproperty!(stateP, s, getproperty(stateM, s))
+    end
+
+    ## reflect velocities
+    ρu⃗M = stateM.ρu⃗
+    n⃗ = SVector(nM)
+    n⃗_ρu⃗M = n⃗' * ρu⃗M
+    stateP.ρu⃗ = ρu⃗M - 2n⃗_ρu⃗M * n⃗
+  end
+end
+
+function boundarycondition!(::EulerModel, stateP::Vars, _, auxP::Vars, normalM,
+                            stateM::Vars, _, auxM::Vars, bctype, t)
+  if bctype == 1
+    nofluxbc!((:ρ, :ρe), stateP, normalM, stateM)
+  else
+    error("unknown boundary condition type!")
+  end
 end
 
 function flux!(m::EulerModel, flux::Grad, state::Vars, _::Vars, aux::Vars,
@@ -40,7 +79,7 @@ function flux!(m::EulerModel, flux::Grad, state::Vars, _::Vars, aux::Vars,
 
   ρinv = 1 / ρ
   u⃗ = ρinv * ρu⃗
-  ϕ = -zero(eltype(ρ)) # FIXME: need gravity model
+  ϕ = geopotential(m.gravity, aux)
   p = pressure(ρ, ρinv, ρe, ρu⃗, ϕ)
 
   # compute the flux!
@@ -49,29 +88,193 @@ function flux!(m::EulerModel, flux::Grad, state::Vars, _::Vars, aux::Vars,
   flux.ρe = u⃗ * (ρe + p)
 end
 
-gradvariables!(::EulerModel, _...) = nothing
-diffusive!(::EulerModel, _...) = nothing
-
 function source!(m::EulerModel, source::Vars, state::Vars, aux::Vars, t::Real)
   source.ρ = 0
   source.ρu⃗ = @SVector zeros(eltype(source.ρu⃗), 3)
   source.ρe = 0
+  geopotential_source!(m.gravity, state.ρ, source, state, aux)
+  coriolis_source!(m.coriolis, source, state, aux)
 end
 
-init_aux!(m::EulerModel, aux::Vars, (x1, x2, x3)) = nothing
+function init_aux!(m::EulerModel, aux::Vars, x⃗)
+  init_aux!(m.gravity, aux, x⃗)
+end
 
-function wavespeed(m::EulerModel, nM, state::Vars, aux::Vars, t::Real)
-  T = eltype(state)
+function wavespeed_euler(ϕ::T, ρ::T, ρu⃗, ρe::T, nM) where T
   γ = T(γ_exact)
-
-  (ρ, ρu⃗, ρe) = (state.ρ, state.ρu⃗, state.ρe)
-
   ρinv = 1 / ρ
   u⃗ = ρinv * ρu⃗
-  ϕ = -zero(eltype(ρ)) # FIXME: need gravity model
   p = pressure(ρ, ρinv, ρe, ρu⃗, ϕ)
   @inbounds n⃗ = SVector{3, T}(nM[1], nM[2], nM[3])
   abs(n⃗' * u⃗) + sqrt(ρinv * γ * p)
 end
 
-boundarycondition!(::EulerModel, _...) = nothing
+
+function wavespeed(m::EulerModel, nM, state::Vars, aux::Vars, t::Real)
+  (ρ, ρu⃗, ρe) = (state.ρ, state.ρu⃗, state.ρe)
+  ϕ = geopotential(m.gravity, aux)
+  wavespeed_euler(ϕ, ρ, ρu⃗, ρe, nM)
+end
+
+abstract type GravityModel end
+vars_aux(m::GravityModel, T) = @vars(ϕ::T, ∇ϕ::SVector{3, T})
+geopotential(::GravityModel, aux) = aux.gravity.ϕ
+function geopotential_source!(::GravityModel, ρ, source, state, aux)
+  source.ρu⃗ -= ρ * aux.gravity.∇ϕ
+end
+
+struct NoGravity <: GravityModel end
+vars_aux(m::NoGravity, T) = @vars()
+init_aux!(::NoGravity, _...) = nothing
+geopotential(::NoGravity, _...) = 0
+geopotential_source!(::NoGravity, _...) = nothing
+
+struct SphereGravity{T} <: GravityModel
+  h::T
+end
+function init_aux!(g::SphereGravity, aux, x⃗)
+  x⃗ = SVector(x⃗)
+  r = hypot(x⃗...)
+  aux.gravity.ϕ = grav * (r-g.h)
+  T = eltype(aux.gravity.∇ϕ)
+  aux.gravity.∇ϕ = T(grav) * x⃗ / r
+end
+
+struct BoxGravity{dim} <: GravityModel end
+function init_aux!(::BoxGravity{dim}, aux, x⃗) where dim
+  @inbounds aux.gravity.ϕ = grav * x⃗[dim]
+
+  T = eltype(aux.gravity.∇ϕ)
+  if dim == 2
+    aux.gravity.∇ϕ = SVector{3, T}(0, grav, 0)
+  else
+    aux.gravity.∇ϕ = SVector{3, T}(0, 0, grav)
+  end
+end
+
+struct NoGravity <: GravityModel end
+vars_aux(m::NoGravity, T) = @vars()
+init_aux!(::NoGravity, _...) = nothing
+geopotential(::NoGravity, _...) = 0
+geopotential_source!(::NoGravity, _...) = nothing
+
+struct ReferenceStateEulerModel{P, C, G} <: AbstractEulerModel
+  pde_level_hydrostatic_balance::Bool
+  problem::P
+  coriolis::C
+  gravity::G
+end
+
+function ReferenceStateEulerModel(balanced, problem, coriolis)
+  gravity = gravitymodel(problem)
+  args = problem, coriolis, gravity
+  ReferenceStateEulerModel{typeof.(args)...}(balanced, args...)
+end
+
+function vars_state(::ReferenceStateEulerModel, T)
+  @vars begin
+    δρ::T
+    ρu⃗::SVector{3, T}
+    δρe::T
+  end
+end
+
+function vars_aux(m::ReferenceStateEulerModel, T)
+  @vars begin
+    coord::SVector{3, T}
+    ρ_ref::T
+    ρe_ref::T
+    gravity::vars_aux(m.gravity, T)
+  end
+end
+
+function init_aux!(m::ReferenceStateEulerModel, aux::Vars, x⃗)
+  aux.coord = SVector(x⃗)
+  init_aux!(m.gravity, aux, x⃗)
+  reference_state!(m, m.problem, aux, x⃗)
+end
+
+function wavespeed(m::ReferenceStateEulerModel, nM, state::Vars, aux::Vars,
+                   t::Real)
+  δρ, ρu⃗, δρe = state.δρ, state.ρu⃗, state.δρe
+  ρ_ref, ρe_ref = aux.ρ_ref, aux.ρe_ref
+  ρ, ρe = ρ_ref + δρ, ρe_ref + δρe
+  ϕ = geopotential(m.gravity, aux)
+  wavespeed_euler(ϕ, ρ, ρu⃗, ρe, nM)
+end
+
+function flux!(m::ReferenceStateEulerModel, flux::Grad, state::Vars, _::Vars,
+               aux::Vars, t::Real)
+
+  δρ, ρu⃗, δρe = state.δρ, state.ρu⃗, state.δρe
+  ρ_ref, ρe_ref = aux.ρ_ref, aux.ρe_ref
+  ρ, ρe = ρ_ref + δρ, ρe_ref + δρe
+
+  ρinv = 1 / ρ
+  u⃗ = ρinv * ρu⃗
+  ϕ = geopotential(m.gravity, aux)
+  p = pressure(ρ, ρinv, ρe, ρu⃗, ϕ)
+
+  p_ρu⃗ = p
+  if m.pde_level_hydrostatic_balance
+    ρinv_ref = 1 / ρ_ref
+    u⃗_ref = @SVector zeros(eltype(ρu⃗), 3)
+    p_ref = pressure(ρ_ref, ρinv_ref, ρe_ref, u⃗_ref, ϕ)
+    p_ρu⃗ -= p_ref
+  end
+
+  # compute the flux!
+  flux.δρ  = ρu⃗
+  flux.ρu⃗ = ρu⃗ .* u⃗' + p_ρu⃗ * I
+  flux.δρe = u⃗ * (ρe + p)
+end
+
+function source!(m::ReferenceStateEulerModel, source::Vars, state::Vars, aux::Vars, t::Real)
+  source.δρ = 0
+  source.ρu⃗ = @SVector zeros(eltype(source.ρu⃗), 3)
+  source.δρe = 0
+  if m.pde_level_hydrostatic_balance
+    geopotential_source!(m.gravity, state.δρ, source, state, aux)
+  else
+    ρ = aux.ρ_ref + state.δρ
+    geopotential_source!(m.gravity, ρ, source, state, aux)
+  end
+  coriolis_source!(m.coriolis, source, state, aux)
+  
+  parametrized_source!(m, m.problem, source, state, aux)
+end
+parametrized_source!(::ReferenceStateEulerModel, ::EulerProblem, _...) = nothing
+
+abstract type CoriolisModel end
+vars_aux(m::CoriolisModel, T) = @vars()
+function coriolis_source!(::CoriolisModel, source, state, aux)
+   Ω⃗ = SVector(0, 0, Omega)
+   source.ρu⃗ += -2Ω⃗ × state.ρu⃗
+end
+struct Coriolis <: CoriolisModel end
+
+struct NoCoriolis <: CoriolisModel end
+coriolis_source!(::NoCoriolis, _...) = nothing
+
+function boundarycondition!(::ReferenceStateEulerModel, stateP::Vars, _, auxP::Vars, normalM,
+                            stateM::Vars, _, auxM::Vars, bctype, t)
+  if bctype == 1
+    nofluxbc!((:δρ, :δρe), stateP, normalM, stateM)
+  else
+    error("unknown boundary condition type!")
+  end
+end
+
+#function nofluxbc!(stateP, nM, stateM, auxM)
+#  @inbounds begin
+#    ρM, ρu⃗M, ρeM = stateM.ρ, stateP.ρu⃗, stateM.ρe
+#
+#    ## scalars are preserved
+#    stateP.ρ, stateP.ρe = ρM, ρeM
+#
+#    ## reflect velocities
+#    n⃗ = SVector(nM)
+#    n⃗_ρu⃗M = n⃗' * ρu⃗M
+#    stateP.ρu⃗ = ρu⃗M - 2n⃗_ρu⃗M * n⃗
+#  end
+#end
