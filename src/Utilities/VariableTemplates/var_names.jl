fa0d3be401608652ef3dc63262319fd9e0fd853a
Charles_Kawczynski <kawczynski.charles@gmail.com>
2019-08-26 16:18:14 -0700

    => flattenednames

--- src/Utilities/VariableTemplates/var_names.jl
+++ src/Utilities/VariableTemplates/var_names.jl
@@ -1,40 +1,12 @@
-export var_names, flatten_state
+export flattenednames
 
-function var_names(model, DT, state::Function=vars_state)
-  return string.(flatten_state(model, DT, state))
-end
-
-function flatten_state(model, DT, state::Function=vars_state)
-  vn_all = Symbol[]
-  vs = state(model, DT)
-  expand_sizes_from_var_names!(vn_all, vs, Symbol())
-  rflatten_state!(vn_all, state, model, typeof(model), Symbol(), DT)
-  return vn_all
-end
-
-function rflatten_state!(vn_all, state, model, ::Type{T}, prefix, DT, i::Int=0; recursive::Bool=true) where T
-    fields = fieldnames(T)
-    if !isempty(fields)
-        for field in fields
-            sub_model = getfield(model, field)
-            prefix = Symbol(prefix, Symbol(field, "."))
-            try
-              vs = state(sub_model, DT)
-              expand_sizes_from_var_names!(vn_all, vs, Symbol(field, "."))
-            catch
-            end
-            if recursive
-                rflatten_state!(vn_all, state, sub_model, fieldtype(T, field), prefix, DT, i+1, recursive=true)
-            end
-        end
-    end
-end
-
-function expand_sizes_from_var_names!(vn_all, vs, prefix)
-  for (ft, fn) in zip(fieldtypes(vs), fieldnames(vs))
-    if !(ft <: NamedTuple)
-      X = ft <: SArray ? range(1,stop=length(ft)) : (Symbol(),)
-      push!(vn_all, [Symbol(prefix, fn, x) for x in X]...)
-    end
-  end
+flattenednames(::Type{NamedTuple{(),Tuple{}}}; prefix="") = ()
+flattenednames(::Type{T}; prefix="") where {T<:Real} = (prefix, )
+flattenednames(::Type{T}; prefix="") where {T<:SVector} = ntuple(i -> "$prefix[$i]", length(T))
+function flattenednames(::Type{T}; prefix="") where {T<:NamedTuple}
+  map(1:fieldcount(T)) do i
+    Ti = fieldtype(T,i)
+    name = fieldname(T,i)
+    flattenednames(Ti, prefix= prefix=="" ? string(name) : string(prefix, '.', name))
+  end |> Iterators.flatten |> collect
 end
