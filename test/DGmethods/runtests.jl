c10c42d6ebca3c7c62a59a0cd2ea8be182acb0a4
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-08-26 15:09:34 -0700

    add to test suite

--- test/DGmethods/runtests.jl
+++ test/DGmethods/runtests.jl
@@ -2,7 +2,7 @@ using MPI, Test
 include("../testhelpers.jl")
 
 @testset "DGmethods" begin
-  tests = [
+  tests = [(1, "compressible_Navier_Stokes/hydrostatic_state.jl")
           ]
 
   runmpi(tests, @__FILE__)
