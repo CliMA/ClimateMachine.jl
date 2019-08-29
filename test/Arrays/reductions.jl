3e9657393f75f2a2293f6dcd5b5d05aa3f2205bc
Simon Byrne <simonbyrne@gmail.com>
2019-08-29 15:31:25 -0700

    reinstate gpu tests

--- test/Arrays/reductions.jl
+++ test/Arrays/reductions.jl
@@ -48,9 +48,14 @@ QB .= B
   QA = MPIStateArray{Tuple{localsize[1:2]...}, Float32, CuArray}(mpicomm, localsize[3])
   QA .= A
 
-  @test isapprox(norm(QA, 1), norm(globalA, 1))
-  @test isapprox(norm(QA), norm(globalA))
-  @test isapprox(norm(QA, Inf), norm(globalA, Inf))
+  @test norm(QA, 1) ≈ norm(globalA, 1)
+  @test norm(QA) ≈ norm(globalA)
+  @test norm(QA, Inf) ≈ norm(globalA, Inf)
+
+  @test norm(QA; dims=(1,3))      ≈ mapslices(norm, globalA; dims=(1,3))
+  @test norm(QA, 1; dims=(1,3))   ≈ mapslices(S -> norm(S, 1), globalA, dims=(1,3))
+  @test norm(QA, Inf; dims=(1,3)) ≈ mapslices(S -> norm(S, Inf), globalA, dims=(1,3))
+
 
   B = Array{Float32}(reshape(reverse(1:prod(localsize)), localsize))
   globalB = vcat([B for _ in 1:mpisize]...)
