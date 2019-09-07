slifer50/CLIMA

Cleanup NumericalFluxes.jl a bit

src/DGmethods/NumericalFluxes.jl
@@ -3,9 +3,9 @@ module NumericalFluxes
 export Rusanov, DefaultGradNumericalFlux
 
 using StaticArrays
-import ..DGmethods:  BalanceLaw, Grad, Vars,
-   vars_state, vars_diffusive, vars_aux, vars_gradient, boundarycondition!, wavespeed, flux!, diffusive!,
-   num_state, num_gradient
+import ..DGmethods: BalanceLaw, Grad, Vars, vars_state, vars_diffusive,
+                    vars_aux, vars_gradient, boundarycondition!, wavespeed,
+                    flux!, diffusive!, num_state, num_gradient
 
 
 """
@@ -13,9 +13,10 @@ import ..DGmethods:  BalanceLaw, Grad, Vars,
 
 Any `P <: GradNumericalFlux` should define methods for:
 
-   diffusive_penalty!(gnf::P, bl::BalanceLaw, diffF, nM, QM, QdiffM, QauxM, QP, QdiffP, QauxP, t)
-l_Qvisc, nM, l_GM, l_QM, l_auxM, l_GP, l_QP, l_auxP, t)
-   diffusive_boundary_penalty!(gnf::P, bl::BalanceLaw, l_Qvisc, nM, l_GM, l_QM, l_auxM, l_GP, l_QP, l_auxP, bctype, t)
+   diffusive_penalty!(gnf::P, bl::BalanceLaw, diffF, nM, QM, QdiffM, QauxM, QP,
+                      QdiffP, QauxP, t)
+   diffusive_boundary_penalty!(gnf::P, bl::BalanceLaw, l_Qvisc, nM, l_GM, l_QM,
+                               l_auxM, l_GP, l_QP, l_auxP, bctype, t)
 
 """
 abstract type GradNumericalFlux end
@@ -27,13 +28,10 @@ function diffusive_boundary_penalty! end
     DefaultGradNumericalFlux <: GradNumericalFlux
 
 """
-struct DefaultGradNumericalFlux <: GradNumericalFlux
-end
+struct DefaultGradNumericalFlux <: GradNumericalFlux end
 
 function diffusive_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw, 
-  VF, nM, 
-  velM, QM, aM, 
-  velP, QP, aP, t)
+                            VF, nM, velM, QM, aM, velP, QP, aP, t)
   DFloat = eltype(QM)
 
   @inbounds begin
@@ -43,12 +41,15 @@ function diffusive_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw,
     for j = 1:ngradstate, i = 1:ndim
       n_Δvel[i, j] = nM[i] * (velP[j] - velM[j]) / 2
     end
-    diffusive!(bl, Vars{vars_diffusive(bl,DFloat)}(VF), Grad{vars_gradient(bl,DFloat)}(n_Δvel),
-      Vars{vars_state(bl,DFloat)}(QM), Vars{vars_aux(bl,DFloat)}(aM), t)
+    diffusive!(bl, Vars{vars_diffusive(bl,DFloat)}(VF),
+               Grad{vars_gradient(bl,DFloat)}(n_Δvel),
+               Vars{vars_state(bl,DFloat)}(QM), Vars{vars_aux(bl,DFloat)}(aM),
+               t)
   end
 end
 
-@inline diffusive_boundary_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw, VF, _...) = VF.=0
+@inline diffusive_boundary_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw,
+                                    VF, _...) = VF.=0
 
 
 """
@@ -56,7 +57,8 @@ end
 
 Any `N <: DivNumericalFlux` should define the a method for
 
-    numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM, QP, QdiffP, QauxP, t)
+    numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM, QP,
+                    QdiffP, QauxP, t)
 
 where
 - `F` is the numerical flux array
@@ -68,7 +70,8 @@ where
 
 An optional method can also be defined for
 
-    boundary_numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM, QP, QdiffP, QauxP, bctype, t)
+    boundary_numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM,
+                             QP, QdiffP, QauxP, bctype, t)
 
 """
 abstract type DivNumericalFlux end
@@ -81,8 +84,12 @@ function numerical_boundary_flux!(dnf::DivNumericalFlux, bl::BalanceLaw,
                                   QP, QVP, auxP,
                                   bctype, t) where {nstate}
   DFloat = eltype(F)
-  boundarycondition!(bl, Vars{vars_state(bl,DFloat)}(QP), Vars{vars_diffusive(bl,DFloat)}(QVP), Vars{vars_aux(bl,DFloat)}(auxP),
-                     nM, Vars{vars_state(bl,DFloat)}(QM), Vars{vars_diffusive(bl,DFloat)}(QVM), Vars{vars_aux(bl,DFloat)}(auxM),
+  boundarycondition!(bl, Vars{vars_state(bl,DFloat)}(QP),
+                     Vars{vars_diffusive(bl,DFloat)}(QVP),
+                     Vars{vars_aux(bl,DFloat)}(auxP),
+                     nM, Vars{vars_state(bl,DFloat)}(QM),
+                     Vars{vars_diffusive(bl,DFloat)}(QVM),
+                     Vars{vars_aux(bl,DFloat)}(auxM),
                      bctype, t)
   numerical_flux!(dnf, bl, F, nM, QM, QVM, auxM, QP, QVP, auxP, t)
 end
@@ -112,32 +119,28 @@ function numerical_flux!(::Rusanov, bl::BalanceLaw,
   DFloat = eltype(F)
   nstate = num_state(bl,DFloat)
 
-  λM = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QM), Vars{vars_aux(bl,DFloat)}(auxM), t)
+  λM = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QM),
+                 Vars{vars_aux(bl,DFloat)}(auxM), t)
   FM = similar(F, Size(3, nstate))
   fill!(FM, -zero(eltype(FM)))
-  flux!(bl, Grad{vars_state(bl,DFloat)}(FM), Vars{vars_state(bl,DFloat)}(QM), Vars{vars_diffusive(bl,DFloat)}(QVM), Vars{vars_aux(bl,DFloat)}(auxM), t)
+  flux!(bl, Grad{vars_state(bl,DFloat)}(FM), Vars{vars_state(bl,DFloat)}(QM),
+        Vars{vars_diffusive(bl,DFloat)}(QVM), Vars{vars_aux(bl,DFloat)}(auxM),
+        t)
   
-  λP = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QP), Vars{vars_aux(bl,DFloat)}(auxP), t)
+  λP = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QP),
+                 Vars{vars_aux(bl,DFloat)}(auxP), t)
   FP = similar(F, Size(3, nstate))
   fill!(FP, -zero(eltype(FP)))
-  flux!(bl, Grad{vars_state(bl,DFloat)}(FP), Vars{vars_state(bl,DFloat)}(QP), Vars{vars_diffusive(bl,DFloat)}(QVP), Vars{vars_aux(bl,DFloat)}(auxP), t)
+  flux!(bl, Grad{vars_state(bl,DFloat)}(FP), Vars{vars_state(bl,DFloat)}(QP),
+        Vars{vars_diffusive(bl,DFloat)}(QVP), Vars{vars_aux(bl,DFloat)}(auxP),
+        t)
 
   λ  =  max(λM, λP)
 
-  #TODO: support a "computeQjump!" function
-  # if computeQjump! === nothing
-    @inbounds for s = 1:nstate
-      F[s] = (nM[1] * (FM[1, s] + FP[1, s]) + nM[2] * (FM[2, s] + FP[2, s]) +
-              nM[3] * (FM[3, s] + FP[3, s]) + λ * (QM[s] - QP[s])) / 2
-    end
-  # else
-  #   ΔQ = copy(QM)
-  #   computeQjump!(ΔQ, QM, auxM, QP, auxP)
-  #   @inbounds for s = 1:nstate
-  #     F[s] = (nM[1] * (FM[1, s] + FP[1, s]) + nM[2] * (FM[2, s] + FP[2, s]) +
-  #             nM[3] * (FM[3, s] + FP[3, s]) + λ * ΔQ[s]) / 2
-  #   end
-  # end
+  @inbounds for s = 1:nstate
+    F[s] = (nM[1] * (FM[1, s] + FP[1, s]) + nM[2] * (FM[2, s] + FP[2, s]) +
+            nM[3] * (FM[3, s] + FP[3, s]) + λ * (QM[s] - QP[s])) / 2
+  end
 end
 
 

CliMA/ClimateMachine.jl

src/DGmethods/NumericalFluxes.jl
@@ -3,9 +3,9 @@ module NumericalFluxes
 export Rusanov, DefaultGradNumericalFlux
 
 using StaticArrays
-import ..DGmethods:  BalanceLaw, Grad, Vars,
-   vars_state, vars_diffusive, vars_aux, vars_gradient, boundarycondition!, wavespeed, flux!, diffusive!,
-   num_state, num_gradient
+import ..DGmethods: BalanceLaw, Grad, Vars, vars_state, vars_diffusive,
+                    vars_aux, vars_gradient, boundarycondition!, wavespeed,
+                    flux!, diffusive!, num_state, num_gradient
 
 
 """
@@ -13,9 +13,10 @@ import ..DGmethods:  BalanceLaw, Grad, Vars,
 
 Any `P <: GradNumericalFlux` should define methods for:
 
-   diffusive_penalty!(gnf::P, bl::BalanceLaw, diffF, nM, QM, QdiffM, QauxM, QP, QdiffP, QauxP, t)
-l_Qvisc, nM, l_GM, l_QM, l_auxM, l_GP, l_QP, l_auxP, t)
-   diffusive_boundary_penalty!(gnf::P, bl::BalanceLaw, l_Qvisc, nM, l_GM, l_QM, l_auxM, l_GP, l_QP, l_auxP, bctype, t)
+   diffusive_penalty!(gnf::P, bl::BalanceLaw, diffF, nM, QM, QdiffM, QauxM, QP,
+                      QdiffP, QauxP, t)
+   diffusive_boundary_penalty!(gnf::P, bl::BalanceLaw, l_Qvisc, nM, l_GM, l_QM,
+                               l_auxM, l_GP, l_QP, l_auxP, bctype, t)
 
 """
 abstract type GradNumericalFlux end
@@ -27,13 +28,10 @@ function diffusive_boundary_penalty! end
     DefaultGradNumericalFlux <: GradNumericalFlux
 
 """
-struct DefaultGradNumericalFlux <: GradNumericalFlux
-end
+struct DefaultGradNumericalFlux <: GradNumericalFlux end
 
 function diffusive_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw, 
-  VF, nM, 
-  velM, QM, aM, 
-  velP, QP, aP, t)
+                            VF, nM, velM, QM, aM, velP, QP, aP, t)
   DFloat = eltype(QM)
 
   @inbounds begin
@@ -43,12 +41,15 @@ function diffusive_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw,
     for j = 1:ngradstate, i = 1:ndim
       n_Δvel[i, j] = nM[i] * (velP[j] - velM[j]) / 2
     end
-    diffusive!(bl, Vars{vars_diffusive(bl,DFloat)}(VF), Grad{vars_gradient(bl,DFloat)}(n_Δvel),
-      Vars{vars_state(bl,DFloat)}(QM), Vars{vars_aux(bl,DFloat)}(aM), t)
+    diffusive!(bl, Vars{vars_diffusive(bl,DFloat)}(VF),
+               Grad{vars_gradient(bl,DFloat)}(n_Δvel),
+               Vars{vars_state(bl,DFloat)}(QM), Vars{vars_aux(bl,DFloat)}(aM),
+               t)
   end
 end
 
-@inline diffusive_boundary_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw, VF, _...) = VF.=0
+@inline diffusive_boundary_penalty!(::DefaultGradNumericalFlux, bl::BalanceLaw,
+                                    VF, _...) = VF.=0
 
 
 """
@@ -56,7 +57,8 @@ end
 
 Any `N <: DivNumericalFlux` should define the a method for
 
-    numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM, QP, QdiffP, QauxP, t)
+    numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM, QP,
+                    QdiffP, QauxP, t)
 
 where
 - `F` is the numerical flux array
@@ -68,7 +70,8 @@ where
 
 An optional method can also be defined for
 
-    boundary_numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM, QP, QdiffP, QauxP, bctype, t)
+    boundary_numerical_flux!(dnf::N, bl::BalanceLaw, F, nM, QM, QdiffM, QauxM,
+                             QP, QdiffP, QauxP, bctype, t)
 
 """
 abstract type DivNumericalFlux end
@@ -82,9 +85,15 @@ function numerical_boundary_flux!(dnf::DivNumericalFlux, bl::BalanceLaw,
                                   bctype, t,
                                   Q1, QV1, aux1) where {nstate}
   DFloat = eltype(F)
-  boundarycondition!(bl, Vars{vars_state(bl,DFloat)}(QP), Vars{vars_diffusive(bl,DFloat)}(QVP), Vars{vars_aux(bl,DFloat)}(auxP),
-                     nM, Vars{vars_state(bl,DFloat)}(QM), Vars{vars_diffusive(bl,DFloat)}(QVM), Vars{vars_aux(bl,DFloat)}(auxM),
-                     bctype, t, Vars{vars_state(bl,DFloat)}(Q1), Vars{vars_diffusive(bl,DFloat)}(QV1), Vars{vars_aux(bl,DFloat)}(aux1))
+  boundarycondition!(bl, Vars{vars_state(bl,DFloat)}(QP),
+                     Vars{vars_diffusive(bl,DFloat)}(QVP),
+                     Vars{vars_aux(bl,DFloat)}(auxP), nM,
+                     Vars{vars_state(bl,DFloat)}(QM),
+                     Vars{vars_diffusive(bl,DFloat)}(QVM),
+                     Vars{vars_aux(bl,DFloat)}(auxM), bctype, t,
+                     Vars{vars_state(bl,DFloat)}(Q1),
+                     Vars{vars_diffusive(bl,DFloat)}(QV1),
+                     Vars{vars_aux(bl,DFloat)}(aux1))
   numerical_flux!(dnf, bl, F, nM, QM, QVM, auxM, QP, QVP, auxP, t)
 end
 
@@ -113,32 +122,28 @@ function numerical_flux!(::Rusanov, bl::BalanceLaw,
   DFloat = eltype(F)
   nstate = num_state(bl,DFloat)
 
-  λM = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QM), Vars{vars_aux(bl,DFloat)}(auxM), t)
+  λM = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QM),
+                 Vars{vars_aux(bl,DFloat)}(auxM), t)
   FM = similar(F, Size(3, nstate))
   fill!(FM, -zero(eltype(FM)))
-  flux!(bl, Grad{vars_state(bl,DFloat)}(FM), Vars{vars_state(bl,DFloat)}(QM), Vars{vars_diffusive(bl,DFloat)}(QVM), Vars{vars_aux(bl,DFloat)}(auxM), t)
+  flux!(bl, Grad{vars_state(bl,DFloat)}(FM), Vars{vars_state(bl,DFloat)}(QM),
+        Vars{vars_diffusive(bl,DFloat)}(QVM), Vars{vars_aux(bl,DFloat)}(auxM),
+        t)
   
-  λP = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QP), Vars{vars_aux(bl,DFloat)}(auxP), t)
+  λP = wavespeed(bl, nM, Vars{vars_state(bl,DFloat)}(QP),
+                 Vars{vars_aux(bl,DFloat)}(auxP), t)
   FP = similar(F, Size(3, nstate))
   fill!(FP, -zero(eltype(FP)))
-  flux!(bl, Grad{vars_state(bl,DFloat)}(FP), Vars{vars_state(bl,DFloat)}(QP), Vars{vars_diffusive(bl,DFloat)}(QVP), Vars{vars_aux(bl,DFloat)}(auxP), t)
+  flux!(bl, Grad{vars_state(bl,DFloat)}(FP), Vars{vars_state(bl,DFloat)}(QP),
+        Vars{vars_diffusive(bl,DFloat)}(QVP), Vars{vars_aux(bl,DFloat)}(auxP),
+        t)
 
   λ  =  max(λM, λP)
 
-  #TODO: support a "computeQjump!" function
-  # if computeQjump! === nothing
-    @inbounds for s = 1:nstate
-      F[s] = (nM[1] * (FM[1, s] + FP[1, s]) + nM[2] * (FM[2, s] + FP[2, s]) +
-              nM[3] * (FM[3, s] + FP[3, s]) + λ * (QM[s] - QP[s])) / 2
-    end
-  # else
-  #   ΔQ = copy(QM)
-  #   computeQjump!(ΔQ, QM, auxM, QP, auxP)
-  #   @inbounds for s = 1:nstate
-  #     F[s] = (nM[1] * (FM[1, s] + FP[1, s]) + nM[2] * (FM[2, s] + FP[2, s]) +
-  #             nM[3] * (FM[3, s] + FP[3, s]) + λ * ΔQ[s]) / 2
-  #   end
-  # end
+  @inbounds for s = 1:nstate
+    F[s] = (nM[1] * (FM[1, s] + FP[1, s]) + nM[2] * (FM[2, s] + FP[2, s]) +
+            nM[3] * (FM[3, s] + FP[3, s]) + λ * (QM[s] - QP[s])) / 2
+  end
 end
 
 