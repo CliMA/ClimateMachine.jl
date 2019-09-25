fe91d154715d3c6868fb296ad49ec3d363ac1317
asraero <asridhar@caltech.edu>
2019-09-25 15:19:48 -0700

    1) Working AMD (Viscosity component only) tested with RTB and DensityCurrent 2) TODO: Tracer gradients and diffusivity

--- test/DGmethods/compressible_Navier_Stokes/rising_bubble-model.jl
+++ test/DGmethods/compressible_Navier_Stokes/rising_bubble-model.jl
@@ -38,30 +38,19 @@ if !@isdefined integration_testing
 end
 
 # -------------- Problem constants ------------------- # 
-const (xmin,xmax)      = (0,1000)
-const (ymin,ymax)      = (0,400)
-const (zmin,zmax)      = (0,1000)
-const Ne        = (20,5,20)
+const xmin      = 0
+const ymin      = 0
+const zmin      = 0
+const xmax      = 1000
+const ymax      = 400
+const zmax      = 1000
+const Ne        = (10,2,10)
 const polynomialorder = 4
 const dim       = 3
-const dt        = 0.0007
-const timeend   = 1000
+const dt        = 0.01
+const timeend   = 10dt
 # ------------- Initial condition function ----------- # 
-"""
-@article{doi:10.1175/1520-0469(1993)050<1865:BCEWAS>2.0.CO;2,
-author = {Robert, A},
-title = {Bubble Convection Experiments with a Semi-implicit Formulation of the Euler Equations},
-journal = {Journal of the Atmospheric Sciences},
-volume = {50},
-number = {13},
-pages = {1865-1873},
-year = {1993},
-doi = {10.1175/1520-0469(1993)050<1865:BCEWAS>2.0.CO;2},
-URL = {https://doi.org/10.1175/1520-0469(1993)050<1865:BCEWAS>2.0.CO;2},
-eprint = {https://doi.org/10.1175/1520-0469(1993)050<1865:BCEWAS>2.0.CO;2},
-}
-"""
-function Initialise_Rising_Bubble!(state::Vars, aux::Vars, (x1,x2,x3), t)
+function initialise_rising_bubble!(state::Vars, aux::Vars, (x1,x2,x3), t)
   DF            = eltype(state)
   R_gas::DF     = R_d
   c_p::DF       = cp_d
@@ -69,6 +58,7 @@ function Initialise_Rising_Bubble!(state::Vars, aux::Vars, (x1,x2,x3), t)
   γ::DF         = c_p / c_v
   p0::DF        = MSLP
   
+  # perturbation parameters for rising bubble
   xc::DF        = 500
   zc::DF        = 260
   r             = sqrt((x1 - xc)^2 + (x3 - zc)^2)
@@ -79,17 +69,20 @@ function Initialise_Rising_Bubble!(state::Vars, aux::Vars, (x1,x2,x3), t)
   if r <= rc 
     Δθ          = DF(1//2) 
   end
+  
   #Perturbed state:
   θ            = θ_ref + Δθ # potential temperature
   π_exner      = DF(1) - grav / (c_p * θ) * x3 # exner pressure
   ρ            = p0 / (R_gas * θ) * (π_exner)^ (c_v / R_gas) # density
   P            = p0 * (R_gas * (ρ * θ) / p0) ^(c_p/c_v) # pressure (absolute)
   T            = P / (ρ * R_gas) # temperature
+  
   ρu           = SVector(DF(0),DF(0),DF(0))
   # energy definitions
   e_kin        = DF(0)
   e_pot        = grav * x3
   ρe_tot       = ρ * total_energy(e_kin, e_pot, T)
+  
   state.ρ      = ρ
   state.ρu     = ρu
   state.ρe     = ρe_tot
@@ -108,12 +101,12 @@ function run(mpicomm, ArrayType,
   # -------------- Define model ---------------------------------- # 
   model = AtmosModel(FlatOrientation(),
                      NoReferenceState(),
-                     AnisoMinDiss{DF}(),
+                     AnisoMinDiss{DF}(1),
                      EquilMoist(), 
                      NoRadiation(),
                      Gravity(),
                      NoFluxBC(),
-                     Initialise_Rising_Bubble!)
+                     initialise_rising_bubble!)
   # -------------- Define dgbalancelaw --------------------------- # 
   dg = DGModel(model,
                grid,
@@ -130,8 +123,7 @@ function run(mpicomm, ArrayType,
   eng0 = norm(Q)
   @info @sprintf """Starting
   norm(Q₀) = %.16e
-  ArrayType = %s
-  FloatType = %s""" eng0 ArrayType DF
+  ArrayType = %s""" eng0 ArrayType
 
   # Set up the information callback (output field dump is via vtk callback: see cbinfo)
   starttime = Ref(now())
@@ -154,14 +146,14 @@ function run(mpicomm, ArrayType,
   step = [0]
   cbvtk = GenericCallbacks.EveryXSimulationSteps(3000)  do (init=false)
     mkpath("./vtk-rtb/")
-      outprefix = @sprintf("./vtk-rtb/DC_%dD_mpirank%04d_step%04d", dim,
+      outprefix = @sprintf("./vtk-rtb/RTB_%dD_mpirank%04d_step%04d", dim,
                            MPI.Comm_rank(mpicomm), step[1])
       @debug "doing VTK output" outprefix
       writevtk(outprefix, Q, dg, flattenednames(vars_state(model,DF)), param[1], flattenednames(vars_aux(model,DF)))
       step[1] += 1
       nothing
   end
-
+  
   solve!(Q, lsrk, param; timeend=timeend, callbacks=(cbinfo,cbvtk))
   # End of the simulation information
   engf = norm(Q)
@@ -192,17 +184,15 @@ let
       device!(MPI.Comm_rank(mpicomm) % length(devices()))
   end
   @testset "$(@__FILE__)" for ArrayType in ArrayTypes
-    FloatType = (Float32, Float64)
-    for DF in FloatType
-      brickrange = (range(DF(xmin); length=Ne[1]+1, stop=xmax),
-                    range(DF(ymin); length=Ne[2]+1, stop=ymax),
-                    range(DF(zmin); length=Ne[3]+1, stop=zmax))
-      topl = StackedBrickTopology(mpicomm, brickrange, periodicity = (false, true, false))
-      engf_eng0 = run(mpicomm, ArrayType, 
-                      topl, dim, Ne, polynomialorder, 
-                      timeend, DF, dt)
-      @test engf_eng0 ≈ DF(9.9999993807738441e-01)
-    end
+  DF = Float64
+  brickrange = (range(DF(xmin); length=Ne[1]+1, stop=xmax),
+                range(DF(ymin); length=Ne[2]+1, stop=ymax),
+                range(DF(zmin); length=Ne[3]+1, stop=zmax))
+  topl = StackedBrickTopology(mpicomm, brickrange, periodicity = (false, true, false))
+  engf_eng0 = run(mpicomm, ArrayType, 
+                  topl, dim, Ne, polynomialorder, 
+                  timeend, DF, dt)
+  @test engf_eng0 ≈ DF(9.9999993807738441e-01)
   end
 end
 
