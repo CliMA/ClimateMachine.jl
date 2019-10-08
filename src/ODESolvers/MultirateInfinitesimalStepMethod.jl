36285f817b339eaec74d5018f3dfdba0d171276d
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-10-08 00:55:35 -0700

    Running & converging draft of Knoth and Wensch multirate methods

--- src/ODESolvers/MultirateInfinitesimalStepMethod.jl
+++ src/ODESolvers/MultirateInfinitesimalStepMethod.jl
@@ -1,103 +1,183 @@
 module MultirateInfinitesimalStepMethod
 using ..ODESolvers
-using ..AdditiveRungeKuttaMethod
-using ..LowStorageRungeKuttaMethod
-using ..StrongStabilityPreservingRungeKuttaMethod
 using ..MPIStateArrays: device, realview
 
 using StaticArrays
 
-struct OffsetRHS{R,O}
-  rhs!::R
+export MIS2, MIS3C
+
+ODEs = ODESolvers
+
+struct OffsetRHS{O}
   "storage for offset term"
   offset::O
+  rhs!
 end
 
-function (o::OffsetRHS)(dQ, Q, params, t)
-  copyto!(dQ, o.offset)
-  o.rhs!(dQ, Q, params, r; increment=true)
+function (o::OffsetRHS)(dQ, Q, params, t; increment)
+  o.rhs!(dQ, Q, params, t; increment=increment)
+  dQ .+= o.offset
 end
-
 
 using GPUifyLoops
 
-struct MultirateInfinitesimalStep{T, RT, AT, OR, N1, N2} <: ODEs.AbstractODESolver
+struct MultirateInfinitesimalStep{T, RT, AT, Nstages, Nstages_sq} <: ODEs.AbstractODESolver
   "time step"
-  dt::Array{RT,1}
+  dt::Array{RT, 1}
   "time"
-  t::Array{RT,1}
-  "rhs function"
-  rhs!
+  t::Array{RT, 1}
   "storage for y_n"
-  yn::A
+  yn::AT
   "Storage for ``Y_nj - y_n``"
-  deltaYnj::NTuple{Nstages,A}
+  ΔYnj::NTuple{Nstages, AT}
   "Storage for ``f(Y_nj)``"
-  fYnj::NTuple{Nstages,A}
+  fYnj::NTuple{Nstages, AT}
+  "slow rhs function"
+  slowrhs!
   "RHS for fast solver"
-  fastrhs!::OffsetRHS{F,A}
-  fastsolver
+  fastrhs!::OffsetRHS{AT}
+  "fast rhs method"
+  fastmethod
+  α::SArray{NTuple{2, Nstages}, RT, 2, Nstages_sq} 
+  β::SArray{NTuple{2, Nstages}, RT, 2, Nstages_sq}
+  γ::SArray{NTuple{2, Nstages}, RT, 2, Nstages_sq} 
 
-  beta::NTuple{N1, RT}
-  alpha::NTuple{N2, RT}
-  gamma::NTuple{N2, RT}
-
-  function MultirateInfinitesimalStep(rhs!, RKA, RKB, RKC,
-                                  Q::AT; dt=0, t0=0) where {AT<:AbstractArray}
+  function MultirateInfinitesimalStep(slowrhs!, fastrhs!, fastmethod,
+                                      α, β, γ,
+                                      Q::AT; dt=0, t0=0) where {AT<:AbstractArray}
 
     T = eltype(Q)
     RT = real(T)
     dt = [dt]
     t0 = [t0]
 
-    dQ = similar(Q)
-    fill!(dQ, 0)
+    Nstages = size(α, 1)
+
+    yn = similar(Q)
+    ΔYnj = ntuple(_ -> similar(Q), Nstages)
+    fYnj = ntuple(_ -> similar(Q), Nstages)
+    fastrhs! = OffsetRHS(similar(Q), fastrhs!)
     
-    new{T, RT, AT, length(RKA)}(dt, t0, rhs!, dQ, RKA, RKB, RKC)
+    new{T, RT, AT, Nstages, Nstages ^ 2}(dt, t0, yn, ΔYnj, fYnj,
+                                         slowrhs!, fastrhs!, fastmethod,
+                                         α, β, γ)
   end
 end
 
-function MultirateInfinitesimalStep(spacedisc::AbstractSpaceMethod, RKA, RKB, RKC,
-                                Q::AT; dt=0, t0=0) where {AT<:AbstractArray}
-  rhs! = (x...; increment) -> SpaceMethods.odefun!(spacedisc, x..., increment = increment)
-  MultirateInfinitesimalStep(rhs!, RKA, RKB, RKC, Q; dt=dt, t0=t0)
-end
+# TODO write this function
+#function MultirateInfinitesimalStep(spacedisc::AbstractSpaceMethod, RKA, RKB, RKC,
+#                                Q::AT; dt=0, t0=0) where {AT<:AbstractArray}
+#  rhs! = (x...; increment) -> SpaceMethods.odefun!(spacedisc, x..., increment = increment)
+#  MultirateInfinitesimalStep(rhs!, RKA, RKB, RKC, Q; dt=dt, t0=t0)
+#end
 
-
-function MIS2(F, Q::AT; dt=0, t0=0) where {AT <: AbstractArray}
+function MIS2(slowrhs!, fastrhs!, fastmethod, Q::AT; dt=0, t0=0) where {AT <: AbstractArray}
   T = eltype(Q)
   RT = real(T)
 
-  0.126848494553
-  -0.784838278826  1.37442675268
-  
-  ...
+  α = [0 0              0              0;
+       0 0              0              0;
+       0 0.536946566710 0              0;
+       0 0.480892968551 0.500561163566 0]
+
+  β = [ 0                0                0              0;
+        0.126848494553   0                0              0;
+       -0.784838278826   1.37442675268    0              0;
+       -0.0456727081749 -0.00875082271190 0.524775788629 0]
+
+  γ = [0  0               0              0;
+       0  0               0              0;
+       0  0.652465126004  0              0;
+       0 -0.0732769849457 0.144902430420 0]
+
+  MultirateInfinitesimalStep(slowrhs!, fastrhs!, fastmethod, α, β, γ, Q; dt=dt, t0=t0)
 end
 
-function ODEs.dostep!(Q, mis::MultirateInfinitesimalStep{SS}, param,
-  time::AbstractFloat, dt::AbstractFloat,
-  in_slow_δ = nothing, in_slow_rv_dQ = nothing,
-  in_slow_scaling = nothing) where {SS <: LSRK2N}
+function MIS3C(slowrhs!, fastrhs!, fastmethod, Q::AT; dt=0, t0=0) where {AT <: AbstractArray}
+  T = eltype(Q)
+  RT = real(T)
 
-  copyto!(mis.yn, Q)
-  for i = 1:??
-    t += ??
+  α = [0 0              0              0;
+       0 0              0              0;
+       0 0.589557277145 0              0;
+       0 0.544036601551 0.565511042564 0]
 
-    slow.rhs!(fYnj[i], Q, params, t)
-    # if not last loop
-    deltaYnj[i] .= Q .- mis.yn
+  β = [ 0                 0                0              0;
+        0.397525189225    0                0              0;
+       -0.227036463644    0.624528794618   0              0;
+       -0.00295238076840 -0.270971764284   0.671323159437 0]
 
-    # TODO write a kernel to do this
-    Q .= mis.yn .+ sum(j -> alpha[i,j].*deltaYnj[j], 1:i)  # (1a) 
-    fastrhs.offset = sum(j -> gamma[i,j].*deltaYnj[j].+beta[i,j].*fYnj[j], 1:i)  # (1b) 
+  γ = [0  0               0               0;
+       0  0               0               0;
+       0  0.142798786398  0               0;
+       0 -0.0428918957402 0.0202720980282 0]
 
-    tau = t
-    # solve!(Q, fast, param; timeend = t+ ??) (1c)
-    while tau < t + ??
-      dostep!(Q, fast, param, tau, dt)
-    end
+  MultirateInfinitesimalStep(slowrhs!, fastrhs!, fastmethod, α, β, γ, Q; dt=dt, t0=t0)
+end
+
+# TODO almost identical functions seem to be defined for every ode solver,
+# define a common one in ODEsolvers ?
+function ODEs.dostep!(Q, mis::MultirateInfinitesimalStep, param,
+                      timeend::AbstractFloat, adjustfinalstep::Bool)
+  time, dt = mis.t[1], mis.dt[1]
+  @assert dt > 0
+  if adjustfinalstep && time + dt > timeend
+    dt = timeend - time
+    @assert dt > 0
   end
 
+  ODEs.dostep!(Q, mis, param, time, dt)
 
+  if dt == mis.dt[1]
+    mis.t[1] += dt
+  else
+    mis.t[1] = timeend
+  end
+  return mis.t[1]
+end
+
+function ODEs.dostep!(Q, mis::MultirateInfinitesimalStep, p,
+                      time::AbstractFloat, dt::AbstractFloat)
+  DT = eltype(dt)
+  α = mis.α
+  β = mis.β
+  γ = mis.γ
+  yn = mis.yn
+  ΔYnj = mis.ΔYnj
+  fYnj = mis.fYnj
+  slowrhs! = mis.slowrhs!
+  fastmethod = mis.fastmethod
+  fastrhs! = mis.fastrhs!
+
+  nstages = size(α, 1)
+
+  # first stage
+  copyto!(yn, Q)
+  fill!(ΔYnj[1], 0)
+  slowrhs!(fYnj[1], yn, p, time, increment=false)
+
+  for i = 2:nstages
+    d_i = sum(j -> β[i, j], 1:i-1)
+
+    # TODO write a kernel to do this
+    Q .= yn .+ sum(j -> α[i, j] .* ΔYnj[j], 1:i-1)  # (1a) 
+    fastrhs!.offset .= sum(j -> γ[i, j] .* ΔYnj[j] ./ dt .+ β[i,j] .* fYnj[j], 1:i-1) ./ d_i # (1b) 
+
+    tau = zero(DT)
+    fastdt = d_i * dt # TODO substepping
+    fastsolver = fastmethod(fastrhs!, Q; dt=fastdt, t0=tau)
+
+    solve!(Q, fastsolver, p; timeend = d_i * dt) #(1c)
+    #while tau < t + ??
+    #  ODEs.dostep!(Q, fastsolver, fastrhs!, param, tau, dt)
+    #end
+
+    if i < nstages
+      t = time # TODO evaluate slow rhs at proper times
+      slowrhs!(fYnj[i], Q, p, t, increment=false)
+      @. ΔYnj[i] = Q - yn
+    end
+  end
+end
 
 end
