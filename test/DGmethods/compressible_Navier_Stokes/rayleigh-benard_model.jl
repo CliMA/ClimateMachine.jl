db39521ae56a72219bc9eb739182e99ef3a93647
asraero <asridhar@caltech.edu>
2019-10-11 15:06:59 -0700

    1) Add sgs tests for rayleigh-benard problem

--- test/DGmethods/compressible_Navier_Stokes/rayleigh-benard_model.jl
+++ test/DGmethods/compressible_Navier_Stokes/rayleigh-benard_model.jl
@@ -1,10 +1,8 @@
-
 # Load Packages 
 using MPI
 using CLIMA
 using CLIMA.Mesh.Topologies
 using CLIMA.Mesh.Grids
-using CLIMA.Mesh.Geometry
 using CLIMA.DGmethods
 using CLIMA.DGmethods.NumericalFluxes
 using CLIMA.MPIStateArrays
@@ -15,7 +13,6 @@ using CLIMA.Atmos
 using CLIMA.VariableTemplates
 using CLIMA.MoistThermodynamics
 using CLIMA.PlanetParameters
-using CLIMA.SubgridScaleParameters
 using LinearAlgebra
 using StaticArrays
 using Logging, Printf, Dates
@@ -39,87 +36,61 @@ if !@isdefined integration_testing
 end
 
 # -------------- Problem constants ------------------- # 
-const dim               = 3
-const (xmin, xmax)      = (0,12800)
-const (ymin, ymax)      = (0,400)
-const (zmin, zmax)      = (0,6400)
-const Ne                = (100,2,50)
-const polynomialorder   = 4
-const dt                = 0.01
-const timeend           = 1000
-
+const xmin      = 0
+const ymin      = 0
+const zmin      = 0
+const xmax      = 1000
+const ymax      = 400
+const zmax      = 1000
+const Ne        = (30,5,30)
+const polynomialorder = 4
+const dim       = 3
+const Δx        = (xmax-xmin)/(Ne[1]*polynomialorder+1)
+const Δy        = (ymax-ymin)/(Ne[2]*polynomialorder+1)
+const Δz        = (zmax-zmin)/(Ne[3]*polynomialorder+1)
+const Δ         = cbrt(Δx * Δy * Δz) 
+const dt        = 0.005
+const timeend   = 100
+const T_bot     = 299
+const T_lapse   = -0.01
+const T_top     = T_bot + T_lapse*zmax
+const C_smag    = 0.18
 # ------------- Initial condition function ----------- # 
-"""
-@article{doi:10.1002/fld.1650170103,
-author = {Straka, J. M. and Wilhelmson, Robert B. and Wicker, Louis J. and Anderson, John R. and Droegemeier, Kelvin K.},
-title = {Numerical solutions of a non-linear density current: A benchmark solution and comparisons},
-journal = {International Journal for Numerical Methods in Fluids},
-volume = {17},
-number = {1},
-pages = {1-22},
-doi = {10.1002/fld.1650170103},
-url = {https://onlinelibrary.wiley.com/doi/abs/10.1002/fld.1650170103},
-eprint = {https://onlinelibrary.wiley.com/doi/pdf/10.1002/fld.1650170103},
-year = {1993}
-}
-"""
-function Initialise_Density_Current!(state::Vars, aux::Vars, (x1,x2,x3), t)
-  DF                = eltype(state)
-  R_gas::DF         = R_d
-  c_p::DF           = cp_d
-  c_v::DF           = cv_d
-  p0::DF            = MSLP
-  # initialise with dry domain 
-  q_tot::DF         = 0
-  q_liq::DF         = 0
-  q_ice::DF         = 0 
-  # perturbation parameters for rising bubble
-  rx                = 4000
-  rz                = 2000
-  xc                = 0
-  zc                = 3000
-  r                 = sqrt((x1 - xc)^2/rx^2 + (x3 - zc)^2/rz^2)
-  θ_ref::DF         = 300
-  θ_c::DF           = -15
-  Δθ::DF            = 0
-  if r <= 1
-    Δθ = θ_c * (1 + cospi(r))/2
-  end
-  qvar              = PhasePartition(q_tot)
-  θ                 = θ_ref + Δθ # potential temperature
-  π_exner           = DF(1) - grav / (c_p * θ) * x3 # exner pressure
-  ρ                 = p0 / (R_gas * θ) * (π_exner)^ (c_v / R_gas) # density
-
-  P                 = p0 * (R_gas * (ρ * θ) / p0) ^(c_p/c_v) # pressure (absolute)
-  T                 = P / (ρ * R_gas) # temperature
-  U, V, W           = DF(0) , DF(0) , DF(0)  # momentum components
-  # energy definitions
-  e_kin             = (U^2 + V^2 + W^2) / (2*ρ)/ ρ
-  e_pot             = grav * x3
-  e_int             = internal_energy(T, qvar)
-  E                 = ρ * (e_int + e_kin + e_pot)  #* total_energy(e_kin, e_pot, T, q_tot, q_liq, q_ice)
-  state.ρ      = ρ
-  state.ρu     = SVector(U, V, W)
-  state.ρe     = E
-  state.moisture.ρq_tot = DF(0)
+function initialise_rayleigh_benard!(state::Vars, aux::Vars, (x1,x2,x3), t)
+  FT            = eltype(state)
+  R_gas::FT     = R_d
+  c_p::FT       = cp_d
+  c_v::FT       = cv_d
+  γ::FT         = c_p / c_v
+  p0::FT        = MSLP
+  δT            = sinpi(6*x3/(zmax-zmin)) * cospi(6*x3/(zmax-zmin))
+  δw            = sinpi(6*x3/(zmax-zmin)) * cospi(6*x3/(zmax-zmin))
+  ΔT            = T_lapse * x3 + δT
+  T             = T_bot + ΔT 
+  P             = p0*(T/T_bot)^(grav/R_gas/T_lapse)
+  ρ             = P / (R_gas * T)
+  ρu, ρv, ρw    = FT(0) , FT(0) , ρ * δw
+  E_int         = ρ * c_v * (T-T_0)
+  E_pot         = ρ * grav * x3
+  E_kin         = ρ * FT(1/2) * δw^2 
+  ρe_tot        = E_int + E_pot + E_kin
+  state.ρ       = ρ
+  state.ρu      = SVector(ρu, ρv, ρw)
+  state.ρe      = ρe_tot
+  state.moisture.ρq_tot = FT(0)
 end
 # --------------- Driver definition ------------------ # 
 function run(mpicomm, ArrayType, 
              topl, dim, Ne, polynomialorder, 
-             timeend, DF, dt)
+             timeend, FT, dt, model)
   # -------------- Define grid ----------------------------------- # 
   grid = DiscontinuousSpectralElementGrid(topl,
-                                          FloatType = DF,
+                                          FloatType = FT,
                                           DeviceArray = ArrayType,
                                           polynomialorder = polynomialorder
                                            )
   # -------------- Define model ---------------------------------- # 
-  model = AtmosModel(FlatOrientation(),
-                     NoReferenceState(),
-                     AnisoMinDiss{DF}(1), 
-                     EquilMoist(), 
-                     NoRadiation(),
-                     Gravity(), NoFluxBC(), Initialise_Density_Current!)
+  model = model
   # -------------- Define dgbalancelaw --------------------------- # 
   dg = DGModel(model,
                grid,
@@ -127,7 +98,7 @@ function run(mpicomm, ArrayType,
                CentralNumericalFluxDiffusive(),
                CentralGradPenalty())
 
-  Q = init_ode_state(dg, DF(0))
+  Q = init_ode_state(dg, FT(0))
 
   lsrk = LSRK54CarpenterKennedy(dg, Q; dt = dt, t0 = 0)
 
@@ -135,11 +106,11 @@ function run(mpicomm, ArrayType,
   @info @sprintf """Starting
   norm(Q₀) = %.16e
   ArrayType = %s
-  FloatType = %s""" eng0 ArrayType DF
+  FloatType = %s""" eng0 ArrayType FT
 
   # Set up the information callback (output field dump is via vtk callback: see cbinfo)
   starttime = Ref(now())
-  cbinfo = GenericCallbacks.EveryXWallTimeSeconds(10, mpicomm) do (s=false)
+  cbinfo = GenericCallbacks.EveryXWallTimeSeconds(60, mpicomm) do (s=false)
     if s
       starttime[] = now()
     else
@@ -157,20 +128,19 @@ function run(mpicomm, ArrayType,
 
   step = [0]
   cbvtk = GenericCallbacks.EveryXSimulationSteps(3000)  do (init=false)
-    mkpath("./vtk-dc/")
-      outprefix = @sprintf("./vtk-dc/DC_%dD_mpirank%04d_step%04d", dim,
+    mkpath("./vtk-rb/")
+      outprefix = @sprintf("./vtk-rb/RB_%dD_mpirank%04d_step%04d", dim,
                            MPI.Comm_rank(mpicomm), step[1])
       @debug "doing VTK output" outprefix
-      writevtk(outprefix, Q, dg, flattenednames(vars_state(model,DF)), dg.auxstate, flattenednames(vars_aux(model,DF)))
+      writevtk(outprefix, Q, dg, flattenednames(vars_state(model,FT)), dg.auxstate, flattenednames(vars_aux(model,FT)))
       step[1] += 1
       nothing
   end
 
-
   solve!(Q, lsrk; timeend=timeend, callbacks=(cbinfo,cbvtk))
   # End of the simulation information
   engf = norm(Q)
-  Qe = init_ode_state(dg, DF(timeend))
+  Qe = init_ode_state(dg, FT(timeend))
   engfe = norm(Qe)
   errf = euclidean_distance(Q, Qe)
   @info @sprintf """Finished
@@ -197,19 +167,28 @@ let
       device!(MPI.Comm_rank(mpicomm) % length(devices()))
   end
   @testset "$(@__FILE__)" for ArrayType in ArrayTypes
-    FloatType = (Float32, Float64)
-    for DF in FloatType
-      brickrange = (range(DF(xmin); length=Ne[1]+1, stop=xmax),
-                    range(DF(ymin); length=Ne[2]+1, stop=ymax),
-                    range(DF(zmin); length=Ne[3]+1, stop=zmax))
-      topl = StackedBrickTopology(mpicomm, brickrange, periodicity = (false, true, false))
+    FT = Float32
+    SGSmodels = (AnisoMinDiss{FT}(1), Vreman{FT}(C_smag), SmagorinskyLilly{FT}(C_smag))
+    Expected = (FT(9.9859344959259033e-01),FT(1.0038942098617554e+00),FT(1.0027571916580200e+00))
+    for ii=1:length(SGSmodels)
+      model = AtmosModel(FlatOrientation(),
+                         NoReferenceState(),
+                         SGSmodels[ii],
+                         EquilMoist(), 
+                         NoRadiation(),
+                         Gravity(), 
+                         RayleighBenardBC{FT}(T_bot,T_top), 
+                         initialise_rayleigh_benard!)
+      brickrange = (range(FT(xmin); length=Ne[1]+1, stop=xmax),
+                    range(FT(ymin); length=Ne[2]+1, stop=ymax),
+                    range(FT(zmin); length=Ne[3]+1, stop=zmax))
+      topl = StackedBrickTopology(mpicomm, brickrange, periodicity = (true, true, false), boundary=((0,0),(0,0),(1,2)))
       engf_eng0 = run(mpicomm, ArrayType, 
                       topl, dim, Ne, polynomialorder, 
-                      timeend, DF, dt)
-      @test engf_eng0 ≈ DF(9.9999970927037096e-01)
+                      timeend, FT, dt, model)
+      @test engf_eng0 ≈ Expected[ii]
     end
   end
 end
-
 
 #nothing
