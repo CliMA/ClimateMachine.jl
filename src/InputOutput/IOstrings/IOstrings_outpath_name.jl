21034c7dee52c022a08137ab63e5a4cae493262c
sm <smarras@njit.edu>
2019-10-15 11:02:51 -0400

    Added IOstrings to list of modules in CLIMA.jl

--- src/InputOutput/IOstrings/IOstrings_outpath_name.jl
+++ src/InputOutput/IOstrings/IOstrings_outpath_name.jl
@@ -1,8 +1,21 @@
 #
 # Define output path string:
 #
+# called by a driver right before run()
+#
+# ex. 
+#    #Create unique output path directory:
+#    OUTPATH = IOstrings_outpath_name(problem_name, grid_resolution)
+#
+#
 function IOstrings_outpath_name(problem_name, grid_resolution)
-
+    #
+    # Arguments:
+    # problem_name = "NAME"
+    # grid_resolution[1]   = Δx
+    # grid_resolution[2]   = Δy
+    # grid_resolution[end] = Δz
+    #
     ndim = length(grid_resolution)
 
     outpath_string = string(grid_resolution[1], "mx")
