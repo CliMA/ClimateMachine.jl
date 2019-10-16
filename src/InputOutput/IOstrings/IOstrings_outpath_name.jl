0341c0cdc6750dfe22511dcec3124dbb4f7490bd
sm <smarras@njit.edu>
2019-10-16 09:37:05 -0400

    As per Charlies comment to PR 490: Removed obsolete creation of directory appended with *_previous.

--- src/InputOutput/IOstrings/IOstrings_outpath_name.jl
+++ src/InputOutput/IOstrings/IOstrings_outpath_name.jl
@@ -25,18 +25,6 @@ function IOstrings_outpath_name(problem_name, grid_resolution)
     end        
     OUTPATH = string("./output/",problem_name, "/", outpath_string,"_", randstring(6))
 
-    #                                                                                                                                                                                                                                          
-    # Create output directories to store diagnostics and vtks:
-    #
-    if (isdir(OUTPATH) == false)
-        mkpath(OUTPATH)
-    else
-        #Move an existing directory to dir_previous if it already exists
-        auxi = OUTPATH[1:end]
-        previous_run = string(OUTPATH, "_previous");
-        mv(OUTPATH, previous_run, force=true)
-    end
-
     return OUTPATH
 end
 
