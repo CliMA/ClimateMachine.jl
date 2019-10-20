5d79e64acc765ba71c37f65d58f8f980c019c396
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-20 18:11:27 -0400

    Fix missing decimal place in `ρ_cloud_ice`

--- src/Common/PlanetParameters/PlanetParameters.jl
+++ src/Common/PlanetParameters/PlanetParameters.jl
@@ -25,7 +25,7 @@ using ..ParametersType
 
 # Properties of water
 @exportparameter ρ_cloud_liq       1e3           "Density of liquid water (kg/m^3)"
-@exportparameter ρ_cloud_ice       9167          "Density of ice water (kg/m^3)"
+@exportparameter ρ_cloud_ice       916.7         "Density of ice water (kg/m^3)"
 @exportparameter molmass_water     18.01528e-3   "Molecular weight (kg/mol)"
 @exportparameter molmass_ratio     molmass_dryair/
                                    molmass_water "Molar mass ratio dry air/water"
