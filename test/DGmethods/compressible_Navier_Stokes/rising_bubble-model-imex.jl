90c5c2042483925612587e2151223eb625f96aea
Jeremy E Kozdon <jekozdon@nps.edu>
2019-10-28 18:00:08 +0000

    Test remainder model with RTB

--- test/DGmethods/compressible_Navier_Stokes/rising_bubble-model-imex.jl
+++ test/DGmethods/compressible_Navier_Stokes/rising_bubble-model-imex.jl
@@ -1,4 +1,4 @@
-# Load Packages 
+# Load Packages
 using MPI
 using CLIMA
 using CLIMA.Mesh.Topologies
@@ -39,7 +39,7 @@ if !@isdefined integration_testing
     parse(Bool, lowercase(get(ENV,"JULIA_CLIMA_INTEGRATION_TESTING","false")))
 end
 
-# -------------- Problem constants ------------------- # 
+# -------------- Problem constants ------------------- #
 const (xmin,xmax)      = (0,1000)
 const (ymin,ymax)      = (0,400)
 const (zmin,zmax)      = (0,1000)
@@ -48,7 +48,7 @@ const polynomialorder = 4
 const dim       = 3
 const dt        = 0.1
 const timeend   = 10dt
-# ------------- Initial condition function ----------- # 
+# ------------- Initial condition function ----------- #
 """
 @article{doi:10.1175/1520-0469(1993)050<1865:BCEWAS>2.0.CO;2,
 author = {Robert, A},
@@ -70,16 +70,16 @@ function Initialise_Rising_Bubble!(state::Vars, aux::Vars, (x1,x2,x3), t)
   c_v::FT       = cv_d
   γ::FT         = c_p / c_v
   p0::FT        = MSLP
-  
+
   xc::FT        = 500
   zc::FT        = 260
   r             = sqrt((x1 - xc)^2 + (x3 - zc)^2)
   rc::FT        = 250
   θ_ref::FT     = 303
   Δθ::FT        = 0
-  
-  if r <= rc 
-    Δθ          = FT(1//2) 
+
+  if r <= rc
+    Δθ          = FT(1//2)
   end
   #Perturbed state:
   θ            = θ_ref + Δθ # potential temperature
@@ -97,26 +97,26 @@ function Initialise_Rising_Bubble!(state::Vars, aux::Vars, (x1,x2,x3), t)
   state.ρe     = ρe_tot
   state.moisture.ρq_tot = FT(0)
 end
-# --------------- Driver definition ------------------ # 
+# --------------- Driver definition ------------------ #
 function run(mpicomm, ArrayType, LinearType,
-             topl, dim, Ne, polynomialorder, 
-             timeend, FT, dt)
-  # -------------- Define grid ----------------------------------- # 
+             topl, dim, Ne, polynomialorder,
+             timeend, FT, dt, split_nonlinear_linear)
+  # -------------- Define grid ----------------------------------- #
   grid = DiscontinuousSpectralElementGrid(topl,
                                           FloatType = FT,
                                           DeviceArray = ArrayType,
                                           polynomialorder = polynomialorder
-                                           )
-  # -------------- Define model ---------------------------------- # 
+                                         )
+  # -------------- Define model ---------------------------------- #
   model = AtmosModel(FlatOrientation(),
                      HydrostaticState(IsothermalProfile(FT(T_0)),FT(0)),
                      Vreman{FT}(C_smag),
-                     EquilMoist(), 
+                     EquilMoist(),
                      NoRadiation(),
                      Gravity(),
                      NoFluxBC(),
                      Initialise_Rising_Bubble!)
-  # -------------- Define dgbalancelaw --------------------------- # 
+  # -------------- Define dgbalancelaw --------------------------- #
   dg = DGModel(model,
                grid,
                Rusanov(),
@@ -125,22 +125,37 @@ function run(mpicomm, ArrayType, LinearType,
 
   linmodel = LinearType(model)
   lindg = DGModel(linmodel,
-               grid,
-               Rusanov(),
-               CentralNumericalFluxDiffusive(),
-               CentralGradPenalty(); auxstate=dg.auxstate)
+                  grid,
+                  Rusanov(),
+                  CentralNumericalFluxDiffusive(),
+                  CentralGradPenalty(); auxstate=dg.auxstate)
+
+  if split_nonlinear_linear
+    nonlinmodel = RemainderModel(model, (linmodel,))
+    dg_nonlinear = DGModel(nonlinmodel,
+                           grid, Rusanov(), CentralNumericalFluxDiffusive(),
+                           CentralGradPenalty(); auxstate=dg.auxstate)
+  end
 
   Q = init_ode_state(dg, FT(0))
 
   linearsolver = GeneralizedMinimalResidual(10, Q, sqrt(eps(FT)))
-  ark = ARK548L2SA2KennedyCarpenter(dg, lindg, linearsolver, Q; dt = dt, t0 = 0)
+  ark = ARK548L2SA2KennedyCarpenter(split_nonlinear_linear ? dg_nonlinear : dg,
+                                    lindg,
+                                    linearsolver,
+                                    Q; dt = dt, t0 = 0,
+                                    split_nonlinear_linear = split_nonlinear_linear)
 
 
   eng0 = norm(Q)
+  split = split_nonlinear_linear ? "(Nonlinear, Linear)" : "(Full, Linear)"
   @info @sprintf """Starting
   norm(Q₀) = %.16e
   ArrayType = %s
-  FloatType = %s""" eng0 ArrayType FT
+  FloatType = %s
+  splitting = %s
+  linear type = %s
+  """ eng0 ArrayType FT split LinearType
 
   # Set up the information callback (output field dump is via vtk callback: see cbinfo)
   starttime = Ref(now())
@@ -161,14 +176,14 @@ function run(mpicomm, ArrayType, LinearType,
   end
 
   step = [0]
-  cbvtk = GenericCallbacks.EveryXSimulationSteps(3000)  do (init=false)
+  cbvtk = GenericCallbacks.EveryXSimulationSteps(3000) do (init=false)
     mkpath("./vtk-rtb/")
-      outprefix = @sprintf("./vtk-rtb/DC_%dD_mpirank%04d_step%04d", dim,
-                           MPI.Comm_rank(mpicomm), step[1])
-      @debug "doing VTK output" outprefix
-      writevtk(outprefix, Q, dg, flattenednames(vars_state(model,FT)), param[1], flattenednames(vars_aux(model,FT)))
-      step[1] += 1
-      nothing
+    outprefix = @sprintf("./vtk-rtb/DC_%dD_mpirank%04d_step%04d", dim,
+                         MPI.Comm_rank(mpicomm), step[1])
+    @debug "doing VTK output" outprefix
+    writevtk(outprefix, Q, dg, flattenednames(vars_state(model,FT)))
+    step[1] += 1
+    nothing
   end
 
   solve!(Q, ark; timeend=timeend, callbacks=(cbinfo,cbvtk))
@@ -184,9 +199,9 @@ function run(mpicomm, ArrayType, LinearType,
   norm(Q - Qe)            = %.16e
   norm(Q - Qe) / norm(Qe) = %.16e
   """ engf engf/eng0 engf-eng0 errf errf / engfe
-engf/eng0
+  engf/eng0
 end
-# --------------- Test block / Loggers ------------------ # 
+# --------------- Test block / Loggers ------------------ #
 using Test
 let
   MPI.Initialized() || MPI.Init()
@@ -203,15 +218,17 @@ let
   @testset "$(@__FILE__)" for ArrayType in ArrayTypes
     FloatType = (Float32, Float64)
     for FT in FloatType
-      brickrange = (range(FT(xmin); length=Ne[1]+1, stop=xmax),
-                    range(FT(ymin); length=Ne[2]+1, stop=ymax),
-                    range(FT(zmin); length=Ne[3]+1, stop=zmax))
-      topl = StackedBrickTopology(mpicomm, brickrange, periodicity = (false, true, false))
-      for LinearType in (AtmosAcousticLinearModel, AtmosAcousticGravityLinearModel)
-        engf_eng0 = run(mpicomm, ArrayType, LinearType,
-                        topl, dim, Ne, polynomialorder, 
-                        timeend, FT, dt)
-        @test engf_eng0 ≈ FT(0.9999997771981113)
+      for split_nonlinear_linear in (true, false)
+        brickrange = (range(FT(xmin); length=Ne[1]+1, stop=xmax),
+                      range(FT(ymin); length=Ne[2]+1, stop=ymax),
+                      range(FT(zmin); length=Ne[3]+1, stop=zmax))
+        topl = StackedBrickTopology(mpicomm, brickrange, periodicity = (false, true, false))
+        for LinearType in (AtmosAcousticLinearModel, AtmosAcousticGravityLinearModel)
+          engf_eng0 = run(mpicomm, ArrayType, LinearType,
+                          topl, dim, Ne, polynomialorder,
+                          timeend, FT, dt, split_nonlinear_linear)
+          @test engf_eng0 ≈ FT(0.9999997771981113)
+        end
       end
     end
   end
