de30372b1e70900cba10e649a42921212ed73690
Jeremy Edward Kozdon <jekozdon@nps.edu>
2019-11-01 21:56:30 -0700

    Symmetric treatment of metrics for 2nd derivatives

--- test/DGmethods/compressible_Navier_Stokes/mms_bc_atmos.jl
+++ test/DGmethods/compressible_Navier_Stokes/mms_bc_atmos.jl
@@ -197,8 +197,8 @@ let
   polynomialorder = 4
   base_num_elem = 4
 
-  expected_result = [1.5606226382564500e-01 5.3302790086802504e-03 2.2574728860707139e-04;
-                     2.5803100360042141e-02 1.1794776908545315e-03 6.1785354745749247e-05]
+  expected_result = [1.5606126271800694e-01 5.3315235468477966e-03 2.2572701271274964e-04;
+                     2.5754410198970158e-02 1.1781217145186288e-03 6.1752962472904071e-05]
 lvls = integration_testing ? size(expected_result, 2) : 1
 
 @testset "$(@__FILE__)" for ArrayType in ArrayTypes
