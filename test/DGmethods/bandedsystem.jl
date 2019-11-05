67baa9a13599799b15572d66a2fa1e1388945a2e
Jeremy E Kozdon <jekozdon@nps.edu>
2019-11-05 14:51:42 -0800

    Build banded matrix for each column

---
+++ test/DGmethods/bandedsystem.jl
@@ -0,0 +1,121 @@
+using CLIMA: haspkg
+using MPI
+using CLIMA.Mesh.Topologies
+using CLIMA.Mesh.Grids
+using CLIMA.VTK: writemesh
+using Logging
+using LinearAlgebra
+using Random
+using StaticArrays
+using CLIMA.Atmos: AtmosModel, AtmosAcousticLinearModel,
+                   DryModel, NoRadiation, NoFluxBC,
+                   ConstantViscosityWithDivergence, IsothermalProfile,
+                   HydrostaticState, NoOrientation
+using CLIMA.VariableTemplates: flattenednames
+
+using CLIMA.PlanetParameters: T_0
+using CLIMA.DGmethods: VerticalDirection, DGModel, Vars, vars_state, num_state,
+                       banded_matrix, banded_matrix_vector_product!
+using CLIMA.DGmethods.NumericalFluxes: Rusanov, CentralNumericalFluxDiffusive,
+                                       CentralGradPenalty
+using CLIMA.MPIStateArrays: MPIStateArray, euclidean_distance
+
+@static if haspkg("CuArrays")
+  using CUDAdrv
+  using CUDAnative
+  using CuArrays
+  CuArrays.allowscalar(false)
+  const ArrayTypes = (CuArray,)
+else
+  const ArrayTypes = (Array,)
+end
+
+using Test
+let
+  # boiler plate MPI stuff
+  MPI.Initialized() || MPI.Init()
+  mpicomm = MPI.COMM_WORLD
+  ll = uppercase(get(ENV, "JULIA_LOG_LEVEL", "INFO"))
+  loglevel = ll == "DEBUG" ? Logging.Debug :
+    ll == "WARN"  ? Logging.Warn  :
+    ll == "ERROR" ? Logging.Error : Logging.Info
+  logger_stream = MPI.Comm_rank(mpicomm) == 0 ? stderr : devnull
+  global_logger(ConsoleLogger(logger_stream, loglevel))
+  Random.seed!(777 + MPI.Comm_rank(mpicomm))
+
+  # Mesh generation parameters
+  N = 4
+  Nq = N+1
+  Neh = 10
+  Nev = 4
+
+  @testset "$(@__FILE__)" begin
+    for AT in ArrayTypes
+      for FT in (Float64, Float32)
+        for dim = 2:3
+          # Setup the topology
+          if dim == 2
+            brickrange = (range(FT(0); length=Neh+1, stop=1),
+                          range(FT(1); length=Nev+1, stop=2))
+          elseif dim == 3
+            brickrange = (range(FT(0); length=Neh+1, stop=1),
+                          range(FT(0); length=Neh+1, stop=1),
+            range(FT(1); length=Nev+1, stop=2))
+          end
+          topl = StackedBrickTopology(mpicomm, brickrange)
+
+          # Warp mesh
+          function warpfun(ξ1, ξ2, ξ3)
+            ξ2 = ξ2 + sin(2π * ξ1) / 5
+            ξ1 = ξ1 + sin(2π * ξ1 * ξ2) / 10
+            dim== 3 && (ξ3 = ξ3 + sin(8π * ξ1 * ξ2) / 10)
+            @inbounds (ξ1, ξ2, ξ3)
+          end
+
+          # create the actual grid
+          grid = DiscontinuousSpectralElementGrid(topl,
+                                                  FloatType = FT,
+                                                  DeviceArray = AT,
+                                                  polynomialorder = N,
+                                                  meshwarp = warpfun)
+          model = AtmosModel(NoOrientation(),
+                             HydrostaticState(IsothermalProfile(FT(T_0)), FT(0)),
+          ConstantViscosityWithDivergence(0.0),
+          DryModel(),
+          NoRadiation(),
+          nothing,
+          NoFluxBC(),
+          nothing)
+          linear_model = AtmosAcousticLinearModel(model)
+
+          # the nonlinear model is needed so we can grab the auxstate below
+          dg = DGModel(model,
+                       grid,
+                       Rusanov(),
+                       CentralNumericalFluxDiffusive(),
+                       CentralGradPenalty())
+          dg_linear = DGModel(linear_model,
+                              grid,
+                              Rusanov(),
+                              CentralNumericalFluxDiffusive(),
+                              CentralGradPenalty();
+                              direction=VerticalDirection(),
+                              auxstate=dg.auxstate)
+
+          A_banded = banded_matrix(dg_linear)
+
+          Q = MPIStateArray(dg_linear)
+          dQ1 = MPIStateArray(dg_linear)
+          dQ2 = MPIStateArray(dg_linear)
+          Q.data .= rand(size(Q.data))
+          dg_linear(dQ1, Q, nothing, 0; increment=false)
+
+          banded_matrix_vector_product!(dg_linear, A_banded, dQ2, Q)
+          @test euclidean_distance(dQ1, dQ2) / norm(dQ1) < 10 * eps(FT)
+        end
+      end
+    end
+  end
+end
+
+nothing
--- test/DGmethods/runtests.jl
+++ test/DGmethods/runtests.jl
@@ -3,6 +3,7 @@ include("../testhelpers.jl")
 
 @testset "DGmethods" begin
   tests = [
+    (1,"bandedsystem.jl")
     (1,"integral_test.jl")
     (1,"integral_test_sphere.jl")
     (1, "Euler/isentropicvortex.jl")
