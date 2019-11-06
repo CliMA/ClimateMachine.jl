b98bcf62c087e5d7d6d1686b79fd60c471291849
Lucas C Wilcox <lucas@swirlee.com>
2019-11-06 11:50:57 -0800

    Move GCRS and GMRES to the linear solver interface

--- src/LinearSolvers/LinearSolvers.jl
+++ src/LinearSolvers/LinearSolvers.jl
@@ -40,11 +40,14 @@ Sets the tolerance of the iterative linear solver `solver` to `tolerance`.
 settolerance!(solver::AbstractIterativeLinearSolver, tolerance) =
   (solver.tolerance[1] = tolerance)
 
-doiteration!(Q, Qrhs, solver::AbstractIterativeLinearSolver, tolerance) =
-  throw(MethodError(doiteration!, (Q, Qrhs, solver, tolerance)))
+doiteration!(linearoperator!, Q, Qrhs, solver::AbstractIterativeLinearSolver,
+             tolerance, args...) =
+  throw(MethodError(doiteration!, (linearoperator!, Q, Qrhs, solver,
+                                   tolerance, args...)))
 
-initialize!(Q, Qrhs, solver::AbstractIterativeLinearSolver) =
-  throw(MethodError(initialize!, (Q, Qrhs, solver))) 
+initialize!(linearoperator!, Q, Qrhs, solver::AbstractIterativeLinearSolver,
+            args...) =
+  throw(MethodError(initialize!, (linearoperator!, Q, Qrhs, solver, args...)))
 
 """
   prefactorize(linop!, linearsolver, args...)
@@ -55,7 +58,7 @@ prefactorize(linop!, linearsolver::AbstractIterativeLinearSolver, args...) =
   linop!
 
 """
-    linearsolve!(linearoperator!, solver::AbstractIterativeLinearSolver, Q, Qrhs)
+    linearsolve!(linearoperator!, solver::AbstractIterativeLinearSolver, Q, Qrhs, args...)
 
 Solves a linear problem defined by the `linearoperator!` function and the state
 `Qrhs`, i.e,
@@ -64,18 +67,20 @@ Solves a linear problem defined by the `linearoperator!` function and the state
   L(Q) = Q_{rhs}
 ```
 
-using the `solver` and the initial guess `Q`. After the call `Q` contains the solution.
+using the `solver` and the initial guess `Q`. After the call `Q` contains the
+solution.  The arguments `args` is passed to `linearoperator!` when it is
+called.
 """
-function linearsolve!(linearoperator!, solver::AbstractIterativeLinearSolver, Q, Qrhs)
+function linearsolve!(linearoperator!, solver::AbstractIterativeLinearSolver, Q, Qrhs, args...)
   converged = false
   iters = 0
 
-  converged, threshold = initialize!(linearoperator!, Q, Qrhs, solver)
+  converged, threshold = initialize!(linearoperator!, Q, Qrhs, solver, args...)
   converged && return iters
 
   while !converged
     converged, inner_iters, residual_norm = 
-      doiteration!(linearoperator!, Q, Qrhs, solver, threshold)
+      doiteration!(linearoperator!, Q, Qrhs, solver, threshold, args...)
 
     iters += inner_iters
 
