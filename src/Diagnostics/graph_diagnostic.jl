4b82f91852d549dab9b4d6a7246d1c8fce5e527a
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-11-27 11:17:02 -0500

    add src/Diagnostics/graph_diagnostic.jl

--- src/Diagnostics/graph_diagnostic.jl
+++ src/Diagnostics/graph_diagnostic.jl
@@ -33,7 +33,7 @@ function start(args::Vector{String})
     data = load(joinpath(out_dir, data_file))
 
     time = 0.0
-    time = 7667.958
+#    time = 7667.958
 
     @show keys(data)
     println("data for $(length(data)) time steps in file")
