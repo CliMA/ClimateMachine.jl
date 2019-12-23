8124d43ce3633bc7a472dd8e0735154e1c4e88a4
smarras79 <smarras@njit.edu>
2019-12-23 17:10:40 -0500

    Added gather_dt_given_Courant to Courant.jl

--- src/Diagnostics/Courant.jl
+++ src/Diagnostics/Courant.jl
@@ -16,10 +16,11 @@ using ..MPIStateArrays
 using ..PlanetParameters
 using ..VariableTemplates
 
-export gather_Courant
+export gather_Courant, gather_dt_given_Courant
 
 function gather_Courant(mpicomm, dg, Q,
-                            xmax, ymax, out_dir,Dx,Dy,Dz,dt)
+                        xmax, ymax, Dx, Dy, Dz, dt)
+    
     mpirank = MPI.Comm_rank(mpicomm)
     nranks = MPI.Comm_size(mpicomm)
 
@@ -128,8 +129,103 @@ function gather_Courant(mpicomm, dg, Q,
            CFLdiff_v        = %.5f
 """ dt_exp dt_imp cfl_h cfl_v cflu_h cflu_v cfldiff_h cfldiff_v
  
-    return nothing
+    return dt_exp, dt_imp
    
 end # function gather_Courant
+
+function gather_dt_given_Courant(mpicomm, dg, Q, xmax, ymax, Dx, Dy, Dz, Courant)
+    
+    mpirank = MPI.Comm_rank(mpicomm)
+    nranks = MPI.Comm_size(mpicomm)
+
+    # extract grid information
+    bl = dg.balancelaw
+    grid = dg.grid
+    topology = grid.topology
+    N = polynomialorder(grid)
+    Nq = N + 1
+    Nqk = dimensionality(grid) == 2 ? 1 : Nq
+    npoints = Nq * Nq * Nqk
+    nrealelem = length(topology.realelems)
+    nvertelem = topology.stacksize
+    nhorzelem = div(nrealelem, nvertelem)
+
+    # get the state, auxiliary and geo variables onto the host if needed
+    if Array ∈ typeof(Q).parameters
+        localQ    = Q.realdata
+        localaux  = dg.auxstate.realdata
+        localvgeo = grid.vgeo
+        localdiff = dg.diffstate.realdata
+    else
+        localQ    = Array(Q.realdata)
+        localaux  = Array(dg.auxstate.realdata)
+        localvgeo = Array(grid.vgeo)
+        localdiff = Array(dg.diffstate.realdata)
+    end
+    FT = eltype(localQ)
+
+
+    zvals = zeros(Nqk, nvertelem)
+    thermoQ = zeros(Nq*Nq*Nqk,1,nrealelem)
+    ss = 0
+    sau = 0
+    sav = 0
+    saw = 0
+    sdiff = 0
+    for eh in 1:nhorzelem
+            for ev in 1:nvertelem
+                e = ev + (eh - 1) * nvertelem
+                for k in 1:Nqk
+                    for j in 1:Nq
+                        for i in 1:Nq
+                         ijk = i + Nq * ((j-1) + Nq * (k-1))
+                          u̅ = localQ[ijk,2,e] / localQ[ijk,1,e]
+                          v̅ = localQ[ijk,3,e] / localQ[ijk,1,e]
+                          w̅ = localQ[ijk,4,e] / localQ[ijk,1,e]
+                          e̅_tot = localQ[ijk,5,e] / localQ[ijk,1,e]
+                          q̅_tot = localQ[ijk,6,e] / localQ[ijk,1,e]
+
+                          e_int = e̅_tot - 1//2 * (u̅^2 + v̅^2 + w̅^2) - grav * localvgeo[ijk,grid.x3id,e]
+
+                          ts = PhaseEquil(convert(FT, e_int), localQ[ijk,1,e],q̅_tot)
+                          thermoQ[ijk,1,e]=soundspeed_air(ts)
+                          if ss <= thermoQ[ijk,1,e]
+                          ss = abs(thermoQ[ijk,1,e])
+                          end
+                          if abs(sau) <= abs(localQ[ijk,2,e]) / localQ[ijk,1,e]
+                          sau = abs(localQ[ijk,2,e]) / localQ[ijk,1,e]
+                          end
+                          if abs(sav) <= abs(localQ[ijk,2,e]) / localQ[ijk,1,e]
+                          sav = abs(localQ[ijk,2,e]) / localQ[ijk,1,e]
+                          end
+                          if abs(saw) <= abs(localQ[ijk,4,e]) / localQ[ijk,1,e]
+                          saw = abs(localQ[ijk,4,e]) / localQ[ijk,1,e]
+                          end
+                          if abs(sdiff) <= abs(localdiff[ijk,3,e]) / localQ[ijk,1,e]
+                          sdiff = abs(localdiff[ijk,3,e]) / localQ[ijk,1,e]
+                          end
+                        end
+                    end
+                end
+             end
+    end
+    max_sound_speed = MPI.Allreduce(ss,MPI.MAX,mpicomm)
+    dt_exp = Courant * min(Dx, Dy, Dz) / max_sound_speed
+    dt_imp = Courant * max(Dx, Dy, Dz) / max_sound_speed
+    
+    @info @sprintf """Courant numbers:
+           dt_exp           = %.5e
+           dt_imp           = %.5e
+           CFL_h            = %.5f
+           CFL_v            = %.5f
+           CFLadv_h         = %.5f
+           CFLadv_v         = %.5f
+           CFLdiff_h        = %.5f
+           CFLdiff_v        = %.5f
+""" dt_exp dt_imp cfl_h cfl_v cflu_h cflu_v cfldiff_h cfldiff_v
+ 
+    return dt_exp, dt_imp
+   
+end # function gather_dt
 
 end # module Courant
