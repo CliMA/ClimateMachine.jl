f934e1ec3b579a41ec10ffe6d97aa29e25e5c193
Simon Byrne <simonbyrne@gmail.com>
2020-02-04 13:12:25 -0800

    update to match temperature

--- examples/Land/soilmodel.jl
+++ examples/Land/soilmodel.jl
@@ -6,10 +6,6 @@ import CLIMA.DGmethods: BalanceLaw,
                         gradvariables!, diffusive!,
                         init_aux!, init_state!,
                         boundary_state!, wavespeed, LocalGeometry
-import CLIMA.DGmethods.NumericalFluxes: NumericalFluxNonDiffusive,
-                                       NumericalFluxDiffusive,
-                                       GradNumericalPenalty,
-                                       numerical_boundary_flux_diffusive!
 
 """
     SoilModel
@@ -38,25 +34,25 @@ To write this in the form
 we write `Y = ρcT` and `F(Y, t) = -λ ∇T`.
 
 """
-struct SoilModel <: BalanceLaw
-  ρ::Float64
-  c::Float64
-  λ::Float64
-  surfaceT::Float64
-  initialT::Float64
+Base.@kwdef struct SoilModel <: BalanceLaw
+  ρc::Float64 = 2.49e6
+  λ::Float64  = 2.42
+  initialT::Float64 = 10.0
+  meanT₀::Float64 = 15.0
+  A₀::Float64 = 5.0
 end
 
 # Stored in the aux state are:
 #   `coord` coordinate points (needed for BCs)
 #   `u` advection velocity
 #   `D` Diffusion tensor
-vars_aux(::SoilModel, FT) = @vars(z::FT, ρ::FT, c::FT, λ::FT) # stored dg.auxstate
+vars_aux(::SoilModel, FT) = @vars(z::FT, ρc::FT, λ::FT) # stored dg.auxstate
 vars_state(::SoilModel, FT) = @vars(ρcT::FT) # stored in Q
 vars_gradient(::SoilModel, FT) = @vars(T::FT) # not stored
 vars_diffusive(::SoilModel, FT) = @vars(∇T::SVector{3,FT}) # stored in dg.diffstate
 
 function gradvariables!(m::SoilModel, transform::Vars, state::Vars, aux::Vars, t::Real)
-  transform.T = state.ρcT / (m.ρ * m.c)
+  transform.T = state.ρcT / (m.ρc)
 end
 function diffusive!(m::SoilModel, diffusive::Vars, ∇transform::Grad, state::Vars, aux::Vars, t::Real)
   diffusive.∇T = ∇transform.T
@@ -82,76 +78,33 @@ function init_aux!(m::SoilModel, aux::Vars, geom::LocalGeometry)
 end
 
 function init_state!(m::SoilModel, state::Vars, aux::Vars, coords, t::Real)
-  state.ρcT = 10.0 #+ sin(aux.z)
+  state.ρcT = m.ρc * m.initialT
 end
 
-# Neumann boundary conditions
 
-function numerical_boundary_flux_diffusive!(nf::NumericalFluxDiffusive,
-    bl::SoilModel, fluxᵀn::Vars{S}, n::SVector,
-    state⁻::Vars{S}, diff⁻::Vars{D}, aux⁻::Vars{A},
-    state⁺::Vars{S}, diff⁺::Vars{D}, aux⁺::Vars{A},
-    bctype, t,
-    state1⁻::Vars{S}, diff1⁻::Vars{D}, aux1⁻::Vars{A}) where {S,D,A}
-
-  if bctype == 1 # top
-    fluxᵀn.ρcT = 2 #state⁻.ρcT/(60*60*24)*sinpi(2t/(60*60*24))
-  else # bottom
-    fluxᵀn.ρcT = 0
+function boundary_state!(nf, m::SoilModel, state⁺::Vars, aux⁺::Vars,
+                         nM, state⁻::Vars, aux⁻::Vars, bctype, t, _...)
+  if bctype == 1
+    # surface
+    t_hours = t/(60*60)
+    T = m.meanT₀ + m.A₀ * sinpi(2*(t_hours-8)/24)
+    state⁺.ρcT = m.ρc * T
+  elseif bctype == 2
+    # bottom
+    nothing
   end
 end
 
-#=
-# set up domain
-topl = StackedBrickTopology(mpicomm, (0:1,0:1,0:-1:-10); periodicity = (true,true,false),boundary=((0,0),(0,0),(1,2)))
-grid = DiscontinuousSpectralElementGrid(topl, FloatType = Float64, DeviceArray = Array, polynomialorder = 5)
-
-m = SoilModel(1.0,1.0,1.0,20.0,10.0)
-
-# Set up DG scheme
-dg = DGModel( #
-  m, # "PDE part"
-  grid,
-  CentralNumericalFluxNonDiffusive(), # penalty terms for discretizations
-  CentralNumericalFluxDiffusive(),
-  CentralGradPenalty())
-
-
-Δ = min_node_distance(grid)
-CFL_bound = (Δ^2 / (2m.λ/(m.ρ*m.c)))
-dt = CFL_bound*0.001 # TODO: provide a "default" timestep based on  Δx,Δy,Δz
-
-# state variable
-Q = init_ode_state(dg, Float64(0))
-
-# initialize ODE solver
-lsrk = LSRK54CarpenterKennedy(dg, Q; dt = dt, t0 = 0)
-
-function plotstate(grid, Q)
-    # TODO:
-    # this currently uses some internals: provide a better way to do this
-    Xg = reshape(grid.vgeo[(1:6^2:6^3),CLIMA.Mesh.Grids.vgeoid.x3id,:],:)
-    Yg = reshape(Q.data[(1:6^2:6^3),1,:],:)
-    plot(Xg, Yg, xlabel="depth", ylabel="ρcT", ylimit=(0,40))
-end
-
-#plotstate(grid, Q)
-=#
-
-function boundary_state!(nf, m::SoilModel, stateP::Vars, auxP::Vars,
-                         nM, stateM::Vars, auxM::Vars, bctype, t, _...)
-  nothing
-end
-#=
-function boundary_state!(nf, m::SoilModel, stateP::Vars, diffP::Vars,
-                         auxP::Vars, nM, stateM::Vars, diffM::Vars, auxM::Vars,
+function boundary_state!(nf, m::SoilModel, state⁺::Vars, diff⁺::Vars,
+                         aux⁺::Vars, nM, state⁻::Vars, diff⁻::Vars, aux⁻::Vars,
                          bctype, t, _...)
   if bctype == 1
-    # top
-    #stateP.ρcT = 15
-    diffP.∇T = SVector(0,0,1.0)
+    # surface
+    t_hours = t/(60*60)
+    T = m.meanT₀ + m.A₀ * sinpi(2*(t_hours-8)/24)
+    state⁺.ρcT = m.ρc * T
   elseif bctype == 2
-    #diffP.∂T∂z = -diffM.∂T∂z
+    # bottom
+    diff⁺.∇T = -diff⁻.∇T
   end
 end
-=#
