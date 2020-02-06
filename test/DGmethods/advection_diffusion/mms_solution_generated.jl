b9ea7f1933dfe7d1a98c6ae07a59256bdb7262a1
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-02-05 23:29:33 -0800

    try

--- test/DGmethods/advection_diffusion/mms_solution_generated.jl
+++ test/DGmethods/advection_diffusion/mms_solution_generated.jl
@@ -1,3 +1,5 @@
 const ν_exact = 1//100
-ρ_g(t, φ, θ, r) = sin(θ)*sin(φ)*cos(t)*cos(pi*r) + 3
-Sρ_g(t, φ, θ, r) = (-50*r^2*sin(t) + cos(t))*sin(θ)*sin(φ)*cos(pi*r)/(50*r^2)
+const a_exact = 1000000
+const b_exact = 1000
+ρ_g(t, φ, θ, r) = -sin(θ)^4*sin(φ)^2*sin(pi*(r/999000 - 1000/999))*cos(t) + 3
+Sρ_g(t, φ, θ, r) = (50*r^2*sin(t)*sin(θ)^2*sin(φ)^2 - 10*sin(θ)^2*sin(φ)^2*cos(t) + 6*sin(φ)^2*cos(t) + cos(t))*sin(θ)^2*sin(pi*(r/999000 - 1000/999))/(50*r^2)
