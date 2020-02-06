1a07be7e57ecfc662ff2e0602fca899156a7485c
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-02-06 00:41:55 -0800

    try

--- test/DGmethods/advection_diffusion/mms_solution_generated.jl
+++ test/DGmethods/advection_diffusion/mms_solution_generated.jl
@@ -2,4 +2,4 @@ const ν_exact = 1//100
 const a_exact = 1
 const b_exact = 1
 ρ_g(t, φ, θ, r) = sin(θ)^4*sin(φ)^2*sin(pi*(r - 1))*cos(t) + 3
-Sρ_g(t, φ, θ, r) = (50*r^2*sin(t)*sin(θ)^2*sin(φ)^2 - 10*sin(θ)^2*sin(φ)^2*cos(t) + 6*sin(φ)^2*cos(t) + cos(t))*sin(θ)^2*sin(pi*r)/(50*r^2)
+Sρ_g(t, φ, θ, r) = (25*r^4*sin(t)*sin(θ)^4*sin(φ)^2 - 100*sin(θ)^4*sin(φ)^2*cos(t) + 78*sin(θ)^2*sin(φ)^2*cos(t) + 13*sin(θ)^2*cos(t) - 8*cos(t))*sin(pi*r)/(25*r^4)
