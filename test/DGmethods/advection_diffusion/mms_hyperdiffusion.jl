55622b3d666de41f625345be7df13ef9e9632eaf
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-02-05 23:46:44 -0800

    try

--- test/DGmethods/advection_diffusion/mms_hyperdiffusion.jl
+++ test/DGmethods/advection_diffusion/mms_hyperdiffusion.jl
@@ -2,18 +2,23 @@
 using LinearAlgebra, SymPy, Printf
 
 @syms φ θ r t real=true
-ν = 1 // 10000
+ν = 1 // 100
 
 output = open("mms_solution_generated.jl", "w")
 
-@printf output "const ν_exact = %s\n" ν
+a = 10 ^ 6
+b = 10 ^ 4
 
-ρ = cos(t) * sin(φ) * sin(θ) * cos(π * r) + 3
+@printf output "const ν_exact = %s\n" ν
+@printf output "const a_exact = %s\n" a
+@printf output "const b_exact = %s\n" b
+
+ρ = cos(t) * sin(φ) ^ 2 * sin(θ) ^ 4 * sin(pi * (r - a) / b) + 3
 
 Δρ = (diff(sin(θ) * diff(ρ, θ), θ) + diff(ρ, φ, φ) / sin(θ)) / (r ^ 2 * sin(θ))
 Δ²ρ = (diff(sin(θ) * diff(Δρ, θ), θ) + diff(Δρ, φ, φ) / sin(θ)) / (r ^ 2 * sin(θ))
 
-dρdt = simplify(diff(ρ, t) + ν * Δ²ρ)
+dρdt = simplify(diff(ρ, t) - ν * Δρ)
 
 @printf output "ρ_g(t, φ, θ, r) = %s\n" ρ
 @printf output "Sρ_g(t, φ, θ, r) = %s\n" dρdt
