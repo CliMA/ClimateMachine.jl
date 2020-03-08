5153a43e0a4d65b0a2b1a7df88904e56e44a24de
Thomas Gibson <t.gibson15@imperial.ac.uk>
2020-03-07 16:23:10 -0800

    Add gcm driver test

--- test/Driver/runtests.jl
+++ test/Driver/runtests.jl
@@ -3,7 +3,8 @@ include("../testhelpers.jl")
 
 @testset "Driver" begin
   tests = [
-    (1,"driver_test.jl")
+    (1,"driver_test.jl"),
+    (1,"gcm_driver_test.jl")
    ]
 
   runmpi(tests, @__FILE__)
