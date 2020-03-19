3d57946a4f93c3aeeb8c5a1a89d2f492cfb32b4b
Jeremy E Kozdon <jekozdon@nps.edu>
2020-03-19 13:54:35 -0700

    JuliaFormatter commit for files
    Formatting the following files before making functionality changes
    
      src/Utilities/VariableTemplates/VariableTemplates.jl
      test/Arrays/runtests.jl
      test/DGmethods/runtests.jl

--- test/Arrays/runtests.jl
+++ test/Arrays/runtests.jl
@@ -2,11 +2,12 @@ using Test
 include("../testhelpers.jl")
 
 @testset "MPIStateArrays reductions" begin
-  tests = [(1, "basics.jl")
-           (1, "broadcasting.jl")
-           (1, "reductions.jl")
-           (3, "reductions.jl")
-          ]
+    tests = [
+        (1, "basics.jl")
+        (1, "broadcasting.jl")
+        (1, "reductions.jl")
+        (3, "reductions.jl")
+    ]
 
-  runmpi(tests, @__FILE__)
+    runmpi(tests, @__FILE__)
 end
