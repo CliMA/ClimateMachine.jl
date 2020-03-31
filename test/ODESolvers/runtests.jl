df4b7a92c3dde9a85a96bc02b71b296f07f27e79
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-03-31 01:50:23 -0700

    Split ODE solvers test suite

--- test/ODESolvers/runtests.jl
+++ test/ODESolvers/runtests.jl
@@ -2,6 +2,6 @@ using Test, MPI
 include("../testhelpers.jl")
 
 @testset "ODE Solvers" begin
-  tests = [(1, "ode_tests.jl")]
-  runmpi(tests, @__FILE__)
+    tests = [(1, "ode_tests_basic.jl")]
+    runmpi(tests, @__FILE__)
 end
