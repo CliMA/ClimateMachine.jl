62fc54907461674a007ce14569a7587e60900e9c
K Pamnany <kpamnany@gmail.com>
2020-03-31 07:32:03 -0700

    Move GCM driver test from AzureCI to SlurmCI

--- test/Driver/runtests.jl
+++ test/Driver/runtests.jl
@@ -2,7 +2,9 @@ using MPI, Test
 include("../testhelpers.jl")
 
 @testset "Driver" begin
-    tests = [(1, "driver_test.jl"), (1, "gcm_driver_test.jl")]
+    tests = [
+        (1, "driver_test.jl"),
+    ]
 
     runmpi(tests, @__FILE__)
 end
