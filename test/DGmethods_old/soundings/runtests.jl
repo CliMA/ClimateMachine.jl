bbf91afc059ccdf78b26b2ffd06fbdc1d7206a10
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-03-31 12:18:05 -0700

    Apply formatter

--- test/DGmethods_old/soundings/runtests.jl
+++ test/DGmethods_old/soundings/runtests.jl
@@ -7,12 +7,30 @@ soundings_dataset = ArtifactWrapper(
     joinpath(@__DIR__, "Artifacts.toml"),
     "soundings",
     ArtifactFile[
-    ArtifactFile(url = "https://caltech.box.com/shared/static/rjnvt2dlw7etm1c7mmdfrkw5gnfds5lx.nc", filename = "sounding_gabersek.nc"),
-    ArtifactFile(url = "https://caltech.box.com/shared/static/th1a0p5bduej2hx6lisphlpo7pm2wvys.nc", filename = "sounding_WKR88.nc"),
-    ArtifactFile(url = "https://caltech.box.com/shared/static/etz2f6qn45m5x4ow1nwx2mcams9fno9c.nc", filename = "sounding_JCP2013_with_pressure.nc"),
-    ArtifactFile(url = "https://caltech.box.com/shared/static/90um61xxjtxh45cwg2d2t5mucl8cbphe.nc", filename = "sounding_JCP2013.nc"),
-    ArtifactFile(url = "https://caltech.box.com/shared/static/satr1neltu8pedvukn125ywr5eks8m0r.nc", filename = "sounding_GC1991.nc"),
-    ArtifactFile(url = "https://caltech.box.com/shared/static/3kmfcqrlf9scx9166c2fx0g1c36i124l.nc", filename = "sounding_blend.nc"),
+        ArtifactFile(
+            url = "https://caltech.box.com/shared/static/rjnvt2dlw7etm1c7mmdfrkw5gnfds5lx.nc",
+            filename = "sounding_gabersek.nc",
+        ),
+        ArtifactFile(
+            url = "https://caltech.box.com/shared/static/th1a0p5bduej2hx6lisphlpo7pm2wvys.nc",
+            filename = "sounding_WKR88.nc",
+        ),
+        ArtifactFile(
+            url = "https://caltech.box.com/shared/static/etz2f6qn45m5x4ow1nwx2mcams9fno9c.nc",
+            filename = "sounding_JCP2013_with_pressure.nc",
+        ),
+        ArtifactFile(
+            url = "https://caltech.box.com/shared/static/90um61xxjtxh45cwg2d2t5mucl8cbphe.nc",
+            filename = "sounding_JCP2013.nc",
+        ),
+        ArtifactFile(
+            url = "https://caltech.box.com/shared/static/satr1neltu8pedvukn125ywr5eks8m0r.nc",
+            filename = "sounding_GC1991.nc",
+        ),
+        ArtifactFile(
+            url = "https://caltech.box.com/shared/static/3kmfcqrlf9scx9166c2fx0g1c36i124l.nc",
+            filename = "sounding_blend.nc",
+        ),
     ],
 )
 data_folder = get_data_folder(soundings_dataset)
