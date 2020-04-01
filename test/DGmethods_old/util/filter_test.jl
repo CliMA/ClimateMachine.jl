026a23a4051447fb5c4db4d6072e63d8aab67278
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-03-31 22:58:32 -0700

    Apply formatter

--- test/DGmethods_old/util/filter_test.jl
+++ test/DGmethods_old/util/filter_test.jl
@@ -11,98 +11,107 @@ using Logging
 
 using Test
 function run(mpicomm, dim, ArrayType, Ne, FT)
-  N = 3
+    N = 3
 
-  brickrange = ntuple(j->range(FT(-1); length=Ne[j]+1, stop=1), dim)
-  topl = BrickTopology(mpicomm, brickrange, periodicity=ntuple(j->true, dim))
+    brickrange = ntuple(j -> range(FT(-1); length = Ne[j] + 1, stop = 1), dim)
+    topl =
+        BrickTopology(mpicomm, brickrange, periodicity = ntuple(j -> true, dim))
 
-  grid = DiscontinuousSpectralElementGrid(topl,
-                                          FloatType = FT,
-                                          DeviceArray = ArrayType,
-                                          polynomialorder = N,
-                                         )
+    grid = DiscontinuousSpectralElementGrid(
+        topl,
+        FloatType = FT,
+        DeviceArray = ArrayType,
+        polynomialorder = N,
+    )
 
-  spacedisc = DGBalanceLaw(grid = grid,
-                           length_state_vector = 4,
-                           flux! = (x...) -> (),
-                           numerical_flux! = (x...) -> ())
+    spacedisc = DGBalanceLaw(
+        grid = grid,
+        length_state_vector = 4,
+        flux! = (x...) -> (),
+        numerical_flux! = (x...) -> (),
+    )
 
-  filter = CutoffFilter(grid, 2)
+    filter = CutoffFilter(grid, 2)
 
-  # Legendre Polynomials
-  l0(r) = 1
-  l1(r) = r
-  l2(r) = (3*r^2-1)/2
-  l3(r) = (5*r^3-3r)/2
+    # Legendre Polynomials
+    l0(r) = 1
+    l1(r) = r
+    l2(r) = (3 * r^2 - 1) / 2
+    l3(r) = (5 * r^3 - 3r) / 2
 
-  low(x, y, z)  = l0(x) * l0(y) + 4 * l1(x) * l1(y) + 5 * l1(z) +
-                  6 * l1(z) * l1(x)
+    low(x, y, z) =
+        l0(x) * l0(y) + 4 * l1(x) * l1(y) + 5 * l1(z) + 6 * l1(z) * l1(x)
 
-  high(x, y, z) = l2(x) * l3(y) + l3(x) + l2(y) + l3(z) * l1(y)
+    high(x, y, z) = l2(x) * l3(y) + l3(x) + l2(y) + l3(z) * l1(y)
 
-  filtered_both(x, y, z) = high(x, y, z)
+    filtered_both(x, y, z) = high(x, y, z)
 
-  filtered_vertical(x, y, z) = (dim == 2) ? l2(x) * l3(y) + l2(y) :
-                                            l3(z) * l1(y)
-  filtered_horizontal(x, y, z) = (dim == 2) ? l2(x) * l3(y) + l3(x) :
-                                              l2(x) * l3(y) + l3(x) + l2(y)
+    filtered_vertical(x, y, z) =
+        (dim == 2) ? l2(x) * l3(y) + l2(y) : l3(z) * l1(y)
+    filtered_horizontal(x, y, z) =
+        (dim == 2) ? l2(x) * l3(y) + l3(x) : l2(x) * l3(y) + l3(x) + l2(y)
 
-  for (horizontal, vertical) = ((true, true), (true, false), (false, true))
-    if horizontal && vertical
-      filtered = filtered_both
-      direction = EveryDirection()
-    elseif horizontal
-      filtered = filtered_horizontal
-      direction = HorizontalDirection()
-    elseif vertical
-      filtered = filtered_vertical
-      direction = VerticalDirection()
+    for (horizontal, vertical) in ((true, true), (true, false), (false, true))
+        if horizontal && vertical
+            filtered = filtered_both
+            direction = EveryDirection()
+        elseif horizontal
+            filtered = filtered_horizontal
+            direction = HorizontalDirection()
+        elseif vertical
+            filtered = filtered_vertical
+            direction = VerticalDirection()
+        end
+
+        Q = MPIStateArray(spacedisc) do Q, x, y, z, _...
+            @inbounds begin
+                Q[1] = low(x, y, z) + high(x, y, z)
+                Q[2] = low(x, y, z) + high(x, y, z)
+                Q[3] = low(x, y, z) + high(x, y, z)
+                Q[4] = low(x, y, z) + high(x, y, z)
+            end
+        end
+        P = MPIStateArray(spacedisc) do P, x, y, z, _...
+            @inbounds begin
+                P[1] = low(x, y, z) + high(x, y, z) - filtered(x, y, z)
+                P[2] = low(x, y, z) + high(x, y, z)
+                P[3] = low(x, y, z) + high(x, y, z) - filtered(x, y, z)
+                P[4] = low(x, y, z) + high(x, y, z)
+            end
+        end
+
+        Filters.apply!(Q, (1, 3), spacedisc.grid, filter, direction)
+
+        @test Array(Q.data) ≈ Array(P.data)
     end
-
-    Q = MPIStateArray(spacedisc) do Q, x, y, z, _...
-      @inbounds begin
-        Q[1] = low(x, y, z) + high(x, y, z)
-        Q[2] = low(x, y, z) + high(x, y, z)
-        Q[3] = low(x, y, z) + high(x, y, z)
-        Q[4] = low(x, y, z) + high(x, y, z)
-      end
-    end
-    P = MPIStateArray(spacedisc) do P, x, y, z, _...
-      @inbounds begin
-        P[1] = low(x, y, z) + high(x, y, z) - filtered(x, y, z)
-        P[2] = low(x, y, z) + high(x, y, z)
-        P[3] = low(x, y, z) + high(x, y, z) - filtered(x, y, z)
-        P[4] = low(x, y, z) + high(x, y, z)
-      end
-    end
-
-    Filters.apply!(Q, (1,3), spacedisc.grid, filter, direction)
-
-    @test Array(Q.data) ≈ Array(P.data)
-  end
 end
 
 let
-  CLIMA.init()
-  ArrayTypes = (CLIMA.array_type(),)
+    CLIMA.init()
+    ArrayTypes = (CLIMA.array_type(),)
 
-  mpicomm = MPI.COMM_WORLD
+    mpicomm = MPI.COMM_WORLD
 
-  numelem = (1, 1, 1)
-  lvls = 1
+    numelem = (1, 1, 1)
+    lvls = 1
 
-  @testset "$(@__FILE__)" for ArrayType in ArrayTypes
-    for FT in (Float64, Float32)
-      for dim = 2:3
-        err = zeros(FT, lvls)
-        for l = 1:lvls
-          @info (ArrayType, FT, dim)
-          run(mpicomm, dim, ArrayType, ntuple(j->2^(l-1) * numelem[j], dim),
-              FT)
+    @testset "$(@__FILE__)" for ArrayType in ArrayTypes
+        for FT in (Float64, Float32)
+            for dim in 2:3
+                err = zeros(FT, lvls)
+                for l in 1:lvls
+                    @info (ArrayType, FT, dim)
+                    run(
+                        mpicomm,
+                        dim,
+                        ArrayType,
+                        ntuple(j -> 2^(l - 1) * numelem[j], dim),
+                        FT,
+                    )
+                end
+            end
         end
-      end
     end
-  end
 end
 
 nothing
