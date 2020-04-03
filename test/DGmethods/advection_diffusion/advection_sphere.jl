c40b091499d058394d2cc4660fcc86d39ff007d9
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-04-03 15:32:30 -0700

    Apply formatter

--- test/DGmethods/advection_diffusion/advection_sphere.jl
+++ test/DGmethods/advection_diffusion/advection_sphere.jl
@@ -44,7 +44,7 @@ function init_velocity_diffusion!(
     ::SolidBodyRotation,
     aux::Vars,
     geom::LocalGeometry,
-   )
+)
     FT = eltype(aux)
     λ = longitude(SphericalOrientation(), aux)
     φ = latitude(SphericalOrientation(), aux)
@@ -52,17 +52,13 @@ function init_velocity_diffusion!(
 
     uλ = 2 * FT(π) * cos(φ) * r
     uφ = 0
-    aux.u  = SVector(-uλ * sin(λ) - uφ * cos(λ) * sin(φ),
-                     +uλ * cos(λ) - uφ * sin(λ) * sin(φ),
-                     +uφ * cos(φ))
+    aux.u = SVector(
+        -uλ * sin(λ) - uφ * cos(λ) * sin(φ),
+        +uλ * cos(λ) - uφ * sin(λ) * sin(φ),
+        +uφ * cos(φ),
+    )
 end
-function initial_condition!(
-    ::SolidBodyRotation,
-    state,
-    aux,
-    x,
-    t,
-)
+function initial_condition!(::SolidBodyRotation, state, aux, x, t)
     λ = longitude(SphericalOrientation(), aux)
     φ = latitude(SphericalOrientation(), aux)
     state.ρ = exp(-((3λ)^2 + (3φ)^2))
@@ -89,40 +85,43 @@ init_velocity_diffusion!(
     ::ReversingDeformationalFlow,
     aux::Vars,
     geom::LocalGeometry,
-   ) = nothing
-function initial_condition!(
-    ::ReversingDeformationalFlow,
-    state,
-    aux,
-    coord,
-    t,
-)
+) = nothing
+function initial_condition!(::ReversingDeformationalFlow, state, aux, coord, t)
     x, y, z = aux.coord
     r = norm(aux.coord)
     h_max = 0.95
     b = 5
     state.ρ = 0
-    for (λ, φ) in ((5π/6, 0), (7π/6, 0))
-      xi = r * cos(φ) * cos(λ)
-      yi = r * cos(φ) * sin(λ)
-      zi = r * sin(φ)
-      state.ρ += h_max * exp(-b * ((x - xi) ^ 2 + (y - yi) ^ 2 + (z - zi) ^ 2))
+    for (λ, φ) in ((5π / 6, 0), (7π / 6, 0))
+        xi = r * cos(φ) * cos(λ)
+        yi = r * cos(φ) * sin(λ)
+        zi = r * sin(φ)
+        state.ρ += h_max * exp(-b * ((x - xi)^2 + (y - yi)^2 + (z - zi)^2))
     end
 end
 has_variable_coefficients(::ReversingDeformationalFlow) = true
-function update_velocity_diffusion!(::ReversingDeformationalFlow,
-                                    ::AdvectionDiffusion, state::Vars, aux::Vars, t::Real)
+function update_velocity_diffusion!(
+    ::ReversingDeformationalFlow,
+    ::AdvectionDiffusion,
+    state::Vars,
+    aux::Vars,
+    t::Real,
+)
     FT = eltype(aux)
     λ = longitude(SphericalOrientation(), aux)
     φ = latitude(SphericalOrientation(), aux)
     r = norm(aux.coord)
     T = FT(5)
     λp = λ - FT(2π) * t / T
-    uλ = 10 * r / T * sin(λp) ^ 2 * sin(2φ) * cos(FT(π) * t / T) + FT(2π) * r / T * cos(φ)
-    uφ = 10 * r / T * sin(2λp) * cos(φ) * cos(FT(π) * t / T) 
-    aux.u  = SVector(-uλ * sin(λ) - uφ * cos(λ) * sin(φ),
-                     +uλ * cos(λ) - uφ * sin(λ) * sin(φ),
-                     +uφ * cos(φ))
+    uλ =
+        10 * r / T * sin(λp)^2 * sin(2φ) * cos(FT(π) * t / T) +
+        FT(2π) * r / T * cos(φ)
+    uφ = 10 * r / T * sin(2λp) * cos(φ) * cos(FT(π) * t / T)
+    aux.u = SVector(
+        -uλ * sin(λ) - uφ * cos(λ) * sin(φ),
+        +uλ * cos(λ) - uφ * sin(λ) * sin(φ),
+        +uφ * cos(φ),
+    )
 end
 u_scale(::ReversingDeformationalFlow) = 2.9
 finaltime(::ReversingDeformationalFlow) = 5
@@ -136,12 +135,22 @@ function advective_courant(
     Δt,
     direction,
 )
-  return Δt * norm(aux.u) / Δx
+    return Δt * norm(aux.u) / Δx
 end
 
-function boundary_state!(::Rusanov, ::AdvectionDiffusion, stateP::Vars, auxP::Vars,
-                         nM, stateM::Vars, auxM::Vars, bctype, t, _...)
-  auxP.u = -auxM.u
+function boundary_state!(
+    ::Rusanov,
+    ::AdvectionDiffusion,
+    stateP::Vars,
+    auxP::Vars,
+    nM,
+    stateM::Vars,
+    auxM::Vars,
+    bctype,
+    t,
+    _...,
+)
+    auxP.u = -auxM.u
 end
 
 function do_output(mpicomm, vtkdir, vtkstep, dg, Q, Qe, model, testname)
@@ -193,9 +202,9 @@ function run(
         FloatType = FT,
         DeviceArray = ArrayType,
         polynomialorder = N,
-        meshwarp = cubedshellwarp
+        meshwarp = cubedshellwarp,
     )
-   
+
     dx = min_node_distance(grid, HorizontalDirection())
     dt = FT(cfl * dx / u_scale(problem()))
     dt = outputtime / ceil(Int64, outputtime / dt)
@@ -206,7 +215,7 @@ function run(
         grid,
         Rusanov(),
         CentralNumericalFluxDiffusive(),
-        CentralNumericalFluxGradient()
+        CentralNumericalFluxGradient(),
     )
 
     Q = init_ode_state(dg, FT(0))
@@ -242,22 +251,22 @@ function run(
         end
     end
     cbcfl = EveryXSimulationSteps(10) do
-            dt = ODESolvers.getdt(odesolver)
-            cfl = DGmethods.courant(
-                advective_courant,
-                dg,
-                model,
-                Q,
-                dt,
-                HorizontalDirection(),
-            )
-            @info @sprintf(
-                """Courant number
-                simtime = %.16e
-                courant = %.16e""",
-                gettime(odesolver),
-                cfl
-            )
+        dt = ODESolvers.getdt(odesolver)
+        cfl = DGmethods.courant(
+            advective_courant,
+            dg,
+            model,
+            Q,
+            dt,
+            HorizontalDirection(),
+        )
+        @info @sprintf(
+            """Courant number
+            simtime = %.16e
+            courant = %.16e""",
+            gettime(odesolver),
+            cfl
+        )
     end
     callbacks = (cbinfo,)
     if ~isnothing(vtkdir)
@@ -266,16 +275,7 @@ function run(
 
         vtkstep = 0
         # output initial step
-        do_output(
-            mpicomm,
-            vtkdir,
-            vtkstep,
-            dg,
-            Q,
-            Q,
-            model,
-            "advection_sphere",
-        )
+        do_output(mpicomm, vtkdir, vtkstep, dg, Q, Q, model, "advection_sphere")
 
         # setup the output callback
         cbvtk = EveryXSimulationSteps(floor(outputtime / dt)) do
@@ -325,105 +325,138 @@ let
     polynomialorder = 4
     base_num_elem = 2
 
-    max_cfl = Dict(LSRK144NiegemannDiehlBusch => 5.0,
-                   SSPRK33ShuOsher => 1.0,
-                   SSPRK34SpiteriRuuth => 1.5)
+    max_cfl = Dict(
+        LSRK144NiegemannDiehlBusch => 5.0,
+        SSPRK33ShuOsher => 1.0,
+        SSPRK34SpiteriRuuth => 1.5,
+    )
 
     expected_result = Dict()
 
-    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 1] = 1.3199024557832748e-01
-    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 2] = 1.9868931633120656e-02
-    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 3] = 1.4052110916915061e-03
-    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 4] = 9.0193766298676310e-05
-    
-    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 1] = 1.1055145388897809e-01
-    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 2] = 1.5510740467668628e-02
-    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 3] = 1.8629481690361454e-03
-    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 4] = 2.3567040048588889e-04
-    
-    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 1] = 1.2641959922001456e-01
-    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 2] = 2.2780375948714751e-02
-    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 3] = 3.1274951764826459e-03
-    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 4] = 3.9734060514021565e-04
-    
-    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 1] = 5.5387951598735408e-01
-    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 2] = 3.7610388138383732e-01
-    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 3] = 1.7823508719111605e-01
-    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 4] = 3.8639493470255713e-02
-    
-    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 1] = 5.5353962032596349e-01
-    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 2] = 3.7645487928038762e-01
-    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 3] = 1.7823263736245307e-01
-    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 4] = 3.8605903366230925e-02
-    
-    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 1] = 5.5404045660832824e-01
-    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 2] = 3.7788858038003154e-01
-    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 3] = 1.8007113931230376e-01
-    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 4] = 3.9941331660544775e-02
-    
-    numlevels = integration_testing || CLIMA.Settings.integration_testing ? 4 : 1
+    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 1] =
+        1.3199024557832748e-01
+    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 2] =
+        1.9868931633120656e-02
+    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 3] =
+        1.4052110916915061e-03
+    expected_result[SolidBodyRotation, LSRK144NiegemannDiehlBusch, 4] =
+        9.0193766298676310e-05
+
+    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 1] =
+        1.1055145388897809e-01
+    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 2] =
+        1.5510740467668628e-02
+    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 3] =
+        1.8629481690361454e-03
+    expected_result[SolidBodyRotation, SSPRK33ShuOsher, 4] =
+        2.3567040048588889e-04
+
+    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 1] =
+        1.2641959922001456e-01
+    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 2] =
+        2.2780375948714751e-02
+    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 3] =
+        3.1274951764826459e-03
+    expected_result[SolidBodyRotation, SSPRK34SpiteriRuuth, 4] =
+        3.9734060514021565e-04
+
+    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 1] =
+        5.5387951598735408e-01
+    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 2] =
+        3.7610388138383732e-01
+    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 3] =
+        1.7823508719111605e-01
+    expected_result[ReversingDeformationalFlow, LSRK144NiegemannDiehlBusch, 4] =
+        3.8639493470255713e-02
+
+    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 1] =
+        5.5353962032596349e-01
+    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 2] =
+        3.7645487928038762e-01
+    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 3] =
+        1.7823263736245307e-01
+    expected_result[ReversingDeformationalFlow, SSPRK33ShuOsher, 4] =
+        3.8605903366230925e-02
+
+    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 1] =
+        5.5404045660832824e-01
+    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 2] =
+        3.7788858038003154e-01
+    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 3] =
+        1.8007113931230376e-01
+    expected_result[ReversingDeformationalFlow, SSPRK34SpiteriRuuth, 4] =
+        3.9941331660544775e-02
+
+    numlevels =
+        integration_testing || CLIMA.Settings.integration_testing ? 4 : 1
     @testset "$(@__FILE__)" begin
         for FT in (Float64,)
-          for problem in (SolidBodyRotation, ReversingDeformationalFlow)
-            for explicit_method in (LSRK144NiegemannDiehlBusch,
-                                    SSPRK33ShuOsher,
-                                    SSPRK34SpiteriRuuth)
-            cfl = max_cfl[explicit_method]
-            result = zeros(FT, numlevels)
-            for l in 1:numlevels
-                numelems_horizontal = 2^(l - 1) * base_num_elem
-                numelems_vertical = 1
-
-                topl = StackedCubedSphereTopology(
-                    mpicomm,
-                    numelems_horizontal,
-                    range(FT(1), stop = 2, length = numelems_vertical + 1),
+            for problem in (SolidBodyRotation, ReversingDeformationalFlow)
+                for explicit_method in (
+                    LSRK144NiegemannDiehlBusch,
+                    SSPRK33ShuOsher,
+                    SSPRK34SpiteriRuuth,
                 )
+                    cfl = max_cfl[explicit_method]
+                    result = zeros(FT, numlevels)
+                    for l in 1:numlevels
+                        numelems_horizontal = 2^(l - 1) * base_num_elem
+                        numelems_vertical = 1
 
-                timeend = finaltime(problem())
-                outputtime = timeend
+                        topl = StackedCubedSphereTopology(
+                            mpicomm,
+                            numelems_horizontal,
+                            range(
+                                FT(1),
+                                stop = 2,
+                                length = numelems_vertical + 1,
+                            ),
+                        )
 
-                @info (ArrayType, FT)
-                vtkdir = output ?
-                    "vtk_advection_sphere" *
-                "_$problem" *
-                "_$explicit_method" *
-                "_poly$(polynomialorder)" *
-                "_$(ArrayType)_$(FT)" *
-                "_level$(l)" :
-                    nothing
+                        timeend = finaltime(problem())
+                        outputtime = timeend
 
-                result[l], Δmass = run(
-                    mpicomm,
-                    ArrayType,
-                    topl,
-                    problem,
-                    explicit_method,
-                    cfl,
-                    polynomialorder,
-                    timeend,
-                    FT,
-                    vtkdir,
-                    outputtime,
-                )
-                @test result[l] ≈
-                      FT(expected_result[problem, explicit_method, l])
-                @test Δmass <= FT(5e-14)
-            end
-            @info begin
-                msg = ""
-                for l in 1:(numlevels - 1)
-                    rate = log2(result[l]) - log2(result[l + 1])
-                    msg *= @sprintf(
-                        "\n  rate for level %d = %e\n",
-                        l,
-                        rate
-                    )
+                        @info (ArrayType, FT)
+                        vtkdir = output ?
+                            "vtk_advection_sphere" *
+                        "_$problem" *
+                        "_$explicit_method" *
+                        "_poly$(polynomialorder)" *
+                        "_$(ArrayType)_$(FT)" *
+                        "_level$(l)" :
+                            nothing
+
+                        result[l], Δmass = run(
+                            mpicomm,
+                            ArrayType,
+                            topl,
+                            problem,
+                            explicit_method,
+                            cfl,
+                            polynomialorder,
+                            timeend,
+                            FT,
+                            vtkdir,
+                            outputtime,
+                        )
+                        @test result[l] ≈
+                              FT(expected_result[problem, explicit_method, l])
+                        @test Δmass <= FT(5e-14)
+                    end
+                    @info begin
+                        msg = ""
+                        for l in 1:(numlevels - 1)
+                            rate = log2(result[l]) - log2(result[l + 1])
+                            msg *= @sprintf(
+                                "\n  rate for level %d = %e\n",
+                                l,
+                                rate
+                            )
+                        end
+                        msg
+                    end
                 end
-                msg
             end
-          end
-          end
         end
     end
 end
