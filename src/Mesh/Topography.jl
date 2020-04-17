04b05148a7ee4b3476270c7b350df3403e9935fa
asraero <asridhar@caltech.edu>
2020-04-17 13:45:11 -0700

    format, use do block, remove hardcoded floattypes

--- src/Mesh/Topography.jl
+++ src/Mesh/Topography.jl
@@ -35,40 +35,40 @@ export ReadExternalHeader, ReadExternalTxtCoordinates
  
 """
 function ReadExternalHeader(header_file_in)
-    
+
     @info @sprintf """ Topography header file %s ... DONE""" header_file_in
-    
+
     ftopo_header = open(header_file_in)
     @info @sprintf """ Grids.jl: Opening topography header file ... DONE"""
-    
+
     topo_header = readdlm(ftopo_header)
-    @info @sprintf """ Grids.jl: Reading topography header file ... DONE""" 
+    @info @sprintf """ Grids.jl: Reading topography header file ... DONE"""
 
     #Check that the file is not empty
-    (nrows, _) = size(topo_header)    
+    (nrows, _) = size(topo_header)
 
-    close(ftopo_header)           
+    close(ftopo_header)
     @info @sprintf """ Grids.jl: Closing topography header file ... DONE"""
 
-    
+
     if nrows == 0
         error(" ERROR Grids.jl --> READTOPOtxt_header: grid header file is empty!")
         return
     end
-    
-    nlon      =   Int64(topo_header[1,2])
-    nlat      =   Int64(topo_header[2,2])
-    lonmin    = Float64(topo_header[3,2])
-    latmin    = Float64(topo_header[4,2])
-    deltacell = Float64(topo_header[5,2])
-    lonmax    = lonmin + nlon*deltacell
-    latmax    = latmin + nlat*deltacell
-    
-    dlon  = lonmax - lonmin
-    dlat  = latmax - latmin
+
+    nlon = IT(topo_header[1, 2])
+    nlat = IT(topo_header[2, 2])
+    lonmin = FT(topo_header[3, 2])
+    latmin = FT(topo_header[4, 2])
+    deltacell = FT(topo_header[5, 2])
+    lonmax = lonmin + nlon * deltacell
+    latmax = latmin + nlat * deltacell
+
+    dlon = lonmax - lonmin
+    dlat = latmax - latmin
 
     return (nlon, nlat, lonmin, lonmax, latmin, latmax, dlon, dlat)
-    
+
 end
 # }}}
 
@@ -98,64 +98,64 @@ function ReadExternalTxtCoordinates(body_file_in, TopoBathy_flg, nlon, nlat)
 
     @info @sprintf """ Topography file: %s""" body_file_in
 
-    FT  = Float64 #NOTICE: I recommend the grid is always build in double precision even if the code is run in single 
-     
-    ftopo_body = open(body_file_in)
-    @info @sprintf """ Grids.jl: Opening topography file ... DONE"""
-   
-    topo_body = readdlm(ftopo_body)
-    (npoin_linear_grid, _) = size(topo_body)
-    if npoin_linear_grid == 0
-        error(" ERROR Grids.jl --> READTOPOtxt_xyz: grid file is empty!")
-        return
+    FT = Float64 #NOTICE: I recommend the grid is always build in double precision even if the code is run in single 
+    IT = Int64 #NOTICE: I recommend the grid is always build in double precision even if the code is run in single 
+
+    open(body_file_in, "r") do ftopo_body
+        @info @sprintf """ Grids.jl: Opening topography file ... DONE"""
+
+        topo_body = readdlm(ftopo_body)
+        (npoin_linear_grid, _) = size(topo_body)
+        if npoin_linear_grid == 0
+            error(" ERROR Grids.jl --> READTOPOtxt_xyz: grid file is empty!")
+            return
+        end
+        #
+        # Create array
+        #
+        nnodes_lon, nnodes_lat, nnodes = nlon, nlat, nlon * nlat #Linear grid
+        zTopo2d = Array{FT, 2}(undef, nnodes_lon, nnodes_lat)
+
+        xTopo1d = Array{FT, 1}(undef, nnodes_lon)
+        yTopo1d = Array{FT, 1}(undef, nnodes_lat)
+
+        zTopo2d = reshape(topo_body[:, 3], nnodes_lon, nnodes_lat)
+        #npoin = k
+
+        #
+        # Store 1D X array in the x direction: size -> nnodes_lat
+        # and order in increasing order
+        for i in 1:nnodes_lon
+            xTopo1d[i] = topo_body[i, 1]
+            #@info @sprintf """ X[%d]: %.16e""" i xTopo1d[i]
+        end
+        if minimum(xTopo1d) < 0
+            xTopo1d = xTopo1d .- minimum(xTopo1d)
+        end
+        if xTopo1d[1] > xTopo1d[end]
+            xTopo1d = reverse(xTopo1d)
+        end
+
+        #
+        # Store 1D X array in the x direction: size -> nnodes_lat
+        # and order in increasing order
+        l = 0
+        interval = nnodes_lon
+        yTopo1d = topo_body[1:interval:nnodes, 2]
+
+        #Shift if negative
+        if minimum(yTopo1d) < 0
+            yTopo1d = yTopo1d .- minimum(yTopo1d)
+        end
+        if yTopo1d[1] > yTopo1d[end]
+            yTopo1d = reverse(yTopo1d)
+        end
+
     end
-    #
-    # Create array
-    #
-    nnodes_lon, nnodes_lat, nnodes = nlon, nlat, nlon*nlat #Linear grid
-    zTopo2d = Array{FT, 2}(undef, nnodes_lon, nnodes_lat)
-    
-    xTopo1d = Array{FT, 1}(undef, nnodes_lon)
-    yTopo1d = Array{FT, 1}(undef, nnodes_lat)
-       
-    zTopo2d = reshape(topo_body[:,3], nnodes_lon, nnodes_lat);
-    #npoin = k
-    
-    #
-    # Store 1D X array in the x direction: size -> nnodes_lat
-    # and order in increasing order
-    for i = 1:nnodes_lon     
-        xTopo1d[i] = topo_body[i,1]    
-        #@info @sprintf """ X[%d]: %.16e""" i xTopo1d[i]
-    end
-    if minimum(xTopo1d) < 0
-        xTopo1d =  xTopo1d .- minimum(xTopo1d)
-    end
-    if xTopo1d[1] > xTopo1d[end]
-        xTopo1d = reverse(xTopo1d)
-    end
-  
-    #
-    # Store 1D X array in the x direction: size -> nnodes_lat
-    # and order in increasing order
-    l = 0
-    interval = nnodes_lon
-    yTopo1d = topo_body[1:interval:nnodes,2]
-    
-    #Shift if negative
-    if minimum(yTopo1d) < 0
-        yTopo1d =  yTopo1d .- minimum(yTopo1d)
-    end   
-    if yTopo1d[1] > yTopo1d[end]
-        yTopo1d = reverse(yTopo1d)
-    end
-    
-    close(ftopo_body)
-    
     @info @sprintf """ Grids.jl: Closing topography file ... DONE"""
-    
+
     return xTopo1d, yTopo1d, zTopo2d
- 
+
 end
 #}}}
 
