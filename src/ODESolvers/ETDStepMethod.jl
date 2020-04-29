28a5f4bed4396c0a93c900f447d0465a773fc009
vogel-d <denise.vogel@gmx.net>
2020-04-29 09:27:01 +0200

    added ETD

---
+++ src/ODESolvers/ETDStepMethod.jl
@@ -0,0 +1,287 @@
+
+export EB4, EB1, ETDRK3
+
+"""
+ETDStep(slowrhs!, fastrhs!, fastmethod,
+                           α, β, γ,
+                           Q::AT; dt=0, t0=0) where {AT<:AbstractArray}
+This is a time stepping object for explicitly time stepping the partitioned differential
+equation given by right-hand-side functions `f_fast` and `f_slow` with the state `Q`, i.e.,
+```math
+  \\dot{Q} = f_fast(Q, t) + f_slow(Q, t)
+```
+with the required time step size `dt` and optional initial time `t0`.  This
+time stepping object is intended to be passed to the `solve!` command.
+The constructor builds an exponential time differencing  Runge-Kutta scheme
+based on the provided `β` tableaux and `fastmethod` for solving
+the fast modes.
+### References
+    @article{Krogstad2005,
+      title={Generalized integrating factor methods for stiff PDEs},
+      author={Krogstad, Stein},
+      journal= {Journal of Computational Physics},
+      volume= {203},
+      number= {1},
+      pages = {72 - 88},
+      year = {2005},
+    }
+"""
+mutable struct ETDStep{
+  T,
+  RT,
+  AT,
+  FS,
+  Nstages,
+  Nstagesm1,
+  Nstagesm2,
+  Nstages_sq
+} <: AbstractODESolver
+  "time step"
+  dt::RT
+  "time"
+  t::RT
+  #"storage for y_n"
+  #yn::AT
+  "Storage for ``f(Y_nj)``"
+  fYnj::NTuple{Nstagesm1, AT}
+  "Storage for offset"
+  offset::AT
+  "slow rhs function"
+  slowrhs!
+  "RHS for fast solver"
+  tsfastrhs!::TimeScaledRHS{N,RT} where N
+  "fast rhs method"
+  fastsolver::FS
+  #"number of steps"
+  #nsubsteps::Int
+
+  nStages::Int64
+  nPhi::Int64
+  nPhiStages::Array{Int64,1}
+
+  #α::SArray{NTuple{2, Nstages}, RT, 2, Nstages_sq}
+  #β::SArray{NTuple{2, Nstages}, AT, 2, Nstages_sq}
+  #βS::SArray{NTuple{2, Nstages}, AT, 2, Nstages_sq}
+  β::Array{Array{Float64,1},2};
+  βS::Array{Array{Float64,1},2};
+
+  #γ::SArray{NTuple{2, Nstages}, RT, 2, Nstages_sq}
+  d::SArray{NTuple{1, Nstages}, RT, 1, Nstages}
+  c::SArray{NTuple{1, Nstages}, RT, 1, Nstages}
+  #c̃::SArray{NTuple{1, Nstages}, RT, 1, Nstages}
+
+  function ETDStep(
+    slowrhs!,
+    fastrhs!,
+    fastmethod,
+    nsteps,
+    Nstages,
+    nPhi,
+    nPhiStages,
+    β,
+    βS,
+    c,
+    Q::AT;
+    dt=0,
+    t0=0
+  ) where {AT<:AbstractArray}
+
+    T = eltype(Q)
+    RT = real(T)
+
+
+    #yn = similar(Q)
+    #ΔYnj = ntuple(_ -> similar(Q), Nstages-2)
+    fYnj = ntuple(_ -> similar(Q), Nstages-1)
+    offset = similar(Q)
+    tsfastrhs! = TimeScaledRHS(RT(0), RT(0), fastrhs!)
+    fastsolver = fastmethod(tsfastrhs!, dt/nsteps, Q)
+
+    #d = sum(β, dims=2)
+
+    #c = similar(d)
+    d=copy(c);
+
+    #for i = eachindex(c)
+      #c[i] = d[i]
+      #if i > 1
+      #   c[i] += sum(j-> (α[i,j] + γ[i,j])*c[j], 1:i-1)
+      #end
+    #end
+    #c̃ = α*c
+
+    #new{T, RT, AT, typeof(fastsolver), Nstages, Nstages-1, Nstages-2, Nstages ^ 2}(RT(dt), RT(t0), yn, ΔYnj, fYnj, offset,
+    #                                       slowrhs!, tsfastrhs!, fastsolver,
+    #                                       α, β, γ, d, c, c̃)
+    new{
+      T,
+      RT,
+      AT,
+      typeof(fastsolver),
+      Nstages,
+      Nstages-1,
+      Nstages-2,
+      Nstages ^ 2
+    }(
+      RT(dt),
+      RT(t0),
+      fYnj,
+      offset,
+      slowrhs!,
+      tsfastrhs!,
+      fastsolver,
+      Nstages,
+      nPhi,
+      nPhiStages,
+      β,
+      βS,
+      d,
+      c
+    )
+  end
+end
+
+function dostep!(Q, etd::ETDStep, p, time)
+
+  dt = etd.dt
+  FT = eltype(dt)
+  β = etd.β
+  βS = etd.βS
+  nPhi = etd.nPhi
+  #yn = etd.yn
+  fYnj = etd.fYnj
+  offset = etd.offset
+  d = etd.d
+  c = etd.c
+  #c̃ = etd.c̃
+  slowrhs! = etd.slowrhs!
+  fastsolver = etd.fastsolver
+  fastrhs! = etd.tsfastrhs!
+
+  nStages = etd.nStages
+
+  #copyto!(yn, Q) # first stage
+  for iStage = 1:nStages-1
+    slowrhs!(fYnj[iStage], Q, p, time + c[iStage]*dt, increment=false)
+
+    nsteps = cld(dt, fastsolver.max_inner_dt)
+    inner_dt = dt / nsteps
+
+    nstepsLoc=Int64(ceil(nsteps*c[iStage+1]));
+    dtLoc=Float64(dt)*c[iStage+1];
+    dτ=dtLoc/nstepsLoc;
+
+    dostep!(Q, fastsolver, p, time, dτ, nstepsLoc, iStage, β, βS, nPhi, fYnj, FT(1), realview(offset), nothing)  #(1c)
+  end
+end
+
+function EB1(slowrhs!, fastrhs!, fastmethod, nsteps, Q::AT; dt=0, t0=0) where {AT <: AbstractArray}
+
+  nStages=2;
+  nPhi=1;
+
+  nPhiStages=[0, 1]; #???
+
+  β = [[[0.0]] [[0.0]];
+      [[1.0]] [[0.0]]];
+  βS =[[[0.0]] [[0.0]];
+      [[0.0]] [[0.0]]];
+
+
+  c = [0.0, 1.0, 1.0];
+
+  for i=2:2
+    for j=1:i-1
+    kFac=1;
+      for k=1:1
+        kFac=kFac*max(k-1,1)*c[i];
+        βS[i,j][k]=β[i,j][k]/kFac;
+      end
+    end
+  end
+
+  ETDStep(slowrhs!, fastrhs!, fastmethod, nsteps, nStages, nPhi, nPhiStages, β, βS, c, Q; dt=dt, t0=t0)
+end
+
+function ETDRK3(slowrhs!, fastrhs!, fastmethod, nsteps, Q::AT; dt=0, t0=0) where {AT <: AbstractArray}
+
+    nStages=4;
+    nPhi=1;
+
+    nPhiStages=[0,1,1,1];
+
+    β = [[[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+         [[1.0/3.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+         [[0.0,0.0,0.0]] [[1.0/2.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+         [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[1.0,0.0,0.0]] [[0.0,0.0,0.0]]];
+
+    #βS=similar(β);
+
+    βS = [[[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+          [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+          [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+          [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]]];
+
+    c = [0.0, 0.33333333333333333, 0.5, 1.0];
+       #c[i] is usually sum of first elements in i-th row)
+
+    for i=2:nStages
+      for j=1:i-1
+        kFac=1;
+          for k=1:nPhi
+            kFac=kFac*max(k-1,1);
+            βS[i,j][k]=β[i,j][k]/(kFac*c[i]);
+            β[i,j][k]=β[i,j][k]/c[i];
+          end
+      end
+    end
+
+    #=γ = [0  0               0              0;
+         0  0               0              0;
+         0  0.652465126004  0              0;
+         0 -0.0732769849457 0.144902430420 0]=# #not needed (yet?)
+
+    ETDStep(slowrhs!, fastrhs!, fastmethod, nsteps, nStages, nPhi, nPhiStages, β, βS, c, Q; dt=dt, t0=t0)
+end
+
+function EB4(slowrhs!, fastrhs!, fastmethod, nsteps, Q::AT; dt=0, t0=0) where {AT <: AbstractArray}
+
+  nStages=5;
+  nPhi=3;
+
+  nPhiStages=[0,1,2,2,3];
+
+  β = [[[0.0,0.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]];
+       [[0.5,0.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]];
+       [[0.5,-1.0,0.0]] [[0.0,1.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]];
+       [[1.0,-2.0,0.0]] [[0.0,0.0,0.0]]  [[0.0,2.0,0.0]]  [[0.0,0.0,0.0]]  [[0.0,0.0,0.0]];
+       [[1.0,-3.0,4.0]] [[0.0,2.0,-4.0]] [[0.0,2.0,-4.0]] [[0.0,-1.0,4.0]] [[0.0,0.0,0.0]]];
+
+  #βS=similar(β);
+  βS = [[[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+        [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+        [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+        [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]];
+        [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]] [[0.0,0.0,0.0]]];
+
+  c = [0.0, 0.5, 0.5, 1.0, 1.0];
+     #c[i] is usually sum of first elements in i-th row)
+
+  for i=2:nStages
+    for j=1:i-1
+      kFac=1;
+        for k=1:nPhi
+          kFac=kFac*max(k-1,1);
+          βS[i,j][k]=β[i,j][k]/(kFac*c[i]);
+          β[i,j][k]=β[i,j][k]/c[i];
+        end
+    end
+  end
+
+  #=γ = [0  0               0              0;
+       0  0               0              0;
+       0  0.652465126004  0              0;
+       0 -0.0732769849457 0.144902430420 0]=# #not needed (yet?)
+
+  ETDStep(slowrhs!, fastrhs!, fastmethod, nsteps, nStages, nPhi, nPhiStages, β, βS, c, Q; dt=dt, t0=t0)
+end
---
+++ src/ODESolvers/StormerVerletMethodETD.jl
@@ -0,0 +1,249 @@
+export StormerVerletETD
+
+"""
+    LowStorageRungeKutta2N(f, RKA, RKB, RKC, Q; dt, t0 = 0)
+This is a time stepping object for explicitly time stepping the differential
+equation given by the right-hand-side function `f` with the state `Q`, i.e.,
+```math
+  \\dot{Q} = f(Q, t)
+```
+with the required time step size `dt` and optional initial time `t0`.  This
+time stepping object is intended to be passed to the `solve!` command.
+The constructor builds a low-storage Runge-Kutta scheme using 2N
+storage based on the provided `RKA`, `RKB` and `RKC` coefficient arrays.
+The available concrete implementations are:
+  - [`LSRK54CarpenterKennedy`](@ref)
+  - [`LSRK144NiegemannDiehlBusch`](@ref)
+"""
+struct StormerVerletETD{T, RT, AT} <: AbstractODESolver
+  "time step"
+  dt::Array{RT,1}
+  "time"
+  t::Array{RT,1}
+  "rhs function"
+  rhs!
+
+  max_inner_dt::RT
+
+  mask_a
+  mask_b
+
+  offset1
+  offset0
+
+  dQ::AT
+  function StormerVerletETD(rhs!, max_inner_dt, mask_a, mask_b, Q::AT; dt=0, t0=0) where {AT<:AbstractArray}
+
+    T = eltype(Q)
+    RT = real(T)
+    dt = [dt]
+    t0 = [t0]
+
+    dQ = similar(Q)
+    offset1 = similar(Q)
+    offset0 = similar(Q)
+
+    fill!(dQ, 0)
+    fill!(offset1, 0)
+    fill!(offset0, 0)
+
+    new{T, RT, AT}(dt, t0, rhs!, max_inner_dt, mask_a, mask_b, offset1, offset0, dQ)
+  end
+end
+
+updatedt!(sv::StormerVerletETD, dt) = sv.dt[1] = dt
+
+"""
+    ODESolvers.dostep!(Q, lsrk::LowStorageRungeKutta2N, p, time::Real,
+                       dt::Real, [slow_δ, slow_rv_dQ, slow_scaling])
+Use the 2N low storage Runge--Kutta method `lsrk` to step `Q` forward in time
+from the current time `time` to final time `time + dt`.
+If the optional parameter `slow_δ !== nothing` then `slow_rv_dQ * slow_δ` is
+added as an additional ODE right-hand side source. If the optional parameter
+`slow_scaling !== nothing` then after the final stage update the scaling
+`slow_rv_dQ *= slow_scaling` is performed.
+"""
+function dostep!(Q, sv::StormerVerletETD, p, time::Real,
+  dτ::Real, nsLoc::Int, iStage, βS, β, nPhi, fYnj, slow_δ, slow_rv_dQ, slow_rka)
+
+  rhs!, dQ = sv.rhs!, sv.dQ
+
+  Qa = realview(Q[:,sv.mask_a,:])
+  Qb = realview(Q[:,sv.mask_b,:])
+  dQa = @view(dQ.realdata[:,sv.mask_a,:])
+  dQb = @view(dQ.realdata[:,sv.mask_b,:])
+
+
+  offset1=sv.offset1;
+  offset0=sv.offset0;
+  offset1a = offset1[:,sv.mask_a,:];
+  offset1b = offset1[:,sv.mask_b,:];
+  offset0a = offset0[:,sv.mask_a,:];
+  offset0b = offset0[:,sv.mask_b,:];
+
+  τ=0.0;
+  dTime=nsLoc*dτ;
+
+  groupsize = 256
+
+  event = Event(device(Q))
+  event = update!(device(Q), groupsize)(
+         realview(offset1),
+         Val(iStage),
+         map(realview, fYnj[1:iStage]),
+         βS[iStage+1,:],
+         τ,
+         nPhi;
+         ndrange = length(realview(offset1)),
+         dependencies = (event,),
+  )
+  wait(device(Q), event)
+
+  rhs!(dQ, Q, p, time, increment = false)
+  event = Event(device(Q))
+  event = update!(device(Q), groupsize)(
+         dQa.+offset1a,
+         Qa,
+         dτ/2;
+         ndrange = length(Qa),
+         dependencies = (event,),
+  )
+  wait(device(Q), event)
+  Q.realdata[:,sv.mask_a,:].=Qa
+
+  τ += dτ/dTime;
+
+  for i = 1:nsLoc-1
+
+    offset0=deepcopy(offset1);
+    offset0a = @view(offset0[:,sv.mask_a,:]);
+    offset0b = @view(offset0[:,sv.mask_b,:]);
+
+    event = Event(device(Q))
+    event = update!(device(Q), groupsize)(
+           realview(offset1),
+           Val(iStage),
+           map(realview, fYnj[1:iStage]),
+           βS[iStage+1,:],
+           τ,
+           nPhi;
+           ndrange = length(realview(offset1)),
+           dependencies = (event,),
+    )
+    wait(device(Q), event)
+
+    rhs!(dQ, Q, p, time, increment = false)
+    event = Event(device(Q))
+    event = update!(device(Q), groupsize)(
+           dQb.+0.5*(offset0b.+offset1b),
+           Qb,
+           dτ;
+           ndrange = length(Qb),
+           dependencies = (event,),
+    )
+    wait(device(Q), event)
+    Q.realdata[:,sv.mask_b,:].=Qb
+
+    rhs!(dQ, Q, p, time, increment = false) #increment = true? damit auf dQ draufaddiert
+    event = Event(device(Q))
+    event = update!(device(Q), groupsize)(
+           dQa .+ offset1a,
+           Qa,
+           dτ;
+           ndrange = length(Qa),
+           dependencies = (event,),
+    )
+    wait(device(Q), event)
+    Q.realdata[:,sv.mask_a,:].=Qa
+
+    τ += dτ/dTime
+  end
+
+  if τ-1.0>0.01
+    error("tau ist nicht 1, sondern: $τ !!")
+  end
+
+  offset0=deepcopy(offset1);
+  offset0a = @view(offset0[:,sv.mask_a,:]);
+  offset0b = @view(offset0[:,sv.mask_b,:]);
+
+  event = Event(device(Q))
+  event = update!(device(Q), groupsize)(
+         realview(offset1),
+         Val(iStage),
+         map(realview, fYnj[1:iStage]),
+         βS[iStage+1,:],
+         τ,
+         nPhi;
+         ndrange = length(realview(offset1)),
+         dependencies = (event,),
+  )
+  wait(device(Q), event)
+
+  rhs!(dQ, Q, p, time, increment = false)
+  event = Event(device(Q))
+  event = update!(device(Q), groupsize)(
+         dQb .+ 0.5*(offset0b.+offset1b),
+         Qb,
+         dτ;
+         ndrange = length(Qb),
+         dependencies = (event,),
+  )
+  wait(device(Q), event)
+  Q.realdata[:,sv.mask_b,:].=Qb
+
+  rhs!(dQ, Q, p, time, increment = false)
+  event = Event(device(Q))
+  event = update!(device(Q), groupsize)(
+         dQa .+ offset1a,
+         Qa,
+         dτ/2;
+         ndrange = length(Qa),
+         dependencies = (event,),
+  )
+  wait(device(Q), event)
+  Q.realdata[:,sv.mask_a,:].=Qa
+
+  if slow_rka !== nothing
+    slow_rv_dQ .*= slow_rka
+  end
+
+end
+
+@kernel function update!(
+    dQ,
+    Q,
+    dt,
+)
+    i = @index(Global, Linear)
+    @inbounds begin
+      Q[i] += (dQ[i]) * dt
+    end
+end
+
+@kernel function update!(
+    offset,
+    ::Val{iStage},
+    fYnj,
+    βSiStage,
+    τ,
+    nPhi
+) where {iStage}
+    e = @index(Global, Linear)
+    @inbounds begin
+      fac = βSiStage[1][nPhi];
+      @unroll for k in (nPhi-1):-1:1
+        fac=fac.*τ.+βSiStage[1][k];
+      end
+      offset[e]=fac.*fYnj[1][e];
+
+      @unroll for jStage in 2:iStage
+        fac = βSiStage[jStage][nPhi];
+
+        @unroll for k in (nPhi-1):-1:1
+          fac=fac.*τ.+βSiStage[jStage][k];
+        end
+         offset[e]+=fac.*fYnj[jStage][e];
+      end
+    end
+end
