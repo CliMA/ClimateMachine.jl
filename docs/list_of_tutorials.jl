d1675c494f747a616f2dd2353d8c99573d6b72a8
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-05-06 16:27:08 -0700

    Rename extending clima -> how-to-guides

--- docs/list_of_tutorials.jl
+++ docs/list_of_tutorials.jl
@@ -54,10 +54,7 @@ if generate_tutorials
 
     # These files mirror the .jl files in `CLIMA/tutorials/`:
     tutorials = Any[
-        "Atmos" => Any[
-            "Dry Idealized GCM" => "generated/Atmos/heldsuarez.md",
-            "Rising Bubble LES" => "ExtendingCLIMA/Atmos/Model/risingbubble.md",
-        ],
+        "Atmos" => Any["Dry Idealized GCM" => "generated/Atmos/heldsuarez.md",],
         "Ocean" => Any[],
         "Numerics" => Any[
             "LinearSolvers" => Any["Conjugate Gradient" => "generated/Numerics/LinearSolvers/cg.md",],
