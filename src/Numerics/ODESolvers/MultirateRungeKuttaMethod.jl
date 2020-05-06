f89f0985182f4941ce0799b3ca07649b331c3b49
Thomas Gibson <t.gibson15@imperial.ac.uk>
2020-05-06 13:00:28 -0700

    Debug: Remove substepping initially

--- src/Numerics/ODESolvers/MultirateRungeKuttaMethod.jl
+++ src/Numerics/ODESolvers/MultirateRungeKuttaMethod.jl
@@ -188,9 +188,29 @@ end
     end
 end
 
+dostep!(
+    Q,
+    mrrk::MultirateRungeKutta{SS},
+    param,
+    time::Real,
+    in_slow_δ = nothing,
+    in_slow_rv_dQ = nothing,
+    in_slow_scaling = nothing,
+) where {SS <: ARK} = dostep!(
+    Q,
+    mrrk,
+    mrrk.slow_solver.variant,
+    param,
+    time,
+    in_slow_δ,
+    in_slow_rv_dQ,
+    in_slow_scaling,
+)
+
 function dostep!(
     Q,
     mrrk::MultirateRungeKutta{SS},
+    variant::LowStorageVariant,
     param,
     time::Real,
     in_slow_δ = nothing,
@@ -199,198 +219,115 @@ function dostep!(
 ) where {SS <: ARK}
 
     dt = mrrk.dt
-    slow = mrrk.slow_solver
-    fast = mrrk.fast_solver
+    outer_solver = mrrk.slow_solver
+    inner_solver = mrrk.fast_solver
 
-    # Get the linear solver for the outer IMEX loop
-    slow_besolver! = slow.besolver!
+    outer_besolver! = outer_solver.besolver!
 
-    # Butcher tableau for the ARK method (explicit and implicit parts)
-    slow_RKA_explicit = slow.RKA_explicit
-    slow_RKA_implicit = slow.RKA_implicit
-    slow_RKB = slow.RKB
-    slow_RKC = slow.RKC
+    outer_RKA_explicit = outer_solver.RKA_explicit
+    outer_RKA_implicit = outer_solver.RKA_implicit
+    outer_RKB = outer_solver.RKB
+    outer_RKC = outer_solver.RKC
 
-    # Explicit (slow_rhs!) and implicit (slow_rhs_implicit!) tendencies
-    slow_rhs! = slow.rhs!
-    slow_rhs_implicit! = slow.rhs_implicit!
-    slow_Qhat = slow.Qhat
-    slow_Qtt = slow.variant_storage.Qtt
+    outer_rhs! = outer_solver.rhs!
+    outer_rhs_implicit! = outer_solver.rhs_implicit!
 
-    slow_Qstages = slow.Qstages
-    slow_Rstages = slow.Rstages
-    # slow_Lstages = slow.variant_storage.Lstages
-    slow_rv_Q = realview(Q)
-    slow_rv_Qstages = realview.(slow_Qstages)
-    # slow_rv_Lstages = realview.(slow_Lstages)
-    slow_rv_Rstages = realview.(slow_Rstages)
-    slow_rv_Qhat = realview(slow_Qhat)
-    slow_rv_Qtt = realview(slow_Qtt)
-    slow_split_explicit_implicit = slow.split_explicit_implicit
+    outer_Qstages = outer_solver.Qstages
+    outer_Rstages = outer_solver.Rstages
+
+    Qhat = outer_solver.Qhat
+    split_explicit_implicit = outer_solver.split_explicit_implicit
+    Qtt = outer_solver.variant_storage.Qtt
+
+    rv_Q = realview(Q)
+    rv_outerQstages = realview.(outer_Qstages)
+    rv_outerRstages = realview.(outer_Rstages)
+    rv_Qhat = realview(Qhat)
+    rv_Qtt = realview(Qtt)
+
+    # Number of outer stages
+    Nstages = length(outer_RKB)
 
     groupsize = 256
-    Nouter_stages = length(slow_RKB)
-    fast_dt_in = getdt(fast)
 
-    for slow_s in 1:Nouter_stages
-        # Currnent slow state time
-        slow_stage_time = time + slow_RKC[slow_s] * dt
+    # calculate the rhs at first stage to initialize the stage loop
+    outer_rhs!(
+        outer_Rstages[1],
+        outer_Qstages[1],
+        param,
+        time + outer_RKC[1] * dt,
+        increment = false,
+    )
 
-        # Evaluate the slow mode using an IMEX method.
-        # NOTE: This part of the code assumes that the IMEX method
-        # employs an additive RK method with an explicit first stage
-        # (no linear solve in the first stage).
-        if slow_s === 1
-            # No implicit solve during first slow stage
-            slow_rhs!(
-                slow_Rstages[slow_s],
-                slow_Qstages[slow_s],
+    # note that it is important that this loop does not modify Q!
+    for istage in 2:Nstages
+        stagetime = time + outer_RKC[istage] * dt
+
+        # this kernel also initializes Qtt for the linear solver
+        event = Event(device(Q))
+        event = stage_update!(device(Q), groupsize)(
+            variant,
+            rv_Q,
+            rv_outerQstages,
+            rv_outerRstages,
+            rv_Qhat,
+            rv_Qtt,
+            outer_RKA_explicit,
+            outer_RKA_implicit,
+            dt,
+            Val(istage),
+            Val(split_explicit_implicit),
+            in_slow_δ,
+            in_slow_rv_dQ;
+            ndrange = length(rv_Q),
+            dependencies = (event,),
+        )
+        wait(device(Q), event)
+
+        # solves
+        # Q_tt = Qhat + dt * outer_RKA_implicit[istage, istage] * outer_rhs_implicit!(Q_tt)
+        α = dt * outer_RKA_implicit[istage, istage]
+        outer_besolver!(Qtt, Qhat, α, param, stagetime)
+
+        # update Qstages
+        outer_Qstages[istage] .+= Qtt
+
+        outer_rhs!(
+            outer_Rstages[istage],
+            outer_Qstages[istage],
+            param,
+            stagetime,
+            increment = false,
+        )
+    end
+
+    if split_explicit_implicit
+        for istage in 1:Nstages
+            stagetime = time + outer_RKC[istage] * dt
+            rhs_implicit!(
+                outer_Rstages[istage],
+                outer_Qstages[istage],
                 param,
-                slow_stage_time,
-                increment = false,
+                stagetime,
+                increment = true,
             )
-        else
-            # implicit linear solve only appears after the
-            # first slow stage is completed
-            if in_slow_δ !== nothing
-                slow_scaling = nothing
-                if slow_s == length(slow_RKB)
-                    slow_scaling = in_slow_scaling
-                end
-                # Update solution and scale RHS
-                event = Event(device(Q))
-                event = stage_update!(device(Q), groupsize)(
-                    slow.variant,
-                    slow_rv_Q,
-                    slow_rv_Qstages,
-                    slow_rv_Rstages,
-                    slow_rv_Qhat,
-                    slow_rv_Qtt,
-                    slow_RKA_explicit,
-                    slow_RKA_implicit,
-                    dt,
-                    Val(slow_s),
-                    Val(slow_split_explicit_implicit),
-                    slow_δ,
-                    slow_scaling;
-                    ndrange = length(slow_rv_Q),
-                    dependencies = (event,),
-                )
-                wait(device(Q), event)
-            end
-
-            # Solves
-            # Q_tt = Qhat + dt * RKA_implicit[istage, istage] * rhs_implicit!(Q_tt)
-            α = dt * slow_RKA_implicit[slow_s, slow_s]
-            slow_besolver!(slow_Qtt, slow_Qhat, α, param, slow_stage_time)
-
-            # update Qstages
-            slow_Qstages[slow_s] .+= slow_Qtt
-
-            slow_rhs!(
-                slow_Rstages[slow_s],
-                slow_Qstages[slow_s],
-                param,
-                slow_stage_time,
-                increment = false,
-            )
-        end
-
-        # Fractional time for slow stage
-        if slow_s == Nouter_stages
-            γ = 1 - slow_RKC[slow_s]
-        else
-            γ = slow_RKC[slow_s + 1] - slow_RKC[slow_s]
-        end
-
-        # RKB for the slow with fractional time factor remove (since full
-        # integration of fast will result in scaling by γ)
-        slow_δ = slow_RKB[slow_s] / γ
-        nsubsteps = fast_dt_in > 0 ? ceil(Int, γ * dt / fast_dt_in) : 1
-        fast_dt = γ * dt / nsubsteps
-
-        updatedt!(fast, fast_dt)
-
-        for substep in 1:nsubsteps
-            slow_rka = nothing
-            if substep == nsubsteps
-                slow_rka = slow_RKA_explicit[slow_s % length(slow_RKA_explicit) + 1]
-            end
-            fast_time = slow_stage_time + (substep - 1) * fast_dt
-            dostep!(Q, fast, param, fast_time, slow_δ, slow_rv_Rstages, slow_rka)
         end
     end
 
-    # if slow_split_explicit_implicit
-    #     for istage in 1:Nouter_stages
-    #         stagetime = time + RKC[istage] * dt
-    #         slow_rhs_implicit!(
-    #             slow_Rstages[istage],
-    #             slow_Qstages[istage],
-    #             param,
-    #             slow_stage_time,
-    #             increment = true,
-    #         )
-    #     end
-    # end
-    #
-    # # compose the final solution
-    # event = Event(device(Q))
-    # event = solution_update!(device(Q), groupsize)(
-    #     slow.variant,
-    #     slow_rv_Q,
-    #     slow_rv_Rstages,
-    #     slow_RKB,
-    #     dt,
-    #     Val(Nouter_stages),
-    #     in_slow_δ,
-    #     in_slow_rv_dQ,
-    #     in_slow_scaling;
-    #     ndrange = length(slow_rv_Q),
-    #     dependencies = (event,),
-    # )
-    # wait(device(Q), event)
-    updatedt!(fast, fast_dt_in)
-end
-
-@kernel function stage_mrrk_update!(
-    ::LowStorageVariant,
-    Q,
-    Qstages,
-    Rstages,
-    Qhat,
-    Qtt,
-    RKA_explicit,
-    RKA_implicit,
-    dt,
-    ::Val{is},
-    ::Val{split_explicit_implicit},
-    slow_δ,
-    slow_dQ,
-) where {is, split_explicit_implicit}
-    i = @index(Global, Linear)
-    @inbounds begin
-        Qhat_i = Q[i]
-        Qstages_is_i = -zero(eltype(Q))
-
-        if slow_δ !== nothing
-            Rstages[is - 1][i] += slow_δ * slow_dQ[i]
-        end
-
-        @unroll for js in 1:(is - 1)
-            if split_explicit_implicit
-                rkcoeff = RKA_implicit[is, js] / RKA_implicit[is, is]
-            else
-                rkcoeff =
-                    (RKA_implicit[is, js] - RKA_explicit[is, js]) /
-                    RKA_implicit[is, is]
-            end
-            commonterm = rkcoeff * Qstages[js][i]
-            Qhat_i += commonterm + dt * RKA_explicit[is, js] * Rstages[js][i]
-            Qstages_is_i -= commonterm
-        end
-        Qstages[is][i] = Qstages_is_i
-        Qhat[i] = Qhat_i
-        Qtt[i] = Qhat_i
-    end
+    # compose the final solution
+    event = Event(device(Q))
+    event = solution_update!(device(Q), groupsize)(
+        variant,
+        rv_Q,
+        rv_outerRstages,
+        outer_RKB,
+        dt,
+        Val(Nstages),
+        in_slow_δ,
+        in_slow_rv_dQ,
+        in_slow_scaling;
+        ndrange = length(rv_Q),
+        dependencies = (event,),
+    )
+    wait(device(Q), event)
 end
