8078ea08b322c2959c29f0409b3c01546f58843d
Jeremy E Kozdon <jekozdon@nps.edu>
2020-05-11 20:35:05 +0000

    Pass keyword remainder model arguments in Driver

--- src/Driver/solver_configs.jl
+++ src/Driver/solver_configs.jl
@@ -185,7 +185,13 @@ function SolverConfiguration(
             state_gradient_flux = dg.state_gradient_flux,
             states_higher_order = dg.states_higher_order,
         )
-        slow_dg = remainder_DGModel(dg, (fast_dg,); ode_solver_type.kwargs...)
+        remainder_kwargs = ode_solver_type.discrete_splitting ? NamedTuple() :
+            (
+            numerical_flux_first_order = numerical_flux_first_order,
+            numerical_flux_second_order = numerical_flux_second_order,
+            numerical_flux_gradient = numerical_flux_gradient,
+        )
+        slow_dg = remainder_DGModel(dg, (fast_dg,); remainder_kwargs...)
         slow_solver = ode_solver_type.slow_method(slow_dg, Q; dt = ode_dt)
         fast_dt = ode_dt / ode_solver_type.timestep_ratio
         fast_solver = ode_solver_type.fast_method(fast_dg, Q; dt = fast_dt)
