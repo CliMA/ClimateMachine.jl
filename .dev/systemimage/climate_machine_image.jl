18cb64344e71d9bbb30b72db46d3c9adc4a5fe19
Jeremy E Kozdon <jekozdon@nps.edu>
2020-05-12 19:27:20 +0000

    Add to partial test of system image script to CI

--- .dev/systemimage/climate_machine_image.jl
+++ .dev/systemimage/climate_machine_image.jl
@@ -17,17 +17,22 @@ Pkg.instantiate()
 pkgs = Pkg.installed()
 delete!(pkgs, "Pkg")
 
-PackageCompiler.create_sysimage(
-    [Symbol(s) for s in keys(pkgs)];
-    sysimage_path = sysimage_path,
-    precompile_execution_file = joinpath(
-        @__DIR__,
-        "..",
-        "..",
-        "test",
-        "Numerics",
-        "DGmethods",
-        "Euler",
-        "isentropicvortex.jl",
-    ),
-)
+# "TF_BUILD" is check to see if we are running on Azure for CI
+if !haskey(ENV, "TF_BUILD")
+    PackageCompiler.create_sysimage(
+        [Symbol(s) for s in keys(pkgs)];
+        sysimage_path = sysimage_path,
+        precompile_execution_file = joinpath(
+            @__DIR__,
+            "..",
+            "..",
+            "test",
+            "Numerics",
+            "DGmethods",
+            "Euler",
+            "isentropicvortex.jl",
+        ),
+    )
+else
+    return true
+end
