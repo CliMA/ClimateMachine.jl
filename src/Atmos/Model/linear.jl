bfe309caab3d995f69da5f2112aea213ca308ee2
Jeremy E Kozdon <jekozdon@nps.edu>
2020-05-15 15:07:10 -0700

    Adding UpwindNumericalFlux to atmos model
    By using the upwind numerical flux in the linear atmos model continuity
    of only the vector components that are in the direction of the face will
    be penalized. This will allow for the future possibility of rotating the
    linear system to a more minimal size for vertical solves.

--- src/Atmos/Model/linear.jl
+++ src/Atmos/Model/linear.jl
@@ -1,4 +1,6 @@
 using CLIMAParameters.Planet: R_d, cv_d, T_0, e_int_v0, e_int_i0
+import ..DGmethods.NumericalFluxes:
+    numerical_flux_first_order!, UpwindNumericalFlux
 
 """
     linearized_air_pressure(ρ, ρe_tot, ρe_pot, ρq_tot=0, ρq_liq=0, ρq_ice=0)
@@ -36,30 +38,44 @@ function linearized_air_pressure(
 end
 
 @inline function linearized_pressure(
-    ::DryModel,
+    moisture::MoistureModel,
     param_set::AbstractParameterSet,
     orientation::Orientation,
     state::Vars,
     aux::Vars,
 )
-    ρe_pot = state.ρ * gravitational_potential(orientation, aux)
-    return linearized_air_pressure(param_set, state.ρ, state.ρe, ρe_pot)
+    linearized_pressure(
+        moisture,
+        param_set,
+        orientation,
+        state.ρ,
+        state.ρe,
+        aux,
+    )
+end
+
+# These two versions are used to pick out terms for the upwind flux below
+@inline function linearized_pressure(
+    ::DryModel,
+    param_set::AbstractParameterSet,
+    orientation::Orientation,
+    ρ::T,
+    ρe::T,
+    aux::Vars,
+) where {T}
+    ρe_pot = ρ * gravitational_potential(orientation, aux)
+    return linearized_air_pressure(param_set, ρ, ρe, ρe_pot)
 end
 @inline function linearized_pressure(
     ::EquilMoist,
     param_set::AbstractParameterSet,
     orientation::Orientation,
-    state::Vars,
+    ρ::T,
+    ρe::T,
     aux::Vars,
-)
-    ρe_pot = state.ρ * gravitational_potential(orientation, aux)
-    linearized_air_pressure(
-        param_set,
-        state.ρ,
-        state.ρe,
-        ρe_pot,
-        aux.ref_state.ρq_tot,
-    )
+) where {T}
+    ρe_pot = ρ * gravitational_potential(orientation, aux)
+    linearized_air_pressure(param_set, ρ, ρe, ρe_pot, aux.ref_state.ρq_tot)
 end
 
 abstract type AtmosLinearModel <: BalanceLaw end
@@ -237,4 +253,107 @@ function source!(
         source.ρu -= state.ρ * ∇Φ
     end
     nothing
+end
+
+
+# Note: this assumes that the reference state is continuous across element
+# boundaries
+function numerical_flux_first_order!(
+    ::UpwindNumericalFlux,
+    balance_law::Union{
+        AtmosAcousticLinearModel,
+        AtmosAcousticGravityLinearModel,
+    },
+    fluxᵀn::Vars{S},
+    normal_vector::SVector,
+    state_conservative⁻::Vars{S},
+    state_auxiliary⁻::Vars{A},
+    state_conservative⁺::Vars{S},
+    state_auxiliary⁺::Vars{A},
+    t,
+) where {S, A}
+
+    FT = eltype(state_conservative⁻)
+
+    # Coefficients for the flux matrix
+    param_set = balance_law.atmos.param_set
+
+    # Query pressure contribution for each component
+    α = linearized_pressure(
+        balance_law.atmos.moisture,
+        balance_law.atmos.param_set,
+        balance_law.atmos.orientation,
+        one(FT),
+        -zero(FT),
+        state_auxiliary⁻,
+    )
+    β = linearized_pressure(
+        balance_law.atmos.moisture,
+        balance_law.atmos.param_set,
+        balance_law.atmos.orientation,
+        -zero(FT),
+        one(FT),
+        state_auxiliary⁻,
+    )
+
+    e_pot =
+        gravitational_potential(balance_law.atmos.orientation, state_auxiliary⁻)
+    ref = state_auxiliary⁻.ref_state
+    γ = balance_law isa AtmosAcousticLinearModel ?
+        (ref.ρe + ref.p) / ref.ρ - e_pot : (ref.ρe + ref.p) / ref.ρ
+
+    # wave speed
+    λ = sqrt(β * γ + α)
+
+    #=
+    #! format: off
+    # Matrix for the flux is:
+    n = normal_vector
+    Tn = [1 0    0     0   0;
+          0 n[1] n[2] n[3] 0;
+          0 0    0    0    1]
+
+    B = [0 1 0;
+         α 0 β;
+         0 γ 0]
+
+    An = Tn' * B * Tn
+
+    # The upwinding is based on the following eigenvalue decomposition of B
+    V = [-1  β 1;
+          λ  0 λ;
+         -γ -α γ]
+
+    W = [-α / (2α + 2γ * β)   1 / 2λ  -β / (2α + 2γ * β);
+         2γ / (2α + 2γ * β)   0       -2 / (2α + 2γ * β);
+          α / (2α + 2γ * β)   1 / 2λ   β / (2α + 2γ * β)]
+
+    @assert B ≈ V * Diagonal([-λ, 0, λ]) * W
+    #! format: on
+    =#
+
+    # rotated state vector based on outward normal
+    ρ⁻ = state_conservative⁻.ρ
+    ρu⁻ = dot(normal_vector, state_conservative⁻.ρu)
+    ρe⁻ = state_conservative⁻.ρe
+    ρ⁺ = state_conservative⁺.ρ
+    ρu⁺ = dot(normal_vector, state_conservative⁺.ρu')
+    ρe⁺ = state_conservative⁺.ρe
+
+    # Left eigenvector entries
+    δ1 = -α / (2α + 2γ * β)
+    δ2 = 1 / 2λ
+    δ3 = -β / (2α + 2γ * β)
+
+    # incoming wave
+    ω⁺ = -λ * (δ1 * ρ⁺ + δ2 * ρu⁺ + δ3 * ρe⁺)
+
+    # outgoing wave
+    ω⁻ = +λ * (-δ1 * ρ⁻ + δ2 * ρu⁻ - δ3 * ρe⁻)
+
+    # compute the upwind flux using the right eigenvectors and rotate back based
+    # on the outward normal
+    fluxᵀn.ρ = ω⁻ - ω⁺
+    fluxᵀn.ρu = λ * (ω⁻ + ω⁺) * normal_vector
+    fluxᵀn.ρe = γ * (ω⁻ - ω⁺)
 end
