4e3c82a76c680e8a57b80ef2af5ac4e631acac27
christophernhill <christophernhill@gmail.com>
2020-05-17 22:26:51 -0400

    Add quick debug/regression statistics on raw state

--- src/Common/ConfigTypes/ConfigTypes.jl
+++ src/Common/ConfigTypes/ConfigTypes.jl
@@ -6,15 +6,11 @@ Module containing ClimateMachine configuration types.
 module ConfigTypes
 
 export ClimateMachineConfigType,
-    AtmosLESConfigType,
-    AtmosGCMConfigType,
-    OceanBoxGCMConfigType,
-    SingleStackConfigType
+    AtmosLESConfigType, AtmosGCMConfigType, OceanBoxGCMConfigType
 
 abstract type ClimateMachineConfigType end
 struct AtmosLESConfigType <: ClimateMachineConfigType end
 struct AtmosGCMConfigType <: ClimateMachineConfigType end
 struct OceanBoxGCMConfigType <: ClimateMachineConfigType end
-struct SingleStackConfigType <: ClimateMachineConfigType end
 
 end
