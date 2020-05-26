f435e9cf49a1abeb6e8e45f6177d97dcc175b255
Brandon Allen <ballen@mit.edu>
2020-05-26 13:05:11 -0400

    Revert PR #1142

--- src/Ocean/HydrostaticBoussinesq/SimpleBoxProblem.jl
+++ src/Ocean/HydrostaticBoussinesq/SimpleBoxProblem.jl
@@ -48,6 +48,11 @@ function ocean_init_aux!(m::HBModel, p::AbstractSimpleBoxProblem, A, geom)
     FT = eltype(A)
     @inbounds A.y = geom.coord[2]
 
+    # needed for proper CFL condition calculation
+    A.w = 0
+    A.pkin = 0
+    A.wz0 = 0
+
     return nothing
 end
 
