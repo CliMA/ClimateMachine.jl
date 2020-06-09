27d889d09c2440d53110103ae84395aad823fdd8
Thomas Gibson <thomas.gibson@nps.edu>
2020-06-08 18:59:52 -0700

    Store initial perturbation and fixup expressions

--- src/Atmos/Model/AtmosModel.jl
+++ src/Atmos/Model/AtmosModel.jl
@@ -289,6 +289,7 @@ function vars_state_auxiliary(m::AtmosModel, FT)
         ∫dnz::vars_reverse_integrals(m, FT)
         coord::SVector{3, FT}
         θ₀::FT
+        θ′::FT
         orientation::vars_state_auxiliary(m.orientation, FT)
         ref_state::vars_state_auxiliary(m.ref_state, FT)
         turbulence::vars_state_auxiliary(m.turbulence, FT)
@@ -596,6 +597,7 @@ Store Cartesian coordinate information in `aux.coord`.
 """ init_state_auxiliary!
 function init_state_auxiliary!(m::AtmosModel, aux::Vars, geom::LocalGeometry)
     aux.coord = geom.coord
+    aux.θ′ = eltype(aux)(0.0)
     aux.θ₀ = eltype(aux)(0.0)
     init_aux!(m.orientation, m.param_set, aux)
     atmos_init_aux!(m.ref_state, m, aux, geom)
