ad5412de56c30497454eff02f5b9882f8c68360f
Brandon Allen <ballen@mit.edu>
2020-06-09 22:01:55 -0400

    add hydrostatic test case.
    add analytic solution to compare against.
    
    add to slurm CI
    
    explicitly using `euclidean_distance`
    
    Add reference values using state_check
    
    bug fix
    
    lower precisions
    
    add test to azure / github actions
    
    add param set to test

--- test/Ocean/runtests.jl
+++ test/Ocean/runtests.jl
@@ -3,6 +3,10 @@ include("../testhelpers.jl")
 
 @testset "Ocean" begin
     runmpi(joinpath(@__DIR__, "HydrostaticBoussinesq/test_divergence_free.jl"))
+    runmpi(joinpath(
+        @__DIR__,
+        "../../experiments/OceanBoxGCM/hydrostatic_spindown.jl",
+    ))
     runmpi(joinpath(@__DIR__, "HydrostaticBoussinesq/test_ocean_gyre.jl"))
     # runmpi(joinpath(@__DIR__,"shallow_water/GyreDriver.jl"))
 end
