25ffe6a73164f68e668167e2f9fbaa024f0c4198
bischtob <tobias.bischoff@pm.me>
2020-06-11 11:50:49 -0700

    sri fix

--- src/Common/Orientations/Orientations.jl
+++ src/Common/Orientations/Orientations.jl
@@ -151,9 +151,9 @@ function sphr_to_cart_vec(
     vec::AbstractVector,
     aux::Vars,
 )
-    FT = eltype(state)
+    FT = eltype(aux)
     lat = latitude(orientation, aux)
-    long = longitute(orientation, aux)
+    lon = longitude(orientation, aux)
 
     slat, clat = sin(lat), cos(lat)
     slon, clon = sin(lon), cos(lon)
@@ -177,9 +177,9 @@ function cart_to_sphr_vec(
     vec::AbstractVector,
     aux::Vars,
 )
-    FT = eltype(state)
+    FT = eltype(aux)
     lat = latitude(orientation, aux)
-    long = longitute(orientation, aux)
+    lon = longitude(orientation, aux)
 
     slat, clat = sin(lat), cos(lat)
     slon, clon = sin(lon), cos(lon)
