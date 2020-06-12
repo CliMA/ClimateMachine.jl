b2d8a57633843658583a5cbd8e1d4e636b731e47
Thomas Gibson <thomas.gibson@nps.edu>
2020-06-11 18:16:47 -0700

    Some more notes

--- src/Common/Equations/Equations.jl
+++ src/Common/Equations/Equations.jl
@@ -12,8 +12,8 @@ abstract type AbstractTerm end
 """
 Sample equation:
 
-∂ₜ q = S(q) - ∇⋅(F_1(q)) - ∇⋅(F_2(q, σ)),
-   σ = Σ(∇q)
+∂ₜ q = S(q) - ∇⋅(F_1(q)) - ∇⋅(F_2(q, σ)) + ...,
+   σ = Σ(∇q, ...)
 
 q - state (ρ, ρu, ρe)
 F_1 - First order (advective) flux of q
@@ -21,19 +21,18 @@ F_2 - Second order (diffusive) flux of q
 S - source
 """
 # Field Signature
-struct Signature{𝒮, 𝒯, 𝒰, 𝒱} <: AbstractFruitSignature
+struct Signature{𝒮, 𝒯, 𝒰, 𝒱} <: AbstractSignature
     time_scale::𝒮
     domain_space::𝒯
     range_space::𝒰
     model::𝒱
 end
 
-
 # What we want:
 """
-dt(Q)
+∂t(Q)
 """
-function dt(Q, ...)
+function ∂t(Q, ...)
     return Tendency(Q, ...)
 end
 
@@ -42,6 +41,8 @@ end
 """
 struct Tendency{L} <: AbstractTerm
     label::L
+    ...
+    ...
     function Tendency(Q, ...)
         ...
     end
@@ -119,5 +120,80 @@ S = 1/2 (∇u + ∇u^t)
 
 # Think about solving equations with linear/nonlinear algebraic constraints
 # (see split-explicit equations by Andre and Brandon)
+
+
+abstract type PrognosticQuantity <: AbstractField end
+
+q = PrognosticQuantity()
+
+struct Mass <: PrognosticQuantity end
+struct Momentum <: PrognosticQuantity end
+
+struct Divergence <: DifferentialTerm
+    operand
+end
+struct Gradient <: DifferentialTerm
+    operand
+end
+
+# define operators
+struct Grad end
+const ∇ = Grad()
+(::Grad)(operand) = Gradient(operand)
+(⋅)(::Grad, operand) = Divergence(operand)
+
+struct TermSum <: AbstractTerm
+    operands
+end
+Base.(:+)(t::AbstractTerm...) = TermSum(t)
+
+linearization(o::AbstractTerm) = o
+
+islinear(::PrognosticQuantity) = true
+islinear(d::Divergence) = islinear(d.operand)
+islinear(d::Gradient) = islinear(d.operand)
+islinear(d::TermSum) = all(islinear, d.operands)
+
+isvertical(::Momentum) = false
+isvertical(::VericalProjection) = true
+
+struct Pressure <: DiagnosticQuantity
+end
+
+islinear(::Pressure) = false
+
+const ρ = Mass()
+const ρu = Momentum()
+
+u = ρu / ρ
+p = Pressure()
+
+∂t(ρ) ~ ∇ ⋅ ρu + s(ρ)
+S ~ (∇(u) + ∇(u)')/2
+τ = -2*ν .* S
+
+ρu_euler = ∇⋅(u ⊗ ρu + p * I)
+ρu_diffusive = ∇⋅(ρ * τ)
+
+
+abstractmodel = ∂t(ρu) ~  ∇⋅(u ⊗ ρu + p * I) + ∇⋅(ρ * τ)
+
+"""
+Idea:
+
+Lowering from Continuum to fully discrete (3 stages):
+
+Continuum -T_1-> semi-discrete (temporally) -T_2-> fully discrete (full DG model)
+
+"""
+
+
+# challenges
+# - how to "name" subexpressions
+#   - numerical fluxes
+#   - boundary conditions
+#   - time rates
+#   - Computational performance:
+#     - communication/computation (fluxes!)
 
 end
