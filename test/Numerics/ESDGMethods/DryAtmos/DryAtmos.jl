4a5858db59a2f7fb9445b8b4f1f540a1a1972f8a
Jeremy E Kozdon <jekozdon@nps.edu>
2020-06-27 23:19:55 +0000

    Add entropy conservative volume flux to DryAtmos
    Adds the two-point volume flux for the DryAtmosModel as well as adds a
    test to make sure the shuffle is satisfied.
    
    Co-authored-by: Thomas Gibson <thomas.gibson@nps.edu>

--- test/Numerics/ESDGMethods/DryAtmos/DryAtmos.jl
+++ test/Numerics/ESDGMethods/DryAtmos/DryAtmos.jl
@@ -7,7 +7,13 @@ import ClimateMachine.BalanceLaws:
     state_to_entropy_variables!,
     entropy_variables_to_state!
 using StaticArrays: SVector
-using LinearAlgebra: dot
+using LinearAlgebra: dot, I
+import ClimateMachine.DGMethods.NumericalFluxes:
+    EntropyConservative,
+    numerical_volume_conservative_flux_first_order!,
+    numerical_volume_fluctuation_flux_first_order!,
+    ave,
+    logave
 
 struct DryAtmosModel <: BalanceLaw end
 
@@ -134,4 +140,71 @@ function entropy_variables_to_state!(
     state.ρu = ρu
     state.ρe = ρe
     aux.Φ = Φ
+end
+
+function numerical_volume_conservative_flux_first_order!(
+    ::EntropyConservative,
+    ::DryAtmosModel,
+    F::Grad,
+    state_1::Vars,
+    aux_1::Vars,
+    state_2::Vars,
+    aux_2::Vars,
+)
+    FT = eltype(F)
+    ρ_1, ρu_1, ρe_1 = state_1.ρ, state_1.ρu, state_1.ρe
+    ρ_2, ρu_2, ρe_2 = state_2.ρ, state_2.ρu, state_2.ρe
+    Φ_1, Φ_2 = aux_1.Φ, aux_2.Φ
+    u_1 = ρu_1 / ρ_1
+    u_2 = ρu_2 / ρ_2
+    p_1 = pressure(ρ_1, ρu_1, ρe_1, Φ_1)
+    p_2 = pressure(ρ_2, ρu_2, ρe_2, Φ_2)
+    b_1 = ρ_1 / 2p_1
+    b_2 = ρ_2 / 2p_2
+
+    ρ_avg = ave(ρ_1, ρ_2)
+    u_avg = ave(u_1, u_2)
+    b_avg = ave(b_1, b_2)
+    Φ_avg = ave(Φ_1, Φ_2)
+
+    usq_avg = ave(dot(u_1, u_1), dot(u_2, u_2))
+
+    ρ_log = logave(ρ_1, ρ_2)
+    b_log = logave(b_1, b_2)
+    α = b_avg * ρ_log / 2b_1
+
+    γ = FT(_γ)
+
+    Fρ = u_avg * ρ_log
+    Fρu = u_avg * Fρ' + ρ_avg / 2b_avg * I
+    Fρe = (1 / (2 * (γ - 1) * b_log) - usq_avg / 2 + Φ_avg) * Fρ + Fρu * u_avg
+
+    F.ρ += Fρ
+    F.ρu += Fρu
+    F.ρe += Fρe
+end
+
+function numerical_volume_fluctuation_flux_first_order!(
+    ::EntropyConservative,
+    ::DryAtmosModel,
+    D::Grad,
+    state_1::Vars,
+    aux_1::Vars,
+    state_2::Vars,
+    aux_2::Vars,
+)
+    FT = eltype(D)
+    ρ_1, ρu_1, ρe_1 = state_1.ρ, state_1.ρu, state_1.ρe
+    ρ_2, ρu_2, ρe_2 = state_2.ρ, state_2.ρu, state_2.ρe
+    Φ_1, Φ_2 = aux_1.Φ, aux_2.Φ
+    p_1 = pressure(ρ_1, ρu_1, ρe_1, Φ_1)
+    p_2 = pressure(ρ_2, ρu_2, ρe_2, Φ_2)
+    b_1 = ρ_1 / 2p_1
+    b_2 = ρ_2 / 2p_2
+
+    ρ_log = logave(ρ_1, ρ_2)
+    b_avg = ave(b_1, b_2)
+    α = b_avg * ρ_log / 2b_1
+
+    D.ρu -= α * (Φ_1 - Φ_2) * I
 end
--- test/Numerics/ESDGMethods/DryAtmos/runtests.jl
+++ test/Numerics/ESDGMethods/DryAtmos/runtests.jl
@@ -1,8 +1,13 @@
 using Test
 using ClimateMachine.BalanceLaws:
     number_state_conservative, number_state_auxiliary, number_state_entropy
+using ClimateMachine.DGMethods.NumericalFluxes:
+    numerical_volume_flux_first_order!
+using StaticArrays: MArray
 using Random
-Random.seed!(8)
+using DoubleFloats
+
+Random.seed!(7)
 
 include("DryAtmos.jl")
 
@@ -13,7 +18,7 @@ let
     num_entropy = number_state_entropy(model)
 
     @testset "state to entropy variable transforms" begin
-        for FT in (BigFloat, Float32, Float64)
+        for FT in (Float32, Float64, Double64, BigFloat)
             state_in =
                 [1, 2, 2, 2, 1] .* rand(FT, num_state) + [3, -1, -1, -1, 100]
             aux_in = rand(FT, num_aux)
@@ -26,6 +31,62 @@ let
 
             @test all(state_in .≈ state_out)
             @test all(aux_in .≈ aux_out)
+        end
+    end
+
+    @testset "test numerical flux for Tadmor shuffle" begin
+        # Vars doesn't work with BigFloat so we will use Double64
+        for FT in (Float32, Float64, Double64)
+            # Create some random states
+            state_1 =
+                [1, 2, 2, 2, 1] .* rand(FT, num_state) + [3, -1, -1, -1, 100]
+            aux_1 = 0 * rand(FT, num_aux)
+
+            state_2 =
+                [1, 2, 2, 2, 1] .* rand(FT, num_state) + [3, -1, -1, -1, 100]
+            aux_2 = 0 * rand(FT, num_aux)
+
+            # Get the entropy variables for the two states
+            entropy_1 = similar(state_1, num_entropy)
+            state_to_entropy_variables!(model, entropy_1, state_1, aux_1)
+
+            entropy_2 = similar(state_1, num_entropy)
+            state_to_entropy_variables!(model, entropy_2, state_2, aux_2)
+
+            # Get the values of Ψ_j = β^T f_j - ζ_j = ρu_j where β is the
+            # entropy variables, f_j is the conservative flux, and ζ_j is the
+            # entropy flux. For conservation laws this is the entropy potential.
+            Ψ_1 = Vars{vars_state_conservative(model, FT)}(state_1).ρu
+            Ψ_2 = Vars{vars_state_conservative(model, FT)}(state_2).ρu
+
+            # Evaluate the flux with both orders of the two states
+            H_12 = fill!(MArray{Tuple{3, num_state}, FT}(undef), -zero(FT))
+            numerical_volume_flux_first_order!(
+                EntropyConservative(),
+                model,
+                H_12,
+                state_1,
+                aux_1,
+                state_2,
+                aux_2,
+            )
+
+            H_21 = fill!(MArray{Tuple{3, num_state}, FT}(undef), -zero(FT))
+            numerical_volume_flux_first_order!(
+                EntropyConservative(),
+                model,
+                H_21,
+                state_2,
+                aux_2,
+                state_1,
+                aux_1,
+            )
+
+            # Check that we satisfy the Tadmor shuffle
+            @test all(
+                H_12 * entropy_1[1:num_state] - H_21 * entropy_2[1:num_state] .≈
+                Ψ_1 - Ψ_2,
+            )
         end
     end
 end
