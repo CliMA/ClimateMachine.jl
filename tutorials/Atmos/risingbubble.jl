647430ac56e276e08f374e61f28de3614c033134
asraero <asridhar@caltech.edu>
2020-07-02 15:04:33 -0700

    Update name 	modified:   experiments/AtmosGCM/heldsuarez.jl 	modified:   tutorials/Atmos/heldsuarez.jl 	modified:   tutorials/Atmos/risingbubble.jl

--- tutorials/Atmos/risingbubble.jl
+++ tutorials/Atmos/risingbubble.jl
@@ -233,7 +233,7 @@ function config_risingbubble(FT, N, resolution, xmax, ymax, zmax)
         param_set;                                    # Parameter set corresponding to earth parameters
         turbulence = SmagorinskyLilly(_C_smag),       # Turbulence closure model
         moisture = DryModel(),                        # Exclude moisture variables
-        hyperdiffusion = StandardHyperDiffusion(60),  # Hyperdiffusion (4th order) model
+        hyperdiffusion = BiharmonicHyperDiffusion(60),  # Hyperdiffusion (4th order) model
         source = (Gravity(),),                        # Gravity is the only source term here
         tracers = NTracers{ntracers, FT}(δ_χ),        # Tracer model with diffusivity coefficients
         ref_state = ref_state,                        # Reference state
