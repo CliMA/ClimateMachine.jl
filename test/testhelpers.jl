946f05f745c8eb47d232681c0151e69847b6c875
jakebolewski <jakebolewski@gmail.com>
2020-07-20 11:41:53 -0600

    workaround for osx mpich issue with forcing localhost

--- test/testhelpers.jl
+++ test/testhelpers.jl
@@ -1,6 +1,19 @@
 using MPI
 
-function runmpi(file; ntasks = 1)
+function runmpi(file; ntasks = 1, localhost = false)
+    localhostenv = try
+        parse(Bool, get(ENV, "CLIMATEMACHINE_TEST_RUNMPI_LOCALHOST", "false"))
+    catch
+        false
+    end
+    # Force mpiexec to exec on the localhost node
+    # TODO: MicrosoftMPI mpiexec has issues if mpiexec.exe is in a different 
+    # folder as the MPI script to run so ignore for now.
+    if (localhost || localhostenv) && (MPI.MPI_LIBRARY != MPI.MicrosoftMPI)
+        localhostonly = `-host localhost`
+    else
+        localhostonly = ``
+    end
     # by default some mpi runtimes will
     # complain if more resources (processes)
     # are requested than available on the node
@@ -14,7 +27,7 @@ function runmpi(file; ntasks = 1)
     #   Balance Law Solver | No tests
     # since external tests are not returned as passed/fail
     @time @test mpiexec() do cmd
-        run(`$cmd $oversubscribe -n $ntasks $(Base.julia_cmd()) --startup-file=no --project=$(Base.active_project()) $file`)
+        run(`$cmd $localhostonly $oversubscribe -np $ntasks $(Base.julia_cmd()) --startup-file=no --project=$(Base.active_project()) $file`)
         true
     end
 end
