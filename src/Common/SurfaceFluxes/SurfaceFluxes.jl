0e5abb6248636b960b97433dd7ec40878bc087f5
asraero <asridhar@caltech.edu>
2020-08-17 13:15:32 -0700

    Isolate unit tests for GPU demo 	modified:   src/Common/SurfaceFluxes/SurfaceFluxes.jl 	new file:   test/Common/SurfaceFluxes/test-profiles.jl
    Isolate unit tests

--- src/Common/SurfaceFluxes/SurfaceFluxes.jl
+++ src/Common/SurfaceFluxes/SurfaceFluxes.jl
@@ -22,37 +22,36 @@
   publisher={Wiley Online Library}
 }
 
-@article{byun1990analytical,
-  title={On the analytical solutions of flux-profile relationships for the atmospheric surface layer},
-  author={Byun, Daewon W},
-  journal={Journal of Applied Meteorology},
-  volume={29},
-  number={7},
-  pages={652--657},
-  year={1990}
-}
 """
 module SurfaceFluxes
 
-using ..Thermodynamics
-using KernelAbstractions: @print
-using DocStringExtensions
+using RootSolvers
 using NLsolve
-using CLIMAParameters: AbstractParameterSet
-using CLIMAParameters
+
+using ..Thermodynamics
+using DocStringExtensions
+using CLIMAParameters: AbstractEarthParameterSet
 using CLIMAParameters.Planet: molmass_ratio, grav
 using CLIMAParameters.SubgridScale: von_karman_const
+using StaticArrays
 
-const APS = AbstractParameterSet
 abstract type SurfaceFluxesModel end
 
 struct Momentum end
 struct Heat end
 
-export surface_conditions
+export surface_conditions, 
+    compute_R_z0,
+    compute_exchange_coefficients,
+    compute_f_h,
+    compute_f_m,
+    compute_Psi,
+    compute_psi,
+    Momentum,
+    Heat
 
 """
-    SurfaceFluxConditions{FT, VFT}
+    SurfaceFluxConditions{FT}
 
 Surface flux conditions, returned from `surface_conditions`.
 
@@ -60,17 +59,26 @@ Surface flux conditions, returned from `surface_conditions`.
 
 $(DocStringExtensions.FIELDS)
 """
-struct SurfaceFluxConditions{FT, VFT}
+struct SurfaceFluxConditions{FT}
     L_MO::FT
-    pottemp_flux_star::FT
-    flux::VFT
-    x_star::VFT
-    K_exchange::VFT
+    VDSE_flux_star::FT
+    flux::AbstractVector
+    x_star::AbstractVector
+    K_exchange::AbstractVector
+end
+
+function Base.show(io::IO, sfc::SurfaceFluxConditions)
+    println(io, "----------------------- SurfaceFluxConditions")
+    println(io, "L_MO              = ", sfc.L_MO)
+    println(io, "VDSE_flux_star = ", sfc.VDSE_flux_star)
+    println(io, "flux              = ", sfc.flux)
+    println(io, "x_star            = ", sfc.x_star)
+    println(io, "K_exchange        = ", sfc.K_exchange)
+    println(io, "-----------------------")
 end
 
 """
     surface_conditions
-
 Surface conditions given
  - `x_initial` initial guess for solution (`L_MO, u_star, θ_star, ϕ_star, ...`)
  - `x_ave` volume-averaged value for variable `x`
@@ -82,29 +90,30 @@ Surface conditions given
       - Heat: Turbulent Prantl number at neutral stratification
       - Mass: Turbulent Schmidt number
       - ...
- - `θ_bar` basic potential temperature
+ - `VDSE_scale` basic potential temperature
  - `Δz` layer thickness (not spatial discretization)
  - `z` coordinate axis
  - `a` free model parameter with prescribed value of 4.7
- - `pottemp_flux_given` potential temperature flux (optional)
+ - `VDSE_flux_star_given` potential temperature flux (optional)
 
-If `pottemp_flux` is not given, then it is computed by iteration
+If `VDSE_flux_star` is not given, then it is computed by iteration
 of equations 3, 17, and 18 in Nishizawa2018.
 """
 function surface_conditions(
-    param_set::APS,
-    x_initial::Vector{FT},
-    x_ave::Vector{FT},
-    x_s::Vector{FT},
-    z_0::Vector{FT},
-    F_exchange::Vector{FT},
-    dimensionless_number::Vector{FT},
-    θ_bar::FT,
+    param_set::AbstractEarthParameterSet,
+    x_initial::AbstractVector,
+    x_ave::AbstractVector,
+    x_s::AbstractVector,
+    z_0::AbstractVector,
+    F_exchange::AbstractVector,
+    dimensionless_number::AbstractVector,
+    VDSE_scale::FT,
+    qt_bar::FT,
     Δz::FT,
     z::FT,
     a::FT,
-    pottemp_flux_given::Union{Nothing, FT} = nothing,
-) where {FT <: AbstractFloat, APS <: AbstractParameterSet}
+    VDSE_flux_star_given::Union{Nothing, FT} = nothing,
+) where {FT <: AbstractFloat, AbstractEarthParameterSet}
 
     n_vars = length(x_initial) - 1
     @assert length(x_initial) == n_vars + 1
@@ -114,13 +123,15 @@ function surface_conditions(
     @assert length(F_exchange) == n_vars
     @assert length(dimensionless_number) == n_vars
     local sol
+    u, θ, qt = x_initial[1], x_initial[2], x_initial[3]
     let param_set = param_set
         function f!(F, x_all)
             L_MO, x_vec = x_all[1], x_all[2:end]
             u, θ = x_vec[1], x_vec[2]
-            pottemp_flux =
-                pottemp_flux_given == nothing ? -u * θ : pottemp_flux_given
-            F[1] = L_MO - monin_obukhov_len(param_set, u, θ_bar, pottemp_flux)
+            VDSE_flux_star =
+                VDSE_flux_star_given == nothing ? -u * θ : VDSE_flux_star_given
+            flux = VDSE_flux_star
+            F[1] = L_MO - monin_obukhov_length(param_set, u, VDSE_scale, flux)
             for i in 1:n_vars
                 ϕ = x_vec[i]
                 transport = i == 1 ? Momentum() : Heat()
@@ -131,8 +142,8 @@ function surface_conditions(
                         u,
                         Δz,
                         a,
-                        θ_bar,
-                        pottemp_flux,
+                        VDSE_scale,
+                        flux,
                         z_0[i],
                         x_ave[i],
                         x_s[i],
@@ -144,47 +155,68 @@ function surface_conditions(
         sol = nlsolve(f!, x_initial, autodiff = :forward)
     end
     if converged(sol)
-        L_MO, x_star = sol.zero[1], sol.zero[2:end]
+        L_MO, x_star = sol.zero[1], collect(sol.zero[2:end])
         u_star, θ_star = x_star[1], x_star[2]
     else
+        #println("Warning: Unconverged Surface Fluxes")
         L_MO, x_star = sol.zero[1], sol.zero[2:end]
         u_star, θ_star = x_star[1], x_star[2]
-        @print("Non-converged surface fluxes")
     end
 
     _grav::FT = grav(param_set)
     _von_karman_const::FT = von_karman_const(param_set)
-    pottemp_flux_star = -u_star^3 * θ_bar / (_von_karman_const * _grav * L_MO)
+    VDSE_flux_star = -u_star^3 * VDSE_scale / (_von_karman_const * _grav * L_MO)
     flux = -u_star * x_star
+
     K_exchange = compute_exchange_coefficients(
         param_set,
         z,
         F_exchange,
         a,
         x_star,
-        θ_bar,
+        VDSE_scale,
         dimensionless_number,
         L_MO,
     )
+    
+    C_exchange = get_flux_coefficients(
+        param_set,
+        z,
+        a,
+        x_star,
+        VDSE_scale,
+        dimensionless_number,
+        L_MO,
+        z_0
+    )
 
+    #=
+    @show(C_exchange)
+    @show(L_MO)
+    @show(x_star[1], x_star[2])
+    @show(x_star .* x_star[1])
+    =# 
     return SurfaceFluxConditions(
         L_MO,
-        pottemp_flux_star,
+        VDSE_flux_star,
         flux,
         x_star,
-        K_exchange,
+        C_exchange,
     )
 end
 
-
+"""
+    compute_physical_scale
+    (3), (17), (18), (19)
+"""
 function compute_physical_scale(
-    param_set::APS,
+    param_set::AbstractEarthParameterSet,
     x,
     u,
     Δz,
     a,
-    θ_bar,
-    pottemp_flux,
+    VDSE_scale,
+    flux,
     z_0,
     x_ave,
     x_s,
@@ -193,19 +225,46 @@ function compute_physical_scale(
 )
     FT = typeof(u)
     _von_karman_const::FT = von_karman_const(param_set)
-    L = monin_obukhov_len(param_set, u, θ_bar, pottemp_flux)
+    L = monin_obukhov_length(param_set, u, VDSE_scale, flux)
     R_z0 = compute_R_z0(z_0, Δz)
-    term1 = log(Δz / z_0)
-    term2 = -compute_Psi(Δz / L, L, a, dimensionless_number, transport)
-    term3 =
+    temp1 = log(Δz / z_0)
+    temp2 = -compute_Psi(Δz / L, L, a, dimensionless_number, transport)
+    temp3 =
         z_0 / Δz * compute_Psi(z_0 / L, L, a, dimensionless_number, transport)
-    term4 =
+    temp4 =
         R_z0 * (compute_psi(z_0 / L, L, a, dimensionless_number, transport) - 1)
     return (1 / dimensionless_number) * _von_karman_const /
-           (term1 + term2 + term3 + term4) * (x_ave - x_s)
+           (temp1 + temp2 + temp3 + temp4) * (x_ave - x_s)
 end
 
-""" Computes `R_z0` expression, defined after Eq. 15 """
+function compute_physical_scale2(
+    param_set::AbstractEarthParameterSet,
+    x,
+    u,
+    Δz,
+    a,
+    VDSE_scale,
+    flux,
+    z_0,
+    x_ave,
+    x_s,
+    dimensionless_number,
+    transport,
+)
+    FT = eltype(u)
+    _von_karman_const::FT = von_karman_const(param_set)
+    L = monin_obukhov_length(param_set, u, VDSE_scale, flux)
+    temp1 = log(Δz / z_0)
+    temp2 = -compute_psi(Δz/L, L, a, dimensionless_number, transport)
+    temp3 = compute_psi(z_0/L, L, a, dimensionless_number, transport)
+    return (1 / dimensionless_number) * _von_karman_const /
+        (temp1 + temp2 + temp3) * (x_ave - x_s)
+end
+
+
+### Nishizawa equations ### 
+
+""" Computes R_z0 expression, defined after Eq. 15 """
 compute_R_z0(z_0, Δz) = 1 - z_0 / Δz
 
 """ Computes f_m in Eq. A7 """
@@ -214,8 +273,21 @@ compute_f_m(ζ) = sqrt(sqrt(1 - 15 * ζ))
 """ Computes f_h in Eq. A8 """
 compute_f_h(ζ) = sqrt(1 - 9 * ζ)
 
+""" Computes phi_m in Eq. A1 """
+function compute_phi(ζ, L, a, dimensionless_number, ::Momentum)
+    FT = eltype(ζ)
+    return L >= 0 ? a * ζ + 1 : 1/(sqrt(sqrt((1 - 15 * ζ))))
+end
+
+""" Computes phi_h in Eq. A2 """
+function compute_phi(ζ, L, a, dimensionless_number, ::Heat)
+    FT = eltype(ζ)
+    return L >= 0 ? a * ζ / dimensionless_number + 1 : 1/(sqrt((1 - 9 * ζ)))
+end
+
 """ Computes psi_m in Eq. A3 """
 function compute_psi(ζ, L, a, dimensionless_number, ::Momentum)
+    FT = eltype(L)
     f_m = compute_f_m(ζ)
     return L >= 0 ? -a * ζ :
            log((1 + f_m)^2 * (1 + f_m^2) / 8) - 2 * atan(f_m) + FT(π) / 2
@@ -223,7 +295,8 @@ end
 
 """ Computes psi_h in Eq. A4 """
 function compute_psi(ζ, L, a, dimensionless_number, ::Heat)
-    L >= 0 ? -a * ζ / dimensionless_number : 2 * log((1 + compute_f_h(ζ)) / 2)
+    f_h = compute_f_h(ζ)
+    L >= 0 ? -a * ζ / dimensionless_number : 2 * log((1 + f_h) / 2)
 end
 
 """ Computes Psi_m in Eq. A5 """
@@ -236,8 +309,8 @@ function compute_Psi(ζ, L, a, dimensionless_number, ::Momentum)
         # Note that "1-f^3" in is a typo, it is supposed to be "1-f_m^3".
         # This was confirmed by communication with the author.
         return L >= 0 ? -a * ζ / 2 :
-               log((1 + f_m)^2 * (1 + f_m^2) / 8) - 2 * atan(f_m) + FT(π) / 2 -
-               1 + (1 - f_m^3) / (12 * ζ)
+               log((1 + f_m)^2 * (1 + f_m^2) / 8) - 2 * atan(f_m) + FT(π) / 2 - 1 +
+               (1 - f_m^3) / (12 * ζ)
     end
 end
 
@@ -248,27 +321,88 @@ function compute_Psi(ζ, L, a, dimensionless_number, ::Heat)
     else
         f_h = compute_f_h(ζ)
         return L >= 0 ? -a * ζ / (2 * dimensionless_number) :
-               2 * log((1 + f_h) / 2) + 2 * (1 - f_h) / (9 * ζ)
+               2 * log((1 + f_h) / 2) + 2 * (1 - f_h) / (9 * ζ) - 1
+    end
+end
+
+### Gryanik et. al. 2020 equations ### 
+# Typical values for empirical coefficients
+# Pr_0 = 0.98
+# a_m = 5.0
+# a_h = 5.0
+# b_m = 0.3
+# b_h = 0.4
+
+"""
+    compute_phi_GLGS(ζ, L, a, dimensionless_number, ::Momentum)
+Compute ϕ_m, stability function following Gryanik et al. 2020
+"""
+function compute_phi_GLGS(ζ, L, a, dimensionless_number, ::Momentum)
+    if 0 <= ζ < 100 
+        return 1 + (a_m * ζ)/(1 + b_m * ζ)^(2/3)
     end
 end
 
 """
-    monin_obukhov_len(param_set, u, θ, flux)
-
-Monin-Obukhov length. Eq. 3
+    compute_phi_GLGS(ζ, L, a, dimensionless_number, ::Heat)
+Compute ϕ_h, stability function following Gryanik et al. 2020
 """
-function monin_obukhov_len(param_set::APS, u, θ_bar, flux)
-    FT = typeof(u)
-    _grav::FT = grav(param_set)
-    _von_karman_const::FT = von_karman_const(param_set)
-    return -u^3 * θ_bar / (_von_karman_const * _grav * flux)
+function compute_phi_GLGS(ζ, L, a, dimensionless_number, ::Heat)
+    if 0 <= ζ < 100 
+        return Pr_0 * (1 + (a_h * ζ)/(1 + b_h * ζ))
+    end
 end
 
 """
-    compute_exchange_coefficients(z, F_exchange, a, x_star, θ_bar, dimensionless_number, L_MO)
+    compute_psi_GLGS(ζ, L, a, dimensionless_number, ::Momentum)
+Compute ψ_m, stability correction function following Gryanik et al. 2020
+"""
+function compute_psi_GLGS(ζ, L, a, dimensionless_number, ::Momentum)
+    if 0 <= ζ < 100
+        return -3 * (a_m/b_m) * ((1 + b_m * ζ)^(1/3) - 1)
+    end
+end
+
+"""
+    compute_psi_GLGS(ζ, L, a, dimensionless_number, ::Momentum)
+Compute ψ_h, stability correction function following Gryanik et al. 2020
+"""
+function compute_psi_GLGS(ζ, L, a, dimensionless_number, ::Heat)
+    if 0 <= ζ < 100
+        return -Pr_0 * (a_h / b_h) * log(1 + b_h * ζ) 
+    end
+end
+
+"""
+    compute_bulk_Richardson(ζ, z_0, L, Pr_0)
+Compute Bulk Richardson number, equal to Ri/Pr_0.
+Required for computation of exchange coefficients C_d and C_h
+"""
+function compute_bulk_Richardson(ζ, z_0, L, Pr_0)
+    term1 = (1-1/ε_m)^2/(1-1/ε_t)*ζ
+    term2 = log(ε_t) - 1/Pr_0 * (compute_psi_GLGS(ζ, L, a, dimensionless_number, Heat())+compute_psi_GLGS(ζ/ε_t, L, a, dimensionless_number, Heat()))
+    term3 = log(ε_m) - compute_psi_GLGS(ζ, L, a, dimensionless_number, Momentum() + compute_psi_GLGS(ζ/ε_m, L, a, dimensionless_number, ))
+    return term1 + term2/term3
+end
+
+### Generic terms
+
+"""
+    monin_obukhov_length(param_set, u, θ, flux)
+
+Monin-Obukhov length. Eq. 3
+"""
+function monin_obukhov_length(param_set::AbstractEarthParameterSet, u_star, VDSE_scale, flux)
+    FT = typeof(u_star)
+    _grav::FT = grav(param_set)
+    _von_karman_const::FT = von_karman_const(param_set)
+    return -u_star^3 * VDSE_scale / (_von_karman_const * _grav * flux)
+end
+
+"""
+    compute_exchange_coefficients(z, F_exchange, a, x_star, VDSE_scale, dimensionless_number, L_MO)
 
 Computes exchange transfer coefficients
-
   - `K_exchange` exchange coefficients
 """
 function compute_exchange_coefficients(
@@ -277,23 +411,50 @@ function compute_exchange_coefficients(
     F_exchange,
     a,
     x_star,
-    θ_bar,
+    VDSE_scale,
     dimensionless_number,
     L_MO,
 )
-
     N = length(F_exchange)
     FT = typeof(z)
-    K_exchange = Vector{FT}(undef, N)
     _von_karman_const::FT = von_karman_const(param_set)
+    K_exchange = zeros(length(x_star))
     for i in 1:N
         transport = i == 1 ? Momentum() : Heat()
-        psi = compute_psi(z / L_MO, L_MO, a, dimensionless_number[i], transport)
+        phi = compute_phi(z / L_MO, L_MO, a, dimensionless_number[i], transport)
         K_exchange[i] =
-            -F_exchange[i] * _von_karman_const * z / (x_star[i] * psi) # Eq. 19 in
+        -F_exchange[i] * _von_karman_const * z / dimensionless_number[i] / (x_star[i] * phi) # Eq. 19 in
     end
-
     return K_exchange
+end
+
+function get_flux_coefficients(
+    param_set,
+    z,
+    a,
+    x_star,
+    VDSE_scale,
+    dimensionless_number,
+    L_MO,
+    z0
+)
+    N = length(x_star)
+    L = L_MO
+    FT = typeof(z)
+    _von_karman_const::FT = von_karman_const(param_set)
+    C = zeros(length(x_star))
+    for i in 1:N
+        if i == 1
+            psi = compute_psi(z/L, L, a, dimensionless_number, Momentum())
+            C[i] = _von_karman_const^2/(log(z/z0[i] - psi))^2
+        else
+            psi_m = compute_psi(z/L, L, a, dimensionless_number, Momentum())
+            psi_h = compute_psi(z/L, L, a, dimensionless_number, Heat())
+            C[i] = _von_karman_const^2/(log(z/z0[i] - psi_m))/(log(z/z0[i] - psi_h))
+        end
+    end
+    return (C)
+
 end
 
 end # SurfaceFluxes module
---
+++ test/Common/SurfaceFluxes/test-profiles.jl
@@ -0,0 +1,86 @@
+using Test
+using ClimateMachine
+using CLIMAParameters: AbstractEarthParameterSet
+using ClimateMachine.SurfaceFluxes
+using StaticArrays
+
+using NCDatasets
+
+struct EarthParameterSet <: AbstractEarthParameterSet end
+const param_set = EarthParameterSet()
+
+@testset "SurfaceFluxes - FMS Profiles" begin
+    FT = Float64;
+
+    ## Discretisation altitude z
+    z      = [29.432779269303, 30.0497139076724, 31.6880000418153, 34.1873479240475]
+    ## Pottemp at height z
+    pt     = [268.559120403867, 269.799228886728, 277.443023238556, 295.79192777341]
+    ## Surface Pottemp
+    pt0    = [273.42369841804 , 272.551410044203, 278.638168565727, 298.133068766049]
+    ## Roughness lengths
+    z0     = [5.86144925739178e-05, 0.0001, 0.000641655193293549, 3.23383768877187e-05]
+    zt     = [3.69403636275411e-05, 0.0001, 1.01735489109205e-05, 7.63933834969505e-05]
+    zq     = [5.72575636226887e-05, 0.0001, 5.72575636226887e-05, 5.72575636226887e-05]
+    ## Speed
+    speed  = [2.9693638452068, 2.43308757772094, 5.69418282305367, 9.5608693754561]
+    ## Scale velocity and moisture
+    u_star =  [0.109462510724615, 0.0932942802513508, 0.223232887323184, 0.290918439028557]
+    q_star =   [0.000110861442197537, 9.44983279664197e-05, 4.17643828631936e-05, 0.000133135421415819]
+    # No explicit buoyancy terms in ClimateMachine
+    #b_star =  [0.00690834676781433, 0.00428178089592372, 0.00121229800895103, 0.00262353784027441, -0.000570314880866852]
+
+    for ii = 1:length(u_star)
+        # Data at first interior node (x_ave)
+        qt_ave = FT(0);
+        z_ave = z[ii];
+        thv_ave = pt[ii];
+        u_ave = speed[ii];
+        x_ave = [u_ave, thv_ave, qt_ave];
+
+        ## Initial guesses for MO parameters
+        LMO_init = eps(FT);
+        u_star_init = FT(0.1);
+        th_star_init = -FT(0.1);
+        qt_star_init = -FT(1e-5);
+        x_init = [LMO_init, u_star_init, th_star_init, qt_star_init];
+
+        # Surface values for variables
+        u_sfc = FT(0);
+        thv_sfc = pt0[ii];
+        qt_sfc = q_star[ii];
+        z_sfc = FT(0);
+        x_s = [u_sfc, thv_sfc, qt_sfc];
+
+        # Dimensionless numbers
+        dimless_num = [FT(1), FT(1/3), FT(1/3)]
+
+        # Roughness
+        z_rough = [z0[ii], zt[ii], zq[ii]]
+
+        # Constants
+        a  = FT(4.7);
+        Δz = z[ii];
+
+        # F_exchange
+        F_exchange = [FT(0.01), -FT(0.01), -FT(0.000001)];
+
+        ## Assuming surface fluxes are not given
+        result = surface_conditions(
+                    param_set,
+                    x_init,
+                    x_ave,
+                    x_s,
+                    z_rough,
+                    F_exchange,
+                    dimless_num,
+                    thv_ave,
+                    qt_ave,
+                    Δz,
+                    z_ave / 2,
+                    a,
+                 );
+        x_star =result.x_star
+        @test (abs((x_star[1] - u_star[ii])/u_star[ii])) ≈ FT(0.15)
+    end
+end
