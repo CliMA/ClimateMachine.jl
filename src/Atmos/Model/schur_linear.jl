549c41a53a360d5d567a2bffec3314704047790e
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-08-17 16:57:55 -0700

    wip on gravity

--- src/Atmos/Model/schur_linear.jl
+++ src/Atmos/Model/schur_linear.jl
@@ -133,3 +133,197 @@ function schur_update_boundary_state!(::AtmosAcousticLinearSchurComplement,
   schur_grad⁺.∇p = schur_grad⁻.∇p - 2 * dot(schur_grad⁻.∇p, n) * n
   state_rhs⁺.ρu = state_rhs⁻.ρu - 2 * dot(state_rhs⁻.ρu, n) * n
 end
+
+# GRAVITY
+struct AtmosAcousticGravityLinearSchurComplement <: SchurComplement end
+
+function vars_state(::AtmosAcousticGravityLinearSchurComplement, ::SchurAuxiliary, FT)
+  @vars begin
+    h0::FT
+    Φ::FT # FIXME: get this from aux directly
+    ∇Φ::SVector{3, FT} # FIXME: get this from aux directly
+    ∇h0::SVector{3, FT}
+  end
+end
+vars_state(::AtmosAcousticGravityLinearSchurComplement, ::SchurAuxiliaryGradient, FT) = @vars(h0::FT)
+
+function schur_init_aux!(::AtmosAcousticGravityLinearSchurComplement,
+                         lm, schur_aux, aux, geom)
+  Φ = gravitational_potential(lm.atmos, aux)
+  ∇Φ = ∇gravitational_potential(lm.atmos, aux)
+  schur_aux.h0 = (aux.ref_state.ρe + aux.ref_state.p) / aux.ref_state.ρ - Φ
+  schur_aux.Φ = Φ
+  schur_aux.∇Φ = ∇Φ
+end
+
+function schur_init_state!(::AtmosAcousticGravityLinearSchurComplement,
+                           lm,
+                           schur_state, schur_aux, state_rhs, aux)
+  Φ = schur_aux.Φ
+  param_set = lm.atmos.param_set
+  γ = 1 / (1 - kappa_d(param_set))
+  schur_state.p = (γ - 1) * (state_rhs.ρe - state_rhs.ρ * (Φ - R_d(param_set) * T_0(param_set) / (γ - 1)))
+end
+
+function schur_lhs_conservative!(::AtmosAcousticGravityLinearSchurComplement, 
+                                 lm, lhs_flux, schur_state, schur_grad, schur_aux, α)
+  FT = eltype(schur_state)
+  Φ = schur_aux.Φ
+  param_set = lm.atmos.param_set
+  γ::FT = 1 / (1 - kappa_d(param_set))
+  _grav::FT = grav(lm.atmos.param_set)
+  p = schur_state.p
+  h0 = schur_aux.h0
+  ∇h0 = schur_aux.∇h0
+  k = vertical_unit_vector(lm.atmos, schur_aux)
+
+  A = I + α ^ 2 * _grav / (h0 - Φ) * k * ∇h0'
+  lhs_flux.p = -α * (A \ (schur_grad.∇p + _grav * k / ((h0 - Φ) * (γ - 1)) * p))
+end
+function schur_lhs_nonconservative!(::AtmosAcousticGravityLinearSchurComplement,
+                                    lm, lhs_state, schur_state, schur_grad, schur_aux, α)
+  FT = eltype(schur_state)
+  Φ = schur_aux.Φ
+  param_set = lm.atmos.param_set
+  γ::FT = 1 / (1 - kappa_d(param_set))
+  p = schur_state.p
+  h0 = schur_aux.h0
+  ∇h0 = schur_aux.∇h0
+  ∇p = schur_grad.∇p
+  Δp = -R_d(param_set) * T_0(param_set) / (γ - 1)
+  
+  k = vertical_unit_vector(lm.atmos, schur_aux)
+  _grav::FT = grav(lm.atmos.param_set)
+  A = I + α ^ 2 * _grav / (h0 - Φ) * k * ∇h0'
+
+  lhs_state.p = p / ((γ - 1) * α * (h0 - Δp - Φ)) -
+                α * ∇h0' * (A \ (∇p + _grav * k / ((h0 - Φ) * (γ - 1)))) / (h0 - Δp - Φ)
+end
+function schur_gradient_boundary_state!(::AtmosAcousticGravityLinearSchurComplement, 
+                                   balance_law,
+                                   schur_state⁺, n,
+                                   schur_state⁻, bctype
+                                  )
+end
+function schur_lhs_boundary_state!(::AtmosAcousticGravityLinearSchurComplement, 
+                                   balance_law,
+                                   schur_state⁺, schur_grad⁺, schur_aux⁺, n,
+                                   schur_state⁻, schur_grad⁻, schur_aux⁻, bctype
+                                  )
+  schur_grad⁺.∇p = schur_grad⁻.∇p - 2 * dot(schur_grad⁻.∇p, n) * n
+end
+
+function schur_rhs_conservative!(::AtmosAcousticGravityLinearSchurComplement, 
+                                 lm,
+                                 rhs_flux, state, schur_aux, α)
+  FT = eltype(schur_aux)
+  Φ = schur_aux.Φ
+  param_set = lm.atmos.param_set
+  γ::FT = 1 / (1 - kappa_d(param_set))
+  _grav::FT = grav(lm.atmos.param_set)
+  h0 = schur_aux.h0
+  ∇h0 = schur_aux.∇h0
+  k = vertical_unit_vector(lm.atmos, schur_aux)
+
+  ρ = state.ρ
+  ρu = state.ρu
+  ρe = state.ρe
+
+  A = I + α ^ 2 * _grav / (h0 - Φ) * k * ∇h0'
+  rhs_flux.p = -A \ (ρu - α * _grav * k / (h0 - Φ) * (h0 * ρ - ρe))
+end
+function schur_rhs_nonconservative!(::AtmosAcousticGravityLinearSchurComplement, 
+                                    lm,
+                                    rhs_state, state, schur_aux, α)
+  FT = eltype(schur_aux)
+  Φ = schur_aux.Φ
+  param_set = lm.atmos.param_set
+  γ::FT = 1 / (1 - kappa_d(param_set))
+  ρ = state.ρ
+  ρu = state.ρu
+  ρe = state.ρe
+  h0 = schur_aux.h0
+  ∇h0 = schur_aux.∇h0
+  Δp = -R_d(param_set) * T_0(param_set) / (γ - 1)
+  
+  k = vertical_unit_vector(lm.atmos, schur_aux)
+  _grav::FT = grav(lm.atmos.param_set)
+  A = I + α ^ 2 * _grav / (h0 - Φ) * k * ∇h0'
+  
+  rhs_state.p = (ρe - (Φ + Δp) * ρ) / (α * (h0 - Δp - Φ)) -
+                ∇h0' * (ρu - α * _grav * k / (h0 - Φ) * (h0 * ρ - ρe)) / (h0 - Δp - Φ)
+end
+function schur_rhs_boundary_state!(::AtmosAcousticGravityLinearSchurComplement, 
+                                   balance_law,
+                                   state⁺, schur_aux⁺, n,
+                                   state⁻, schur_aux⁻, bctype
+                                  )
+  state⁺.ρu = state⁻.ρu - 2 * dot(state⁻.ρu, n) * n
+end
+
+function schur_update_conservative!(::AtmosAcousticGravityLinearSchurComplement, 
+                                    lm,
+                                    lhs_flux, schur_state, schur_grad, schur_aux, state_rhs, α)
+  FT = eltype(schur_state)
+  param_set = lm.atmos.param_set
+  p = schur_state.p
+  h0 = schur_aux.h0
+  ∇p = schur_grad.∇p
+
+  Φ = schur_aux.Φ
+  k = vertical_unit_vector(lm.atmos, schur_aux)
+  ∇h0 = schur_aux.∇h0
+  _grav::FT = grav(param_set)
+  γ::FT = 1 / (1 - kappa_d(param_set))
+  A = I + α ^ 2 * _grav / (h0 - Φ) * k * ∇h0'
+
+  ρ = state_rhs.ρ
+  ρu = state_rhs.ρu
+  ρe = state_rhs.ρe
+  
+  updated_ρu = A \ (ρu -
+                    α * ∇p -
+                    α * _grav * k / (h0 - Φ) * (h0 * ρ - ρe) -
+                    α * _grav * k / ((h0 - Φ) * (γ - 1)) * p)
+  
+  lhs_flux.ρ = -α * updated_ρu
+  #lhs_flux.ρu -= α * (A \ (p * I))
+  lhs_flux.ρe = -α * h0 * updated_ρu
+end
+function schur_update_nonconservative!(::AtmosAcousticGravityLinearSchurComplement, 
+                                       lm,
+                                       lhs_state,
+                                       schur_state, schur_grad, schur_aux, state_rhs, α)
+ 
+  FT = eltype(schur_state)
+  param_set = lm.atmos.param_set
+  Φ = schur_aux.Φ
+  k = vertical_unit_vector(lm.atmos, schur_aux)
+  ∇h0 = schur_aux.∇h0
+  h0 = schur_aux.h0
+  _grav::FT = grav(param_set)
+  A = I + α ^ 2 * _grav / (h0 - Φ) * k * ∇h0'
+  γ::FT = 1 / (1 - kappa_d(param_set))
+  
+  ρ = state_rhs.ρ
+  ρu = state_rhs.ρu
+  ρe = state_rhs.ρe
+  ∇p = schur_grad.∇p
+  p = schur_state.p
+
+  lhs_state.ρ = ρ
+  lhs_state.ρu = A \ (ρu -
+                      α * ∇p -
+                      α * _grav * k / (h0 - Φ) * (h0 * ρ - ρe) -
+                      α * _grav * k / ((h0 - Φ) * (γ - 1)) * p)
+  lhs_state.ρe = ρe
+end
+
+function schur_update_boundary_state!(::AtmosAcousticGravityLinearSchurComplement, 
+                                   balance_law,
+                                   schur_state⁺, schur_grad⁺, schur_aux⁺, state_rhs⁺, n,
+                                   schur_state⁻, schur_grad⁻, schur_aux⁻, state_rhs⁻, bctype
+                                  )
+  schur_grad⁺.∇p = schur_grad⁻.∇p - 2 * dot(schur_grad⁻.∇p, n) * n
+  state_rhs⁺.ρu = state_rhs⁻.ρu - 2 * dot(state_rhs⁻.ρu, n) * n
+end
