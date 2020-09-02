01ce18a9b9412e8beed3e89ed14595026cda8188
Jeremy E Kozdon <jekozdon@nps.edu>
2020-09-02 21:29:29 +0000

    WIP: Working on improving the banded matrix tests

--- test/Numerics/SystemSolvers/bandedsystem.jl
+++ test/Numerics/SystemSolvers/bandedsystem.jl
@@ -28,14 +28,14 @@ struct Pseudo1D{n, α, β, μ, δ} <: AdvectionDiffusionProblem end
 
 function init_velocity_diffusion!(
     ::Pseudo1D{n, α, β},
-    aux::Vars,
+    aux::Vars{ST},
     geom::LocalGeometry,
-) where {n, α, β}
+) where {ST, n, α, β}
     # Direction of flow is n with magnitude α
     aux.u = α * n
 
     # diffusion of strength β in the n direction
-    aux.D = β * n * n'
+    hasfield(ST, :D) && (aux.D = β * n * n')
 end
 
 struct BigAdvectionDiffusion <: BalanceLaw end
@@ -46,16 +46,11 @@ function vars_state(::BigAdvectionDiffusion, ::Prognostic, FT)
     end
 end
 
-function initial_condition!(
-    ::Pseudo1D{n, α, β, μ, δ},
-    state,
-    aux,
-    x,
-    t,
-) where {n, α, β, μ, δ}
+function initial_condition!(::Pseudo1D{n}, state, aux, x, _) where {n}
     ξn = dot(n, x)
     # ξT = SVector(x) - ξn * n
-    state.ρ = exp(-(ξn - μ - α * t)^2 / (4 * β * (δ + t))) / sqrt(1 + t / δ)
+
+    state.ρ = sin(ξn / 2)
 end
 
 let
@@ -76,185 +71,222 @@ let
         for FT in (Float64, Float32)
             for dim in (2, 3)
                 for single_column in (false, true)
-                    # Setup the topology
-                    if dim == 2
-                        brickrange = (
-                            range(FT(0); length = Neh + 1, stop = 1),
-                            range(FT(1); length = Nev + 1, stop = 2),
-                        )
-                    elseif dim == 3
-                        brickrange = (
-                            range(FT(0); length = Neh + 1, stop = 1),
-                            range(FT(0); length = Neh + 1, stop = 1),
-                            range(FT(1); length = Nev + 1, stop = 2),
-                        )
-                    end
-                    topl = StackedBrickTopology(mpicomm, brickrange)
-
-                    # Warp mesh
-                    function warpfun(ξ1, ξ2, ξ3)
-                        # single column currently requires no geometry warping
-
-                        # Even if the warping is in only the horizontal, the way we
-                        # compute metrics causes problems for the single column approach
-                        # (possibly need to not use curl-invariant computation)
-                        if !single_column
-                            ξ1 = ξ1 + sin(2π * ξ1 * ξ2) / 10
-                            ξ2 = ξ2 + sin(2π * ξ1) / 5
-                            if dim == 3
-                                ξ3 = ξ3 + sin(8π * ξ1 * ξ2) / 10
-                            end
+                    for no_diffusion in (true, false)
+                        @show FT, dim, single_column, no_diffusion
+                        # Setup the topology
+                        if dim == 2
+                            brickrange = (
+                                range(FT(0); length = Neh + 1, stop = 1),
+                                range(FT(1); length = Nev + 1, stop = 2),
+                            )
+                        elseif dim == 3
+                            brickrange = (
+                                range(FT(0); length = Neh + 1, stop = 1),
+                                range(FT(0); length = Neh + 1, stop = 1),
+                                range(FT(1); length = Nev + 1, stop = 2),
+                            )
                         end
-                        (ξ1, ξ2, ξ3)
+                        topl = StackedBrickTopology(mpicomm, brickrange)
+
+                        # Warp mesh
+                        function warpfun(ξ1, ξ2, ξ3)
+                            # single column currently requires no geometry
+                            # warping
+
+                            # Even if the warping is in only the horizontal, the
+                            # way we compute metrics causes problems for the
+                            # single column approach (possibly need to not use
+                            # curl-invariant computation)
+                            if !single_column
+                                ξ1 = ξ1 + sin(2π * ξ1 * ξ2) / 10
+                                ξ2 = ξ2 + sin(2π * ξ1) / 5
+                                if dim == 3
+                                    ξ3 = ξ3 + sin(8π * ξ1 * ξ2) / 10
+                                end
+                            end
+                            (ξ1, ξ2, ξ3)
+                        end
+
+                        # create the actual grid
+                        grid = DiscontinuousSpectralElementGrid(
+                            topl,
+                            FloatType = FT,
+                            DeviceArray = ArrayType,
+                            polynomialorder = N,
+                            meshwarp = warpfun,
+                        )
+                        d = dim == 2 ? FT[1, 10, 0] : FT[1, 1, 10]
+                        n = SVector{3, FT}(d ./ norm(d))
+
+                        α = FT(1)
+                        β = FT(1 // 100)
+                        μ = FT(-1 // 2)
+                        δ = FT(1 // 10)
+                        prob = Pseudo1D{n, α, β, μ, δ}()
+                        model =
+                            AdvectionDiffusion{dim, false, no_diffusion}(prob)
+
+                        # the nonlinear model is needed so we can grab the
+                        # state_auxiliary below
+                        dg = DGModel(
+                            model,
+                            grid,
+                            RusanovNumericalFlux(),
+                            CentralNumericalFluxSecondOrder(),
+                            CentralNumericalFluxGradient(),
+                        )
+
+                        vdg = DGModel(
+                            model,
+                            grid,
+                            RusanovNumericalFlux(),
+                            CentralNumericalFluxSecondOrder(),
+                            CentralNumericalFluxGradient();
+                            direction = VerticalDirection(),
+                            state_auxiliary = dg.state_auxiliary,
+                        )
+
+                        A_banded = banded_matrix(
+                            vdg,
+                            MPIStateArray(dg),
+                            MPIStateArray(dg);
+                            single_column = single_column,
+                        )
+
+                        Q = init_ode_state(dg, FT(0))
+                        dQ1 = MPIStateArray(dg)
+                        dQ2 = MPIStateArray(dg)
+
+                        vdg(dQ1, Q, nothing, 0; increment = false)
+                        Q.data .= dQ1.data / maximum(abs.(Array(dQ1.data)))
+
+                        vdg(dQ1, Q, nothing, 0; increment = false)
+                        banded_matrix_vector_product!(A_banded, dQ2, Q)
+                        @test all(isapprox.(
+                            Array(dQ1.realdata),
+                            Array(dQ2.realdata),
+                            atol = sqrt(eps(FT)),
+                            rtol = sqrt(eps(FT)),
+                        ))
+                        @show extrema(Array(dQ1.realdata) - Array(dQ2.realdata))
+
+                        big_Q = create_state(
+                            BigAdvectionDiffusion(),
+                            grid,
+                            Prognostic(),
+                        )
+                        big_dQ = create_state(
+                            BigAdvectionDiffusion(),
+                            grid,
+                            Prognostic(),
+                        )
+
+                        big_Q .= NaN
+                        big_dQ .= NaN
+
+                        big_Q.data[:, 1:size(Q, 2), :] .= Q.data
+
+                        vdg(big_dQ, big_Q, nothing, 0; increment = false)
+
+                        # These should be exactly the same
+                        @test all(isequal.(
+                            Array(big_dQ.realdata[:, 1:size(Q, 2), :]),
+                            Array(dQ1.realdata),
+                        ))
+
+                        @test all(big_dQ[:, (size(Q, 2) + 1):end, :] .== 0)
+
+                        big_dQ.data[:, size(Q, 2), :] .= 0
+                        big_dQ.data[:, (size(Q, 2) + 1):end, :] .= -7
+
+                        vdg(big_dQ, big_Q, nothing, 0; increment = true)
+
+                        @test all(big_dQ[:, (size(Q, 2) + 1):end, :] .== -7)
+
+                        # These should be exactly the same
+                        @test all(isequal.(
+                            Array(big_dQ.realdata[:, 1:size(Q, 2), :]),
+                            Array(dQ1.realdata),
+                        ))
+
+                        big_A = banded_matrix(
+                            vdg,
+                            similar(big_Q),
+                            similar(big_dQ);
+                            single_column = single_column,
+                        )
+                        @test all(isequal.(
+                            Array(big_A.data),
+                            Array(A_banded.data),
+                        ))
+
+                        # Make the system better conditioned for Float32!
+                        α = FT == Float64 ? FT(-1 // 10) : FT(-1 // 40)
+                        function op!(LQ, Q)
+                            vdg(LQ, Q, nothing, 0; increment = false)
+                            @. LQ = Q + α * LQ
+                        end
+
+                        A_banded = banded_matrix(
+                            op!,
+                            vdg,
+                            MPIStateArray(dg),
+                            MPIStateArray(dg);
+                            single_column = single_column,
+                        )
+
+                        Q = init_ode_state(dg, FT(0))
+                        dQ1 = MPIStateArray(vdg)
+                        dQ2 = MPIStateArray(vdg)
+
+                        op!(dQ1, Q)
+                        Q.data .= dQ1.data / maximum(abs.(Array(dQ1.data)))
+
+                        op!(dQ1, Q)
+                        banded_matrix_vector_product!(A_banded, dQ2, Q)
+                        @test all(isapprox.(
+                            Array(dQ1.realdata),
+                            Array(dQ2.realdata),
+                            atol = sqrt(eps(FT)),
+                            rtol = sqrt(eps(FT)),
+                        ))
+                        @show extrema(Array(dQ1.realdata) - Array(dQ2.realdata))
+
+                        big_A = banded_matrix(
+                            op!,
+                            vdg,
+                            similar(big_Q),
+                            similar(big_dQ);
+                            single_column = single_column,
+                        )
+
+                        @test all(isequal.(
+                            Array(big_A.data),
+                            Array(A_banded.data),
+                        ))
+
+                        big_Q.data[:, 1:size(Q, 2), :] .= Q.data
+                        banded_matrix_vector_product!(big_A, big_dQ, big_Q)
+
+                        band_lu!(big_A)
+                        band_forward!(big_dQ, big_A)
+                        band_back!(big_dQ, big_A)
+
+                        @test all(isapprox.(
+                            Array(big_dQ.realdata[:, 1:size(Q, 2), :]),
+                            Array(Q.realdata),
+                            atol = sqrt(eps(FT)),
+                            rtol = sqrt(eps(FT)),
+                        ))
+                        @show extrema(
+                            Array(big_dQ.realdata[:, 1:size(Q, 2), :]) -
+                            Array(Q.realdata),
+                        )
+                        println()
+                        println()
+                        println()
+                        println()
                     end
-
-                    # create the actual grid
-                    grid = DiscontinuousSpectralElementGrid(
-                        topl,
-                        FloatType = FT,
-                        DeviceArray = ArrayType,
-                        polynomialorder = N,
-                        meshwarp = warpfun,
-                    )
-                    d = dim == 2 ? FT[1, 10, 0] : FT[1, 1, 10]
-                    n = SVector{3, FT}(d ./ norm(d))
-
-                    α = FT(1)
-                    β = FT(1 // 100)
-                    μ = FT(-1 // 2)
-                    δ = FT(1 // 10)
-                    model = AdvectionDiffusion{dim}(Pseudo1D{n, α, β, μ, δ}())
-
-                    # the nonlinear model is needed so we can grab the state_auxiliary below
-                    dg = DGModel(
-                        model,
-                        grid,
-                        RusanovNumericalFlux(),
-                        CentralNumericalFluxSecondOrder(),
-                        CentralNumericalFluxGradient(),
-                    )
-
-                    vdg = DGModel(
-                        model,
-                        grid,
-                        RusanovNumericalFlux(),
-                        CentralNumericalFluxSecondOrder(),
-                        CentralNumericalFluxGradient();
-                        direction = VerticalDirection(),
-                        state_auxiliary = dg.state_auxiliary,
-                    )
-
-                    A_banded = banded_matrix(
-                        vdg,
-                        MPIStateArray(dg),
-                        MPIStateArray(dg);
-                        single_column = single_column,
-                    )
-
-                    Q = init_ode_state(dg, FT(0))
-                    dQ1 = MPIStateArray(dg)
-                    dQ2 = MPIStateArray(dg)
-
-                    vdg(dQ1, Q, nothing, 0; increment = false)
-                    Q.data .= dQ1.data
-
-                    vdg(dQ1, Q, nothing, 0; increment = false)
-                    banded_matrix_vector_product!(A_banded, dQ2, Q)
-                    @test all(isapprox.(
-                        Array(dQ1.realdata),
-                        Array(dQ2.realdata),
-                        atol = 100 * eps(FT),
-                    ))
-
-                    big_Q = create_state(
-                        BigAdvectionDiffusion(),
-                        grid,
-                        Prognostic(),
-                    )
-                    big_dQ = create_state(
-                        BigAdvectionDiffusion(),
-                        grid,
-                        Prognostic(),
-                    )
-
-                    big_Q .= NaN
-                    big_dQ .= NaN
-
-                    big_Q.data[:, 1:size(Q, 2), :] .= Q.data
-
-                    vdg(big_dQ, big_Q, nothing, 0; increment = false)
-
-                    @test all(isapprox.(
-                        Array(big_dQ.realdata[:, 1:size(Q, 2), :]),
-                        Array(dQ1.realdata),
-                        atol = 100 * eps(FT),
-                    ))
-
-                    @test all(big_dQ[:, (size(Q, 2) + 1):end, :] .== 0)
-
-                    big_dQ.data[:, (size(Q, 2) + 1):end, :] .= -7
-
-                    vdg(big_dQ, big_Q, nothing, 0; increment = true)
-
-                    @test all(big_dQ[:, (size(Q, 2) + 1):end, :] .== -7)
-
-                    @test all(isapprox.(
-                        Array(big_dQ.realdata[:, 1:size(Q, 2), :]),
-                        Array(dQ1.realdata),
-                        atol = 100 * eps(FT),
-                    ))
-
-                    big_A = banded_matrix(
-                        vdg,
-                        similar(big_Q),
-                        similar(big_dQ);
-                        single_column = single_column,
-                    )
-                    @test all(isapprox.(
-                        Array(big_A.data),
-                        Array(A_banded.data),
-                        atol = 100 * eps(FT),
-                    ))
-
-                    band_lu!(big_A)
-                    band_forward!(big_dQ, big_A)
-                    band_back!(big_dQ, big_A)
-
-                    @test all(isapprox.(
-                        Array(big_dQ.realdata[:, 1:size(Q, 2), :]),
-                        Array(Q.realdata),
-                        atol = 100 * eps(FT),
-                    ))
-                    @test all(big_dQ[:, (size(Q, 2) + 1):end, :] .== -7)
-
-                    α = FT(1 // 10)
-                    function op!(LQ, Q)
-                        vdg(LQ, Q, nothing, 0; increment = false)
-                        @. LQ = Q + α * LQ
-                    end
-
-                    A_banded = banded_matrix(
-                        op!,
-                        vdg,
-                        MPIStateArray(dg),
-                        MPIStateArray(dg);
-                        single_column = single_column,
-                    )
-
-                    Q = init_ode_state(dg, FT(0))
-                    dQ1 = MPIStateArray(vdg)
-                    dQ2 = MPIStateArray(vdg)
-
-                    op!(dQ1, Q)
-                    Q.data .= dQ1.data
-
-                    op!(dQ1, Q)
-                    banded_matrix_vector_product!(A_banded, dQ2, Q)
-                    @test all(isapprox.(
-                        Array(dQ1.realdata),
-                        Array(dQ2.realdata),
-                        atol = 100 * eps(FT),
-                    ))
                 end
             end
         end
