d2083d8a3466099f7e035d9bc05128eadf374702
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-09-16 10:18:01 -0700

    Revamp atmos ref state, add plots to docs

--- src/Driver/solver_configs.jl
+++ src/Driver/solver_configs.jl
@@ -95,6 +95,7 @@ function SolverConfiguration(
     timeend_dt_adjust = true,
     CFL_direction = get_direction(driver_config.config_type),
     fixed_number_of_steps = Settings.fixed_number_of_steps,
+    skip_update_aux = false,
 ) where {FT <: AbstractFloat}
     @tic SolverConfiguration
 
@@ -171,7 +172,9 @@ function SolverConfiguration(
             )
         end
     end
-    update_auxiliary_state!(dg, bl, Q, FT(0), dg.grid.topology.realelems)
+    if !skip_update_aux
+        update_auxiliary_state!(dg, bl, Q, FT(0), dg.grid.topology.realelems)
+    end
 
     if Settings.debug_init
         write_debug_init_vtk_and_pvtu(
