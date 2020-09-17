cb5659811e5c32d0a9487d5d1918fbe720435e70
lenka <lenka@caltech.edu>
2020-09-17 10:33:41 -0700

    basic

---
+++ pipeline_logging_basic_gcmd.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#SBATCH --ntasks=128
+#SBATCH --job-name=gcmdriver
+#SBATCH --time=100:00:00
+#SBATCH --output=model_log_err.out
+
+set -euo pipefail # kill the job if anything fails
+set -x # echo script
+
+# load correct modules
+module purge;
+module load julia/1.4.2 hdf5/1.10.1 netcdf-c/4.6.1 openmpi/4.0.1
+
+#export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK:=1}
+export JULIA_MPI_BINARY=system
+export JULIA_CUDA_USE_BINARYBUILDER=false
+
+# Import helper functions for this script
+source ./helper_mod.sh
+
+# User envirnoment setup
+RUNNAME="demo-gcm-run"
+
+# Specify your home directories and output location on scratch
+CLIMA_HOME='/central/groups/esm/lenka/ClimaTests5/ClimateMachine_optimise11.jl'
+VIZCLIMA_HOME='/central/groups/esm/lenka/VizCLIMA.jl'
+SCRATCH_HOME='/central/scratch/elencz/output'
+
+# Choose the ClimateMachine.jl experiment script and VizCLIMA script
+EXPERIMENT=$CLIMA_HOME'/experiments/AtmosGCM/GCMDriver/GCMDriver.jl'
+VIZCLIMA_SCRIPT=$VIZCLIMA_HOME'/src/scripts/general-gcm-notebook-setup.jl'
+
+# ------------ No need to change anything below this for the demo ------------
+# Setup directory structure
+CLIMA_OUTPUT=$SCRATCH_HOME'/'$RUNNAME
+CLIMA_RESTART=$CLIMA_OUTPUT'/restart'
+CLIMA_NETCDF=$CLIMA_OUTPUT'/netcdf'
+CLIMA_ANALYSIS=$CLIMA_OUTPUT'/analysis'
+CLIMA_LOG=$CLIMA_OUTPUT'/log'
+mkdir -p $CLIMA_OUTPUT
+mkdir -p $CLIMA_RESTART
+mkdir -p $CLIMA_NETCDF
+mkdir -p $CLIMA_ANALYSIS
+mkdir -p $CLIMA_LOG
+
+# Run model
+julia --project=$CLIMA_HOME -e 'using Pkg; Pkg.instantiate()'
+julia --project=$CLIMA_HOME -e 'using Pkg; Pkg.precompile()'
+
+mpiexec julia --project=$CLIMA_HOME $CLIMA_RUNFILE --experiment=heldsuarez --diagnostics 6shours --monitor-courant-numbers 6shours --output-dir $CLIMA_NETCDF --checkpoint-at-end --checkpoint-dir $CLIMA_RESTART --init-moisture-profile dry --checkpoint 6shours
+
+# Move log/error file to the experiment's output location
+mv model_log_err.out $CLIMA_LOG
+
+# Move VizCLIMA script to the experiment's output location
+VIZCLIMA_SCRIPT_BN=$(basename "$VIZCLIMA_SCRIPT")
+if [ -d "$CLIMA_ANALYSIS/$VIZCLIMA_SCRIPT_BN" ]; then rm $CLIMA_ANALYSIS/$VIZCLIMA_SCRIPT_BN; fi
+cp $VIZCLIMA_SCRIPT $CLIMA_ANALYSIS/$VIZCLIMA_SCRIPT_BN;
+
+# Post-process output data from ClimateMachine using VizCLIMA
+export GKSwstype=null
+julia -e 'import Pkg; Pkg.add("IJulia"); Pkg.add("DelimitedFiles"); Pkg.add("PrettyTables"); Pkg.add("PaddedViews"); Pkg.add("Dates"); Pkg.add("GR")'
+julia --project=$VIZCLIMA_HOME -e 'using Pkg; Pkg.instantiate(); Pkg.API.precompile()'
+VIZCLIMA_LITERATE=$VIZCLIMA_HOME'/src/utils/make_literate.jl'
+julia --project=$VIZCLIMA_HOME $VIZCLIMA_LITERATE --input-file $CLIMA_ANALYSIS/$VIZCLIMA_SCRIPT_BN --output-dir $CLIMA_ANALYSIS
+
+mv ${VIZCLIMA_SCRIPT_BN%.jl}.ipynb $CLIMA_ANALYSIS
+
+# View your notebook on a local machine
+# on remote terminal (e.g. login2): jupyter notebook --no-browser --port=9999
+# on local terminal: ssh -N -f -L 9999:localhost:9999 <yourusername>@login2.hpc.caltech.edu
+# in local browser: localhost:9999
+# see https://github.com/CliMA/ClimateMachine.jl/wiki/Visualization
