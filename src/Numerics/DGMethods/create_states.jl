717c62a3b489decf19f0eef94a6a5b803333d85e
Simon Byrne <simonbyrne@gmail.com>
2020-09-23 12:12:11 -0700

    split out create_state

--- src/Numerics/DGMethods/create_states.jl
+++ src/Numerics/DGMethods/create_states.jl
@@ -1,7 +1,7 @@
 #### Create states
 
 function create_state(
-    ArrayType::Union{Type{MPIStateArray}, Type{Array}},
+    ::Type{MPIStateArray},
     balance_law,
     grid,
     st::AbstractStateType;
@@ -10,42 +10,63 @@ function create_state(
     topology = grid.topology
     Np = dofs_per_element(grid)
 
-    h_vgeo = Array(grid.vgeo)
-    FT = eltype(h_vgeo)
+    FT = eltype(grid.vgeo)
     DA = arraytype(grid)
 
-    weights = view(h_vgeo, :, grid.Mid, :)
+    weights = view(grid.vgeo, :, grid.Mid, :)
     weights = reshape(weights, size(weights, 1), 1, size(weights, 2))
 
     # TODO: Clean up this MPIStateArray interface...
     ns = number_states(balance_law, st)
     st isa GradientLaplacian && (ns = 3ns)
     V = vars_state(balance_law, st, FT)
-    if ArrayType <: MPIStateArray
-        state = MPIStateArray{FT, V}(
-            topology.mpicomm,
-            DA,
-            Np,
-            ns,
-            length(topology.elems),
-            realelems = topology.realelems,
-            ghostelems = topology.ghostelems,
-            vmaprecv = grid.vmaprecv,
-            vmapsend = grid.vmapsend,
-            nabrtorank = topology.nabrtorank,
-            nabrtovmaprecv = grid.nabrtovmaprecv,
-            nabrtovmapsend = grid.nabrtovmapsend,
-            weights = weights,
-        )
-    else
-        state = Array{FT}(
-            undef,
-            (Np, number_states(balance_law, st), length(topology.elems)),
-        )
-    end
+
+    state = MPIStateArray{FT, V}(
+        topology.mpicomm,
+        DA,
+        Np,
+        ns,
+        length(topology.elems),
+        realelems = topology.realelems,
+        ghostelems = topology.ghostelems,
+        vmaprecv = grid.vmaprecv,
+        vmapsend = grid.vmapsend,
+        nabrtorank = topology.nabrtorank,
+        nabrtovmaprecv = grid.nabrtovmaprecv,
+        nabrtovmapsend = grid.nabrtovmapsend,
+        weights = weights,
+    )
     fill_nan && fill!(state, NaN)
     return state
 end
+
+function create_state(
+    ::Type{Array},
+    balance_law,
+    grid,
+    st::AbstractStateType;
+    fill_nan = false,
+)
+    topology = grid.topology
+    Np = dofs_per_element(grid)
+
+    FT = eltype(grid.vgeo)
+    DA = arraytype(grid)
+
+    weights = view(grid.vgeo, :, grid.Mid, :)
+    weights = reshape(weights, size(weights, 1), 1, size(weights, 2))
+
+    ns = number_states(balance_law, st)
+    st isa GradientLaplacian && (ns = 3ns)
+    V = vars_state(balance_law, st, FT)
+    state = Array{FT}(
+        undef,
+        (Np, number_states(balance_law, st), length(topology.elems)),
+    )
+    fill_nan && fill!(state, NaN)
+    return state
+end
+
 
 function init_state(state, balance_law, grid, direction, ::Auxiliary)
     init_state_auxiliary!(balance_law, state, grid, direction)
