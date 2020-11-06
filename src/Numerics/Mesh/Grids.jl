8ba7bd48b825f8c0a00840ff4babad051bda63c9
Thomas Gibson <gibsonthomas1120@hotmail.com>
2020-11-06 15:55:27 -0800

    Grid: mask out zero-padded indices in vmap

--- src/Numerics/Mesh/Grids.jl
+++ src/Numerics/Mesh/Grids.jl
@@ -662,7 +662,10 @@ function computegeometry(
     M = kron(1, ntuple(j -> ω[j], dim)...)
     MJ .= M .* J
     MJI .= 1 ./ MJ
-    vMJI .= MJI[vmap⁻]
+    for d = 1:dim
+        vMJI[1:Nfp[d], 2d-1:2d, :] .= MJI[vmap⁻[1:Nfp[d], 2d-1:2d, :]]
+    end
+    sgeo = zeros(FT, _nsgeo, maximum(Nfp), nface, nelem)
 
     MH = kron(ones(FT, Nq[dim]), ntuple(j -> ω[j], dim - 1)...)
 
