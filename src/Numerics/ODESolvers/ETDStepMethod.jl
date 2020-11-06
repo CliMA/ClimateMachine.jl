e73bee58ddebe5543c9221f4c0ab4c98b9f923ab
vogel-d <denise.vogel@gmx.net>
2020-11-06 10:24:14 +0100

    corrected time in dostep!

--- src/Numerics/ODESolvers/ETDStepMethod.jl
+++ src/Numerics/ODESolvers/ETDStepMethod.jl
@@ -169,7 +169,7 @@ function dostep!(Q, etd::ETDStep, p, time)
     mriparam = MRIParam(p, βrow, realview.(fYnj[1:iStage]), ts, dts)
 
     copyto!(Q, yn)
-    dostep!(Q, fastsolver, mriparam, τ, nstepsLoc, iStage, nothing, nothing, nothing)
+    dostep!(Q, fastsolver, mriparam, ts, nstepsLoc, iStage, nothing, nothing, nothing)
   end
 end
 
