913c6c99df3c1d00fddc296b948b10483d0157d0
Gregory Wagner <wagner.greg@gmail.com>
2020-11-12 19:54:24 -0500

    Double gyre tutorial

--- tutorials/Ocean/double_gyre.jl
+++ tutorials/Ocean/double_gyre.jl
@@ -1,7 +1,7 @@
 # # Shear instability of a free-surface flow
 #
-# This script simulates the instability of a sheared, free-surface
-# flow using `ClimateMachine.Ocean.HydrostaticBoussinesqSuperModel`.
+# This script simulates the spin-up of a double-gyre
+# using `ClimateMachine.Ocean.HydrostaticBoussinesqSuperModel`.
 
 using Printf
 using Plots
@@ -23,37 +23,37 @@ using StaticArrays
 τ  = 1e-4
 Lx = 4e6
 Ly = 6e6
-H = -3000
+H = 3000
 
 domain = RectangularDomain(
-    elements = (10, 15, 2),
+    elements = (24, 24, 2),
     polynomialorder = 4,
     x = (0, Lx),
     y = (0, Ly),
-    z = (H, 0),
+    z = (-H, 0),
     periodicity = (false, false, false),
     boundary = ((1, 1), (1, 1), (2, 3)),
 )
 
-# Note that the default solid-wall boundary conditions are free-slip and
-# insulating on tracers. Next, we specify model parameters and the sheared
-# initial conditions
+# Specify initial condition
 
 struct EarthParameters <: AbstractEarthParameterSet end
 Planet.grav(::EarthParameters) = 0.1
 
-linear_gradient(x, y, z) = 20 * (1 - z / H)
+linear_gradient(x, y, z) = 20 * (1 + z / H)
 constant_temperature(x, y, z) = 20
-
 initial_conditions = InitialConditions(θ=linear_gradient)
 
-wind_stress(y) = @SVector [τ * cos(2π * y / Ly), 0]
+# Specify wind stress at the top boundary
 
+wind_stress(y) = @SVector [- τ * cos(2π * y / Ly), 0]
 top_boundary_condition = OceanBC(Penetrable(KinematicStress(wind_stress)), Insulating())
+
+hour = 3600.0
 
 model = Ocean.HydrostaticBoussinesqSuperModel(
     domain = domain,
-    time_step = 30, # seconds
+    time_step = hour / 2, # seconds
     initial_conditions = initial_conditions,
     parameters = EarthParameters(),
     turbulence_closure = (νʰ = 5e3, νᶻ = 5e-3, κʰ = 1e3, κᶻ = 1e-4),
@@ -73,25 +73,30 @@ model = Ocean.HydrostaticBoussinesqSuperModel(
 u, v, η, θ = model.fields
 fetched_states = []
 
-data_fetcher = EveryXSimulationTime(1) do
-    @info "Step: $(steps(model)), t: $(current_time(model)), max|u|: $(maximum(abs, u))"
+data_fetcher = EveryXSimulationTime(24hour) do
+    @info "Step: $(steps(model)), hours: $(current_time(model) / hour), max|u|: $(maximum(abs, u))"
 
     push!(
         fetched_states,
-        (u = assemble(u), θ = assemble(θ), time = current_time(model)),
+        (u = assemble(u),
+         θ = assemble(θ),
+         η = assemble(η),
+         time = current_time(model)),
     )
 end
 
 # and then run the simulation.
 
-model.solver_configuration.timeend = 100 * model.solver_configuration.dt
+day = 24hour
+
+model.solver_configuration.timeend = 300day
 
 result = ClimateMachine.invoke!(
     model.solver_configuration;
     user_callbacks = [data_fetcher],
 )
 
-# Finally, we make an animation of the evolving shear instability.
+# Finally, we make an animation of the double gyre spinning up.
 
 animation = @animate for (i, state) in enumerate(fetched_states)
     @info "Plotting frame $i of $(length(fetched_states))..."
@@ -101,12 +106,14 @@ animation = @animate for (i, state) in enumerate(fetched_states)
     x, y, = state.u.x, state.u.y
 
     u_plot = contourf(x, y, state.u.data[:, :, 1]'; color = :balance, kwargs...)
+    η_plot = contourf(x, y, state.η.data[:, :, 1]'; color = :balance, kwargs...)
     θ_plot = contourf(x, y, state.θ.data[:, :, 1]'; color = :thermal, kwargs...)
 
-    u_title = @sprintf("u at t = %.2f", state.time)
-    θ_title = @sprintf("θ at t = %.2f", state.time)
+    u_title = @sprintf("u (m s⁻¹) at t = %.2f days", state.time / day)
+    η_title = @sprintf("η (m) at t = %.2f days", state.time / day)
+    θ_title = @sprintf("θ (ᵒC) at t = %.2f days", state.time / day)
 
-    plot(u_plot, θ_plot, title = [u_title θ_title], size = (1200, 500))
+    plot(u_plot, η_plot, θ_plot, title = [u_title η_title θ_title], size = (1200, 500), layout=(1, 3))
 end
 
-gif(animation, "shear_instability.gif", fps = 8)
+gif(animation, "double_gyre.gif", fps = 8)
