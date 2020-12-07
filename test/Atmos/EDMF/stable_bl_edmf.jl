02137048ef4c51137c854033d7a22927045c4fda
yairchn <yairchn@caltech.edu>
2020-12-07 08:31:59 -0800

    WIP add daniel's solver

--- test/Atmos/EDMF/stable_bl_edmf.jl
+++ test/Atmos/EDMF/stable_bl_edmf.jl
@@ -223,15 +223,64 @@ function main(::Type{FT}) where {FT}
             discrete_splitting = false,
             # discrete_splitting = true,
         )
-    # isothermal zonal flow
-    # ode_solver_type = ClimateMachine.IMEXSolverType(
-    #     implicit_model = AtmosAcousticGravityLinearModel,
-    #     implicit_solver = ManyColumnLU,
-    #     solver_method = ARK2GiraldoKellyConstantinescu,
-    #     split_explicit_implicit = false,
-    #     discrete_splitting = true,
-    # )
     end
+
+    #################### Change the ode_solver to implicit solver
+    CFLmax = FT(25)
+    dg = solver_config.dg
+    Q = solver_config.Q
+
+
+    vdg = DGModel(
+        driver_config;
+        state_auxiliary = dg.state_auxiliary,
+        direction = VerticalDirection(),
+    )
+
+
+    # linear solver relative tolerance rtol which should be slightly smaller than the nonlinear solver tol
+    linearsolver = BatchedGeneralizedMinimalResidual(
+        dg,
+        Q;
+        max_subspace_size = 30,
+        atol = -1.0,
+        rtol = 5e-5,
+    )
+
+    """
+    N(q)(Q) = Qhat  => F(Q) = N(q)(Q) - Qhat
+    F(Q) == 0
+    ||F(Q^i) || / ||F(Q^0) || < tol
+    """
+    # ϵ is a sensity parameter for this problem, it determines the finite difference Jacobian dF = (F(Q + ϵdQ) - F(Q))/ϵ
+    # I have also try larger tol, but tol = 1e-3 does not work
+    nonlinearsolver =
+        JacobianFreeNewtonKrylovSolver(Q, linearsolver; tol = 1e-4, ϵ = 1.e-10)
+
+    # this is a second order time integrator, to change it to a first order time integrator
+    # change it ARK1ForwardBackwardEuler, which can reduce the cost by half at the cost of accuracy 
+    # and stability
+    # preconditioner_update_freq = 50 means updating the preconditioner every 50 Newton solves, 
+    # update it more freqent will accelerate the convergence of linear solves, but updating it 
+    # is very expensive
+    ode_solver = ARK2ImplicitExplicitMidpoint(
+        dg,
+        vdg,
+        NonLinearBackwardEulerSolver(
+            nonlinearsolver;
+            isadjustable = true,
+            preconditioner_update_freq = 50,
+        ),
+        Q;
+        dt = solver_config.dt,
+        t0 = 0,
+        split_explicit_implicit = false,
+        variant = NaiveVariant(),
+    )
+
+    solver_config.solver = ode_solver
+
+    #######################################
 
 
     # old version
