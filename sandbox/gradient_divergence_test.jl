34c217f5890aaa48a71cc1060c4688890f892c7e
sandreza <andrenogueirasouza@gmail.com>
2020-12-17 14:20:12 -0500

    brandon writes functions that call gradient kernels to get div grad and curl. hurray!

--- sandbox/gradient_divergence_test.jl
+++ sandbox/gradient_divergence_test.jl
@@ -103,6 +103,8 @@ L∞(x) = maximum(abs.(x))
     @test L∞(v_∇Q) < tol # since piecewise constant
     @test L∞(total_divergence - total_trace_gradient) < tol
 end
+
+
 ##
 abstract type AbstractCalculus end
 
@@ -113,10 +115,18 @@ end
 function grad(Q::MPIStateArray, _1)
     Q_x1 = view(Q, (:,:,_1))
 
-    event = launch_volume_gradient!(grid, v_∇Q, Q, nrealelem, device)
+    v_∇Q_x1 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
+    s_∇Q_x1 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
+
+    event = launch_volume_gradient!(grid, v_∇Q, Q_x1)
     wait(event)
-    event = launch_interface_gradient!(grid, s_∇Q, Q, device)
+
+    event = launch_interface_gradient!(grid, s_∇Q, Q_x1)
     wait(event)
+
+    grad = v_∇Q_x1 + s_∇Q_x1
+
+    return grad
 end
 
 function grad(Q::MPIStateArray, _1, _2, _3)
@@ -132,19 +142,18 @@ function grad(Q::MPIStateArray, _1, _2, _3)
     s_∇Q_x2 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
     s_∇Q_x3 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
 
-
-    event_x1 = launch_volume_gradient!(grid, v_∇Q, Q_x1)
-    event_x2 = launch_volume_gradient!(grid, v_∇Q, Q_x2)
-    event_x3 = launch_volume_gradient!(grid, v_∇Q, Q_x3)
-
+    ## strong form gradients here
+    event_x1 = launch_volume_gradient!(grid, v_∇Q_x1, Q_x1)
+    event_x2 = launch_volume_gradient!(grid, v_∇Q_x2, Q_x2)
+    event_x3 = launch_volume_gradient!(grid, v_∇Q_x3, Q_x3)
     wait(event_x1)
     wait(event_x2)
     wait(event_x3)
 
-    event_x1 = launch_interface_gradient!(grid, s_∇Q, Q_x1)
-    event_x2 = launch_interface_gradient!(grid, s_∇Q, Q_x2)
-    event_x3 = launch_interface_gradient!(grid, s_∇Q, Q_x3)
-    
+    ## strong form gradients here
+    event_x1 = launch_interface_gradient!(grid, s_∇Q_x1, Q_x1)
+    event_x2 = launch_interface_gradient!(grid, s_∇Q_x2, Q_x2)
+    event_x3 = launch_interface_gradient!(grid, s_∇Q_x3, Q_x3)
     wait(event_x1)
     wait(event_x2)
     wait(event_x3)
@@ -159,48 +168,21 @@ end
 function div(Q::MPIStateArray, _1, _2, _3)
     ∇Q_x1, ∇Q_x2, ∇Q_x3 = grad(Q, _1, _2, _3)
 
+    div = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 1)
+    div[:,:,1] = ∇Q_x1[:,:,1] + ∇Q_x2[:,:,2] + ∇Q_x3[:,:,3]
 
+    return div
 end
 
 function curl(Q::MPIStateArray, _1, _2, _3)
-    Q_x1 = view(Q, (:,:,_1))
-    Q_x2 = view(Q, (:,:,_2))
-    Q_x3 = view(Q, (:,:,_3))
+    ∇Q_x1, ∇Q_x2, ∇Q_x3 = grad(Q, _1, _2, _3)
 
-    v_∇Q_x1 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
-    v_∇Q_x2 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
-    v_∇Q_x3 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
+    curl = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
 
-    s_∇Q_x1 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
-    s_∇Q_x2 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
-    s_∇Q_x3 = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
+    curl[:,:,1] = ∇Q_x3[:,:,2] - ∇Q_x2[:,:,3]
+    curl[:,:,2] = ∇Q_x1[:,:,3] - ∇Q_x3[:,:,1]
+    curl[:,:,3] = ∇Q_x2[:,:,1] - ∇Q_x1[:,:,2]
 
-
-    event_x1 = launch_volume_gradient!(grid, v_∇Q, Q_x1)
-    event_x2 = launch_volume_gradient!(grid, v_∇Q, Q_x2)
-    event_x3 = launch_volume_gradient!(grid, v_∇Q, Q_x3)
-
-    wait(event_x1)
-    wait(event_x2)
-    wait(event_x3)
-
-    event_x1 = launch_interface_gradient!(grid, s_∇Q, Q_x1)
-    event_x2 = launch_interface_gradient!(grid, s_∇Q, Q_x2)
-    event_x3 = launch_interface_gradient!(grid, s_∇Q, Q_x3)
-    
-    wait(event_x1)
-    wait(event_x2)
-    wait(event_x3)
-
-    ∇Q_x1 = v_∇Q_x1 + s_∇Q_x1
-    ∇Q_x2 = v_∇Q_x2 + s_∇Q_x2
-    ∇Q_x3 = v_∇Q_x3 + s_∇Q_x3
-
-    ω = MPIStateArray{FT}(mpicomm, ArrayType, ijksize, nrealelem, 3)
-
-    ω[:,:,1] = ∇Q_x3[:,:,2] - ∇Q_x2[:,:,3]
-    ω[:,:,2] = ∇Q_x1[:,:,3] - ∇Q_x3[:,:,1]
-    ω[:,:,3] = ∇Q_x2[:,:,1] - ∇Q_x1[:,:,2]
-
+    return curl
 end
 
