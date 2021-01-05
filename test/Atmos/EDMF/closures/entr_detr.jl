49869ce76f1b639c4a4806bd756f6b2b33dc8740
yairchn <yairchn@caltech.edu>
2021-01-05 10:41:24 -0800

    add if commands i n entr and press funcs

--- test/Atmos/EDMF/closures/entr_detr.jl
+++ test/Atmos/EDMF/closures/entr_detr.jl
@@ -59,46 +59,52 @@ function entr_detr(
 
     N_up = n_updrafts(m.turbconv)
     ρ_inv = 1 / gm.ρ
-    a_up_i = up[i].ρa * ρ_inv
-    lim_E = entr.lim_ϵ
-    lim_amp = entr.lim_amp
-    w_min = entr.w_min
-    # precompute vars
-    w_up_i = up[i].ρaw / up[i].ρa
-    sqrt_tke = sqrt(max(en.ρatke, 0) * ρ_inv / env.a)
-    # ensure far from zero
-    Δw = filter_w(w_up_i - env.w, w_min)
-    w_up_i = filter_w(w_up_i, w_min)
-    Δb = up_aux[i].buoyancy - en_aux.buoyancy
-    D_E, D_δ, M_δ, M_E = nondimensional_exchange_functions(
-        m,
-        entr,
-        state,
-        aux,
-        ts_up,
-        ts_en,
-        env,
-        i,
-    )
+    if up[i].ρa*ρ_inv>m.turbconv.subdomains.a_min
+        a_up_i = up[i].ρa * ρ_inv
+        lim_E = entr.lim_ϵ
+        lim_amp = entr.lim_amp
+        w_min = entr.w_min
+        # precompute vars
+        w_up_i = up[i].ρaw / up[i].ρa
+        sqrt_tke = sqrt(max(en.ρatke, 0) * ρ_inv / env.a)
+        # ensure far from zero
+        Δw = filter_w(w_up_i - env.w, w_min)
+        w_up_i = filter_w(w_up_i, w_min)
+        Δb = up_aux[i].buoyancy - en_aux.buoyancy
+        D_E, D_δ, M_δ, M_E = nondimensional_exchange_functions(
+            m,
+            entr,
+            state,
+            aux,
+            ts_up,
+            ts_en,
+            env,
+            i,
+        )
 
-    # I am commenting this out for now, to make sure there is no slowdown here
-    Λ_w = abs(Δb / Δw)
-    Λ_tke = entr.c_λ * abs(Δb / (max(en.ρatke * ρ_inv, 0) + w_min))
-    λ = lamb_smooth_minimum(
-        SVector(Λ_w, Λ_tke),
-        m.turbconv.mix_len.smin_ub,
-        m.turbconv.mix_len.smin_rm,
-    )
+        # I am commenting this out for now, to make sure there is no slowdown here
+        Λ_w = abs(Δb / Δw)
+        Λ_tke = entr.c_λ * abs(Δb / (max(en.ρatke * ρ_inv, 0) + w_min))
+        λ = lamb_smooth_minimum(
+            SVector(Λ_w, Λ_tke),
+            m.turbconv.mix_len.smin_ub,
+            m.turbconv.mix_len.smin_rm,
+        )
 
-    # compute entrainment/detrainment components
-    E_trb =
-        2 * up[i].ρa * entr.c_t * sqrt_tke /
-        max(m.turbconv.pressure.H_up, entr.H_up_min)
-    E_dyn = up[i].ρa * λ * (D_E + M_E)
-    Δ_dyn = up[i].ρa * λ * (D_δ + M_δ)
+        # compute entrainment/detrainment components
+        E_trb =
+            2 * up[i].ρa * entr.c_t * sqrt_tke /
+            max(m.turbconv.pressure.H_up, entr.H_up_min)
+        E_dyn = up[i].ρa * λ * (D_E + M_E)
+        Δ_dyn = up[i].ρa * λ * (D_δ + M_δ)
 
-    E_dyn = max(E_dyn, FT(0))
-    Δ_dyn = max(Δ_dyn, FT(0))
-    E_trb = max(E_trb, FT(0))
+        E_dyn = max(E_dyn, FT(0))
+        Δ_dyn = max(Δ_dyn, FT(0))
+        E_trb = max(E_trb, FT(0))
+    else
+        E_dyn = FT(0)
+        Δ_dyn = FT(0)
+        E_trb = FT(0)
+    end
     return E_dyn, Δ_dyn, E_trb
 end;
