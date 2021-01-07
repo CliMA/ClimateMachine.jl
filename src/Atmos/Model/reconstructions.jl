5f291793264d4cd07f0f454cffe8e38982db1bec
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2021-01-06 16:22:31 -0800

    Fix HB reconstruction on the GPU

--- src/Atmos/Model/reconstructions.jl
+++ src/Atmos/Model/reconstructions.jl
@@ -18,8 +18,6 @@ function (hb_recon::HBFVReconstruction)(
     cell_states::SVector{D},
     cell_weights,
 ) where {D}
-
-
     FT = eltype(state_bot)
     vars_prim = Vars{vars_state(hb_recon._atmo, Primitive(), FT)}
     _grav = FT(grav(hb_recon._atmo.param_set))
@@ -29,46 +27,46 @@ function (hb_recon::HBFVReconstruction)(
     stencil_width = div(D - 1, 2)
     stencil_center = stencil_width + 1
     # save the pressure Δpressure_ref states
-    ps = similar(state_bot, stencil_diameter)
-    ρgΔz_half = similar(state_bot, stencil_diameter)
+    ps = similar(state_bot, Size(stencil_diameter))
+    ρgΔz_half = similar(state_bot, Size(stencil_diameter))
 
-    @unroll for i in 1:stencil_diameter
-        ps[i] = vars_prim(cell_states[i]).p
-        ρgΔz_half[i] = vars_prim(cell_states[i]).ρ * _grav * cell_weights[i] / 2
-    end
+    @inbounds begin
+        @unroll for i in 1:stencil_diameter
+            ps[i] = vars_prim(cell_states[i]).p
+            ρgΔz_half[i] =
+                vars_prim(cell_states[i]).ρ * _grav * cell_weights[i] / 2
+        end
 
-    # construct reference pressure steates and update pressure states
-    p_ref = ps[stencil_center]
-    p_bot_ref = p_ref + ρgΔz_half[stencil_center]
-    p_top_ref = p_ref - ρgΔz_half[stencil_center]
+        # construct reference pressure steates and update pressure states
+        p_ref = ps[stencil_center]
+        p_bot_ref = p_ref + ρgΔz_half[stencil_center]
+        p_top_ref = p_ref - ρgΔz_half[stencil_center]
 
-    vars_prim(cell_states[stencil_center]).p -= p_ref
+        vars_prim(cell_states[stencil_center]).p -= p_ref
 
-    p⁺_ref, p⁻_ref = p_ref, p_ref
-    @unroll for i in 1:stencil_width
-        # stencil_center - i , stencil_center - i + 1
-        p⁻_ref +=
-            ρgΔz_half[stencil_center - i + 1] + ρgΔz_half[stencil_center - i]
-        vars_prim(cell_states[stencil_center - i]).p -= p⁻_ref
+        p⁺_ref, p⁻_ref = p_ref, p_ref
+        @unroll for i in 1:stencil_width
+            # stencil_center - i , stencil_center - i + 1
+            p⁻_ref +=
+                ρgΔz_half[stencil_center - i + 1] +
+                ρgΔz_half[stencil_center - i]
+            vars_prim(cell_states[stencil_center - i]).p -= p⁻_ref
 
-        # stencil_center + i - 1 , stencil_center + i
-        p⁺_ref -=
-            ρgΔz_half[stencil_center + i - 1] + ρgΔz_half[stencil_center + i]
-        vars_prim(cell_states[stencil_center + i]).p -= p⁺_ref
-    end
+            # stencil_center + i - 1 , stencil_center + i
+            p⁺_ref -=
+                ρgΔz_half[stencil_center + i - 1] +
+                ρgΔz_half[stencil_center + i]
+            vars_prim(cell_states[stencil_center + i]).p -= p⁺_ref
+        end
 
+        hb_recon._recon(state_bot, state_top, cell_states, cell_weights)
 
+        vars_prim(state_bot).p += p_bot_ref
+        vars_prim(state_top).p += p_top_ref
 
-
-    hb_recon._recon(state_bot, state_top, cell_states, cell_weights)
-
-    vars_prim(state_bot).p += p_bot_ref
-    vars_prim(state_top).p += p_top_ref
-
-
-
-    # reverse the pressure states back
-    @unroll for i in 1:stencil_diameter
-        vars_prim(cell_states[i]).p = ps[i]
+        # reverse the pressure states back
+        @unroll for i in 1:stencil_diameter
+            vars_prim(cell_states[i]).p = ps[i]
+        end
     end
 end
