8edf6d4fcbf4e02750b60191099edd9378cd8d14
yairchn <yair.chn@gmail.com>
2021-01-28 12:50:34 -0800

    use one driver for both FV and DG in SBL

---
+++ test/Atmos/EDMF/report_mse_sbl.jl
@@ -0,0 +1,49 @@
+using ClimateMachine
+const clima_dir = dirname(dirname(pathof(ClimateMachine)));
+
+if parse(Bool, get(ENV, "CLIMATEMACHINE_PLOT_EDMF_COMPARISON", "false"))
+    plot_dir = joinpath(clima_dir, "output", "sbl_edmf", "pycles_comparison")
+else
+    plot_dir = nothing
+end
+
+include(joinpath(@__DIR__, "compute_mse.jl"))
+
+data_file = Dataset(joinpath(PyCLES_output_dataset_path, "Gabls.nc"), "r")
+
+#! format: off
+best_mse = OrderedDict()
+best_mse["prog_ρ"] = 8.3680112312261336e-03
+best_mse["prog_ρu_1"] = 6.2785134765912162e+03
+best_mse["prog_ρu_2"] = 2.4045175004577872e-04
+best_mse["prog_turbconv_environment_ρatke"] = 2.4914848140548023e+02
+best_mse["prog_turbconv_environment_ρaθ_liq_cv"] = 8.7727430628661253e+01
+best_mse["prog_turbconv_updraft_1_ρa"] = 3.7451325681073804e+01
+best_mse["prog_turbconv_updraft_1_ρaw"] = 2.2105942785946642e+00
+best_mse["prog_turbconv_updraft_1_ρaθ_liq"] = 1.0323459917678932e+01
+#! format: on
+
+computed_mse = compute_mse(
+    solver_config.dg.grid,
+    solver_config.dg.balance_law,
+    time_data,
+    dons_arr,
+    data_file,
+    "Gabls",
+    best_mse,
+    60,
+    plot_dir,
+)
+
+@testset "SBL EDMF Solution Quality Assurance (QA) tests" begin
+    #! format: off
+    test_mse(computed_mse, best_mse, "prog_ρ")
+    test_mse(computed_mse, best_mse, "prog_ρu_1")
+    test_mse(computed_mse, best_mse, "prog_ρu_2")
+    test_mse(computed_mse, best_mse, "prog_turbconv_environment_ρatke")
+    test_mse(computed_mse, best_mse, "prog_turbconv_environment_ρaθ_liq_cv")
+    test_mse(computed_mse, best_mse, "prog_turbconv_updraft_1_ρa")
+    test_mse(computed_mse, best_mse, "prog_turbconv_updraft_1_ρaw")
+    test_mse(computed_mse, best_mse, "prog_turbconv_updraft_1_ρaθ_liq")
+    #! format: on
+end
