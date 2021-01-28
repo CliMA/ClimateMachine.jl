9e3f6175f31a4ddfc3df3e391aa9a3c7bccf0fca
yairchn <yairchn@caltech.edu>
2021-01-28 11:12:44 -0800

    add if for dry model
    WIP update bomex table

--- test/Atmos/EDMF/report_mse_bomex.jl
+++ test/Atmos/EDMF/report_mse_bomex.jl
@@ -12,7 +12,7 @@ include(joinpath(@__DIR__, "compute_mse.jl"))
 data_file = Dataset(joinpath(PyCLES_output_dataset_path, "Bomex.nc"), "r")
 
 #! format: off
-best_mse = Dict()
+best_mse = OrderedDict()
 best_mse["prog_ρ"] = 3.4917543567416755e-02
 best_mse["prog_ρu_1"] = 3.0715061616506719e+03
 best_mse["prog_ρu_2"] = 1.2895273328644972e-03
