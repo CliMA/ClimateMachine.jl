0cd1e337f4ab8acff0b161eadb89e7d05a613467
christophernhill <cnh@mit.edu>
2021-01-29 23:30:52 -0500

    source! experiment

--- experiments/CouplingDesignTests/CplTestingBL.jl
+++ experiments/CouplingDesignTests/CplTestingBL.jl
@@ -219,7 +219,7 @@ Land
   for export to coupler.
 """
 function source!(bl::l_type,S::Vars,Q::Vars,G::Vars,A::Vars,_...)
-  S.θ=bl.bl_prop.source_theta(S.θ,A.npt,A.elnum,A.xc,A.yc,A.zc)
+  S.θ=bl.bl_prop.source_theta(S.θ,A.npt,A.elnum,A.xc,A.yc,A.zc,A.boundary_in)
   # Record boundary condition fluxes as needed by adding to shadow 
   # prognostic variable
   S.θ_boundary_export=bl.bl_prop.theta_shadow_boundary_flux(S.θ_boundary_export,A.boundary_in,A.npt,A.elnum,A.xc,A.yc,A.zc)
