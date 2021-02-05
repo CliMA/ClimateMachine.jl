71c97415aff57a020d426a63158714a968fa7829
jbmackay <jbmackay@caltech.edu>
2021-02-05 18:23:15 -0500

    Clean up benchmarking and create plots.

--- test/Land/Model/infiltration_benchmark.jl
+++ test/Land/Model/infiltration_benchmark.jl
@@ -85,7 +85,7 @@ m = LandModel(
 
 
 N_poly = 1
-nelem_vert = 30
+nelem_vert = 50
 
 # Specify the domain boundaries
 zmax = FT(0)
@@ -137,74 +137,108 @@ function setupsolver(nlalg, driver_config, t0, timeend, dt)
     return solver_config
 end
 
-# (Q, f) -> JacobianFreeNewtonKrylovSolver(Q, BatchedGeneralizedMinimalResidual(
-#     f,
-#     Q;
-#     max_subspace_size = 30,
-#     atol = -1.0,
-#     rtol = 1e-9,
-# ); tol = 1e-9)
-
-# Newton + (B)GMRES
+tol = 1e-12
 algs = (
-    N_FD = (Q, f) -> JacobianFreeNewtonKrylovAlgorithm(
-                GeneralizedMinimalResidualAlgorithm(;
+    N_FD_30 = (Q, f) -> JacobianFreeNewtonKrylovAlgorithm(
+                BatchedGeneralizedMinimalResidualAlgorithm(;
                     preconditioner = ColumnwiseLUPreconditioner(f, Q, 100),
-                    M = 30, atol = eps(Float64), rtol = 1e-9,
+                    M = 30, atol = tol, rtol = tol,
                 );
-            atol = 1e-9, rtol = 1e-9, autodiff=false),
-    N_AD = (Q, f) -> JacobianFreeNewtonKrylovAlgorithm(
-                GeneralizedMinimalResidualAlgorithm(;
+            maxiters = Int(1e3), atol = tol, rtol = tol, autodiff=false),
+    N_AD_30 = (Q, f) -> JacobianFreeNewtonKrylovAlgorithm(
+                BatchedGeneralizedMinimalResidualAlgorithm(;
                     preconditioner = ColumnwiseLUPreconditioner(f, Q, 100),
-                    M = 30, atol = eps(Float64), rtol = 1e-9,
+                    M = 30, atol = tol, rtol = tol,
                 );
-            atol = 1e-9, rtol = 1e-9, autodiff=true),
-
-    P = (Q, f) -> StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000),
-    PA_1_1 = (Q, f) -> AndersonAccelerationAlgorithm(
-                StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
-                depth = 1
-            ),
+            maxiters = Int(1e3), atol = tol, rtol = tol, autodiff=true),
+    # OldFD_30 = (Q, f) -> JacobianFreeNewtonKrylovSolver(Q, BatchedGeneralizedMinimalResidual(
+    #             f,
+    #             Q;
+    #             max_subspace_size = 30,
+    #             atol = tol,
+    #             rtol = tol,
+    #         ); M = Int(1e3), tol = tol),
+    # P = (Q, f) -> StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4)),
+    # PA_1_1 = (Q, f) -> AndersonAccelerationAlgorithm(
+    #             StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
+    #             depth = 1
+    #         ),
     PA_2_1 = (Q, f) -> AndersonAccelerationAlgorithm(
-            StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
+            StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
             depth = 2
         ),
     PA_3_1 = (Q, f) -> AndersonAccelerationAlgorithm(
-            StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
+            StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
             depth = 3
         ),
     PA_4_1 = (Q, f) -> AndersonAccelerationAlgorithm(
-            StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
+            StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
             depth = 4
         ),
-    PA_1_8 = (Q, f) -> AndersonAccelerationAlgorithm(
-            StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
-            depth = 1, ω = 0.8,
-        ),
+    # PA_1_8 = (Q, f) -> AndersonAccelerationAlgorithm(
+    #         StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
+    #         depth = 1, ω = 0.8,
+    #     ),
     PA_2_8 = (Q, f) -> AndersonAccelerationAlgorithm(
-            StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
+            StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
             depth = 2, ω = 0.8,
         ),
     PA_3_8 = (Q, f) -> AndersonAccelerationAlgorithm(
-            StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
+            StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
             depth = 3, ω = 0.8,
         ),
     PA_4_8 = (Q, f) -> AndersonAccelerationAlgorithm(
-            StandardPicardAlgorithm(; atol = 1e-9, rtol = 1e-9, maxiters = 1000);
+            StandardPicardAlgorithm(; atol = tol, rtol = tol, maxiters = Int(1e4));
             depth = 4, ω = 0.8,
         ),
+    N_FD_15 = (Q, f) -> JacobianFreeNewtonKrylovAlgorithm(
+            BatchedGeneralizedMinimalResidualAlgorithm(;
+                preconditioner = ColumnwiseLUPreconditioner(f, Q, 100),
+                M = 15, maxrestarts = 21, atol = tol, rtol = tol,
+            );
+        maxiters = Int(1e3), atol = tol, rtol = tol, autodiff=false),
+    N_AD_15 = (Q, f) -> JacobianFreeNewtonKrylovAlgorithm(
+                BatchedGeneralizedMinimalResidualAlgorithm(;
+                    preconditioner = ColumnwiseLUPreconditioner(f, Q, 100),
+                    M = 15, maxrestarts = 21, atol = tol, rtol = tol,
+                );
+            maxiters = Int(1e3), atol = tol, rtol = tol, autodiff=true),
+    # OldFD_15 = (Q, f) -> JacobianFreeNewtonKrylovSolver(Q, BatchedGeneralizedMinimalResidual(
+    #             f,
+    #             Q;
+    #             max_subspace_size = 15,
+    #             atol = tol,
+    #             rtol = tol,
+    #         ); M = Int(1e3), tol = tol),
 )
 
 # precompile stuff to not screw with timing - must be Newton AD/FD + GMRES, Picard, Picard + Anderson
-for i in 1:4
-    solver_config = setupsolver(algs[i], driver_config, 0.0, 1.0, 1.0)
+for key in keys(algs)[1:4]
+    @info string(key)
+    solver_config = setupsolver(algs[key], driver_config, 0.0, 1.0, 1.0)
     @elapsed ClimateMachine.invoke!(solver_config)
+    GC.gc()
 end
 
 t0 = FT(0)
 timeend = FT(60 * 60)
 
-dts = FT[0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10]
+dts = FT[0.5, 1, 2, 5, 7.5, 10, 15, 20]
+
+function benchmark_solver!(nlalg, driver_config, t0, timeend, dt, solvetimes, rmse, finalstates, i, j)
+    solver_config = setupsolver(nlalg, driver_config, t0, timeend, dt)
+
+    dons_arr = Dict[dict_of_nodal_states(solver_config; interp = true)]  # store initial condition at ``t=t0``
+    solvetimes[i, j] = @elapsed ClimateMachine.invoke!(solver_config)
+    push!(dons_arr, dict_of_nodal_states(solver_config; interp = true)); # store final condition at ``t=timeend``
+
+    # interpolation
+    current_profile = dons_arr[2]["soil.water.ϑ_l"][:]
+    simulation_z = dons_arr[2]["z"][:]
+    true_profile = true_moisture_continuous.(simulation_z)
+    rmse[i, j] = sqrt(sum((true_profile .- current_profile).^2.0))
+    finalstates[i, j] = dons_arr[2]
+end
 
 # load "true" data and create interpolation function
 @load joinpath(@__DIR__, "infiltration_truth.jld2") truedata # a dict_of_nodal_states
@@ -214,31 +248,26 @@ N = length(algs)
 M = length(dts)
 rmse = zeros(N, M)
 solvetimes = zeros(N, M)
+finalstates = Array{Any, 2}(nothing, N, M)
 for (i, nlalg) in enumerate(algs)
     for (j, dt) in enumerate(dts)
-        solver_config = setupsolver(nlalg, driver_config, t0, timeend, dt)
-
-        dons_arr = Dict[dict_of_nodal_states(solver_config; interp = true)]  # store initial condition at ``t=t0``
-        solvetimes[i, j] = @elapsed ClimateMachine.invoke!(solver_config)
-        push!(dons_arr, dict_of_nodal_states(solver_config; interp = true)); # store final condition at ``t=timeend``
-
-        # interpolation
-        current_profile = dons_arr[2]["soil.water.ϑ_l"][:]
-        simulation_z = dons_arr[2]["z"][:]
-        true_profile = true_moisture_continuous.(simulation_z)
-        rmse[i, j] = sqrt(sum((true_profile .- current_profile).^2.0))
+        @info "Solver: $(string(keys(algs)[i])), dt: $dt"
+        benchmark_solver!(nlalg, driver_config, t0, timeend, dt, solvetimes, rmse, finalstates, i, j)
+        GC.gc()
     end
 end
-# save some plots and data
+save some plots and data
 labels = [label for label in string.(keys(algs))]
 labels = reshape(labels, (1, N))
+@save joinpath(@__DIR__, "benchmark_data.jld2") solvetimes rmse finalstates dts labels
 timeplot = plot(dts, [solvetimes[i, :] for i in 1:N],
-    title="Solve Times", xlabel="dt", ylabel="total solve time", markershape=:circle, label = labels)
+    title="Solve Times", xlabel="dt", ylabel="total solve time",
+    markershape=:circle, legend=:outertopright, label = labels)
 png(timeplot, joinpath(@__DIR__, "solvetimes"))
 errplot = plot(dts, [rmse[i, :] for i in 1:N],
-    title="RMSE", xlabel="dt", ylabel="rmse", markershape=:circle, legend=:topleft, label = labels)
+    title="RMSE", xlabel="dt", ylabel="rmse",
+    markershape=:circle, legend=:outertopright, label = labels)
 png(errplot, joinpath(@__DIR__, "rmse"))
-@save joinpath(@__DIR__, "benchmark_data.jld2") solvetimes rmse dts
 
 
 # as long as the solution is "good" rmse < 1e-4, what is the tradeoff
