f695bf8aafa810a62684dd09c8f79f0a699b1f4b
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2021-02-06 18:05:42 -0800

    Add ES shallow water and bickley jet experiment

---
+++ test/Numerics/ESDGMethods/ShallowWater/ShallowWater.jl
@@ -0,0 +1,288 @@
+using ClimateMachine.VariableTemplates: Vars, Grad, @vars
+using ClimateMachine.BalanceLaws
+import ClimateMachine.BalanceLaws:
+    BalanceLaw,
+    vars_state,
+    state_to_entropy_variables!,
+    entropy_variables_to_state!,
+    nodal_init_state_auxiliary!,
+    init_state_prognostic!,
+    state_to_entropy,
+    boundary_conditions,
+    boundary_state!,
+    wavespeed,
+    flux_first_order!,
+    source!
+using StaticArrays
+using ClimateMachine.DGMethods.NumericalFluxes: NumericalFluxFirstOrder
+import ClimateMachine.DGMethods.NumericalFluxes:
+    EntropyConservative,
+    numerical_volume_conservative_flux_first_order!,
+    numerical_volume_fluctuation_flux_first_order!,
+    ave,
+    numerical_flux_first_order!
+using ClimateMachine.Grids
+
+abstract type AbstractShallowWaterProblem end
+
+struct ShallowWaterModel{P, FT} <: BalanceLaw
+    problem::P
+    g::FT
+end
+
+boundary_conditions(::ShallowWaterModel) = (0,)
+
+function boundary_state!(
+    ::NumericalFluxFirstOrder,
+    bctype,
+    ::ShallowWaterModel,
+    state⁺,
+    aux⁺,
+    n,
+    state⁻,
+    aux⁻,
+    _...,
+)
+end
+
+function init_state_prognostic!(
+    m::ShallowWaterModel,
+    args...,
+)
+  init_state_prognostic!(m, m.problem, args...)
+end
+
+function nodal_init_state_auxiliary!(
+    m::ShallowWaterModel,
+    state_auxiliary,
+    tmp,
+    geom,
+)
+  init_state_auxiliary!(m, m.problem, state_auxiliary, geom)
+end
+
+
+function init_state_auxiliary!(
+    ::ShallowWaterModel,
+    ::AbstractShallowWaterProblem,
+    state_auxiliary,
+    geom,
+)
+end
+
+function wavespeed(m::ShallowWaterModel,
+                   nM,
+                   state::Vars,
+                   aux::Vars,
+                   t::Real,
+                   direction)
+  ρ = state.ρ
+  ρu = state.ρu
+
+  u = ρu / ρ
+  v = @SVector [u[1], u[2], -0]
+  uN = abs(dot(nM, v))
+  return uN + sqrt(m.g * ρ)
+end
+
+function vars_state(::ShallowWaterModel, ::Prognostic, FT)
+    @vars begin
+        ρ::FT
+        ρu::SVector{2, FT}
+        ρθ::FT
+    end
+end
+
+function vars_state(m::ShallowWaterModel, st::Auxiliary, FT)
+    @vars begin
+        problem::vars_state(m, m.problem, st, FT)
+    end
+end
+vars_state(::ShallowWaterModel, ::AbstractShallowWaterProblem, ::Auxiliary, FT) = @vars()
+
+function vars_state(::ShallowWaterModel, ::Entropy, FT)
+    @vars begin
+        ρ::FT
+        ρu::SVector{2, FT}
+    end
+end
+
+#"""
+#    state_to_entropy_variables!(
+#        ::DryAtmosModel,
+#        entropy::Vars,
+#        state::Vars,
+#        aux::Vars,
+#    )
+#
+#See [`BalanceLaws.state_to_entropy_variables!`](@ref)
+#"""
+#function state_to_entropy_variables!(
+#    ::DryAtmosModel,
+#    entropy::Vars,
+#    state::Vars,
+#    aux::Vars,
+#)
+#    ρ, ρu, ρe, Φ = state.ρ, state.ρu, state.ρe, aux.Φ
+#
+#    FT = eltype(state)
+#    γ = FT(gamma(param_set))
+#
+#    p = pressure(ρ, ρu, ρe, Φ)
+#    s = log(p / ρ^γ)
+#    b = ρ / 2p
+#    u = ρu / ρ
+#
+#    entropy.ρ = (γ - s) / (γ - 1) - (dot(u, u) - 2Φ) * b
+#    entropy.ρu = 2b * u
+#    entropy.ρe = -2b
+#    entropy.Φ = 2ρ * b
+#end
+#
+#function entropy_variables_to_state!(
+#    ::DryAtmosModel,
+#    state::Vars,
+#    aux::Vars,
+#    entropy::Vars,
+#)
+#    FT = eltype(state)
+#    β = entropy
+#    γ = FT(gamma(param_set))
+#
+#    b = -β.ρe / 2
+#    ρ = β.Φ / (2b)
+#    ρu = ρ * β.ρu / (2b)
+#
+#    p = ρ / (2b)
+#    s = log(p / ρ^γ)
+#    Φ = dot(ρu, ρu) / (2 * ρ^2) - ((γ - s) / (γ - 1) - β.ρ) / (2b)
+#
+#    ρe = p / (γ - 1) + dot(ρu, ρu) / (2ρ) + ρ * Φ
+#
+#    state.ρ = ρ
+#    state.ρu = ρu
+#    state.ρe = ρe
+#    aux.Φ = Φ
+#end
+#
+function state_to_entropy(m::ShallowWaterModel, state::Vars, aux::Vars)
+    FT = eltype(state)
+    ρ, ρu = state.ρ, state.ρu
+    g = m.g
+    η = ρu' * ρu / 2ρ + g * ρ ^ 2 / 2
+    return η
+end
+
+function numerical_volume_conservative_flux_first_order!(
+    ::EntropyConservative,
+    m::ShallowWaterModel,
+    F::Grad,
+    state_1::Vars,
+    aux_1::Vars,
+    state_2::Vars,
+    aux_2::Vars,
+)
+    g = m.g
+    FT = eltype(F)
+    ρ_1, ρu_1, ρθ_1 = state_1.ρ, state_1.ρu, state_1.ρθ
+    ρ_2, ρu_2, ρθ_2 = state_2.ρ, state_2.ρu, state_2.ρθ
+    
+    u_1 = ρu_1 / ρ_1
+    u_2 = ρu_2 / ρ_2
+    
+    θ_1 = ρθ_1 / ρ_1
+    θ_2 = ρθ_2 / ρ_2
+
+    ρ_avg = ave(ρ_1, ρ_2)
+    ρ²_avg = ave(ρ_1^2, ρ_2^2)
+    u_avg = ave(u_1, u_2)
+    ρu_avg = ave(ρu_1, ρu_2)
+    ρθ_avg = ave(ρθ_1, ρθ_2)
+    θ_avg = ave(θ_1, θ_2)
+
+    Iʰ = @SMatrix [ 1 -0
+                   -0  1
+                   -0 -0]
+
+
+    ρv_avg = @SVector [ρu_avg[1], ρu_avg[2], -0]
+    v_avg = @SVector [u_avg[1], u_avg[2], -0]
+
+    Fρ = ρv_avg
+    Fρu = ρv_avg * u_avg' + g * (ρ_avg ^ 2 - ρ²_avg / 2) * Iʰ
+    
+    # not sure what's better for tracer
+    #Fρθ = v_avg * ρθ_avg
+    Fρθ = ρv_avg * θ_avg
+
+    F.ρ += Fρ
+    F.ρu += Fρu
+    F.ρθ += Fρθ
+end
+
+function numerical_volume_fluctuation_flux_first_order!(
+    ::EntropyConservative,
+    ::ShallowWaterModel,
+    D::Grad,
+    state_1::Vars,
+    aux_1::Vars,
+    state_2::Vars,
+    aux_2::Vars,
+)
+end
+
+source!(::ShallowWaterModel, _...) = nothing
+
+struct EntropyConservativeWithPenalty <: NumericalFluxFirstOrder end
+function numerical_flux_first_order!(
+    numerical_flux::EntropyConservativeWithPenalty,
+    balance_law::BalanceLaw,
+    fluxᵀn::Vars{S},
+    normal_vector::SVector,
+    state_prognostic⁻::Vars{S},
+    state_auxiliary⁻::Vars{A},
+    state_prognostic⁺::Vars{S},
+    state_auxiliary⁺::Vars{A},
+    t,
+    direction,
+) where {S, A}
+
+    FT = eltype(fluxᵀn)
+
+    numerical_flux_first_order!(
+        EntropyConservative(),
+        balance_law,
+        fluxᵀn,
+        normal_vector,
+        state_prognostic⁻,
+        state_auxiliary⁻,
+        state_prognostic⁺,
+        state_auxiliary⁺,
+        t,
+        direction,
+    )
+    fluxᵀn = parent(fluxᵀn)
+
+    wavespeed⁻ = wavespeed(
+        balance_law,
+        normal_vector,
+        state_prognostic⁻,
+        state_auxiliary⁻,
+        t,
+        direction,
+    )
+    wavespeed⁺ = wavespeed(
+        balance_law,
+        normal_vector,
+        state_prognostic⁺,
+        state_auxiliary⁺,
+        t,
+        direction,
+    )
+    max_wavespeed = max.(wavespeed⁻, wavespeed⁺)
+    penalty =
+        max_wavespeed .*
+        (parent(state_prognostic⁻) - parent(state_prognostic⁺))
+
+    fluxᵀn .+= penalty / 2
+end
---
+++ test/Numerics/ESDGMethods/ShallowWater/bickley_jet.jl
@@ -0,0 +1,263 @@
+using MPI
+using ClimateMachine
+using Logging
+using ClimateMachine.DGMethods: ESDGModel, init_ode_state
+using ClimateMachine.Mesh.Topologies: StackedBrickTopology
+using ClimateMachine.Mesh.Grids: DiscontinuousSpectralElementGrid, min_node_distance
+using ClimateMachine.Thermodynamics
+using LinearAlgebra
+using Printf
+using Dates
+using ClimateMachine.GenericCallbacks:
+    EveryXWallTimeSeconds, EveryXSimulationSteps
+using ClimateMachine.VTK: writevtk, writepvtu
+using ClimateMachine.VariableTemplates: flattenednames
+using ClimateMachine.ODESolvers
+using StaticArrays: @SVector
+using LazyArrays
+
+using DoubleFloats
+using GaussQuadrature
+GaussQuadrature.maxiterations[Double64] = 40
+
+if !@isdefined integration_testing
+    const integration_testing = parse(
+        Bool,
+        lowercase(get(ENV, "JULIA_CLIMA_INTEGRATION_TESTING", "false")),
+    )
+end
+
+const output = parse(Bool, lowercase(get(ENV, "JULIA_CLIMA_OUTPUT", "false")))
+
+include("ShallowWater.jl")
+include("../diagnostics.jl")
+
+struct BickleyJet <: AbstractShallowWaterProblem end
+
+function init_state_prognostic!(bl::ShallowWaterModel, 
+                                ::BickleyJet,
+                                state, aux, localgeo, t)
+    FT = eltype(state)
+    (x, y, z) = localgeo.coord
+
+    ϵ = 0.1 # perturbation magnitude
+    l = 0.5 # Gaussian width
+    k = 0.5 # Sinusoidal wavenumber
+
+    # The Bickley jet
+    U = cosh(y)^(-2)
+
+    # Slightly off-center vortical perturbations
+    Ψ = exp(-(y + l / 10)^2 / (2 * (l^2))) * cos(k * x) * cos(k * y)
+
+    # Vortical velocity fields (ũ, ṽ) = (-∂ʸ, +∂ˣ) ψ̃
+    u = Ψ * (k * tan(k * y) + y / (l^2))
+    v = -Ψ * k * tan(k * x)
+
+    ρ = 1
+
+    state.ρ = ρ
+    state.ρu = ρ * @SVector [U + ϵ * u, ϵ * v]
+    state.ρθ = ρ * sin(k * y)
+end
+
+function main()
+    ClimateMachine.init()
+    ArrayType = ClimateMachine.array_type()
+    mpicomm = MPI.COMM_WORLD
+    
+    #FT = Double64
+    FT = Float64
+    timeend = FT(200)
+    surfaceflux = EntropyConservativeWithPenalty
+    
+    Lx = FT(4π)
+    Ly = FT(4π)
+
+    for Ndof in (32, 64)
+      for N in (3, 4)
+        Ne = round(Int, Ndof / (N+1))
+        run(
+            mpicomm,
+            Ndof,
+            N,
+            Ne,
+            Lx,
+            Ly,
+            timeend,
+            ArrayType,
+            FT,
+            surfaceflux
+        )
+      end
+    end
+end
+
+function run(
+    mpicomm,
+    Ndof,
+    N,
+    Ne,
+    Lx,
+    Ly,
+    timeend,
+    ArrayType,
+    FT,
+    surfaceflux
+)
+
+    dim = 2
+    brickrange = (
+        range(FT(-Lx / 2), stop = Ly / 2, length = Ne + 1),
+        range(FT(-Lx / 2), stop = Ly / 2, length = Ne + 1),
+    )
+    boundary = ((0, 0), (0, 0))
+    periodicity = (true, true)
+    topology = StackedBrickTopology(
+        mpicomm,
+        brickrange,
+        periodicity = periodicity,
+        boundary = boundary,
+    )
+    grid = DiscontinuousSpectralElementGrid(
+        topology,
+        FloatType = FT,
+        DeviceArray = ArrayType,
+        polynomialorder = N,
+    )
+
+    problem = BickleyJet()
+    g = FT(10)
+    model = ShallowWaterModel(problem, g)
+
+    esdg = ESDGModel(
+        model,
+        grid;
+        volume_numerical_flux_first_order = EntropyConservative(),
+        surface_numerical_flux_first_order = surfaceflux(),
+    )
+
+    # determine the time step
+    dx = min_node_distance(grid)
+    cfl = FT(1.0)
+    dt = cfl * dx / sqrt(10)
+    outputtime = 2
+    dt = outputtime / ceil(outputtime / dt)
+    nsteps = ceil(Int, timeend / dt)
+
+    Q = init_ode_state(esdg, FT(0))
+
+    η = similar(Q, vars = @vars(η::FT), nstate=1)
+    ∫η0 = entropy_integral(esdg, η, Q)
+    #η_int = function(dg, Q1)
+    #  entropy_integral(dg, η, Q1)
+    #end
+    #η_prod = function(dg, Q1, Q2)
+    #  entropy_product(dg, η, Q1, Q2)
+    #end
+
+    odesolver = LSRK144NiegemannDiehlBusch(esdg, Q; dt = dt, t0 = 0)
+    #odesolver = RLSRK144NiegemannDiehlBusch(esdg, η_int, η_prod, Q; dt = dt, t0 = 0)
+    
+
+    eng0 = norm(Q)
+    @info @sprintf """Starting
+                      ArrayType       = %s
+                      FT              = %s
+                      polynomialorder = %d
+                      numelem         = %d
+                      dt              = %.16e
+                      norm(Q₀)        = %.16e
+                      ∫η              = %.16e
+                      """ "$ArrayType" "$FT" N Ne dt eng0 ∫η0
+
+    # Set up the information callback
+    starttime = Ref(now())
+    cbinfo = EveryXSimulationSteps(100) do (s = false)
+        if s
+            starttime[] = now()
+        else
+            ∫η = entropy_integral(esdg, η, Q)
+            dη = (∫η - ∫η0) / abs(∫η0)
+            energy = norm(Q)
+            runtime = Dates.format(
+                convert(DateTime, now() - starttime[]),
+                dateformat"HH:MM:SS",
+            )
+            @info @sprintf """Update
+                              simtime            = %.16e
+                              runtime            = %s
+                              norm(Q)            = %.16e
+                              ∫η                 = %.16e
+                              (∫η - ∫η0) / |∫η0| = %.16e 
+                              """ gettime(odesolver) runtime energy ∫η dη
+        end
+    end
+    callbacks = (cbinfo,)
+
+    output_vtk = true
+    if output_vtk
+        # create vtk dir
+        vtkdir = "esdg_bickley_jet/$surfaceflux/Ndof$(Ndof)/N$(N)"
+        mkpath(vtkdir)
+
+        vtkstep = 0
+        # output initial step
+        do_output(mpicomm, vtkdir, vtkstep, esdg, Q, model)
+
+        # setup the output callback
+        cbvtk = EveryXSimulationSteps(floor(outputtime / dt)) do
+            vtkstep += 1
+            do_output(mpicomm, vtkdir, vtkstep, esdg, Q, model)
+        end
+        callbacks = (callbacks..., cbvtk)
+    end
+
+    solve!(Q, odesolver; callbacks = callbacks, numberofsteps = nsteps)
+
+    # final statistics
+    engf = norm(Q)
+    ∫ηf = entropy_integral(esdg, η, Q)
+    dηf = (∫ηf - ∫η0) / abs(∫η0)
+    @info @sprintf """Finished
+    norm(Q)                 = %.16e
+    norm(Q) / norm(Q₀)      = %.16e
+    norm(Q) - norm(Q₀)      = %.16e
+    ∫η                      = %.16e
+    (∫η - ∫η0) / |∫η0|      = %.16e 
+    """ engf engf / eng0 engf - eng0 ∫ηf dηf
+    engf
+end
+
+function do_output(mpicomm, vtkdir, vtkstep, esdg, Q, model, testname = "bickley")
+    ## name of the file that this MPI rank will write
+    filename = @sprintf(
+        "%s/%s_mpirank%04d_step%04d",
+        vtkdir,
+        testname,
+        MPI.Comm_rank(mpicomm),
+        vtkstep
+    )
+
+    statenames = flattenednames(vars_state(model, Prognostic(), eltype(Q)))
+    auxnames = flattenednames(vars_state(model, Auxiliary(), eltype(Q)))
+
+    writevtk(filename, Q, esdg, statenames, esdg.state_auxiliary, auxnames; number_sample_points = 10)
+
+    ## Generate the pvtu file for these vtk files
+    if MPI.Comm_rank(mpicomm) == 0
+        ## name of the pvtu file
+        pvtuprefix = @sprintf("%s/%s_step%04d", vtkdir, testname, vtkstep)
+
+        ## name of each of the ranks vtk files
+        prefixes = ntuple(MPI.Comm_size(mpicomm)) do i
+            @sprintf("%s_mpirank%04d_step%04d", testname, i - 1, vtkstep)
+        end
+
+        writepvtu(pvtuprefix, prefixes, (statenames..., auxnames...), eltype(Q))
+
+        @info "Done writing VTK: $pvtuprefix"
+    end
+end
+
+main()
