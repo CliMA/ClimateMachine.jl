95b3f0ca76289cc1c390bcad5dbe38e83c8fb301
ilopezgp <ilopezgp@gmail.com>
2021-04-04 11:24:14 -0700

    Dispatch on coupling mode
    Add coupled SBL EDMF and MSE reporting

---
+++ test/Atmos/EDMF/report_mse_sbl_coupled_edmf_an1d.jl
@@ -0,0 +1,49 @@
+using ClimateMachine
+const clima_dir = dirname(dirname(pathof(ClimateMachine)));
+
+if parse(Bool, get(ENV, "CLIMATEMACHINE_PLOT_EDMF_COMPARISON", "false"))
+    plot_dir = joinpath(clima_dir, "output", "sbl_edmf", "pycles_comparison")
+else
+    plot_dir = nothing
+end
+
+include(joinpath(@__DIR__, "compute_mse.jl"))
+
+data_file = Dataset(joinpath(PyCLES_output_dataset_path, "Gabls.nc"), "r")
+
+#! format: off
+best_mse = OrderedDict()
+best_mse["prog_ρ"] = 9.3808122133551899e-03
+best_mse["prog_ρu_1"] = 6.7648718002912910e+03
+best_mse["prog_ρu_2"] = 8.9568591962189448e-01
+best_mse["prog_turbconv_environment_ρatke"] = 3.9877808420252154e+02
+best_mse["prog_turbconv_environment_ρaθ_liq_cv"] = 8.1270320241616531e+01
+best_mse["prog_turbconv_updraft_1_ρa"] = 2.7223017351724246e+02
+best_mse["prog_turbconv_updraft_1_ρaw"] = 5.5909371368198686e+02
+best_mse["prog_turbconv_updraft_1_ρaθ_liq"] = 2.7933498788454813e+02
+#! format: on
+
+computed_mse = compute_mse(
+    solver_config.dg.grid,
+    solver_config.dg.balance_law,
+    time_data,
+    dons_arr,
+    data_file,
+    "Gabls",
+    best_mse,
+    1800,
+    plot_dir,
+)
+
+@testset "SBL Coupled EDMF Solution Quality Assurance (QA) tests" begin
+    #! format: off
+    test_mse(computed_mse, best_mse, "prog_ρ")
+    test_mse(computed_mse, best_mse, "prog_ρu_1")
+    test_mse(computed_mse, best_mse, "prog_ρu_2")
+    test_mse(computed_mse, best_mse, "prog_turbconv_environment_ρatke")
+    test_mse(computed_mse, best_mse, "prog_turbconv_environment_ρaθ_liq_cv")
+    test_mse(computed_mse, best_mse, "prog_turbconv_updraft_1_ρa")
+    test_mse(computed_mse, best_mse, "prog_turbconv_updraft_1_ρaw")
+    test_mse(computed_mse, best_mse, "prog_turbconv_updraft_1_ρaθ_liq")
+    #! format: on
+end
