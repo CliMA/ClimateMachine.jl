ed9bc139edadd6a787defb016cc53e22218b16d6
Katherine Deck <katherinedeck@Katherines-MacBook-Pro.local>
2021-05-12 12:14:37 -0700

    added snow module

---
+++ src/Land/Model/SnowModel.jl
@@ -0,0 +1,30 @@
+module SnowModel
+
+using DocStringExtensions
+using CLIMAParameters
+using CLIMAParameters.Planet: cp_l, cp_i, T_0, LH_f0
+
+
+"""
+    ρc_snow(l::FT,
+            ρ_snow::FT,
+            param_set::AbstractParamSet
+            ) where {FT}
+
+Computes the volumetric heat capacity given the liquid water
+fraction l and snow density ρ_snow.
+"""
+function ρc_snow(l::FT,
+                 ρ_snow::FT,
+                 param_set::AbstractParamSet
+                 ) where {FT}
+    _c_i = FT(cp_i(param_set))
+    _c_l = FT(cp_l(param_set))
+    c_snow = _c_i*(FT(1.0)-l) + l*_c_i
+    ρc_snow = ρ_snow*c_snow
+    return ρc_snow
+end
+
+
+
+end
