4cbd028fbb9e943e575f138e72c8f4af76655dc7
Isabella M. (Isa) Dula Razzolini <idularaz@login2.cm.cluster>
2021-06-23 15:48:46 -0700

    Organized variable names and combined tests for activation

--- test/Atmos/Parameterizations/Microphysics/runtests.jl
+++ test/Atmos/Parameterizations/Microphysics/runtests.jl
@@ -365,6 +365,12 @@ end
 
 end
 
+
+
+#-----------------------------------------------------------------------------------------------#
+
+
+
 @testset "hygroscopicity_test" begin
 
     # assumptions: standard conditions
@@ -372,11 +378,11 @@ end
     osmotic_coefficient = 3 # no units
     temperature = 298 # K
     aerosol_density = 1770 # kg/m^3
-    aerosol_molecular_weight = 0.132 # kg/mol
-    aerosol_particle_density = 1.0*10^-4 # 1/cm^3
+    aerosol_molecular_mass = 0.132 # kg/mol
+    aerosol_particle_density = 100000000# 1/cm^3
     water_density = 1000 # kg/m^3
-    water_particle_density = 22.414 # kg/mol
-    water_molecular_weight = 0.01801528 # kg/mol
+    water_molar_density = 0.022414 # m^3/mol
+    water_molecular_mass = 0.01801528 # kg/mol
 
     # hand calculated value
     updraft_velocity = 3
@@ -385,24 +391,24 @@ end
 
     h = (updraft_velocity * osmotic_coefficient 
         * (aerosol_particle_density * 1/avogadro 
-        * aerosol_molecular_weight)/(1/water_particle_density
-        * 1/1000 * water_molecular_weight) * water_molecular_weight
-        * aerosol_particle_density) / (aerosol_molecular_weight
+        * aerosol_molecular_mass)/(1/water_molar_density
+        * 1/1000 * water_molecular_mass) * water_molecular_mass
+        * aerosol_particle_density) / (aerosol_molecular_mass
         * water_density)
 
     @test hygroscopicity(
                osmotic_coefficient,
                temperature,
                aerosol_density,
-               aerosol_molecular_weight,
+               aerosol_molecular_mass,
                aerosol_particle_density
                water_density,
-               water_particle_density,
-               water_molecular_weight,
+               water_molar_density,
+               water_molecular_mass,
                ) ≈ h
 end 
 
-# @testset "Mean Hygroscopicity" begin
+@testset "Mean Hygroscopicity" begin
     aerosol_component = [1, 2, 3]         
     aerosol_mode_number = [1, 2, 3, 4, 5]
     mass_mixing_ratio = [[1, 2, 3, 4, 5]
@@ -411,68 +417,162 @@ end
     disassociation = [[1, 2, 3, 4, 5]
                       [0.1, 0.2, 0.3, 0.4, 0.5]
                       [0.01, 0.02, 0.03, 0.04, 0.05]]
-    phi = [[1, 2, 3, 4, 5]
+    osmotic_coefficient = [[1, 2, 3, 4, 5]
            [0.1, 0.2, 0.3, 0.4, 0.5]
            [0.01, 0.02, 0.03, 0.04, 0.05]]
-    epsilon = [[1, 2, 3, 4, 5]
+    mass_fraction = [[1, 2, 3, 4, 5]
                [0.1, 0.2, 0.3, 0.4, 0.5]
                [0.01, 0.02, 0.03, 0.04, 0.05]]
-    molecular_weight = [[1, 2, 3, 4, 5]
+    aerosol_molecular_mass = [[1, 2, 3, 4, 5]
                         [0.1, 0.2, 0.3, 0.4, 0.5]
                         [0.01, 0.02, 0.03, 0.04, 0.05]]
     add_top = 0
     add_bottom = 0
-    water_molecular_weight = 0.01801528 # kg/mol
+     water_molecular_mass = 0.01801528 # kg/mol
     water_density = 1000 # kg/m^3
     m_h = zeros(3)
     for i in 1:length(aerosol_mode_number)
-        for j in 1:length(aerosol_component)
+        for j in 1:length(aerosowater_molecular_weightl_component)
             add_top = mass_mixing_ratio[i][j] 
                       * disassociation[i][j] 
-                      * phi[i][j]
-                      * epsilon[i][j]
-                      * molecular_weight[i][j]
+                      * osmotic_coefficient[i][j]
+                      * mass_fraction[i][j]
+                      * aerosol_molecular_mass[i][j]
         end
-        m_h[i] = water_molecular_weight * (add_top) / (add_bottom * water_density)
+        m_h[i] = water_molecular_mass * (add_top) / (add_bottom * water_density)
     end
 
     @test mean_hygroscopicity(aerosol_mode_number[1], 
                               aerosol_component), 
                               mass_mixing_ratio,
                               disassociation,
-                              phi,
-                              epsilon,
-                              molecular_weight,
+                              osmotic_coefficient,
+                              mass_fraction,
+                              aerosol_molecular_mass,
                               ) ≈ m_h[1]
     @test mean_hygroscopicity(aerosol_component[2], 
                               aerosol_mode_number), 
                               mass_mixing_ratio,
                               disassociation,
-                              phi,
-                              epsilon,
-                              molecular_weight,
+                              osmotic_coefficient,
+                              mass_fraction,
+                              aerosol_molecular_mass,
                               ) ≈ m_h[2]
     @test mean_hygroscopicity(aerosol_component[3], 
                               aerosol_mode_number), 
                               mass_mixing_ratio,
                               disassociation,
-                              phi,
-                              epsilon,
-                              molecular_weight,
+                              osmotic_coefficient,
+                              mass_fraction,
+                              aerosol_molecular_mass,
                               ) ≈ m_h[3]
 end
 
 
+@testset "max_supersat_test" begin
+    # parameters inputted into function:
+    particle_radius = [5*(10^(-8))] # particle mode radius (m)
+    particle_radius_stdev = [2] # standard deviation of mode radius (m)
+    activation_time = [1] # time of activation (s)                                                  
+    water_molecular_mass = [0.01801528] # Molecular weight of water (kg/mol)
+    water_density  = [1000] # Density of water (kg/m^3)
+    R = [8.31446261815324] # Gas constant (kg*m^2/s^2*K*mol)
+    temperature = [273.15] # Temperature (K)
+    alpha = [1] # Coefficient in superaturation balance equation       
+    updraft_velocity = [1] # Updraft velocity (m/s)
+    diffusion = [1] # Diffusion of heat and moisture for particles 
+    aerosol_particle_density = [100000000] # Initial particle concentration (1/m^3)
+    gamma = [1] # coefficient 
+
+    # Internal calculations:
+    B_bar = mean_hygrosopicity() # calculated in earlier function    ------ INCOMPLETE-------
+    f = 0.5 .* exp(2.5*(log.(particle_radius_stdev)).^2) # function of particle_radius_stdev (check units)
+    g = 1 .+ 0.25 .* log.(particle_radius_stdev) # function of particle_radius_stdev (log(m))
+    A = (2.*activation_time.*water_molecular_mass)./(water_density .*R.*temperature) # Surface tension effects on Kohler equilibrium equation (s/(kg*m))
+    S_min = ((2)./(B_i_bar).^(.5)).*((A)./(3.*particle_radius)).^(3/2) # Minimum supersaturation
+    zeta = ((2.*A)./(3)).*((alpha.*updraft_velocity)/(diffusion)).^(.5) # dependent parameter 
+    eta = (  ((alpha.*updraft_velocity)./(diffusion)).^(3/2)  ./    (2*pi.*water_density .*gamma.*aerosol_particle_density)   )    # dependent parameter
+
+    
+    # Final value for maximum supersaturation:
+    MS = sum(((1)./(((S_min).^2) * (    f_i.*((zeta./eta).^(3/2))     +    g_i.*(((S_min.^2)./(eta+3.*zeta)).^(3/4))    ) )))
+
+    # Comaparing calculated MS value to function output: 
+    @test maxsupersat(particle_radius, particle_radius_stdev, activation_time, water_molecular_mass, water_density , R, temperature, B_i_bar, alpha, updraft_velocity, diffusion, aerosol_particle_density, gamma) = MS
+end
+
+@testset "smallactpartdryrad_test" begin
+    # Parameter inputs:
+    particle_radius = [5*(10^(-8))] # particle mode radius (m)
+    activation_time = [1] # time of activation (s)                                                  
+    water_molecular_mass = [0.01801528] # Molecular weight of water (kg/mol)
+    water_density  = [1000] # Density of water (kg/m^3)
+    R = [8.31446261815324] # Gas constant (kg)
+    temperature = [273.15] # Temperature (K)
+    particle_radius_stdev = [2] # standard deviation of mode radius (m)
+    alpha = [1] # Coefficient in superaturation balance equation       
+    updraft_velocity = [1] # Updraft velocity (m/s)
+    diffusion = [1] # Diffusion of heat and moisture for particles 
+    aerosol_particle_density = [100000000] # Initial particle concentration (1/m^3)
+    gamma = [1] # coefficient 
+    
+    # Internal calculations:
+    B_bar = mean_hygrosopicity() # calculated in earlier function    ------ INCOMPLETE-------
+    A = (2.*activation_time.*water_molecular_mass)./(water_density .*R.*temperature) # Surface tension effects on Kohler equilibrium equation (s/(kg*m))
+    S_min = ((2)./(B_i_bar).^(.5)).*((A)./(3.*particle_radius)).^(3/2) # Minimum supersaturation
+    S_max = maxsupersat(particle_radius, particle_radius_stdev activation_time, water_molecular_mass, water_density , R, temperature, B_i_bar, alpha, updraft_velocity, diffusion, aerosol_particle_density, gamma)
+    
+    # Final calculation:
+    DRSAP = particle_radius.*((S_mi)./(S_max)).^(2/3)
+    
+    # Running test:
+    @test smallactpartdryrad(particle_radius, particle_radius_stdev, activation_time, water_molecular_mass, water_density , R, temperature, B_i_bar, alpha, updraft_velocity, diffusion, aerosol_particle_density, gamma) = DRSAP
+    
+    end
+        
+    Pkg.add("SpecialFunctions")
+@testset "total_N_Act_test" begin
+    # Input parameters
+    particle_radius = [5*(10^(-8))] # particle mode radius (m)
+    activation_time = [1] # time of activation (s)                                                  
+    water_molecular_mass = [0.01801528] # Molecular weight of water (kg/mol)
+    water_density  = [1000] # Density of water (kg/m^3)
+    R = [8.31446261815324] # Gas constant (kg)
+    temperature = [273.15] # Temperature (K)
+    particle_radius_stdev = [2] # standard deviation of mode radius (m)
+    alpha = [1] # Coefficient in superaturation balance equation       
+    updraft_velocity = [1] # Updraft velocity (m/s)
+    diffusion = [1] # Diffusion of heat and moisture for particles 
+    aerosol_particle_density = [100000000] # Initial particle concentration (1/m^3)
+    gamma = [1] # coefficient 
+    
+    # Internal calculations:
+    B_bar = mean_hygrosopicity() # calculated in earlier function    ------ INCOMPLETE-------
+    A = (2.*activation_time.*water_molecular_mass)./(water_density .*R.*temperature) # Surface tension effects on Kohler equilibrium equation (s/(kg*m))
+    S_min = ((2)./(B_bar).^(.5)).*((A)./(3.*particle_radius)).^(3/2) # Minimum supersaturation
+    S_max = maxsupersat(particle_radius, particle_radius_stdev activation_time, water_molecular_mass, water_density , R, temperature, B_i_bar, alpha, updraft_velocity, diffusion, aerosol_particle_density, gamma)
+
+    u = ((2*log.(S_min/S_max))./(3.*(2.^.5).*log.(particle_radius_stdev)))
+    # Final Calculation: 
+    totN = sum(aerosol_particle_density.*.5.*(1-erf.(u)))
+
+    # Compare results:
+    @test total_N_Act(particle_radius, activation_time, water_molecular_mass, water_density , R, temperature, B_i_bar, particle_radius_stdev, alpha, updraft_velocity, diffusion, aerosol_particle_density, gamma) = totN
+
+end
+
+
+
 #=
     Test conditions:
-        B = v, phi, epsilon, M_w, rho_a, M_a, rho_w 
-        v = number of ions the salt dissociates into within water → 3
-        phi = osmotic coefficient → 1
-        epsilon = mass fraction of soluble material → ????
-        M_w = molecular weight of water → 18.01528 g/mol
-        Rho_a = density of the aerosol material → 1.77 g/cm^3
+        B = updraft_velocity, osmotic_coefficient, mass_fraction, water_molecular_mass, aerosol_density, M_a, water_density  
+        updraft_velocity = number of ions the salt dissociates into within water → 3
+        osmotic_coefficient = osmotic coefficient → 1
+        mass_fraction = mass fraction of soluble material → ????
+        water_molecular_mass = molecular weight of water → 18.01528 g/mol
+        aerosol_density = density of the aerosol material → 1.77 g/cm^3
         M_a = molecular weight of aerosol material → 132
-        Rho_w = density of water → 1 g/cm^3
+        water_density  = density of water → 1 g/cm^3
 
     Test for different values of the variables
 =#
