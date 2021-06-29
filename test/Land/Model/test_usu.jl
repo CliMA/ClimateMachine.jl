dd266c3a5df49f30515b74d74bd00465e2fc81ae
Katherine Deck <kdeck@caltech.edu>
2021-06-29 11:59:19 -0700

    cleaned up a bit more

--- test/Land/Model/test_usu.jl
+++ test/Land/Model/test_usu.jl
@@ -1,7 +1,5 @@
-# to do - make it so bulk ρc_snow can be different...
-# figure out if Qsurf changes if Tsurf changes, so that even the bulk T changes if we change from EQ to FR
-# map between U and ρe_int
-# 
+# how to set G? Is there a single set of ρ,κ_snow that leads to fitting Tsurf, Tbottom, and Tbulk?
+
 
 using MPI
 using OrderedCollections
@@ -53,12 +51,25 @@ Qsurf = -Qsurf # sign convention is opposite as ours
 G = data[:, data[1,:] .== "G (kJ/hr/m2)"][2:end, :][indexrange] ./ 3600 .*1000 # per second
 dates = data[:, data[1,:] .== "Date"][2:end, :][indexrange]
 
+ρ_snow = FT(220) #this is a guess based on "max depth = 0.5m with SWE = 0.14m"
+κ_air = FT(0.023)
+κ_ice = FT(2.29)
+κ_snow = FT(κ_air + (7.75*1e-5 *ρ_snow + 1.105e-6*ρ_snow^2)*(κ_ice-κ_air))
+swe = FT(mean((data[:, data[1,:] .== "mod_SWE(m)"][2:end,:][indexrange])))
+
+z_snow = 1e3 * swe ./ ρ_snow
 
 
-uobs = data[:, data[1,:] .== "obs_int (kJ)"][2:end,:][indexrange]
-ρcz = (1e3*swe*2100+2100*1700*0.1)./1000
-tobs = uobs ./ ρcz .+ 273.15
+# actual observed value
+Tsurf = FT.(data[:, data[1,:] .== "obs_Ts (C)"][2:end,:][indexrange]) .+ 273.15
 
+
+bulk_uobs = data[:, data[1,:] .== "obs_int (kJ)"][2:end,:][indexrange]
+ρcz = (1e3*swe*2100+2100*1700*0.1)./1000 # they give U in kJ/m^2
+bulk_tobs = bulk_uobs ./ ρcz .+ 273.15 # Really, this is estimated from the Tobs profile.
+
+
+# Now we estimate the snow, soil and mixed snow/soil bulk T from the profile
 layer_two_below = FT.(data[:, data[1,:] .== "obs_-7.5cm"][2:end,:][indexrange]) .+ 273.15
 layer_one_below = FT.(data[:, data[1,:] .== "obs_-2.5cm"][2:end,:][indexrange]) .+ 273.15
 ground_T =  FT.(data[:, data[1,:] .== "obs_0cm"][2:end,:][indexrange]) .+ 273.15
@@ -67,32 +78,25 @@ layer2 =    FT.(data[:, data[1,:] .== "obs_12.5cm"][2:end,:][indexrange]) .+ 273
 layer3 =    FT.(data[:, data[1,:] .== "obs_20cm"][2:end,:][indexrange]) .+ 273.15
 layer4 =    FT.(data[:, data[1,:] .== "obs_27.5cm"][2:end,:][indexrange]) .+ 273.15
 layer5 =    FT.(data[:, data[1,:] .== "obs_35cm"][2:end,:][indexrange]) .+ 273.15
-estimated = (ground_T .* 2.5 .+ layer1 .* (8.75-2.5) .+ layer2 .*(16.25-8.75) .+ layer3 .* (23.75-16.25) .+ layer4*(31.25 .- 23.75) .+ layer5*(36 - 31.25) .+ Tsurf .*(z_snow*100 - 36)) ./(z_snow*100)
+# Snow alone (constant params ρc)
+halfway = (z_snow +0.35)/2*100
+estimated = (ground_T .* 2.5 .+ layer1 .* (8.75-2.5) .+ layer2 .*(16.25-8.75) .+ layer3 .* (23.75-16.25) .+ layer4*(31.25 .- 23.75) .+ layer5*(halfway - 31.25) .+ Tsurf .*(z_snow*100 - halfway)) ./(z_snow*100)
+# Ground alone (constant ρc)
 estimated_ground = ((10-5) .* layer_two_below .+ (5-1.25) .* layer_one_below .+ ground_T *(1.25)) ./ 10
+# Here is our estimate of Tobs based on the observed profile.
+# Idea is to "integrate" over profile dU ( = ρcz T), then divide by ρcz (c is the same for snow and soil in their model) to get bulk T
 total_estimated = (estimated_ground .* 10 .*1700 .+ estimated .* (z_snow*100).*ρ_snow) ./ (1e3*swe*100 + 1700*0.1*100)
 
+κ_soil = 2.5^0.6*0.57^0.4 # hacky OOM guess
+#ρc_snow = volumetric_heat_capacity(0.0, ρ_snow, param_set)
+#ν = FT(2.0*π/24/3600)
+#d = (FT(2)*κ_snow/(ρc_snow*ν))^FT(0.5)
 
+ground_5cm = (layer_one_below .+ layer_two_below) ./2
 
+estimated_G = -2*κ_snow*κ_soil/(κ_soil+κ_snow)/(0.05)*(layer1 .- ground_5cm)
+estimated_G2 = -κ_snow/0.05*(layer1 .- ground_T)
 
-
-
-Tave = FT.(data[:, data[1,:] .== "mod_Tave(C)"][2:end,:][indexrange]) .+ 273.15
-Tsurf = FT.(data[:, data[1,:] .== "obs_Ts (C)"][2:end,:][indexrange]) .+ 273.15
-ρ_snow = FT(280) #this is a guess based on "max depth = 0.5m with SWE = 0.14m"
-κ_air = FT(0.023)
-κ_ice = FT(2.29)
-κ_snow = FT(κ_air + (7.75*1e-5 *ρ_snow + 1.105e-6*ρ_snow^2)*(κ_ice-κ_air))
-estimated_G = -κ_snow/0.05*(layer1 .- ground_T)
-divflux = -(Qsurf.-estimated_G)./z_snow
-
-swe = FT(mean((data[:, data[1,:] .== "mod_SWE(m)"][2:end,:][indexrange])))
-
-z_snow = 1e3 * swe ./ ρ_snow
-ρc_snow = volumetric_heat_capacity(0.0, ρ_snow, param_set)
-ν = FT(2.0*π/24/3600)
-d = (FT(2)*κ_snow/(ρc_snow*ν))^FT(0.5)
-#or use layer 1 - ground T?
-estimated_G = -κ_snow/(z_snow-d)*(estimated .- ground_T)
 
 t = FT.(0:1800:length(Qsurf)*1800-1)
 soil_water_model = PrescribedWaterModel()
@@ -193,37 +197,51 @@ coeffs = compute_profile_coefficients.(qs,qb, Ref(snow_parameters.z_snow), Ref(s
 t_profs = get_temperature_profile.(qs,qb, Ref(snow_parameters.z_snow), Ref(snow_parameters.ρ_snow), Ref(snow_parameters.κ_snow), ρe_int, Ref(param_set))
         
 tsurf_pw = [coeffs[k][1] for k in 1:N]
+tbottom = [coeffs[k][2] for k in 1:N]
+th = [coeffs[k][4] for k in 1:N]
+#We can try different definitions of G and see how that affects bulk I
+# simplest approx to integral is cumsum(div flux) *dt
+
+divflux = -(Qsurf.-estimated_G)./z_snow
 predicted = snow_temperature.(cumsum(divflux*1800) .+ρe_int[1], Ref(0.0), Ref(ρ_snow), Ref(param_set))
 
-plot1 = plot(time_data, T_ave, label = "Our Model, Tave")
-plot!(time_data, tobs, label = "UEB reported; Tave soil/snow")
-plot!(time_data, total_estimated, label = "Est Tave, snow/soil")
-plot!(time_data, estimated, label = "Est Tave, snow only")
-plot!(time_data, predicted, label = "Alternate G, snow only")
+divflux2 = -(Qsurf.-estimated_G2)./z_snow
+predicted2 = snow_temperature.(cumsum(divflux2*1800) .+ρe_int[1], Ref(0.0), Ref(ρ_snow), Ref(param_set))
+
+divflux_orig = -(Qsurf.-G)./z_snow
+predicted_orig = snow_temperature.(cumsum(divflux_orig*1800) .+ρe_int[1], Ref(0.0), Ref(ρ_snow), Ref(param_set))
+
+plot1 = plot(time_data, T_ave, label = "Our Model, snow only")
+#plot!(time_data, bulk_tobs, label = "UEB reported; Tave soil/snow")
+plot!(time_data, total_estimated, label = "Est from data, snow/soil")
+plot!(time_data, estimated, label = "Est from data, snow only")
+#plot!(time_data, predicted, label = "Alt G[-5 to 5], snow only")
+#plot!(time_data, predicted, label = "Our Model, using alt G, snow only")
+plot!(title = "bulk T")
 plot!(legend = :bottomright)
 plot!(xticks = ([time_data[1],time_data[250],time_data[end]], [dates[1], dates[250], dates[end]]))
 
 
-plot2 = plot(indexrange, tsurf_pw, label = "piecewise model, EQ",ylim = [240,300])
-plot!(indexrange, tsurf_obs, label = "obs")
+plot3 = plot(time_data, tbottom, label = "Our Model")
+plot!(time_data, ground_T, label = "ground T")
+plot!(title = "T(0)")
+plot!(legend = :bottomright)
+plot!(xticks = ([time_data[1],time_data[250],time_data[end]], [dates[1], dates[250], dates[end]]))
+
+plot2 = plot(indexrange, tsurf_pw, label = "Our Model, Tsurf")
+plot!(indexrange, Tsurf, label = "Observed Tsurf")
+#plot!(indexrange, tbottom, label = "Our Model, Tbott")
+#plot!(indexrange, ground_T, label = "Observed Tbott")
 plot!(xlabel = "date", ylabel = "T surf (K)", legend = :topleft)
 plot!(xticks = ([1,250,indexrange[end]], [dates[1], dates[250], dates[end]]))
 
 
 
-### Force restore model below, but wait on until we get the EQ right?
-
-
+### Force restore model below, but wait on until we get the EQ right? The output for Tsurf actually looks
+### extremely similar to EQ - so let's work on getting it better in EQ first.
 #=
+# Use the same forcing, and initial condition for ρe_int. But now we need an IC for ρe_surf
 
-mod_flux =data[:, data[1,:] .== "Flux(MFR)"][2:end, :][indexrange]
-Qsurf_fr = -mod_flux
-Qsurf_spline = Spline1D(t, Qsurf)
-function Q_surf(t::FT, Q::Spline1D) where {FT}
-    return Q(t)
-end
-
-forcing = PrescribedForcing(FT;Q_surf = (t) -> eltype(t)(Q_surf(t,Qsurf_spline)),Q_bottom = (t) -> eltype(t)(Q_bott(t,Qbott_spline)))
 Tsurf_0 = Tsurf[1]
 l_0 = FT(0.0)
 ρe_surf0 = volumetric_internal_energy(Tsurf_0, snow_parameters.ρ_snow, l_0, param_set)
@@ -294,39 +312,38 @@ callback = GenericCallbacks.EveryXSimulationTime(every_x_simulation_time) do
     nothing
 end;
     
-    # # Run the integration
+# # Run the integration
 ClimateMachine.invoke!(solver_config; user_callbacks = (callback,));
 z = 0:0.01:FT(snow_parameters.z_snow)
 N = length(dons_arr)
 ρe_int_fr = [dons_arr[k]["snow.ρe_int"][1] for k in 1:N]
-T_ave = snow_temperature.(ρe_int, Ref(0.0), Ref(ρ_snow),Ref(param_set))
+
+
+
+T_ave_fr = snow_temperature.(ρe_int_fr, Ref(0.0), Ref(ρ_snow),Ref(param_set))
 l = 0.0
 ρc_snow = volumetric_heat_capacity(l,ρ_snow,param_set)
 ρe_surf = [dons_arr[k]["snow.ρe_surf"][1] for k in 1:N]
 T_surf_fr = snow_temperature.(ρe_surf, Ref(0.0), Ref(ρ_snow),Ref(param_set))
 z = 0:0.01:z_snow
 T_h = [dons_arr[k]["snow.t_h"][1] for k in 1:N]
-T_bottom = T_h #Q_bottom = 0
 
-plot!(indexrange, T_surf_fr, label = "force restore")
-scatter!(indexrange, T_ave, label = "bulk T")
-#scatter!(indexrange, data[:, data[1,:] .== "2mTair"][2:end, :][indexrange], label = "2mTair")
-#plot(plot1,plot2)
-=#
-#=
-plot3=plot(indexrange, tsurf_pw1, label = "pw, EQ, Qsurf",
-linestyle = :dot,linewidth = 3,
-ylim = [240,280],legend = :false)
-plot!(indexrange, tsurf_pw2, label = "pw, EQ, Flux(MFR)",linestyle = :dot,linewidth = 3)
-plot!(indexrange, tsurf_pw3, label = "pw, EQ, Flux(FR)",linestyle = :dot,linewidth = 3)
-plot!(indexrange, tsurf_pw4, label = "pw, EQ, Flux(Tave)",linestyle = :dot,linewidth = 3)
-plot!(indexrange, tsurf_obs, label = "obs",linecolor = :black)
-plot!(xlabel = "date", ylabel = "T surf (K)")
-plot!(xticks = ([1,250,500], [dates[1], dates[250], dates[500]]))
-plot!(indexrange, T_surf_fr, label = "FR Qsurf",linewidth = 3)
-plot!(indexrange, T_surf_fr2, label = "FR Flux(MFR)",linewidth = 3)
-plot!(indexrange, T_surf_fr3, label = "FR Flux(FR)",linewidth = 3)
-plot!(indexrange, T_surf_fr4, label = "FR Flux(Tave)",linewidth = 3)
-scatter!(indexrange, T_ave, label = "bulk T")
-savefig("USU_qsurf.png")
+
+plot1 = plot(time_data, T_ave, label = "Our Model, G [0,5], snow only")
+plot!(time_data, T_ave_fr, label = "Our Model, G [0,5], snow only, FR")
+#plot!(time_data, bulk_tobs, label = "UEB reported; Tave soil/snow")
+plot!(time_data, total_estimated, label = "Est from data, snow/soil")
+plot!(time_data, estimated, label = "Est from data, snow only")
+#plot!(time_data, predicted, label = "Alt G[-5 to 5], snow only")
+plot!(time_data, predicted_orig, label = "Our Model, using their G, snow only")
+plot!(title = "bulk T")
+plot!(legend = :bottomright)
+plot!(xticks = ([time_data[1],time_data[250],time_data[end]], [dates[1], dates[250], dates[end]]))
+
+
+plot2 = plot(indexrange, tsurf_pw, label = "Our Model, Tsurf",ylim = [245,270])
+plot!(indexrange, T_surf_fr, label = "FR, Us")
+plot!(indexrange, Tsurf, label = "Observed Tsurf")
+plot!(xlabel = "date", ylabel = "T surf (K)", legend = :topleft)
+plot!(xticks = ([1,250,indexrange[end]], [dates[1], dates[250], dates[end]]))
 =#
