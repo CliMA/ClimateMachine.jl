5a564fa72f39ff14f8a8110719a7950bf5aa7302
Shevali M. Kadakia <skadakia@login2.cm.cluster>
2021-06-29 11:14:24 -0700

    cleaning up

--- src/Atmos/Parameterizations/CloudPhysics/Activation.jl
+++ src/Atmos/Parameterizations/CloudPhysics/Activation.jl
@@ -1,5 +1,3 @@
-
-
 Pkg.add("SpecialFunctions")
 module Activation
 
@@ -17,14 +15,6 @@ export activation_cloud_droplet_number
     given in Abdul-Razzak and Ghan (2000).
     
 """
-#--------------------------------------------------------------------#
-# TODO: 
-# --Write function to determine mass fraction of soluble 
-#   material (epsilon)
-#--Determine P_saturation origins (do we get this from model?)
-#--Double check math syntax
-#--------------------------------------------------------------------#
-# CONSTANTS 
 
 WTR_MM = 0.01801528 # kg/mol
 WTR_MLR_ρ = 0.022414 # m^3/mol TODO (used for epsilon)
@@ -34,121 +24,100 @@ AVOGADRO =  6.02214076 × 10^23 # particles/mole (used for epsilon)
 G = 9.81 # m/s^2
 LAT_HEAT_EVP = 2.26 * 10^-6  # J/kg
 SPC_HEAT_AIR = 1000
-T = 273.1 # K (STP)
+TEMP = 273.1 # K (STP)
 P = 100000 # Pa (N/m^2) (STP)
 
-#--------------------------------------------------------------------#
-# Critical supersaturation
-function S_m(activation_time, particle_radius, mass_mixing_ratio, 
-            disassociation, osmotic_coefficient, mass_ratio,
-            aerosol_molecular_mass, aerosol_density)
-    A = A(activation_time)
-    B_bar = mean_hygroscopicity(mass_mixing_ratio, disassociation, 
-            osmotic_coefficient, mass_ratio,
-            aerosol_molecular_mass, aerosol_density)
-    S_m = (2./(B_bar).^.5).*(A./(3.*particle_radius)).^(3/2)
+function S_m(act_time, part_radius, mass_mx_rat, diss, osm_coeff, mass_frac,
+            aero_mm, aero_ρ)
+
+    a = A(act_time)
+    B_bar = mean_hygroscopicity(mass_mx_rat, diss, osm_coeff, mass_frac,
+            aero_mm, aero_ρ)
+    S_m = (2./(B_bar).^.5).*(a./(3.*part_radius)).^(3/2)
     return S_m
+
 end
 
-#--------------------------------------------------------------------#
-# Surface tension effects on Kohler equilibrium equation (s/(kg*m))
-function A(activation_time)
-    A = (2*activation_time.*WTR_MM)./(WTR_ρ.*R.*T)
+function A(act_time)
+
+    A = (2 * act_time .* WTR_MM) ./ (WTR_ρ .* R .* TEMP)
     return A
+
 end
 
-#--------------------------------------------------------------------#
-# Mean Hygroscopicity
-function mean_hygroscopicity(mass_mixing_ratio, disassociation, 
-                             osmotic_coefficient, mass_ratio,
-                             aerosol_molecular_mass, aerosol_density)
+function mean_hygroscopicity(mass_mx_rat, diss, osm_coeff, mass_frac, aero_mm, 
+                             aero_ρ)
     
-    B_bar = zeros(size(mass_mixing_ratio, 2))
-    for i in range(size(mass_mixing_ratio, 2))
-        r = mass_mixing_ratio[i]
-        nu = disassociation[i]
-        phi = osmotic_coefficient[i]
-        epsilon = mass_ratio[i]
-        M_a = aerosol_molecular_mass[i]
-        rho_a = aerosol_density[i]
+    B_bar = zeros(size(mass_mx_rat, 2))
+    for i in range(size(mass_mx_rat, 2))
+        r = mass_mx_rat[i]
+        nu = diss[i]
+        phi = osm_coeff[i]
+        epsilon = mass_frac[i]
+        M_a = aero_mm[i]
+        rho_a = aero_ρ[i]
         B_bar[i] = ((WTR_MM)/(WTR_ρ))*
-                   ((sum(r.*nu.*phi.*epsilon.* (1./M_a)))/(sum(r./rho_a)))
+                   ((sum(r .* nu .* phi .* epsilon .* (1./M_a)))/(sum(r ./ rho_a)))
     end
     return B_bar
+
 end
 
-#--------------------------------------------------------------------#
-# Maximum Supersaturation
-function maxsupersat(particle_radius, particle_radius_stdev, activation_time, 
-                     updraft_velocity, diffusion, 
-                     aerosol_particle_density, mass_mixing_ratio, 
-                     disassociation, 
-                     osmotic_coefficient, mass_ratio,
-                     aerosol_molecular_mass, aerosol_density)
+function maxsupersat(part_radius, part_radius_stdev, act_time, updft_velo, diff, 
+                     aero_part_ρ, mass_mx_rat, diss, osm_coeff, mass_frac,
+                     aero_mm, aero_ρ)
 
     # Internal calculations: 
-    alpha = alpha_sic(aerosol_molecular_mass)
-    f = 0.5.*exp.(2.5*(log.(particle_radius_stdev)).^2)
-    g = 1 .+ 0.25 * log.(particle_radius_stdev)        
-
-    A = A(activation_time)
-
-    S_m = S_m(activation_time, particle_radius, mass_mixing_ratio, 
-              disassociation, osmotic_coefficient, mass_ratio,
-              aerosol_molecular_mass, aerosol_density)
-    
-    gamma = gamma_sic(aerosol_molecular_mass, P_saturation)
-
-    zeta = ((2.*A)./3).*((alpha.*updraft_velocity)./diffusion).^(.5)
-    eta = (((alpha.*updraft_velocity)./diffusion).^(3/2))./(2.*pi.*WTR_ρ.*gamma.*aerosol_particle_density)
+    a = alpha_sic(aero_mm)
+    f = 0.5.*exp.(2.5*(log.(part_radius_stdev)).^2)
+    g = 1 .+ 0.25 * log.(part_radius_stdev)        
+    a = A(act_time)
+    S_m = S_m(act_time, part_radius, mass_mx_rat, 
+              diss, osm_coeff, mass_frac,
+              aero_mm, aero_ρ)
+    gamma = gamma_sic(aero_mm, P_sat)
+    zeta = ((2.*A)./3).*((a.*updft_velo)./diff).^(.5)
+    eta = (((a.*updft_velo)./diff).^(3/2))./(2.*pi.*WTR_ρ.*gamma.*aero_part_ρ)
 
     # Final calculation:
-    mss = sum(1./(((1/S_m.^2).*((f.*(zeta/eta).^(3/2)).+
+    mss = sum(1./(((1/S_m .^ 2).*((f.*(zeta/eta).^(3/2)).+
              (g.*((S_m.^2)./(eta.+3.*zeta)).^(3/4)))).^.5))
 
     return mss
+
 end
 
-#--------------------------------------------------------------------#
-# Total Number of Activated Particles
-function total_N_Act(particle_radius, activation_time, 
-                     particle_radius_stdev, updraft_velocity, 
-                     diffusion, aerosol_particle_density)
+function total_N_Act(part_radius, act_time, part_radius_stdev, updft_velo, diff, 
+                     aero_part_ρ)
 
     # Internal calculations:
-    S_m = S_m(activation_time, particle_radius, mass_mixing_ratio, 
-              disassociation, osmotic_coefficient, mass_ratio,
-              aerosol_molecular_mass, aerosol_density)
+    S_m = S_m(act_time, part_radius, mass_mx_rat, diss, osm_coeff, mass_frac,
+              aero_mm, aero_ρ)
+    S_max = maxsupersat(part_radius, part_radius_stdev, act_time, updft_velo, 
+                        diff, aero_part_ρ, mass_mx_rat, diss, osm_coeff, 
+                        mass_frac, aero_mm, aero_ρ)
+    u = (2.*log.(S_m./S_max))./(3.*(2.^.5).*log.(part_radius_stdev))
 
-    S_max = maxsupersat(particle_radius, particle_radius_stdev, activation_time,
-                        updraft_velocity, diffusion, 
-                        aerosol_particle_density, mass_mixing_ratio, 
-                        disassociation, 
-                        osmotic_coefficient, mass_ratio,
-                        aerosol_molecular_mass, aerosol_density)
-
-    u = (2.*log.(S_m./S_max))./(3.*(2.^.5).*log.(particle_radius_stdev))
-    
     # Final Calculation: 
-    totN = sum(aerosol_particle_density.*.5.*(1-erf.(u)))
-
+    totN = sum(aero_part_ρ.*.5.*(1-erf.(u)))
     return totN
+
 end
 
-#--------------------------------------------------------------------#
-# Size Invariant Coefficients 
-function alpha_sic(aerosol_molecular_mass)
-    a = ((G * WTR_MM * LAT_HEAT_EVP) 
-        / (SPC_HEAT_AIR * R * T^2)) .- 
-        ((G * aerosol_molecular_mass ) ./ (R * T))
+function alpha_sic(aero_mm)
+
+    a = ((G * WTR_MM * LAT_HEAT_EVP) / (SPC_HEAT_AIR * R * T^2)) .- 
+        ((G * aerosol_molecular_mass) ./ (R * T))
     return a
+
 end
 
-function gamma_sic(aerosol_molecular_mass, P_saturation)
-    g = (R * T)./ (P_saturation * WTR_MM) .+ 
-        (WTR_MM * LAT_HEAT_EVP) ^ 2 ./ 
-        (SPC_HEAT_AIR * P * aerosol_molecular_mass * T)
+function gamma_sic(aero_mm, P_sat)
+
+    g = (R * T)./ (P_sat * WTR_MM) .+ (WTR_MM * LAT_HEAT_EVP) ^ 2 ./ 
+        (SPC_HEAT_AIR * P * aero_mm * TEMP)
     return g
+
 end
 
 end #module Activation.jl
