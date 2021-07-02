fa2d597ce9fd06314d7fbc6b60ab773f457c073e
Isabella M. (Isa) Dula Razzolini <idularaz@login1.cm.cluster>
2021-07-02 14:41:03 -0700

    got to work for multidim aerosol models

--- src/Atmos/Parameterizations/CloudPhysics/Aerosol-activation/sic_constants.jl
+++ src/Atmos/Parameterizations/CloudPhysics/Aerosol-activation/sic_constants.jl
@@ -1,5 +1,12 @@
 # GET FROM CLIMA PARAMATERS
-
+g = 9.81
+Mw = 18.1
+L = 10.0
+Cp = 1000
+T = 273.15
+R = 8.1
+P=100000
+P_saturation = 100000
 molar_mass_water = 18.0
 density_water = 1000.0
 # Universal parameters:
@@ -8,46 +15,47 @@ density_water = 1000.0
 # 1. Set Aerosol parameters: 
 
 # Sea Salt--universal parameters
-osmotic_coeff_seasalt = (0.9,) # osmotic coefficient
-molar_mass_seasalt = (0.058443,) # sea salt molar mass; kg/mol
-rho_seasalt = (2170.0,) # sea salt density; kg/m^3
-dissoc_seasalt = (2.0,) # Sea salt dissociation                         
-mass_frac_seasalt = (1.0,) # mass fraction                              TODO
-mass_mix_ratio_seasalt = (1.0,) # mass mixing rati0                     TODO
+osmotic_coeff_seasalt = 0.9 # osmotic coefficient
+molar_mass_seasalt = 0.058443 # sea salt molar mass; kg/mol
+rho_seasalt = 2170.0 # sea salt density; kg/m^3
+dissoc_seasalt = 2.0 # Sea salt dissociation                         
+mass_frac_seasalt = 1.0 # mass fraction                              TODO
+mass_mix_ratio_seasalt = 1.0 # mass mixing rati0                     TODO
 
 # Sea Salt -- Accumulation mode
-radius_seasalt_accum = (0.000000243,) # mean particle radius (m)
-radius_stdev_seasalt_accum = (0.0000014,) # mean particle stdev (m)
-particle_density_seasalt_accum = (100.0,) #000000 # particle density (1/m^3)
+radius_seasalt_accum = 0.000000243 # mean particle radius (m)
+radius_stdev_seasalt_accum = 0.0000014 # mean particle stdev (m)
+particle_density_seasalt_accum = 100.0 #000000 # particle density (1/m^3)
 
 # Sea Salt -- Coarse Mode
-radius_seasalt_coarse = (0.0000015,) # mean particle radius (m)
-radius_stdev_seasalt_coarse = (0.0000021,) # mean particle stdev(m)
+radius_seasalt_coarse = 0.0000015 # mean particle radius (m)
+radius_stdev_seasalt_coarse = 0.0000021 # mean particle stdev(m)
 
 # TODO: Dust parameters (just copy and pasted seasalt values rn)
 # Dust--universal parameters
-osmotic_coeff_dust = (0.9,) # osmotic coefficient
-molar_mass_dust = (0.058443,) # sea salt molar mass; kg/mol
-rho_dust = (2170.0,) # sea salt density; kg/m^3
-dissoc_dust = (2.0,) # Sea salt dissociation                         
-mass_frac_dust = (1.0,) # mass fraction                              TODO
-mass_mix_ratio_dust = (1.0,) # mass mixing rati0                     TODO
+osmotic_coeff_dust = 0.9 # osmotic coefficient
+molar_mass_dust = 0.058443*1000 # sea salt molar mass; kg/mol
+particle_density_dust_coarse = 1000.0
+rho_dust = 2170.0 # sea salt density; kg/m^3
+dissoc_dust = 2.0 # Sea salt dissociation                         
+mass_frac_dust = 1.0 # mass fraction                              TODO
+mass_mix_ratio_dust = 1.0 # mass mixing rati0                     TODO
 
 # Dust -- Accumulation mode
-radius_dust_accum = (0.000000243,) # mean particle radius (m)
-radius_stdev_dust_accum = (0.0000014,) # mean particle stdev (m)
+radius_dust_accum = 0.000000243 # mean particle radius (m)
+radius_stdev_dust_accum = 0.0000014 # mean particle stdev (m)
 
 # Dust -- Coarse Mode
-radius_dust_coarse = (0.0000015,) # mean particle radius (m)
-radius_stdev_dust_accum = (0.0000021,) # mean particle stdev(m)
+radius_dust_coarse = 0.0000015 # mean particle radius (m)
+radius_stdev_dust_accum = 0.0000021 # mean particle stdev(m)
 
 # Abdul-Razzak and Ghan 
 
 # 2. Create structs that parameters can be pass through
 # individual aerosol mode struct
 struct mode{T}
-    particle_density::Tuple
-    osmotic_coeff::Tuple
+    particle_density::T
+    osmotic_coeff::T
     molar_mass::T 
     dissoc::T
     mass_frac::T 
@@ -63,17 +71,26 @@ end
 
 # 3. Populate structs to pass into functions/run calculations
 # Test cases 1-3 (Just Sea Salt)
-accum_mode_seasalt = mode(particle_density_seasalt_accum, osmotic_coeff_seasalt, 
-                        molar_mass_seasalt, 
-                            dissoc_seasalt, mass_frac_seasalt, mass_mix_ratio_seasalt,
-                        radius_seasalt_coarse,
-                        radius_stdev_seasalt_coarse)
+accum_mode_seasalt = mode((particle_density_seasalt_accum,), (osmotic_coeff_seasalt,), 
+                        (molar_mass_seasalt,), 
+                            (dissoc_seasalt,), (mass_frac_seasalt,), (mass_mix_ratio_seasalt,),
+                        (radius_seasalt_accum,),
+                        (radius_stdev_seasalt_accum,))
 
-coarse_mode_seasalt = mode(particle_density_seasalt_accum, osmotic_coeff_seasalt, 
-                    molar_mass_seasalt, 
-                   dissoc_seasalt, mass_frac_seasalt, mass_mix_ratio_seasalt,
-                    radius_seasalt_accum,
-                   radius_stdev_seasalt_accum)
+coarse_mode_seasalt = mode((particle_density_seasalt_accum,), (osmotic_coeff_seasalt,), 
+                        (molar_mass_seasalt,), 
+                            (dissoc_seasalt,), (mass_frac_seasalt,), (mass_mix_ratio_seasalt,),
+                        (radius_seasalt_coarse,),
+                        (radius_stdev_seasalt_coarse,))
+
+coarse_mode_ssanddust = mode((particle_density_seasalt_accum, particle_density_dust_coarse), 
+                            (osmotic_coeff_seasalt, osmotic_coeff_dust), 
+                        (molar_mass_seasalt, molar_mass_dust), 
+                            (dissoc_seasalt, dissoc_dust), 
+                            (mass_frac_seasalt,mass_frac_dust), 
+                            (mass_mix_ratio_seasalt, mass_mix_ratio_dust),
+                        (radius_seasalt_coarse, radius_dust_coarse),
+                        (radius_stdev_seasalt_coarse, radius_stdev_dust_accum))
 
 # aerosolmodel_testcase1 = aerosol_model(accum_mode_seasalt)
 # aerosolmodel_testcase2 = aerosol_model(coarse_mode_seasalt)
@@ -92,26 +109,26 @@ alpha_sic(aero_mm)
     Returns coefficient relevant to other functions. Uses aerosol
     Molar mass
 """
-g = 9.81
-Mw = 18.1
-L = 10.0
-Cp = 1000
-T = 273.15
-R = 8.1
-P=100000
-P_saturation = 100000
+
 
 function alpha_sic(am::aerosol_model)
     return ntuple(length(am.modes)) do i 
         mode_i = am.modes[i]
-        aerosol_molar_mass = mode_i.molar_mass[1]
+        # Find weighted molar mass of mode
+        n_comps = length(mode_i.particle_density)
+        println("This is number of comps:", n_comps, "mode:", i)
+        numerator = sum(n_comps) do j
+            mode_i.particle_density[j]*mode_i.molar_mass[j]
+        end
+        denominator = sum(n_comps) do j
+            mode_i.particle_density[j]
+        end
+        avg_molar_mass = numerator/denominator
         exp1 = (g*Mw*L) / (Cp*R*T^2)
-        exp2 = (g*aerosol_molar_mass)/(R*T)
+        exp2 = (g*avg_molar_mass)/(R*T)
         exp1-exp2
     end
 end
-
-
 
 """
 gamma_sic(aero_mm)
@@ -123,17 +140,27 @@ gamma_sic(aero_mm)
 function gamma_sic(am::aerosol_model, P_sat)
     return ntuple(length(am.modes)) do i 
         mode_i = am.modes[i]
-        aerosol_molar_mass = mode_i.molar_mass[1]
+        # Find weighted molar mass of mode
+        n_comps = length(mode_i.particle_density)
+        println("This is number of comps:", n_comps, "mode:", i)
+        numerator = sum(n_comps) do j
+            mode_i.particle_density[j]*mode_i.molar_mass[j]
+        end
+        denominator = sum(n_comps) do j
+            mode_i.particle_density[j]
+        end
+        avg_molar_mass = numerator/denominator
         exp1 = (R*T)/(P_sat*Mw)
-        exp2 = (Mw*L^2)/(Cp*P*aerosol_molar_mass*T)
+        exp2 = (Mw*L^2)/(Cp*P*avg_molar_mass*T)
         exp1+exp2 
+    end
 end
 
-am = aerosol_model((accum_mode_seasalt, coarse_mode_seasalt))
-print(length(am.modes))
+am = aerosol_model((accum_mode_seasalt, coarse_mode_ssanddust))
 testoutput1 = alpha_sic(am)
 testoutput2 = gamma_sic(am, P_saturation)
-print("This is the result of alpha_sic:", testoutput1)
-print("This is the result of gamma_sic:", testouput2)
+println("This is the result of alpha_sic:", testoutput1)
+
+println("This is the result of gamma_sic:", testoutput2)
 
 
