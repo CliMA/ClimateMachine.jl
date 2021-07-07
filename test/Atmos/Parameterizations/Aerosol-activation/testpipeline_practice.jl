cd8846a3275b36fb09b6659459ec7fe52c1e67c4
Shevali M. Kadakia <skadakia@login1.cm.cluster>
2021-07-07 15:41:37 -0700

    playing around with the tests

--- test/Atmos/Parameterizations/Aerosol-activation/testpipeline_practice.jl
+++ test/Atmos/Parameterizations/Aerosol-activation/testpipeline_practice.jl
@@ -7,7 +7,21 @@ using CLIMAParameters.Atmos.Microphysics
 # include("/home/skadakia/clones/ClimateMachine.jl/src/Atmos/Parameterizations/CloudPhysics/Aerosol-activation/AerosolActivation-Shevali.jl")
 
 # using ClimateMachine.Atmos.Parameterizations.CloudPhysics.Aerosol-activation.AerosolActivation-Shevali.jl: alpha_sic, gamma_sic, coeff_of_curvature, mean_hygroscopicity
+# GET FROM CLIMA PARAMATERS
+molar_mass_water = 18
+density_water = 1000.0
+R = 8.314462618
 
+g = 9.81
+Mw = 18.1
+L = 10.0
+Cp = 1000
+T = 273.15
+R = 8.1
+P=100000
+P_saturation = 100000
+molar_mass_water = 18.0
+density_water = 1000.0
 # Universal parameters:
 
 # Building the test structures
@@ -148,8 +162,7 @@ coarse_mode_seasalt_dust = mode((particle_density_seasalt_coarse,
 aerosolmodel_testcase4 = aerosol_model((accum_mode_seasalt_dust,))
 aerosolmodel_testcase5 = aerosol_model((accum_mode_seasalt_dust,
                                         coarse_mode_seasalt_dust))
-println("HERE")
-println(molmass_water)
+
 function total_mass(m::mode)
     num_of_comp = m.n_components
     total_mass = sum(num_of_comp) do j
@@ -269,24 +282,224 @@ end
 #     @test tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) ≈ 1
 #     @test tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) ≈ 1 
 # end
-println("total_n_act") 
-println("LOOK HERE")
-println(tp_mean_hygroscopicity(aerosolmodel_testcase1))
-println(tp_mean_hygroscopicity(aerosolmodel_testcase2))
-println(tp_mean_hygroscopicity(aerosolmodel_testcase3))
-println(tp_mean_hygroscopicity(aerosolmodel_testcase4))
-println(tp_mean_hygroscopicity(aerosolmodel_testcase5))
+# println("total_n_act") 
+# println("LOOK HERE")
+# println(tp_mean_hygroscopicity(aerosolmodel_testcase1))
+# println(tp_mean_hygroscopicity(aerosolmodel_testcase2))
+# println(tp_mean_hygroscopicity(aerosolmodel_testcase3))
+# println(tp_mean_hygroscopicity(aerosolmodel_testcase4))
+# println(tp_mean_hygroscopicity(aerosolmodel_testcase5))
 
-println("test max super sat")
-println(tp_max_super_sat(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0))
-println(tp_max_super_sat(aerosolmodel_testcase2, 2.0, 3.0, 4.0, 1.0))
-println(tp_max_super_sat(aerosolmodel_testcase3, 2.0, 3.0, 4.0, 1.0))
-println(tp_max_super_sat(aerosolmodel_testcase4, 2.0, 3.0, 4.0, 1.0))
-println(tp_max_super_sat(aerosolmodel_testcase5, 2.0, 3.0, 4.0, 1.0))
+# println("test max super sat")
+# println(tp_max_super_sat(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0))
+# println(tp_max_super_sat(aerosolmodel_testcase2, 2.0, 3.0, 4.0, 1.0))
+# println(tp_max_super_sat(aerosolmodel_testcase3, 2.0, 3.0, 4.0, 1.0))
+# println(tp_max_super_sat(aerosolmodel_testcase4, 2.0, 3.0, 4.0, 1.0))
+# println(tp_max_super_sat(aerosolmodel_testcase5, 2.0, 3.0, 4.0, 1.0))
 
-println("test total n activated")
-println(tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0))
-println(tp_total_n_act(aerosolmodel_testcase2, 2.0, 3.0, 4.0, 1.0))
-println(tp_total_n_act(aerosolmodel_testcase3, 2.0, 3.0, 4.0, 1.0))
-println(tp_total_n_act(aerosolmodel_testcase4, 2.0, 3.0, 4.0, 1.0))
-println(tp_total_n_act(aerosolmodel_testcase5, 2.0, 3.0, 4.0, 1.0))
+# println("test total n activated")
+# println(tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0))
+# println(tp_total_n_act(aerosolmodel_testcase2, 2.0, 3.0, 4.0, 1.0))
+# println(tp_total_n_act(aerosolmodel_testcase3, 2.0, 3.0, 4.0, 1.0))
+# println(tp_total_n_act(aerosolmodel_testcase4, 2.0, 3.0, 4.0, 1.0))
+# println(tp_total_n_act(aerosolmodel_testcase5, 2.0, 3.0, 4.0, 1.0))
+
+
+
+function alpha_sic(am::aerosol_model)
+    return ntuple(length(am.modes)) do i 
+        mode_i = am.modes[i]
+        # Find weighted molar mass of mode
+        n_comps = length(mode_i.particle_density)
+        println("This is number of comps:",  1:n_comps, "mode:", i)
+        numerator = sum(1:n_comps) do j
+            mode_i.particle_density[j] * mode_i.molar_mass[j]
+        end
+        denominator = sum(1:n_comps) do j
+            mode_i.particle_density[j]
+        end
+        avg_molar_mass = numerator/denominator
+        exp1 = (g * Mw * L) / (Cp * R * T^2)
+        exp2 = (g * avg_molar_mass)/(R * T)
+        exp1-exp2
+    end
+end
+
+"""
+gamma_sic(aero_mm)
+    - am -- aerosol_model                      
+    
+    Returns coefficient relevant to other functions. Uses aerosol
+    Molar mass and water saturation pressure. 
+"""
+function gamma_sic(am::aerosol_model, P_sat)
+    return ntuple(length(am.modes)) do i 
+        mode_i = am.modes[i]
+        # Find weighted molar mass of mode
+         n_comps = length(mode_i.particle_density)
+        numerator = sum(1:n_comps) do j
+            mode_i.particle_density[j] * mode_i.molar_mass[j]
+        end
+        denominator = sum(1:n_comps) do j
+            mode_i.particle_density[j]
+        end
+        avg_molar_mass = numerator/denominator
+        exp1 = (R * T)/(P_sat * Mw)
+        exp2 = (Mw * L^2)/(Cp * P * avg_molar_mass * T)
+        exp1+exp2 
+    end
+end       
+"""
+mean_hygroscopicity(am::aerosol_model)
+
+DO DOCSTRING
+"""
+function mean_hygroscopicity(am::aerosol_model)
+    return map(am.modes) do mode_i 
+        n_comps = length(mode_i.particle_density)
+        top = sum(1:n_comps) do j
+            mode_i.mass_mix_ratio[j] * mode_i.dissoc[j] * 
+            mode_i.osmotic_coeff[j] * mode_i.mass_frac[j] * 
+            (1 / mode_i.molar_mass[j])
+        end
+        bottom = sum(1:n_comps) do j 
+            mode_i.mass_mix_ratio[j] / mode_i.particle_density[j]
+        end  
+        Mw / density_water * top / bottom
+    end 
+end
+
+function coeff_of_curve(am::aerosol_model)
+    return ntuple(length(am.modes)) do i 
+        mode_i = am.modes[i]
+        # take weighted average of activation times 
+        n_comps = length(mode_i.particle_density)
+        numerator = sum(1:n_comps) do j
+            mode_i.activation_time[j] * mode_i.particle_density[j]
+        end 
+        denominator = sum(1:n_comps) do j
+            mode_i.particle_density[j]
+        end
+        avg_activation_time = numerator / denominator 
+        top = 2 * avg_activation_time * Mw
+        bottom = density_water * R * T
+        top/bottom
+
+    end
+
+end
+
+function critical_supersaturation(am::aerosol_model)
+    coeff_of_curvature = coeff_of_curve(am)
+    mh = mean_hygroscopicity(am)
+    return ntuple(length(am.modes)) do i 
+        mode_i = am.modes[i]
+        # weighted average of mode radius
+         n_comps = length(mode_i.particle_density)
+        numerator = sum( 1:n_comps) do j
+            mode_i.radius[j] * mode_i.particle_density[j]
+        end 
+        denominator = sum( 1:n_comps) do j
+            mode_i.particle_density[j]
+        end
+        avg_radius = numerator/denominator
+        exp1 = 2 / (mh[i])^(.5)
+        exp2 = (coeff_of_curvature[i]/3 * avg_radius)^(3/2)
+        exp1 * exp2
+    end
+end
+
+function max_supersatuation(am::aerosol_model)
+    alpha = alpha_sic(am)
+    gamma = gamma_sic(am, P_saturation)
+    A = coeff_of_curve(am)
+    Sm = critical_supersaturation(am)
+    return sum(1:length(am.modes)) do i 
+
+        mode_i = am.modes[i]
+
+        # weighted avgs of diff params:
+        n_comps = length(mode_i.particle_density)
+        # radius_stdev
+        num = sum( 1:n_comps) do j 
+            mode_i.particle_density[j]  *  mode_i.radius_stdev[j]
+        end
+        den = sum( 1:n_comps) do j 
+            mode_i.particle_density[j]
+        end 
+        avg_radius_stdev = num/den 
+        
+        total_particles = sum(1:n_comps) do j 
+            mode_i.particle_density[j]
+        end
+        f = 0.5  *  exp(2.5  *  (log(avg_radius_stdev))^2 )
+        g = 1 + 0.25  *  log(avg_radius_stdev) 
+
+        zeta = (2 * A[i] * (1/3))  *  ((alpha[i] * updft_velo)/diff)^(.5)
+        eta = (((alpha[i]*updft_velo)/(diff))^(3/2))/(2*pi*density_water*gamma[i]*total_particles)
+
+        exp1 = 1/(Sm[i])^2
+        exp2 = f*(zeta/eta)^(3/2)
+        exp3 = g*((Sm[i]^2)/(eta+3*zeta))^(3/4)
+
+        (exp1*(exp2+exp3))^(1/2)
+    end
+
+end
+
+
+function total_N_activated(am::aerosol_model)
+    smax = max_supersatuation(am)
+    sm = critical_supersaturation(am)
+    return sum(1:length(am.modes)) do i
+        mode_i = am.modes[i]
+        # weighted avgs of diff params:
+        n_comps = length(mode_i.particle_density)
+        # radius_stdev
+        num = sum( 1:n_comps) do j 
+            mode_i.particle_density[j]  *  mode_i.radius_stdev[j]
+        end
+        den = sum( 1:n_comps) do j 
+            mode_i.particle_density[j]
+        end 
+        avg_radius_stdev = num/den 
+        
+        total_particles = sum(1:n_comps) do j 
+            mode_i.particle_density[j]
+        end
+
+        utop = 2*log(sm[i]/smax)
+        ubottom = 3*(2^.5)*log(avg_radius_stdev)
+        u = utop/ubottom
+        total_particles*(1/2)*(1-erf(u))
+    end 
+end
+
+# println("LOOK HERE")
+# println(mean_hygroscopicity(aerosolmodel_testcase1))
+# max_supersatuation(aerosolmodel_testcase1)
+# total_N_activated(aerosolmodel_testcase1)
+
+# @testset "mean_hygroscopicity" begin
+#     @test tp_mean_hygroscopicity(aerosolmodel_testcase1) == mean_hygroscopicity(aerosolmodel_testcase1)
+#     @test tp_mean_hygroscopicity(aerosolmodel_testcase2) == mean_hygroscopicity(aerosolmodel_testcase2)
+#     @test tp_mean_hygroscopicity(aerosolmodel_testcase3) == mean_hygroscopicity(aerosolmodel_testcase3)
+#     @test tp_mean_hygroscopicity(aerosolmodel_testcase4) == mean_hygroscopicity(aerosolmodel_testcase4)
+#     @test tp_mean_hygroscopicity(aerosolmodel_testcase5) == mean_hygroscopicity(aerosolmodel_testcase5)
+# end
+
+# @testset "max_super_sat" begin
+#     @test tp_max_super_sat(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) == max_supersatuation(aerosolmodel_testcase1)
+#     @test tp_max_super_sat(aerosolmodel_testcase2, 2.0, 3.0, 4.0, 1.0) == max_supersatuation(aerosolmodel_testcase2)
+#     @test tp_max_super_sat(aerosolmodel_testcase3, 2.0, 3.0, 4.0, 1.0) == max_supersatuation(aerosolmodel_testcase3)
+#     @test tp_max_super_sat(aerosolmodel_testcase4, 2.0, 3.0, 4.0, 1.0) == max_supersatuation(aerosolmodel_testcase4)
+#     @test tp_max_super_sat(aerosolmodel_testcase5, 2.0, 3.0, 4.0, 1.0) == max_supersatuation(aerosolmodel_testcase5)
+# end
+
+@testset "total_n_act" begin
+    @test tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) = total_N_activated(aerosolmodel_testcase1)
+    @test tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) = total_N_activated(aerosolmodel_testcase2)
+    @test tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) = total_N_activated(aerosolmodel_testcase3)
+    @test tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) = total_N_activated(aerosolmodel_testcase4)
+    @test tp_total_n_act(aerosolmodel_testcase1, 2.0, 3.0, 4.0, 1.0) = total_N_activated(aerosolmodel_testcase5)
+end
