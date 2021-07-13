ad9485f482dde7ea41b17312b80ee85400ea4adc
Shevali M. Kadakia <skadakia@login1.cm.cluster>
2021-07-13 16:54:34 -0700

    working on it

--- src/Atmos/Parameterizations/CloudPhysics/Shevali/test_data.jl
+++ src/Atmos/Parameterizations/CloudPhysics/Shevali/test_data.jl
@@ -4,15 +4,18 @@ material, molar mass, osmotic coefficient, density, mass fraction, mass mixing r
 """
 
 open("/home/skadakia/clones/ClimateMachine.jl/src/Atmos/Parameterizations/CloudPhysics/Shevali/data.txt", "r") do f
-    line = 0
+    line = []
     while !eof(f)
-    r = readline(f)
-        s = read(r, Char)
-        if (s == ',')
-            println("ahe")
-        else
-            line += 1
-            println("$line . $s")
-        end
+        r = readline(f)
+        value = " "
+        for index in 1:(length(r) - 1)
+            value *= r[index]
+            println(value)
+            if (r[index + 1] == ',')
+                append!(line, string(value))
+                index += 1
+            end
+        end       
     end
+    println(line)
 end
