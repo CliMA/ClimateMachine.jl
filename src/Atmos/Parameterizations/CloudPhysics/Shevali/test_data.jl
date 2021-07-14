549ca4ac9eb1c092f8029212fe142bdac3dd111d
Shevali M. Kadakia <skadakia@login2.cm.cluster>
2021-07-14 14:57:53 -0700

    reading data works

--- src/Atmos/Parameterizations/CloudPhysics/Shevali/test_data.jl
+++ src/Atmos/Parameterizations/CloudPhysics/Shevali/test_data.jl
@@ -7,7 +7,9 @@ function get_data(material::String, path::String)
     line = []
     open(path, "r") do f
         while !eof(f)
-            r = readline(f)
+            r = eachline(f)
+            println("THIS IS R")
+            println(r)
             value = ""
             for index in 1:length(r)
                 if r[index] != '"' && r[index] != ','
@@ -17,10 +19,12 @@ function get_data(material::String, path::String)
                     if (length(line) == 0)
                         if (value != material)
                             continue
+                        else
+                            push!(line, value)
+                            value = "" 
                         end
                     end
-                    push!(line, value)
-                    value = "" 
+                    
                 end
             end       
         end
@@ -34,4 +38,34 @@ function get_data(material::String, path::String)
     
 end
 
-get_data("seasalt", "/home/skadakia/clones/ClimateMachine.jl/src/Atmos/Parameterizations/CloudPhysics/Shevali/data.txt")
+function get_data_2(material::String, path::String)
+    line = []
+    for r in eachline(path)
+        value = ""
+        for index in 1:length(r)
+            if r[index] != '"' && r[index] != ','
+                value *= r[index] 
+            end
+            if (r[index] == ',' || index == length(r))
+                if (length(line) == 0)
+                    if (value != material)
+                        continue
+                    end
+                end
+                push!(line, value)
+                value = "" 
+            end      
+        end
+        if (length(line) != 0)
+            break
+        end
+    end
+    for values in 2:length(line)
+        line[values] = parse(Float64, line[values])
+    end
+    println(line[2:length(line)])
+    return line[2:length(line)]
+end
+
+get_data_2("seasalt", "/home/skadakia/clones/ClimateMachine.jl/src/Atmos/Parameterizations/CloudPhysics/Shevali/data.txt")
+get_data_2("dust", "/home/skadakia/clones/ClimateMachine.jl/src/Atmos/Parameterizations/CloudPhysics/Shevali/data.txt")
