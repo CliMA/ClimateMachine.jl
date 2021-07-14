701f4473f9a8be830f40f009de741d7413622078
Shevali M. Kadakia <skadakia@login1.cm.cluster>
2021-07-14 10:43:11 -0700

    wip

--- src/Atmos/Parameterizations/CloudPhysics/Shevali/test_data.jl
+++ src/Atmos/Parameterizations/CloudPhysics/Shevali/test_data.jl
@@ -3,19 +3,30 @@ Text file format:
 material, molar mass, osmotic coefficient, density, mass fraction, mass mixing ratio (?), dissociation
 """
 
-open("/home/skadakia/clones/ClimateMachine.jl/src/Atmos/Parameterizations/CloudPhysics/Shevali/data.txt", "r") do f
-    line = []
-    while !eof(f)
-        r = readline(f)
-        value = " "
-        for index in 1:(length(r) - 1)
-            value *= r[index]
-            println(value)
-            if (r[index + 1] == ',')
-                append!(line, string(value))
-                index += 1
-            end
-        end       
+function get_data(material::String, path::String)
+    open(path, "r") do f
+    println(path)
+        line = []
+        while !eof(f)
+            r = readline(f)
+            value = ""
+            if r[1] == material
+                for index in 2:length(r)
+                    if r[index] != '"' && r[index] != ','
+                        value *= r[index] 
+                    end
+                    if (r[index] == ',')
+                        println(value)
+                        push!(line, parse(Float64, value))
+                        println(line)
+                        # index += 1
+                        value = "" 
+                    end
+                end
+            end       
+        end
+        println(line)
     end
-    println(line)
 end
+
+get_data("seasalt", "/home/skadakia/clones/ClimateMachine.jl/src/Atmos/Parameterizations/CloudPhysics/Shevali/data.txt")
