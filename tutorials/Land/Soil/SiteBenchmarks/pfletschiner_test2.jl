0d15cb28b345ebe25abfd97e794d9567361cae46
Katherine Deck <kdeck@caltech.edu>
2021-07-27 09:09:44 -0700

    pfletschinger test wip

--- tutorials/Land/Soil/SiteBenchmarks/pfletschiner_test2.jl
+++ tutorials/Land/Soil/SiteBenchmarks/pfletschiner_test2.jl
@@ -39,30 +39,39 @@ using ClimateMachine.BalanceLaws:
     end
 
     soil_heat_model = PrescribedTemperatureModel()
-#    wpf_vg = WaterParamFunctions(
-#        FT;
-#        Ksat = 7.8 / (60 * 100),
-#        θ_r = 0.005,
-#        S_s = 1e-3,
-#    )
-
     wpf_bc = WaterParamFunctions(
         FT;
-        Ksat = 1.3e-5,
-        θ_r = 0.0,
+        Ksat = 7.2e-5,
+        θ_r = 0.067,
         S_s = 1e-3,
     )
-    soil_param_functions = SoilParamFunctions(FT; porosity = 0.367, water = wpf_bc)
+    soil_param_functions = SoilParamFunctions(FT; porosity = 0.31, water = wpf_bc)
 
-irr1(t) = t<FT(3600*5) ? FT(-170/5/60/60/1000) : FT(0)
-irr2(t) = t>FT(3600*125) ? FT(-25/2/60/60/1000) : FT(0)
-irr3(t) = t>FT(3600*127) ? FT(25/2/60/60/1000) : FT(0)
+irr1(t) = ((t<FT(3600*7.5)) & (t>FT(3600*2.5))) ? FT(-170/5/60/60/1000) : FT(0)
+irr2(t) = ((t>FT(3600*122.5)) & (t<FT(3600*124.5))) ? FT(-25/2/60/60/1000) : FT(0)
+evap1(t) = ((t<FT(3600*50.5)) & (t>FT(3600*2.5))) ? FT(0.009/60/60/100) : FT(0)
+evap2(t) = ((t>FT(3600*50.5)) & (t<FT(3600*122.5))) ? FT(0.005/60/60/100) : FT(0)
+evap3(t) = ((t>FT(3600*122.5)) & (t<FT(3600*200.5))) ? FT(0.015/60/60/100) : FT(0)
+#evap3(t) = t>FT(3600*200.5) ? FT(0.01/60/60/100) : FT(0)
 
-    surface_flux = (aux, t) -> eltype(aux)(irr1(t)+irr2(t)+irr3(t))
-#bottom_flux = (aux, t) -> aux.soil.water.K * eltype(aux)(-1)
+    surface_flux = (aux, t) -> eltype(aux)(irr1(t)+irr2(t)+evap1(t)+evap2(t) + evap3(t))
 
 Δ = FT(0.0092)
-lower_bc(aux, t) = aux.soil.water.h-aux.z < FT(-0.6) ? FT(0.0) : -aux.soil.water.K*(aux.soil.water.h-aux.z-FT(-0.6))/Δ
+function lower_bc(aux, t)
+    pressure_head = aux.soil.water.h-aux.z
+    if pressure_head < FT(-0.62)
+        flux = FT(0.0)
+    else
+        proposed_flux = -aux.soil.water.K*(aux.soil.water.h-aux.z-FT(-0.62)+Δ)/Δ
+        if proposed_flux < FT(0)
+            flux = proposed_flux
+        else
+            flux = FT(0.0)
+        end
+    end
+    return(flux)
+end
+
 bottom_flux = (aux,t) -> lower_bc(aux,t)
     ϑ_l0 = (aux) -> eltype(aux)(0.07)
 
@@ -71,7 +80,7 @@ bottom_flux = (aux,t) -> lower_bc(aux,t)
         surface_bc = LandComponentBC(soil_water = Neumann(surface_flux)),
     )
 #vg_hydraulics =  vanGenuchten(FT; n = 7.5, α = 5.5)
-bc_hydraulics =  BrooksCorey(FT; m = 1.0/1.19, ψb = 0.315)
+bc_hydraulics =  BrooksCorey(FT; m = 1.0/2.91, ψb = 0.421)
 
     soil_water_model = SoilWaterModel(
         FT;
@@ -113,7 +122,7 @@ bc_hydraulics =  BrooksCorey(FT; m = 1.0/1.19, ψb = 0.315)
     t0 = FT(0)
     timeend = FT(60 * 60*250)
 
-    dt = FT(15)
+    dt = FT(1.1)
 
     solver_config = ClimateMachine.SolverConfiguration(
         t0,
@@ -140,7 +149,7 @@ end;
 # # Run the integration
 ClimateMachine.invoke!(solver_config; user_callbacks = (callback,));
 z = get_z(solver_config.dg.grid; rm_dupes = true);
-f_bottom = [dons_arr[k]["soil.water.K∇h[3]"][1] for k in 1:n_outputs]
+f_bottom = [dons_arr[k]["soil.water.K∇h[3]"][1] for k in 1:length(dons_arr)]
 t = time_data ./ 60
 
 
@@ -149,19 +158,18 @@ l14 = [dons_arr[k]["soil.water.ϑ_l"][43] for k in 1:length(dons_arr)]
 
 
 plot1 = plot(dons_arr[1]["soil.water.ϑ_l"],z, label = "initial")
-plot!(dons_arr[11]["soil.water.ϑ_l"],z, label = "t = 1h")
-plot!(dons_arr[21]["soil.water.ϑ_l"],z, label = "t = 2h")
-plot!(dons_arr[31]["soil.water.ϑ_l"],z, label = "t = 2h")
-plot!(dons_arr[41]["soil.water.ϑ_l"],z, label = "t = 4h")
-plot!(dons_arr[51]["soil.water.ϑ_l"],z, label = "t = 5h")
+plot!(dons_arr[50]["soil.water.ϑ_l"],z, label = "t = 3.3h")
+plot!(dons_arr[100]["soil.water.ϑ_l"],z, label = "t = 6.6h")
+plot!(dons_arr[150]["soil.water.ϑ_l"],z, label = "t = 10h")
+
 plot!(xlim = [0.0, 0.27])
 plot!(legend = :bottomright)
 plot!(ylabel = "Depth [m]")
 plot!(xlabel = "Volumetric water content")
 plot!(ylim = [-0.92, 0])
 plot!(xticks = [0.04,0.06,0.08,0.1,0.12,0.14,0.16,0.18])
-plot2 = plot(t[2:end]./60, f_bottom *60 *1000, ylabel = "Discharge (mm/min)", xlabel = "Time after infiltration start (hr)", xguide_position = :top, xmirror = true, label="", yguidefontsize = 6, yticks = [0,0.1,0.3,0.5])
+plot2 = plot(t./60, f_bottom *60 *1000, ylabel = "Discharge (mm/min)", xlabel = "Time after infiltration start (hr)", xguide_position = :top, xmirror = true, label="", yguidefontsize = 6, yticks = [0,0.1,0.3,0.5])
 plot(plot2,plot1, layout = grid(2,1, heights = [0.2,0.8]))
 #end
 
-plot(time_data[2:end] ./3600,-cumsum(f_bottom).*(time_data[2]-time_data[1]).*100)
+plot(time_data ./3600,-cumsum(f_bottom).*(time_data[2]-time_data[1]).*100)
